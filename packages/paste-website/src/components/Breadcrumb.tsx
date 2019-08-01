e980d8cf00b092b1d4abf202977d70ce4cc051d3
sisber <sisber@twilio.com>
2019-08-01 14:29:31 -0700

    feat: add breadcrumb component

---
+++ packages/paste-website/src/components/Breadcrumb.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import styled from '@emotion/styled';
+import css from '@emotion/css';
+import {themeGet} from 'styled-system';
+import {Link} from 'gatsby';
+
+const CrumbSlashStyled = styled.aside(props => ({
+  padding: `0 ${themeGet('space.space20')(props)}`,
+  display: 'inline',
+  color: themeGet('textColors.colorTextWeak')(props),
+}));
+
+const CrumbSlash = () => <CrumbSlashStyled>/</CrumbSlashStyled>;
+
+interface Breadcrumb {
+  children?: React.ReactElement;
+}
+
+const Breadcrumb: React.FC<Breadcrumb> = ({children}) => {
+  return React.Children.map(children, child => (
+    <>
+      {child}
+      <CrumbSlash />
+    </>
+  ));
+};
+
+const BreadcrumbItem = styled(Link)`
+  height: 24px;
+  font-size: 14px;
+  line-height: 1.71;
+  letter-spacing: normal;
+  color: ${themeGet('textColors.colorTextLink')};
+`;
+
+export {Breadcrumb, BreadcrumbItem};
