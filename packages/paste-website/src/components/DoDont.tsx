55df8b15bf4ced8c89e9a954a5c300e5689f913e
Jason Walker <jason@two24studios.com>
2019-08-09 16:08:52 -0600

    feat: add do/dont component example

---
+++ packages/paste-website/src/components/DoDont.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import styled from '@emotion/styled';
+
+const StyledWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, auto);
+  grid-column-gap: 24px;
+`;
+
+function DoDont(props) {
+  return <StyledWrapper>{props.children}</StyledWrapper>;
+}
+
+const StyledChild = styled.div``;
+
+const StyledChildImg = styled.div`
+  margin-bottom: 16px;
+  border: 1px solid #ccd2dc;
+  border-bottom: ${(props: ChildProps) => (props.do ? '2px solid #23bf6e' : '2px solid #ce241a')};
+`;
+
+const StyledChildContent = styled.div``;
+
+const StyledChildTitle = styled.h5`
+  margin-bottom: 8px;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 24px;
+`;
+
+interface ChildProps {
+  do: boolean;
+}
+
+function Child(props: ChildProps) {
+  console.log(props);
+  return (
+    <StyledChild>
+      <StyledChildImg {...props}>
+        <img src={props.image} alt="" />
+      </StyledChildImg>
+      <StyledChildContent>
+        <StyledChildTitle>{props.do ? 'Do' : `Don't`}</StyledChildTitle>
+        {props.children}
+      </StyledChildContent>
+    </StyledChild>
+  );
+}
+
+function Do(props) {
+  return <Child do={true} {...props} />;
+}
+
+function Dont(props) {
+  return <Child do={false} {...props} />;
+}
+
+export {DoDont, Do, Dont};
