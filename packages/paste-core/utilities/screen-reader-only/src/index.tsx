051edc211831351c958b98f1e45a113021494e08
Si Taggart <me@simontaggart.com>
2019-08-09 14:23:54 -0700

    fix: remove importants and update license field

--- packages/paste-core/utilities/screen-reader-only/src/index.tsx
+++ packages/paste-core/utilities/screen-reader-only/src/index.tsx
@@ -1,14 +1,14 @@
 import styled from '@emotion/styled';
 
 export const ScreenReaderOnly = styled.span<{}>`
-  position: absolute !important;
-  margin: -1px !important;
-  border: 0 !important;
-  padding: 0 !important;
-  width: 1px !important;
-  height: 1px !important;
-  overflow: hidden !important;
-  clip: rect(0 0 0 0) !important;
-  text-transform: none !important;
-  white-space: nowrap !important;
+  position: absolute;
+  margin: -1px;
+  border: 0;
+  padding: 0;
+  width: 1px;
+  height: 1px;
+  overflow: hidden;
+  clip: rect(0 0 0 0);
+  text-transform: none;
+  white-space: nowrap;
 `;
