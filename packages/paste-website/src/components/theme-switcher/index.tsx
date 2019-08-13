af400f9c77a12497d7b14ea9a6a0ed1450f602a8
Si Taggart <me@simontaggart.com>
2019-08-13 16:00:54 -0700

    fix: interim theme switcher

--- packages/paste-website/src/components/theme-switcher/index.tsx
+++ packages/paste-website/src/components/theme-switcher/index.tsx
@@ -3,13 +3,37 @@ import * as React from 'react';
 interface ThemeSwitcherProps {
   children?: React.ReactElement;
 }
-export const ThemeSwitcher: React.FC<ThemeSwitcherProps> = props => {
+export const ThemeSwitcher: React.FC<ThemeSwitcherProps> = () => {
+  const [theme, setTheme] = React.useState('console');
+
+  const updateTheme = (newTheme: string): void => {
+    setTheme(newTheme);
+  };
+
+  const handleChange = (e: React.FormEvent<HTMLInputElement>): void => {
+    updateTheme(e.currentTarget.value);
+  };
+
   return (
     <form action="#">
       <fieldset>
-        <input checked type="radio" id="console" name="theme" />
+        <input
+          checked={theme === 'console'}
+          type="radio"
+          id="console"
+          name="theme"
+          onChange={handleChange}
+          value="console"
+        />
         <label htmlFor="console">Console</label>
-        <input type="radio" id="sendgrid" name="theme" />
+        <input
+          checked={theme === 'sendgrid'}
+          type="radio"
+          id="sendgrid"
+          name="theme"
+          onChange={handleChange}
+          value="sendgrid"
+        />
         <label htmlFor="sendgrid">SendGrid</label>
       </fieldset>
     </form>
