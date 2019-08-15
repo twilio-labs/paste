0d996f6d766e1b25e382de3e35d8c695265b7ea3
Jason Walker <jwalker@twilio.com>
2019-08-15 15:46:27 -0600

    feat(website): do and don't component (#42)
    * feat: add do/dont component example
    
    * fix(website): typsescript errors
    
    * fix(website): update do don’t per feedback
    
    * fix(website): typescript errors
    
    * fix(website): remove value react/jsx-boolean-value
    
    * fix(website): github feedback cleanup

---
+++ packages/paste-website/src/components/DoDont.tsx
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import styled from '@emotion/styled';
+import {themeGet} from 'styled-system';
+import {Text} from '@twilio-paste/text';
+
+const StyledWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, auto);
+  grid-column-gap: 24px;
+`;
+
+interface DoDontProps {
+  children: React.ReactNode;
+}
+
+const DoDont: React.FC<DoDontProps> = props => {
+  return <StyledWrapper>{props.children}</StyledWrapper>;
+};
+
+interface ExampleProps {
+  children: React.ReactNode;
+  do: boolean;
+  image: string;
+}
+
+const StyledExampleImg: React.FC<ExampleProps> = styled.div`
+  margin-bottom: ${themeGet('space.space50')};
+  border: ${themeGet('borderWidths.borderWidth10')} solid #ccd2dc;
+  border-bottom: ${(props: ExampleProps) =>
+    props.do
+      ? `${themeGet('borderWidths.borderWidth20')(props)} solid #23bf6e`
+      : `${themeGet('borderWidths.borderWidth20')(props)} solid #ce241a`};
+  img {
+    display: block;
+    width: 100%;
+  }
+`;
+
+const Example: React.FC<ExampleProps> = props => {
+  return (
+    <div>
+      <StyledExampleImg {...props}>
+        <img src={props.image} alt="" />
+      </StyledExampleImg>
+      <div>
+        <Text
+          as="h5"
+          fontSize="fontSize20"
+          fontWeight="fontWeightSemibold"
+          lineHeight="lineHeight40"
+          marginBottom="space40"
+        >
+          {props.do ? 'Do' : `Don't`}
+        </Text>
+        {props.children}
+      </div>
+    </div>
+  );
+};
+
+const Do: React.FC<ExampleProps> = props => {
+  return <Example do {...props} />;
+};
+
+const Dont: React.FC<ExampleProps> = props => {
+  return <Example do={false} {...props} />;
+};
+
+export {DoDont, Do, Dont};
