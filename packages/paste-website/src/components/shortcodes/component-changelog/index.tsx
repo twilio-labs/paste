e43bb885823fc6d4ad62a8b828357a21e64acf90
richbachman <rb@richbachman.com>
2019-08-26 16:56:08 -0600

    fix(website): ignore public website folder during lint, fix lint error

--- packages/paste-website/src/components/shortcodes/component-changelog/index.tsx
+++ packages/paste-website/src/components/shortcodes/component-changelog/index.tsx
@@ -2,27 +2,29 @@ import * as React from 'react';
 import {StaticQuery, graphql} from 'gatsby';
 import {Box} from '@twilio-paste/box';
 
-const ComponentChangelog = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        file(sourceInstanceName: {eq: "packages"}, relativePath: {regex: "/anchor/"}, name: {eq: "CHANGELOG"}) {
-          id
-          childMarkdownRemark {
-            html
-            rawMarkdownBody
+const ComponentChangelog: React.FC<{}> = () => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          file(sourceInstanceName: {eq: "packages"}, relativePath: {regex: "/anchor/"}, name: {eq: "CHANGELOG"}) {
+            id
+            childMarkdownRemark {
+              html
+              rawMarkdownBody
+            }
           }
         }
-      }
-    `}
-    render={data => (
-      <>
-        <Box key={data.file.id} marginTop="space120">
-          <Box dangerouslySetInnerHTML={{__html: `${data.file.childMarkdownRemark.html}`}} />
-        </Box>
-      </>
-    )}
-  />
-);
+      `}
+      render={data => (
+        <>
+          <Box key={data.file.id} marginTop="space120">
+            <Box dangerouslySetInnerHTML={{__html: `${data.file.childMarkdownRemark.html}`}} />
+          </Box>
+        </>
+      )}
+    />
+  );
+};
 
 export {ComponentChangelog};
