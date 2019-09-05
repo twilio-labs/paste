0c393c5bcc7327eba4f8c2ba700f86b24d9ea57c
Si Taggart <me@simontaggart.com>
2019-09-04 19:59:04 -0700

    feat(website): add website theme switcher

--- packages/paste-website/src/components/ThemeSwitcher.tsx
+++ packages/paste-website/src/components/ThemeSwitcher.tsx
@@ -1,13 +1,79 @@
 import * as React from 'react';
+import styled from '@emotion/styled';
+import {themeGet} from 'styled-system';
 import {ScreenReaderOnly} from '@twilio-paste/screen-reader-only';
+import {Box} from '@twilio-paste/box';
+import {Themes, ThemesType} from '../constants';
+import {useActiveSiteTheme} from '../context/ActiveSiteThemeContext';
 
 interface ThemeSwitcherProps {
   children?: React.ReactElement;
 }
+
+const StyledThemeSwitcherLabel = styled.label<{}>(props => ({
+  cursor: 'pointer',
+  display: 'inline-block',
+  padding: `${themeGet('space.space20')(props)} ${themeGet('space.space40')(props)}`,
+  '&:hover': {
+    textDecoration: 'underline',
+  },
+}));
+
+const StyledThemeSwitcherRadio = styled.input<{}>(props => ({
+  opacity: 0,
+  position: 'absolute',
+  [`&:focus + ${StyledThemeSwitcherLabel}`]: {
+    boxShadow: themeGet('shadows.shadowFocus')(props),
+    textDecoration: 'underline',
+  },
+  [`&:checked + ${StyledThemeSwitcherLabel}`]: {
+    backgroundColor: themeGet('backgroundColors.colorBackgroundPrimary')(props),
+    borderRadius: themeGet('radii.borderRadius10')(props),
+    color: themeGet('textColors.colorTextInverse')(props),
+  },
+}));
+
 export const ThemeSwitcher: React.FC<ThemeSwitcherProps> = () => {
+  const {theme, updateActiveSiteTheme} = useActiveSiteTheme();
+
+  const handleChange = (event: React.FormEvent<HTMLInputElement>): void => {
+    updateActiveSiteTheme(event.currentTarget.value as ThemesType);
+  };
+
   return (
-    <div>
-      <ScreenReaderOnly>Theme Switcher Placeholder</ScreenReaderOnly>
-    </div>
+    <Box
+      as="form"
+      borderColor="colorBorderLight"
+      borderRadius="borderRadius20"
+      borderStyle="solid"
+      borderWidth="borderWidth10"
+      padding="space10"
+    >
+      <Box as="fieldset" borderWidth="borderWidth0" padding="space0" margin="space0">
+        <legend>
+          <ScreenReaderOnly>Change the site theme</ScreenReaderOnly>
+        </legend>
+        <StyledThemeSwitcherRadio
+          checked={theme === Themes.CONSOLE}
+          id="console"
+          name="sitetheme"
+          onClick={handleChange}
+          onChange={handleChange}
+          type="radio"
+          value={Themes.CONSOLE}
+        />
+        <StyledThemeSwitcherLabel htmlFor="console">Console</StyledThemeSwitcherLabel>
+        <StyledThemeSwitcherRadio
+          checked={theme === Themes.SENDGRID}
+          id="sendgrid"
+          name="sitetheme"
+          onClick={handleChange}
+          onChange={handleChange}
+          type="radio"
+          value={Themes.SENDGRID}
+        />
+        <StyledThemeSwitcherLabel htmlFor="sendgrid">SendGrid</StyledThemeSwitcherLabel>
+      </Box>
+    </Box>
   );
 };
--- packages/paste-website/src/components/shortcodes/live-preview/index.tsx
+++ packages/paste-website/src/components/shortcodes/live-preview/index.tsx
@@ -1,11 +1,13 @@
 // https://github.com/FormidableLabs/react-live
 import * as React from 'react';
 import {Box} from '@twilio-paste/box';
+import {Theme} from '@twilio-paste/theme';
 import {LiveProvider, LiveEditor, LiveError, LivePreview as ReactLivePreview} from 'react-live';
 import styled from '@emotion/styled';
 import {themeGet} from 'styled-system';
 import {CodeblockTheme} from './theme';
 import {Language} from '../../codeblock';
+import {useActiveSiteTheme} from '../../../context/ActiveSiteThemeContext';
 
 interface CodeblockProps {
   children: string;
@@ -20,28 +22,31 @@ const StyledPreviewWrapper = styled.div(props => ({
 
 // FIXME use tokens for theme and LiveEditor
 const LivePreview: React.FC<CodeblockProps> = ({children, language = 'jsx', scope, disabled = false}) => {
+  const {theme} = useActiveSiteTheme();
   return (
-    <StyledPreviewWrapper>
-      <LiveProvider code={children} scope={scope} language={language} theme={CodeblockTheme} disabled={disabled}>
-        <Box
-          px="space50"
-          py="space80"
-          backgroundColor="colorBackground"
-          borderTopLeftRadius="borderRadius20"
-          borderTopRightRadius="borderRadius20"
-        >
-          <ReactLivePreview />
-        </Box>
-        <Box
-          css={{padding: '2px 10px', backgroundColor: '#011627'}}
-          borderBottomLeftRadius="borderRadius20"
-          borderBottomRightRadius="borderRadius20"
-        >
-          <LiveEditor />
-        </Box>
-        <LiveError />
-      </LiveProvider>
-    </StyledPreviewWrapper>
+    <Theme.Provider theme={theme}>
+      <StyledPreviewWrapper>
+        <LiveProvider code={children} scope={scope} language={language} theme={CodeblockTheme} disabled={disabled}>
+          <Box
+            px="space50"
+            py="space80"
+            backgroundColor="colorBackground"
+            borderTopLeftRadius="borderRadius20"
+            borderTopRightRadius="borderRadius20"
+          >
+            <ReactLivePreview />
+          </Box>
+          <Box
+            css={{padding: '2px 10px', backgroundColor: '#011627'}}
+            borderBottomLeftRadius="borderRadius20"
+            borderBottomRightRadius="borderRadius20"
+          >
+            <LiveEditor />
+          </Box>
+          <LiveError />
+        </LiveProvider>
+      </StyledPreviewWrapper>
+    </Theme.Provider>
   );
 };
 
