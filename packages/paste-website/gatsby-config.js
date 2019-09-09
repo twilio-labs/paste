45f99924b40782ade3d17b2ecadedf347bc51c12
richbachman <rb@richbachman.com>
2019-09-09 14:05:43 -0600

    fix(website): add assets file-system, change image path in anchor doc

--- packages/paste-website/gatsby-config.js
+++ packages/paste-website/gatsby-config.js
@@ -33,6 +33,13 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
+        name: 'assets',
+        path: `${__dirname}/src/assets`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
         name: 'pages',
         path: `${__dirname}/src/pages`,
         ignore: [
