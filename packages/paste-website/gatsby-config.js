b67d833e94a102cf48d7333179a32a93b4fab3dc
richbachman <rb@richbachman.com>
2019-09-09 14:45:52 -0600

    fix(website): update favicon, add manifest plugin

--- packages/paste-website/gatsby-config.js
+++ packages/paste-website/gatsby-config.js
@@ -126,5 +126,17 @@ module.exports = {
       },
     },
     'gatsby-plugin-sitemap',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Paste: A Design System for Twilio`,
+        short_name: `Paste`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#f22f46`,
+        display: `standalone`,
+        icon: `static/favicon.png`,
+      },
+    },
   ],
 };
