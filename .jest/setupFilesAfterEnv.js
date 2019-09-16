9dd0bf694ad7d7a69676148cb37459d332d74f3a
sisber <sisber@twilio.com>
2019-09-16 14:04:28 -0500

    fix(jest): jest-emotion no longer depeneds on emotion

--- .jest/setupFilesAfterEnv.js
+++ .jest/setupFilesAfterEnv.js
@@ -1,7 +1,6 @@
-const emotion = require('emotion');
-const {createSerializer} = require('jest-emotion');
+const {serializer} = require('jest-emotion');
 const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 
-expect.addSnapshotSerializer(createSerializer(emotion));
+expect.addSnapshotSerializer(serializer);
 Enzyme.configure({adapter: new Adapter()});
