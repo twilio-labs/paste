69f274a3a93b1f2f7d361b92b859a133175d236a
sisber <sisber@twilio.com>
2019-09-20 11:43:12 -0500

    fix(website): update page headers casing

--- packages/paste-website/src/components/empty-state/InDevelopment.tsx
+++ packages/paste-website/src/components/empty-state/InDevelopment.tsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 import {Anchor} from '@twilio-paste/anchor';
 import {Text} from '@twilio-paste/text';
 import {Box} from '@twilio-paste/box';
-import {capitalize} from 'lodash';
 import {Breadcrumb, BreadcrumbItem} from '../breadcrumb';
 import {SidebarCategoryRoutes} from '../../constants';
+import {getComponentNameFromPackageName} from '../../utils/RouteUtils';
 
 interface InDevelopmentProps {
   type?: 'component' | 'primitive' | 'utility';
@@ -27,7 +27,7 @@ const InDevelopment: React.FC<InDevelopmentProps> = ({type, name}) => {
         color="colorText"
         mb="space90"
       >
-        {capitalize(name)}
+        {getComponentNameFromPackageName(name)}
       </Text>
       <Box>
         <Text>This {type} is in active development, but we haven&apos;t gotten to the docs yet.</Text>
--- packages/paste-website/src/components/shortcodes/component-header/index.tsx
+++ packages/paste-website/src/components/shortcodes/component-header/index.tsx
@@ -6,6 +6,7 @@ import {Breadcrumb, BreadcrumbItem} from '../../breadcrumb';
 import {SidebarCategoryRoutes} from '../../../constants';
 import {P} from '../../Typography';
 import {Heading} from '../../Heading';
+import {getComponentNameFromPackageName} from '../../../utils/RouteUtils';
 
 const ComponentHeaderBasic: React.FC<{
   name: string;
@@ -19,7 +20,7 @@ const ComponentHeaderBasic: React.FC<{
       </BreadcrumbItem>
     </Breadcrumb>
     <Heading as="h1" headingStyle="headingStyle10">
-      {name}
+      {getComponentNameFromPackageName(name)}
     </Heading>
   </>
 );
