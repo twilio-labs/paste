13aa6019c4b75571556b7dc80093ff2b5dc96257
Si Taggart <me@simontaggart.com>
2019-10-07 16:33:57 -0700

    chore: try and get the applitools github checks API running

--- applitools.config.js
+++ applitools.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   serverUrl: 'https://twilioeyesapi.applitools.com',
+  batchId: process.env.CIRCLE_SHA1,
+  batchName: `Paste ${process.env.CIRCLE_BRANCH}`,
+  branchName: `${process.env.CIRCLE_BRANCH}`,
+  baselineBranchName: 'master',
+  parentBranchName: 'master',
+  showLogs: true,
 };
