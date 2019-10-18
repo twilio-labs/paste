e9571f8d7082fd45ed7991b5f7142107a9b12048
rakinuddin <rakin@uddin.io>
2019-10-18 14:03:55 -0400

    fix(tokens): updates border color to use color box

--- packages/paste-website/src/components/tokens-example/index.tsx
+++ packages/paste-website/src/components/tokens-example/index.tsx
@@ -117,7 +117,7 @@ export const TokenExample: React.FC<TokenExampleProps> = ({token}) => {
     case 'color':
       return <ColorBox backgroundColor={token.value as any} />;
     case 'border-color':
-      return <BorderBox borderColor={tokenName as keyof ThemeShape['borderColors']} />;
+      return <ColorBox backgroundColor={tokenName as any} />;
     case 'border-width':
       return <BorderBox borderWidth={tokenName as keyof ThemeShape['borderWidths']} />;
     case 'font':
