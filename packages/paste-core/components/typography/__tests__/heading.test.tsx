bf72f4dc1ff3523a933ba02f18d36c736b86da69
richbachman <rb@richbachman.com>
2019-10-26 11:40:55 -0600

    feat(typography): update heading tests

--- packages/paste-core/components/typography/__tests__/heading.test.tsx
+++ packages/paste-core/components/typography/__tests__/heading.test.tsx
@@ -4,11 +4,11 @@ import {Theme} from '@twilio-paste/theme';
 import {Heading} from '../src';
 
 describe('Heading', () => {
-  it('it should render an H1 with 32px font-size', (): void => {
+  it('it should render an H1 at fontSize90', (): void => {
     const tree = renderer
       .create(
         <Theme.Provider>
-          <Heading as="h1" headingStyle="headingStyle60">
+          <Heading as="h1" variant="headingStyle60">
             This is an H1
           </Heading>
         </Theme.Provider>
@@ -16,11 +16,11 @@ describe('Heading', () => {
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
-  it('it should render an H2 with 24px font-size', (): void => {
+  it('it should render an H2 at fontSize70', (): void => {
     const tree = renderer
       .create(
         <Theme.Provider>
-          <Heading as="h2" headingStyle="headingStyle50">
+          <Heading as="h2" variant="headingStyle50">
             This is an H2
           </Heading>
         </Theme.Provider>
@@ -28,11 +28,11 @@ describe('Heading', () => {
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
-  it('it should render an H3 with 20px font-size', (): void => {
+  it('it should render an H3 at fontSize60', (): void => {
     const tree = renderer
       .create(
         <Theme.Provider>
-          <Heading as="h3" headingStyle="headingStyle40">
+          <Heading as="h3" variant="headingStyle40">
             This is an H3
           </Heading>
         </Theme.Provider>
@@ -40,11 +40,11 @@ describe('Heading', () => {
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
-  it('it should render an H4 with 16px font-size', (): void => {
+  it('it should render an H4 at fontSize40', (): void => {
     const tree = renderer
       .create(
         <Theme.Provider>
-          <Heading as="h4" headingStyle="headingStyle30">
+          <Heading as="h4" variant="headingStyle30">
             This is an H4
           </Heading>
         </Theme.Provider>
@@ -52,11 +52,11 @@ describe('Heading', () => {
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
-  it('it should render an H5 with 14px font-size', (): void => {
+  it('it should render an H5 at fontSize30', (): void => {
     const tree = renderer
       .create(
         <Theme.Provider>
-          <Heading as="h5" headingStyle="headingStyle20">
+          <Heading as="h5" variant="headingStyle20">
             This is an H5
           </Heading>
         </Theme.Provider>
@@ -64,11 +64,11 @@ describe('Heading', () => {
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
-  it('it should render an H6 with 12px font-size', (): void => {
+  it('it should render an H6 at fontSize20', (): void => {
     const tree = renderer
       .create(
         <Theme.Provider>
-          <Heading as="h6" headingStyle="headingStyle10">
+          <Heading as="h6" variant="headingStyle10">
             This is an H6
           </Heading>
         </Theme.Provider>
