ab6d18140818063032e9f10aec30070efdc4fd06
Simon Taggart <staggart@twilio.com>
2019-11-15 09:18:49 -0800

    chore: add the accessibilty plugin to storyboook (#188)

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -4,3 +4,4 @@ import '@storybook/addon-knobs/register';
 import './addons/theme-switcher-addon/register';
 import 'storybook-readme/register';
 import '@storybook/addon-actions/register';
+import '@storybook/addon-a11y/register';
