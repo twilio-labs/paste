ad6f44c9b5cdf99daffe82cb0c24f7c99c0b1dd9
richbachman <rb@richbachman.com>
2019-11-20 12:52:08 -0700

    fix: change heading variants

--- packages/paste-core/components/heading/stories/index.stories.tsx
+++ packages/paste-core/components/heading/stories/index.stories.tsx
@@ -1,24 +1,17 @@
 import * as React from 'react';
 import {storiesOf} from '@storybook/react';
 import {withKnobs, text, select} from '@storybook/addon-knobs';
-import {asTags, Heading, HeadingStyle} from '../src';
+import {asTags, Heading, HeadingVariants} from '../src';
 
-const headingStyleOptions = [
-  'headingStyle10',
-  'headingStyle20',
-  'headingStyle30',
-  'headingStyle40',
-  'headingStyle50',
-  'headingStyle60',
-];
+const headingVariantOptions = ['heading60', 'heading50', 'heading40', 'heading30', 'heading20', 'heading10'];
 
 storiesOf('Components|Typography', module)
   .addDecorator(withKnobs)
   .add('Heading', () => {
     const asOptions = text('as', 'h2') as asTags;
-    const headingStyleValue = select('variant', headingStyleOptions, 'headingStyle50') as HeadingStyle;
+    const headingVariantValue = select('variant', headingVariantOptions, 'heading50') as HeadingVariant;
     return (
-      <Heading as={asOptions} variant={headingStyleValue}>
+      <Heading as={asOptions} variant={headingVariantValue}>
         I am a Very Large Heading
       </Heading>
     );
