8299187ae18b5e8334bbf245dd83abf7db561d29
richbachman <rb@richbachman.com>
2019-11-26 13:16:19 -0700

    fix: simplify flex api props, simple story

--- packages/paste-core/utilities/flex/src/index.tsx
+++ packages/paste-core/utilities/flex/src/index.tsx
@@ -1,28 +1,108 @@
 import * as React from 'react';
+import * as PropTypes from 'prop-types';
 import {Box} from '@twilio-paste/box';
-import {FlexboxProps} from '@twilio-paste/types';
 
-const Flex: React.FC<FlexboxProps> = props => {
+export type displayOptions = 'flex' | 'inline-flex';
+export type vAlignOptions = 'top' | 'center' | 'bottom';
+export type hAlignOptions = 'left' | 'center' | 'right';
+
+interface Flex {
+  display?: displayOptions;
+  column?: boolean;
+  vAlignContent?: vAlignOptions;
+  hAlignContent?: hAlignOptions;
+  grow?: boolean | number;
+  shrink?: boolean | number;
+  basis?: string | number;
+  order?: number;
+  wrap?: boolean;
+}
+
+function getGrow(props: Flex): number {
+  const {grow} = props;
+  if (typeof grow === 'number') {
+    return grow;
+  } else if (grow) {
+    return 1;
+  }
+
+  return 0; // default
+}
+
+function getShrink(props: Flex): number {
+  const {shrink, basis} = props;
+  if (typeof shrink === 'number') {
+    return shrink;
+  } else if (shrink) {
+    return 1;
+  } else if (shrink === false) {
+    return 0;
+  }
+
+  if (basis && basis !== 'auto') {
+    return 0;
+  }
+
+  return 1; // default
+}
+
+function getBasis(props: Flex): string {
+  const {basis} = props;
+  if (basis) {
+    const suffix = typeof basis === 'number' || String(parseInt(basis as string, 10)) === basis ? 'px' : '';
+    return basis + suffix;
+  }
+
+  return 'auto'; // default
+}
+
+function getFlexStyles(props: Flex): React.CSSProperties {
+  const {column, wrap, vAlignContent, hAlignContent} = props;
+
+  function alignPropToFlex(align: Flex['vAlignContent'] | Flex['hAlignContent']) {
+    switch (align) {
+      case 'top':
+      case 'left':
+        return 'flex-start';
+      case 'center':
+        return 'center';
+      case 'bottom':
+      case 'right':
+        return 'flex-end';
+    }
+  }
+
+  return {
+    flex: `${getGrow(props)} ${getShrink(props)} ${getBasis(props)}`,
+    flexDirection: column ? 'column' : 'row',
+    flexWrap: wrap ? 'wrap' : 'nowrap',
+    justifyContent: alignPropToFlex(column ? vAlignContent : hAlignContent),
+    alignItems: alignPropToFlex(column ? hAlignContent : vAlignContent),
+  };
+}
+
+const Flex: React.FC<Flex> = props => {
   return (
-    <Box
-      display="flex"
-      alignContent={props.alignContent}
-      alignItems={props.alignItems}
-      alignSelf={props.alignSelf}
-      flex={props.flex}
-      flexBasis={props.flexBasis}
-      flexDirection={props.flexDirection}
-      flexGrow={props.flexGrow}
-      flexShrink={props.flexShrink}
-      flexWrap={props.flexWrap}
-      justifyItems={props.justifyItems}
-      justifyContent={props.justifyContent}
-      justifySelf={props.justifySelf}
-      order={props.order}
-    >
+    <Box {...getFlexStyles(props)} display={props.display} order={props.order}>
       {props.children}
     </Box>
   );
+};
+
+Flex.propTypes = {
+  display: PropTypes.oneOf(['flex', 'inline-flex']),
+  column: PropTypes.bool,
+  vAlignContent: PropTypes.oneOf(['top', 'center', 'bottom']),
+  hAlignContent: PropTypes.oneOf(['left', 'center', 'right']),
+  grow: PropTypes.oneOfType([PropTypes.bool, PropTypes.number]),
+  shrink: PropTypes.oneOfType([PropTypes.bool, PropTypes.number]),
+  basis: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  order: PropTypes.number,
+  wrap: PropTypes.bool,
+};
+
+Flex.defaultProps = {
+  display: 'flex',
 };
 
 Flex.displayName = 'Flex';
--- packages/paste-core/utilities/flex/stories/index.stories.tsx
+++ packages/paste-core/utilities/flex/stories/index.stories.tsx
@@ -1,14 +1,41 @@
 import * as React from 'react';
 import {storiesOf} from '@storybook/react';
-import {withKnobs} from '@storybook/addon-knobs';
-import {Flex} from '../src';
+import {withKnobs, select, boolean} from '@storybook/addon-knobs';
+import {Box} from '@twilio-paste/box';
+import {Paragraph} from '@twilio-paste/paragraph';
+import {Flex, displayOptions, vAlignOptions, hAlignOptions} from '../src';
+
+const flexDisplayOptions = ['flex', 'inline-flex'];
+const flexVerticalAlignOptions = ['top', 'center', 'bottom'];
+const flexHorizontalAlignOptions = ['left', 'center', 'right'];
 
 storiesOf('Utilities|Flex', module)
   .addDecorator(withKnobs)
   .add('Default', () => {
+    const flexDisplayValue = select('display', flexDisplayOptions, 'flex') as displayOptions;
+    const flexVerticalAlignValue = select('vAlignContent', flexVerticalAlignOptions, 'top') as vAlignOptions;
+    const flexHorizontalAlignValue = select('hAlignContent', flexHorizontalAlignOptions, 'left') as hAlignOptions;
     return (
-      <Flex alignContent="center" justifyContent="center" justifySelf="" order={1} flexBasis="" alignSelf>
-        blah
+      <Flex
+        display={flexDisplayValue}
+        column={boolean('column', false)}
+        hAlignContent={flexHorizontalAlignValue}
+        vAlignContent={flexVerticalAlignValue}
+        wrap={boolean('wrap', false)}
+      >
+        <Flex grow basis={200}>
+          <Paragraph>Flexed left column</Paragraph>
+        </Flex>
+        <Flex grow>
+          <Box>
+            <Paragraph>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur
+              adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor
+              sit amet, consectetur adipisicing elit.
+            </Paragraph>
+          </Box>
+        </Flex>
       </Flex>
     );
   });
