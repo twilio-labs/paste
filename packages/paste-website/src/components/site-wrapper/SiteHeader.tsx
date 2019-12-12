e159d1ceb723f1d4fd65ff051df9ab6ef0786a70
Si Taggart <me@simontaggart.com>
2019-12-12 14:05:12 -0800

    feat: create the paste theme and make it the default

--- packages/paste-website/src/components/site-wrapper/SiteHeader.tsx
+++ packages/paste-website/src/components/site-wrapper/SiteHeader.tsx
@@ -8,6 +8,7 @@ import {ThemeSwitcher} from '../ThemeSwitcher';
 import GithubIcon from '../icons/GithubIcon';
 import {SIDEBAR_WIDTH, HEADER_HEIGHT} from './constants';
 import {version} from '../../../../../package.json';
+import {useActiveSiteTheme} from '../../context/ActiveSiteThemeContext';
 
 interface FlexProps {
   justifyContent?: string;
@@ -23,47 +24,61 @@ export const Flex = styled.div<FlexProps>(({justifyContent, alignItems = 'center
 
 export const SiteHeader: React.FC<{}> = () => {
   const theme = useTheme();
+  const {theme: activeTheme} = useActiveSiteTheme();
   return (
-    <Absolute
-      preset="top_fill"
-      as="aside"
-      backgroundColor="colorBackgroundBody"
-      borderColor="colorBorderLight"
-      borderStyle="none none solid none"
-      borderBottomWidth="borderWidth10"
-      paddingLeft="space200"
-      paddingRight="space80"
-      paddingTop="space50"
-      paddingBottom="space50"
-      marginBottom="space140"
-      css={{
-        left: SIDEBAR_WIDTH,
-        height: HEADER_HEIGHT,
-      }}
-    >
-      <Flex justifyContent="space-between">
-        <ThemeSwitcher />
-        <Flex>
-          <Box marginRight="space60">
-            <Anchor href="https://www.github.com/twilio-labs/paste/issues">Ask a question</Anchor>
-          </Box>
-          <Box marginRight="space60">
-            <Anchor href="https://www.github.com/twilio-labs/paste/issues">Report a bug</Anchor>
-          </Box>
-          <Flex>
-            v{version}
-            <Box marginLeft="space30">
-              <Anchor href="https://www.github.com/twilio-labs/paste">
-                <GithubIcon
-                  css={{height: theme.heights.sizeIcon30, width: theme.heights.sizeIcon30}}
-                  title="View this project on github"
-                  color={theme.textColors.colorTextWeak}
-                />
-              </Anchor>
+    <>
+      <Absolute
+        preset="top_fill"
+        as="aside"
+        backgroundColor="colorBackgroundBody"
+        borderColor="colorBorderLight"
+        borderStyle="none none solid none"
+        borderBottomWidth="borderWidth10"
+        paddingLeft="space200"
+        paddingRight="space80"
+        paddingTop="space50"
+        paddingBottom="space50"
+        marginBottom="space140"
+        css={{
+          left: SIDEBAR_WIDTH,
+          height: HEADER_HEIGHT,
+        }}
+      >
+        <Flex justifyContent="space-between">
+          <ThemeSwitcher />
+          {activeTheme === 'default' && (
+            <Box
+              backgroundColor="colorBackgroundWarningLightest"
+              borderRadius="borderRadius20"
+              padding="space20"
+              paddingLeft="space40"
+              paddingRight="space40"
+            >
+              <strong>WARNING:</strong> The Paste theme is an <em>extremely early</em> preview of future work!!!
             </Box>
+          )}
+          <Flex>
+            <Box marginRight="space60">
+              <Anchor href="https://www.github.com/twilio-labs/paste/issues">Ask a question</Anchor>
+            </Box>
+            <Box marginRight="space60">
+              <Anchor href="https://www.github.com/twilio-labs/paste/issues">Report a bug</Anchor>
+            </Box>
+            <Flex>
+              v{version}
+              <Box marginLeft="space30">
+                <Anchor href="https://www.github.com/twilio-labs/paste">
+                  <GithubIcon
+                    css={{height: theme.heights.sizeIcon30, width: theme.heights.sizeIcon30}}
+                    title="View this project on github"
+                    color={theme.textColors.colorTextWeak}
+                  />
+                </Anchor>
+              </Box>
+            </Flex>
           </Flex>
         </Flex>
-      </Flex>
-    </Absolute>
+      </Absolute>
+    </>
   );
 };
