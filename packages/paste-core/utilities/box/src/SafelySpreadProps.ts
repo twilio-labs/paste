b851815276d75c10aa1c51accf4f45a233173482
Si Taggart <me@simontaggart.com>
2019-12-13 20:01:21 -0800

    feat(box): use style-props package, create method to safelySpreadProps
    Use style-props package over the types package
    
    Create a method to safely spread props onto the Box component without
    overriding styles from compositional use of Box

---
+++ packages/paste-core/utilities/box/src/SafelySpreadProps.ts
@@ -0,0 +1,35 @@
+import {
+  LAYOUT_PROPS,
+  SPACE_PROPS,
+  BACKGROUND_PROPS,
+  BORDER_PROPS,
+  SHADOW_PROPS,
+  POSITION_PROPS,
+  FLEXBOX_PROPS,
+} from '@twilio-paste/style-props';
+
+export const BOX_PROPS_TO_BLOCK = [
+  ...LAYOUT_PROPS,
+  ...SPACE_PROPS,
+  ...BACKGROUND_PROPS,
+  ...BORDER_PROPS,
+  ...SHADOW_PROPS,
+  ...POSITION_PROPS,
+  ...FLEXBOX_PROPS,
+  'backgroundColor',
+  'borderColor',
+  'className',
+  'style',
+];
+
+export const safelySpreadBoxProps = (props: {}): {} => {
+  // https://www.measurethat.net/Benchmarks/Show/6642/0/for-in-vs-reduce-vs-pick#latest_results_block
+  const newList = Object.keys(props).reduce((newObj, key) => {
+    if (!BOX_PROPS_TO_BLOCK.includes(key)) {
+      // eslint-disable-next-line no-param-reassign
+      newObj[key] = props[key];
+    }
+    return newObj;
+  }, {});
+  return newList;
+};
