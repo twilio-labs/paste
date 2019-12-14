c204f0272b790d6ddeb149f973e4e69d566ba8a0
Si Taggart <me@simontaggart.com>
2019-12-13 20:00:30 -0800

    feat(text): use style-props package, create method to safelySpreadProps
    Use the style-props package over the types package
    
    Create a method to be able to safely spread props onto the Text
    component without restyling compositions using Text

---
+++ packages/paste-core/utilities/text/src/SafelySpreadProps.ts
@@ -0,0 +1,24 @@
+import {SPACE_PROPS, TYPOGRAPHY_PROPS, OVERFLOW_PROPS} from '@twilio-paste/style-props';
+
+export const TEXT_PROPS_TO_BLOCK = [
+  ...SPACE_PROPS,
+  ...TYPOGRAPHY_PROPS,
+  ...OVERFLOW_PROPS,
+  'display',
+  'textColor',
+  'verticalAlign',
+  'className',
+  'style',
+];
+
+export const safelySpreadTextProps = (props: {}): {} => {
+  // https://www.measurethat.net/Benchmarks/Show/6642/0/for-in-vs-reduce-vs-pick#latest_results_block
+  const newList = Object.keys(props).reduce((newObj, key) => {
+    if (!TEXT_PROPS_TO_BLOCK.includes(key)) {
+      // eslint-disable-next-line no-param-reassign
+      newObj[key] = props[key];
+    }
+    return newObj;
+  }, {});
+  return newList;
+};
