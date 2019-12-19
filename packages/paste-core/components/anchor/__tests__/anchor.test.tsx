3395ed165fc560c4bc9f36957a009d0de92441fa
Si Taggart <me@simontaggart.com>
2019-12-19 14:59:26 -0800

    test(anchor): update tests for default theme

--- packages/paste-core/components/anchor/__tests__/anchor.test.tsx
+++ packages/paste-core/components/anchor/__tests__/anchor.test.tsx
@@ -7,7 +7,7 @@ describe('Anchor', () => {
   it('it should render an anchor', (): void => {
     const tree = renderer
       .create(
-        <Theme.Provider>
+        <Theme.Provider theme="console">
           <Anchor href="/">This is an anchor</Anchor>
         </Theme.Provider>
       )
@@ -17,7 +17,7 @@ describe('Anchor', () => {
   it('it should render an external anchor', (): void => {
     const tree = renderer
       .create(
-        <Theme.Provider>
+        <Theme.Provider theme="console">
           <Anchor href="https://twilio.com">
             This is an anchor that links to Twilio.com with an external target and rel
           </Anchor>
@@ -29,7 +29,7 @@ describe('Anchor', () => {
   it('it should render an external anchor with overwritten target and rel', (): void => {
     const tree = renderer
       .create(
-        <Theme.Provider>
+        <Theme.Provider theme="console">
           <Anchor href="https://twilio.com" target="_self" rel="noopener">
             This is an anchor that links to Twilio.com with the target and rel overwritten
           </Anchor>
