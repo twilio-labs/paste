72852aae444adc14225fe19153002a44acb6d0b0
Aayush Iyer <aiyer@twilio.com>
2020-01-02 16:30:31 -0800

    docs(website): update intro (#241)
    * docs(website): update intro
    
    * Update packages/paste-website/src/pages/index.tsx
    
    Co-Authored-By: Simon Taggart <staggart@twilio.com>
    
    * docs(website): fix escape characters
    
    * fix(website): prettier homepage copy update
    
    Co-authored-by: Simon Taggart <me@simontaggart.com>

--- packages/paste-website/src/pages/index.tsx
+++ packages/paste-website/src/pages/index.tsx
@@ -43,9 +43,10 @@ const IndexPage: React.FC<{}> = (): React.ReactElement => {
           Paste
         </Heading>
         <P variant="lead">
-          Paste is an internal design system for designing and building consistent experiences at Twilio. It is made up
-          of tokens and components, along with in-depth documentation to assist designers and developers faced with
-          solving our customers’ problems. It is open source and contributions are welcome.
+          Paste is a design system for Twilio. We use it to build consistent, high quality customer experiences at
+          Twilio. This site documents the design system: you will find design tokens, primitives, UI components, and
+          utilities, along with in-depth documentation to assist product designers and developers. Paste is open source
+          and contributions are welcome.
         </P>
         <Box marginTop="space80" marginBottom="space80">
           <Box display="inline-block" marginRight="space40">
