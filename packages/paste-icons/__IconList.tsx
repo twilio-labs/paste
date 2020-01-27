c372348f3cf10cf2f8df16e3be52cef4bf124b72
Si Taggart <me@simontaggart.com>
2020-01-27 08:31:29 -0800

    fix(icons): rename TwilioorgIcon to TwilioOrgIcon

--- packages/paste-icons/__IconList.tsx
+++ packages/paste-icons/__IconList.tsx
@@ -11,7 +11,7 @@ import {InformationIcon} from './src/InformationIcon';
 import {LoadingIcon} from './src/LoadingIcon';
 import {MoreIcon} from './src/MoreIcon';
 import {PlusIcon} from './src/PlusIcon';
-import {ProductTwilioorgIcon} from './src/ProductTwilioorgIcon';
+import {ProductTwilioOrgIcon} from './src/ProductTwilioOrgIcon';
 import {WarningIcon} from './src/WarningIcon';
 
 interface IconProps {
@@ -30,7 +30,7 @@ const IconList: React.FC<IconProps> = props => (
 <StoryIcon><LoadingIcon {...props} /><Text as="p" textColor="currentColor">LoadingIcon</Text></StoryIcon>
 <StoryIcon><MoreIcon {...props} /><Text as="p" textColor="currentColor">MoreIcon</Text></StoryIcon>
 <StoryIcon><PlusIcon {...props} /><Text as="p" textColor="currentColor">PlusIcon</Text></StoryIcon>
-<StoryIcon><ProductTwilioorgIcon {...props} /><Text as="p" textColor="currentColor">ProductTwilioorgIcon</Text></StoryIcon>
+<StoryIcon><ProductTwilioOrgIcon {...props} /><Text as="p" textColor="currentColor">ProductTwilioOrgIcon</Text></StoryIcon>
 <StoryIcon><WarningIcon {...props} /><Text as="p" textColor="currentColor">WarningIcon</Text></StoryIcon>
   </Grid>
 );
