21af7a795c79a47409326dfe883be64eae43f67f
Si Taggart <me@simontaggart.com>
2020-01-27 08:26:08 -0800

    chore: stop removing the lock file when we clean the repo

--- tools/build/clean-repo.js
+++ tools/build/clean-repo.js
@@ -1,6 +1,5 @@
-const {removeYarnLockfile} = require('../utils/removeYarnLockfile');
 const {removeTsconfigBuildFiles} = require('../utils/removeTsconfigBuildFiles');
 
 (async () => {
-  await Promise.all([removeYarnLockfile(), removeTsconfigBuildFiles()]);
+  await Promise.all([removeTsconfigBuildFiles()]);
 })();
