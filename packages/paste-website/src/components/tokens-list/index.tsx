cf68ad62682de7b755d6b3333965f3232a252c93
Alex Thomsen <alex.scott.thomsen@gmail.com>
2020-01-29 12:14:49 -0800

    chore(docs): added font and font-size callouts (#259)
    * feat(docs): added font and font-size callouts
    
    * feat(docs): updated engineering guidelines to have a blurb about fonts
    
    * feat(card): update copy based on feedback, fix types
    
    * feat(docs): secondary to primary

--- packages/paste-website/src/components/tokens-list/index.tsx
+++ packages/paste-website/src/components/tokens-list/index.tsx
@@ -10,6 +10,7 @@ import {Label} from '../label';
 import {InlineCode} from '../Typography';
 import {AnchoredHeading} from '../Heading';
 import {useActiveSiteTheme} from '../../context/ActiveSiteThemeContext';
+import {Callout, CalloutTitle, CalloutText} from '../callout';
 
 const sentenceCase = (catName: string): string => {
   return catName
@@ -30,6 +31,7 @@ interface Token {
 
 interface TokenCategory {
   categoryName: string;
+  info?: React.ReactNode;
   tokens: Token[];
 }
 
@@ -47,6 +49,33 @@ interface TokensListProps {
 }
 
 const getTokensByTheme = (theme: ThemeVariants, props: TokensListProps): TokenCategory[] => {
+  const font = props.consoleTokens[0].node.tokens.find(ele => ele.categoryName === 'fonts');
+  if (font) {
+    font.info = (
+      <Callout>
+        <CalloutTitle as="h4">Heads up about fonts in Paste!</CalloutTitle>
+        <CalloutText as="p">
+          Paste leaves it up to you to load the fonts needed for the theme you&apos;ve selected. Console uses Whitney
+          ScreenSmart and SendGrid uses Colfax
+        </CalloutText>
+      </Callout>
+    );
+  }
+
+  const fontSize = props.consoleTokens[0].node.tokens.find(ele => ele.categoryName === 'font-sizes');
+  if (fontSize) {
+    fontSize.info = (
+      <Callout>
+        <CalloutTitle as="h4">Heads up about font sizes in Paste!</CalloutTitle>
+        <CalloutText as="p">
+          Font-sizes in our Paste tokens use rem values. Unlike em values which are relative to their parent element,
+          rem values are relative to the html element. If you aren&apos;t using the Theme.Provider component, you must
+          set font-size: 100%; on your page&apos;s html tag for the font-sizes to be sized correctly as 1rem=16px.
+        </CalloutText>
+      </Callout>
+    );
+  }
+
   let tokens = [] as TokenCategory[];
   if (theme === ThemeVariants.CONSOLE) {
     if (props.consoleTokens != null) {
@@ -126,6 +155,7 @@ export const TokensList: React.FC<TokensListProps> = props => {
               <AnchoredHeading as="h2" variant="heading20">
                 {sentenceCase(cat.categoryName)}
               </AnchoredHeading>
+              {cat.info}
               <Theme.Provider theme={theme}>
                 <Box marginBottom="space160">
                   <Table>
