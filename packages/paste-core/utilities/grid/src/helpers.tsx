0723895911a929d7685a6bed390a94a0ab0bee71
richbachman <rb@richbachman.com>
2020-02-03 16:02:27 -0700

    chore(grid): add comments to helper functions

--- packages/paste-core/utilities/grid/src/helpers.tsx
+++ packages/paste-core/utilities/grid/src/helpers.tsx
@@ -11,6 +11,7 @@ import {
   ColumnWidthSpan,
 } from './types';
 
+// Gets the gutter and returns the value to be used a negative margin to Grid
 export const getOuterGutterPull = (theme: ThemeShape, gutter?: Space): Margin => {
   if (Array.isArray(gutter)) {
     return (gutter as SpaceOptions[]).map((value: SpaceOptions) => {
@@ -25,6 +26,7 @@ export const getOuterGutterPull = (theme: ThemeShape, gutter?: Space): Margin =>
   return 'auto';
 };
 
+// Gets the vertical prop and returns 100% or 0 to be used as Column minWidths
 export const getStackedColumns = (vertical: Vertical): ColumnMinWidth => {
   if (Array.isArray(vertical)) {
     return (vertical as Vertical[]).map((value: Vertical) => {
@@ -42,6 +44,7 @@ export const getStackedColumns = (vertical: Vertical): ColumnMinWidth => {
   return '0';
 };
 
+// Gets the Column span prop, calculates, and returns the value to be used as the Column width
 export const getColumnSpan = ({count, span}: ColumnProps): ColumnWidthSpan => {
   if (Array.isArray(span) && count) {
     return (span as ColumnSpanOptions[]).map((value: ColumnSpanOptions) => {
@@ -60,6 +63,7 @@ export const getColumnSpan = ({count, span}: ColumnProps): ColumnWidthSpan => {
   return `${(1 / 12) * 100}%`;
 };
 
+// Gets the Column offset prop, calculates, and returns the value to be used as the Column marginLeft
 export const getColumnOffset = (offset: ColumnOffset): ResponsiveValue<string> => {
   if (Array.isArray(offset)) {
     return (offset as ColumnOffsetOptions[]).map((value: ColumnOffsetOptions) => {
