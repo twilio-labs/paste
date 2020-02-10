c14178df4a184a6e65c8416cfc07ed7ce62cb6d7
Katie Porter <katie@janekporter.com>
2020-02-09 17:07:05 -0700

    feat(website): add optional source url to blockquote

--- packages/paste-website/src/components/Blockquote.tsx
+++ packages/paste-website/src/components/Blockquote.tsx
@@ -1,11 +1,27 @@
 import * as React from 'react';
+import { Anchor } from '@twilio-paste/anchor';
 import { Box } from '@twilio-paste/box';
 import { Text } from '@twilio-paste/text';
+
 interface Props {
   name: string;
   source: string;
+  sourceUrl: string;
 }
-export const Blockquote: React.FC<Props> = ({ children, name, source }) => {
+
+const Citation: React.FC<Props> = ({ name, source, sourceUrl }) => {
+  return (
+    <Text as="footer" fontWeight="fontWeightSemibold" marginTop="space40">
+      &mdash; {name}
+      {source && ', '}
+      {source && (
+        <cite>{sourceUrl ? <Anchor href={sourceUrl}>{source}</Anchor> : <Text as="span">{source}</Text>}</cite>
+      )}
+    </Text>
+  );
+}
+
+export const Blockquote: React.FC<Props> = ({ children, name, source, sourceUrl }) => {
   return (
     <Box
       borderLeftWidth="borderWidth20"
@@ -21,10 +37,7 @@ export const Blockquote: React.FC<Props> = ({ children, name, source }) => {
         {children}
       </Text>
       {name && (
-        <Text as="footer" fontWeight="fontWeightSemibold" marginTop="space40">
-          &mdash; {name}
-          {source && <cite>, {source}</cite>}
-        </Text>
+        <Citation name={name} source={source} sourceUrl={sourceUrl}></Citation>
       )}
     </Box>
   );
