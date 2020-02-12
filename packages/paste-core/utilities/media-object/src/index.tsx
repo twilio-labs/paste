455a96193f50b51a5c7571c7bf49d3d2b7477e59
Si Taggart <me@simontaggart.com>
2020-02-12 09:28:26 -0800

    feat(media-object): add support for the as prop

--- packages/paste-core/utilities/media-object/src/index.tsx
+++ packages/paste-core/utilities/media-object/src/index.tsx
@@ -1,27 +1,28 @@
 import * as React from 'react';
-import {Box} from '@twilio-paste/box';
+import {Box, BoxProps} from '@twilio-paste/box';
 import {Space} from '@twilio-paste/style-props';
 
-export interface MediaObjectProps {
+export interface MediaObjectProps extends Pick<BoxProps, 'as'> {
   verticalAlign?: 'center' | 'top';
 }
 
-const MediaObject: React.FC<MediaObjectProps> = ({children, verticalAlign = 'top'}) => {
+const MediaObject: React.FC<MediaObjectProps> = ({as = 'span', children, verticalAlign = 'top'}) => {
   return (
-    <Box display="flex" alignItems={verticalAlign === 'top' ? 'flex-start' : 'center'}>
+    <Box as={as} display="flex" alignItems={verticalAlign === 'top' ? 'flex-start' : 'center'}>
       {children}
     </Box>
   );
 };
 MediaObject.displayName = 'MediaObject';
 
-export interface MediaFigureProps {
+export interface MediaFigureProps extends Pick<BoxProps, 'as'> {
   align?: 'start' | 'end';
   spacing?: Space;
 }
-const MediaFigure: React.FC<MediaFigureProps> = ({children, align, spacing}) => {
+const MediaFigure: React.FC<MediaFigureProps> = ({as = 'span', children, align, spacing}) => {
   return (
     <Box
+      as={as}
       display="flex"
       flexShrink={0}
       marginLeft={align === 'end' ? spacing : undefined}
@@ -37,8 +38,13 @@ MediaFigure.defaultProps = {
   spacing: 'space0',
 };
 
-const MediaBody: React.FC<{}> = ({children}) => {
-  return <Box flex={1}>{children}</Box>;
+export type MediaBodyProps = Pick<BoxProps, 'as'>;
+const MediaBody: React.FC<MediaBodyProps> = ({as = 'span', children}) => {
+  return (
+    <Box as={as} flex={1}>
+      {children}
+    </Box>
+  );
 };
 MediaBody.displayName = 'MediaBody';
 
