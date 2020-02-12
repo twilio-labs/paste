899e0657873cc00a13951dc080d0218bfe32b959
Si Taggart <me@simontaggart.com>
2020-02-12 11:57:35 -0800

    feat(heading): allow removing bottom margin

--- packages/paste-core/components/heading/stories/index.stories.tsx
+++ packages/paste-core/components/heading/stories/index.stories.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {storiesOf} from '@storybook/react';
 import {withKnobs, text, select} from '@storybook/addon-knobs';
+import {Card} from '@twilio-paste/card';
 import {asTags, Heading, HeadingVariants} from '../src';
 
 const headingVariantOptions = ['heading10', 'heading20', 'heading30', 'heading40', 'heading50', 'heading60'];
@@ -9,9 +10,10 @@ storiesOf('Components|Heading', module)
   .addDecorator(withKnobs)
   .add('All variants', () => {
     const asOptions = text('as', 'h2') as asTags;
-    const headingVariantValue = select('variant', headingVariantOptions, 'heading20') as HeadingVariant;
+    const headingVariantValue = select('variant', headingVariantOptions, 'heading20') as HeadingVariants;
+    const marginBottomValue = select('marginBottom', [null, 'space0'], null);
     return (
-      <Heading as={asOptions} variant={headingVariantValue}>
+      <Heading as={asOptions} marginBottom={marginBottomValue} variant={headingVariantValue}>
         I am a Very Large Heading
       </Heading>
     );
@@ -56,5 +58,59 @@ storiesOf('Components|Heading', module)
       <Heading as="h1" variant="heading60">
         I am a Very Large Heading
       </Heading>
+    );
+  })
+  .add('heading10 no margin', () => {
+    return (
+      <Card>
+        <Heading as="h1" marginBottom="space0" variant="heading10">
+          I am a Very Large Heading
+        </Heading>
+      </Card>
+    );
+  })
+  .add('heading20 no margin', () => {
+    return (
+      <Card>
+        <Heading as="h1" marginBottom="space0" variant="heading20">
+          I am a Very Large Heading
+        </Heading>
+      </Card>
+    );
+  })
+  .add('heading30 no margin', () => {
+    return (
+      <Card>
+        <Heading as="h1" marginBottom="space0" variant="heading30">
+          I am a Very Large Heading
+        </Heading>
+      </Card>
+    );
+  })
+  .add('heading40 no margin', () => {
+    return (
+      <Card>
+        <Heading as="h1" marginBottom="space0" variant="heading40">
+          I am a Very Large Heading
+        </Heading>
+      </Card>
+    );
+  })
+  .add('heading50 no margin', () => {
+    return (
+      <Card>
+        <Heading as="h1" marginBottom="space0" variant="heading50">
+          I am a Very Large Heading
+        </Heading>
+      </Card>
+    );
+  })
+  .add('heading60 no margin', () => {
+    return (
+      <Card>
+        <Heading as="h1" marginBottom="space0" variant="heading60">
+          I am a Very Large Heading
+        </Heading>
+      </Card>
     );
   });
