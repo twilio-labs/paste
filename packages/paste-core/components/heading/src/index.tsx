b57ccc40e9ac817b1e0f6f8bc9616fc0487221b1
sisber <sisber@twilio.com>
2020-02-14 14:32:17 -0600

    fix(heading): accept "header" an an "asTag"

--- packages/paste-core/components/heading/src/index.tsx
+++ packages/paste-core/components/heading/src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import {Text} from '@twilio-paste/text';
 
-export type asTags = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'div' | 'label' | 'span';
+export type asTags = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'div' | 'label' | 'span' | 'header';
 export type HeadingVariants = 'heading10' | 'heading20' | 'heading30' | 'heading40' | 'heading50' | 'heading60';
 
 export interface HeadingProps {
