f762e475462b75702f2f8f2381f56da51fdda075
Shadi <TheSisb@users.noreply.github.com>
2020-02-28 13:52:39 -0600

    test(design-tokens): snapshot exported token values per theme (#338)

---
+++ packages/paste-design-tokens/__tests__/index.test.tsx
@@ -0,0 +1,47 @@
+import theo from 'theo';
+import path from 'path';
+
+beforeAll(() => {
+  theo.registerTransform('web', ['color/rgb', 'color/hex']);
+});
+
+describe('Design Tokens', () => {
+  it('matches the Global theme', () => {
+    const theme = theo.convertSync({
+      transform: {
+        type: 'web',
+        file: path.resolve(__dirname, '../tokens/tokens.yml'),
+      },
+      format: {
+        type: 'json',
+      },
+    });
+    expect(theme).toMatchSnapshot();
+  });
+
+  it('matches the Console theme', () => {
+    const theme = theo.convertSync({
+      transform: {
+        type: 'web',
+        file: path.resolve(__dirname, '../tokens/themes/console/tokens.yml'),
+      },
+      format: {
+        type: 'json',
+      },
+    });
+    expect(theme).toMatchSnapshot();
+  });
+
+  it('matches the Sendgrid theme', () => {
+    const theme = theo.convertSync({
+      transform: {
+        type: 'web',
+        file: path.resolve(__dirname, '../tokens/themes/sendgrid/tokens.yml'),
+      },
+      format: {
+        type: 'json',
+      },
+    });
+    expect(theme).toMatchSnapshot();
+  });
+});
