f4b2e0726788ac16901b0f73c3046d1ecb0fb464
richbachman <rb@richbachman.com>
2020-03-10 17:03:41 -0600

    chore(input): deprecate and remove package in favor of form

--- packages/paste-core/components/input/stories/index.stories.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-import {storiesOf} from '@storybook/react';
-import {Input} from '../src';
-
-storiesOf('Components|Input', module).add('Default', () => <Input inputId="text_id_1" type="text" />);
