b92f42a74df045714209f83619b899236dcbfda2
sisber <sisber@twilio.com>
2020-03-17 13:13:36 -0500

    chore(media-object): component propTypes cleanup pass

--- packages/paste-core/utilities/media-object/src/index.tsx
+++ packages/paste-core/utilities/media-object/src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
+import * as PropTypes from 'prop-types';
 import {Box, BoxProps} from '@twilio-paste/box';
-import {Space} from '@twilio-paste/style-props';
+import {Space, isSpaceTokenProp} from '@twilio-paste/style-props';
 
 export interface MediaObjectProps extends Pick<BoxProps, 'as' | 'marginTop' | 'marginBottom'> {
   verticalAlign?: 'center' | 'top';
@@ -25,12 +26,19 @@ const MediaObject: React.FC<MediaObjectProps> = ({
     </Box>
   );
 };
+
 MediaObject.displayName = 'MediaObject';
+if (process.env.NODE_ENV === 'development') {
+  MediaObject.propTypes = {
+    verticalAlign: PropTypes.oneOf(['center', 'top']),
+  };
+}
 
 export interface MediaFigureProps extends Pick<BoxProps, 'as'> {
   align?: 'start' | 'end';
   spacing?: Space;
 }
+
 const MediaFigure: React.FC<MediaFigureProps> = ({as = 'span', children, align, spacing}) => {
   return (
     <Box
@@ -44,13 +52,21 @@ const MediaFigure: React.FC<MediaFigureProps> = ({as = 'span', children, align, 
     </Box>
   );
 };
+
 MediaFigure.displayName = 'MediaFigure';
 MediaFigure.defaultProps = {
   align: 'start',
   spacing: 'space0',
 };
+if (process.env.NODE_ENV === 'development') {
+  MediaFigure.propTypes = {
+    align: PropTypes.oneOf(['start', 'end']),
+    spacing: isSpaceTokenProp,
+  };
+}
 
 export type MediaBodyProps = Pick<BoxProps, 'as'>;
+
 const MediaBody: React.FC<MediaBodyProps> = ({as = 'span', children}) => {
   return (
     <Box as={as} flex={1}>
@@ -58,6 +74,7 @@ const MediaBody: React.FC<MediaBodyProps> = ({as = 'span', children}) => {
     </Box>
   );
 };
+
 MediaBody.displayName = 'MediaBody';
 
 export {MediaObject, MediaFigure, MediaBody};
