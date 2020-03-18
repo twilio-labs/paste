48279f5a9ae11dcd5a541d06b8bc9fa1f1c8b684
richbachman <rb@richbachman.com>
2020-03-17 20:02:16 -0600

    chore(absolute): move absolute to layout directory

--- packages/paste-core/layout/absolute/stories/index.stories.tsx
+++ packages/paste-core/layout/absolute/stories/index.stories.tsx
@@ -24,7 +24,7 @@ const PresetValues = [
   'right_fill',
 ];
 
-storiesOf('Utilities|Absolute', module)
+storiesOf('Layout|Absolute', module)
   .addDecorator(withKnobs)
   .add('Default', () => {
     // Get knob values
