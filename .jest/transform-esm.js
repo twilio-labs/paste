fad7b47e8ae1b73780b1655f243210113e8b1ed7
sisber <sisber@twilio.com>
2020-03-24 17:22:23 -0500

    fix: make jest work with monorepo packages' nested imports generically

---
+++ .jest/transform-esm.js
@@ -0,0 +1,24 @@
+const babelOptions = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    '@babel/react',
+    [
+      '@emotion/babel-preset-css-prop',
+      {
+        sourceMap: false,
+        autoLabel: true,
+        labelFormat: '[local]',
+        cssPropOptimization: false,
+      },
+    ],
+  ],
+  plugins: ['macros', '@babel/proposal-class-properties', '@babel/proposal-object-rest-spread'],
+};
+module.exports = require('babel-jest').createTransformer(babelOptions);
