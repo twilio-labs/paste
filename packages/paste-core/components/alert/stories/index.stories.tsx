94092c24cfb5a03347b127b43c9df16a2971cf98
Si Taggart <me@simontaggart.com>
2020-03-27 14:13:18 -0700

    fix(alert): set default elements to divs and update stories and snapshots

--- packages/paste-core/components/alert/stories/index.stories.tsx
+++ packages/paste-core/components/alert/stories/index.stories.tsx
@@ -3,6 +3,8 @@ import {storiesOf} from '@storybook/react';
 import {withKnobs, boolean, select} from '@storybook/addon-knobs';
 import {action} from '@storybook/addon-actions';
 import {Text} from '@twilio-paste/text';
+import {Box} from '@twilio-paste/box';
+import {Truncate} from '@twilio-paste/truncate';
 import {Alert, AlertVariants} from '../src';
 
 storiesOf('Components|Alert', module)
@@ -33,17 +35,22 @@ storiesOf('Components|Alert', module)
             commodo cursus magna.
           </Text>
         </Alert>
-        <Alert variant="neutral" onDismiss={action('dismiss')}>
-          <Text as="div">
-            Donec ullamcorper nulla non metus auctor fringilla. Etiam porta sem malesuada magna mollis euismod. Praesent
-            commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus, nisi erat
-            porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor fringilla. Nulla
-            vitae elit libero, a pharetra augue. Donec ullamcorper nulla non metus auctor fringilla. Etiam porta sem
-            malesuada magna mollis euismod. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis
-            mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec
-            ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.
-          </Text>
-        </Alert>
+        <Box width="size50">
+          <Alert variant="neutral" onDismiss={action('dismiss')}>
+            <Text as="div">
+              Donec ullamcorper nulla non metus auctor fringilla. Etiam porta sem malesuada magna mollis euismod.
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus,
+              nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor
+              fringilla. Nulla vitae elit libero, a pharetra augue. Donec ullamcorper nulla non metus auctor fringilla.
+              Etiam porta sem malesuada magna mollis euismod. Praesent commodo cursus magna, vel scelerisque nisl
+              consectetur et. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec
+              elit. Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.
+            </Text>
+            <Text as="p">
+              <Truncate>http://www.extremelylongurlthatmightbreakthelayout.com</Truncate>
+            </Text>
+          </Alert>
+        </Box>
       </>
     );
   })
@@ -64,17 +71,22 @@ storiesOf('Components|Alert', module)
             commodo cursus magna.
           </Text>
         </Alert>
-        <Alert variant="error">
-          <Text as="div">
-            Donec ullamcorper nulla non metus auctor fringilla. Etiam porta sem malesuada magna mollis euismod. Praesent
-            commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus, nisi erat
-            porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor fringilla. Nulla
-            vitae elit libero, a pharetra augue. Donec ullamcorper nulla non metus auctor fringilla. Etiam porta sem
-            malesuada magna mollis euismod. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis
-            mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec
-            ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.
-          </Text>
-        </Alert>
+        <Box width="size50">
+          <Alert variant="error">
+            <Text as="div">
+              Donec ullamcorper nulla non metus auctor fringilla. Etiam porta sem malesuada magna mollis euismod.
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus,
+              nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor
+              fringilla. Nulla vitae elit libero, a pharetra augue. Donec ullamcorper nulla non metus auctor fringilla.
+              Etiam porta sem malesuada magna mollis euismod. Praesent commodo cursus magna, vel scelerisque nisl
+              consectetur et. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec
+              elit. Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.
+            </Text>
+            <Text as="p">
+              <Truncate>http://www.extremelylongurlthatmightbreakthelayout.com</Truncate>
+            </Text>
+          </Alert>
+        </Box>
       </>
     );
   })
@@ -95,17 +107,22 @@ storiesOf('Components|Alert', module)
             commodo cursus magna.
           </Text>
         </Alert>
-        <Alert variant="warning" onDismiss={action('dismiss')}>
-          <Text as="div">
-            Donec ullamcorper nulla non metus auctor fringilla. Etiam porta sem malesuada magna mollis euismod. Praesent
-            commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus, nisi erat
-            porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor fringilla. Nulla
-            vitae elit libero, a pharetra augue. Donec ullamcorper nulla non metus auctor fringilla. Etiam porta sem
-            malesuada magna mollis euismod. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis
-            mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec
-            ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.
-          </Text>
-        </Alert>
+        <Box width="size50">
+          <Alert variant="warning" onDismiss={action('dismiss')}>
+            <Text as="div">
+              Donec ullamcorper nulla non metus auctor fringilla. Etiam porta sem malesuada magna mollis euismod.
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus,
+              nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor
+              fringilla. Nulla vitae elit libero, a pharetra augue. Donec ullamcorper nulla non metus auctor fringilla.
+              Etiam porta sem malesuada magna mollis euismod. Praesent commodo cursus magna, vel scelerisque nisl
+              consectetur et. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec
+              elit. Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.
+            </Text>
+            <Text as="p">
+              <Truncate>http://www.extremelylongurlthatmightbreakthelayout.com</Truncate>
+            </Text>
+          </Alert>
+        </Box>
       </>
     );
   });
