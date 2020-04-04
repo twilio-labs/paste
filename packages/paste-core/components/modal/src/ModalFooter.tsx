c3484028678cb0a817003b9a78d2ee13a4e3a821
Si Taggart <me@simontaggart.com>
2020-04-03 17:16:22 -0700

    feat(modal): create the modal package

---
+++ packages/paste-core/components/modal/src/ModalFooter.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
+import {Box, safelySpreadBoxProps} from '@twilio-paste/box';
+
+export interface ModalFooterProps extends React.HTMLAttributes<HTMLDivElement> {
+  children: NonNullable<React.ReactNode>;
+}
+const ModalFooter: React.FC<ModalFooterProps> = ({children, ...props}) => {
+  return (
+    <Box
+      {...safelySpreadBoxProps(props)}
+      as="footer"
+      borderTopStyle="solid"
+      borderTopWidth="borderWidth10"
+      borderTopColor="colorBorderLight"
+      padding="space50"
+      display="flex"
+      flexShrink={0}
+    >
+      {children}
+    </Box>
+  );
+};
+ModalFooter.displayName = 'ModalFooter';
+
+if (process.env.NODE_ENV === 'development') {
+  ModalFooter.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+}
+export {ModalFooter};
---
+++ packages/paste-core/components/modal/src/index.tsx
@@ -0,0 +1,6 @@
+export * from './Modal';
+export * from './ModalHeader';
+export * from './ModalHeading';
+export * from './ModalBody';
+export * from './ModalFooter';
+export * from './ModalFooterActions';
