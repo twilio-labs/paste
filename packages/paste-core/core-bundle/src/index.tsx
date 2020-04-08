2885a73f9ff3b4df138b24cf79f36b404153fc56
Si Taggart <me@simontaggart.com>
2020-04-07 19:33:15 -0700

    fix(core): include the modal package

--- packages/paste-core/core-bundle/src/index.tsx
+++ packages/paste-core/core-bundle/src/index.tsx
@@ -11,6 +11,7 @@ export * from '@twilio-paste/grid';
 export * from '@twilio-paste/heading';
 export * from '@twilio-paste/list';
 export * from '@twilio-paste/media-object';
+export * from '@twilio-paste/modal';
 export * from '@twilio-paste/modal-dialog-primitive';
 export * from '@twilio-paste/paragraph';
 export * from '@twilio-paste/screen-reader-only';
