33f296f601d0e7d4852a28266a9d777f3a6b7614
Si Taggart <me@simontaggart.com>
2020-05-01 16:39:47 -0700

    fix(style-props): allow auto as valid margin prop

--- packages/paste-style-props/src/proptypes/space.ts
+++ packages/paste-style-props/src/proptypes/space.ts
@@ -3,5 +3,8 @@ import {propValidator} from './utils/propValidator';
 
 // Tokens
 const SpaceOptions = Object.keys(DefaultTheme.space);
+const MarginOptions = ['auto', ...Object.keys(DefaultTheme.space)];
 
 export const isSpaceTokenProp = propValidator(SpaceOptions);
+export const isPaddingTokenProp = propValidator(SpaceOptions);
+export const isMarginTokenProp = propValidator(MarginOptions);
