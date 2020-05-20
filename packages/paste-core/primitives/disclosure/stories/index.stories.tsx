c0fa49902274d56a25dc3732ca0cebda02550236
sisber <sisber@twilio.com>
2020-05-20 10:17:45 -0500

    feat: add disclosure primitive package

---
+++ packages/paste-core/primitives/disclosure/stories/index.stories.tsx
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import {storiesOf} from '@storybook/react';
+import {withKnobs} from '@storybook/addon-knobs';
+import {Anchor, Heading, Paragraph, Separator} from '@twilio-paste/core';
+import {useDisclosurePrimitiveState, DisclosurePrimitive, DisclosurePrimitiveContent} from '../src';
+
+const BasicExample: React.FC = () => {
+  const disclosure = useDisclosurePrimitiveState();
+  return (
+    <>
+      <Heading as="h3" variant="heading30">
+        Basic Example{' '}
+        <sup>
+          <Anchor href="https://reakit.io/docs/disclosure/#usage">1</Anchor>
+        </sup>
+      </Heading>
+      <Separator orientation="horizontal" />
+
+      <DisclosurePrimitive {...disclosure}>Toggle</DisclosurePrimitive>
+      <DisclosurePrimitiveContent {...disclosure}>Content</DisclosurePrimitiveContent>
+    </>
+  );
+};
+
+const ConditionallyRenderingExample: React.FC = () => {
+  const disclosure = useDisclosurePrimitiveState();
+  return (
+    <>
+      <Heading as="h3" variant="heading30">
+        Conditionally rendering{' '}
+        <sup>
+          <Anchor href="https://reakit.io/docs/disclosure/#conditionally-rendering">1</Anchor>
+        </sup>
+      </Heading>
+      <Paragraph>
+        You shouldn&apos;t conditionally render the DisclosureContent component as this will make some Reakit features
+        not work. Instead, you can use render props and conditionally render the underneath element:
+      </Paragraph>
+      <Separator orientation="horizontal" />
+
+      <DisclosurePrimitive {...disclosure}>Toggle</DisclosurePrimitive>
+      {/* instead of {disclosure.visible && <DisclosureContent {...disclosure}>Content</DisclosureContent>} */}
+      <DisclosurePrimitiveContent {...disclosure}>
+        {props => disclosure.visible && <div {...props}>Content</div>}
+      </DisclosurePrimitiveContent>
+    </>
+  );
+};
+
+const MultipleExample: React.FC = () => {
+  const disclosure1 = useDisclosurePrimitiveState();
+  const disclosure2 = useDisclosurePrimitiveState();
+
+  return (
+    <>
+      <Heading as="h3" variant="heading30">
+        Multiple components{' '}
+        <sup>
+          <Anchor href="https://reakit.io/docs/disclosure/#multiple-components">1</Anchor>
+        </sup>
+      </Heading>
+      <Paragraph>
+        Each DisclosureContent component should have its own useDisclosureState. This is also true for derivative
+        modules like Dialog, Popover, Menu, Tooltip etc.
+      </Paragraph>
+      <Paragraph>
+        If you want to have only one Disclosure element controling multiple DisclosureContent components, you can use
+        render props to apply the same state to different Disclosures that will result in a single element.
+      </Paragraph>
+      <Separator orientation="horizontal" />
+
+      <DisclosurePrimitive {...disclosure1}>
+        {props => (
+          <DisclosurePrimitive {...props} {...disclosure2}>
+            Toggle All
+          </DisclosurePrimitive>
+        )}
+      </DisclosurePrimitive>
+      <DisclosurePrimitiveContent {...disclosure1}>Content 1</DisclosurePrimitiveContent>
+      <DisclosurePrimitiveContent {...disclosure2}>Content 2</DisclosurePrimitiveContent>
+    </>
+  );
+};
+
+storiesOf('Primitives|Disclosure', module)
+  .addDecorator(withKnobs)
+  .add('Basic Example', () => <BasicExample />)
+  .add('Conditional Rendering', () => <ConditionallyRenderingExample />)
+  .add('Multiple Components', () => <MultipleExample />);
