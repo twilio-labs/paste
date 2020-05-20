250ce2e70ad3cf1bdf79e9d63a6f0518b4f5ecb0
sisber <sisber@twilio.com>
2020-05-20 10:17:45 -0500

    feat: add disclosure primitive package

---
+++ packages/paste-core/primitives/disclosure/src/index.tsx
@@ -0,0 +1,6 @@
+// https://reakit.io/docs/disclosure/
+export {
+  useDisclosureState as useDisclosurePrimitiveState,
+  Disclosure as DisclosurePrimitive,
+  DisclosureContent as DisclosurePrimitiveContent,
+} from '@twilio-paste/reakit-library';
