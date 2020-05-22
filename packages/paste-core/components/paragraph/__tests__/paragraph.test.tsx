20ce7607de7c4ee9a344caaf0c1d818609905087
sisber <sisber@twilio.com>
2020-05-22 16:04:54 -0500

    test(paragraph): update to new library

--- packages/paste-core/components/paragraph/__tests__/paragraph.test.tsx
+++ packages/paste-core/components/paragraph/__tests__/paragraph.test.tsx
@@ -1,58 +1,20 @@
 import * as React from 'react';
-import renderer from 'react-test-renderer';
-import {render} from 'react-dom';
+import {render as testRender} from '@testing-library/react';
 import {axe} from 'jest-axe';
-import {Theme} from '@twilio-paste/theme';
 import {Paragraph} from '../src';
 
-describe('Paragraph', () => {
-  it('it should render a single paragraph', (): void => {
-    const tree = renderer
-      .create(
-        <Theme.Provider theme="console">
-          <Paragraph>This is a paragraph</Paragraph>
-        </Theme.Provider>
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
+describe('General', () => {
+  it('should render', (): void => {
+    const textContent = `This is a paragraph`;
+    const {getByText} = testRender(<Paragraph>{textContent}</Paragraph>);
+    expect(getByText(textContent)).toBeDefined();
   });
+});
 
-  it('should have no accessibility violations', async () => {
-    const container = document.createElement('div');
-    document.body.append(container);
-    render(
-      <Theme.Provider theme="console">
-        <Paragraph>This is a paragraph</Paragraph>
-      </Theme.Provider>,
-      container
-    );
-    const results = await axe(document.body);
+describe('Accessibility', () => {
+  it('Should have no accessibility violations', async () => {
+    const {container} = testRender(<Paragraph>Hello world!</Paragraph>);
+    const results = await axe(container);
     expect(results).toHaveNoViolations();
-  });
-
-  it('it should render a single paragraph with italic text', (): void => {
-    const tree = renderer
-      .create(
-        <Theme.Provider theme="console">
-          <Paragraph>
-            <i>This is a paragraph with italic text</i>
-          </Paragraph>
-        </Theme.Provider>
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('it should render a single paragraph with bold text', (): void => {
-    const tree = renderer
-      .create(
-        <Theme.Provider theme="console">
-          <Paragraph>
-            <strong>This is a paragraph with bold text</strong>
-          </Paragraph>
-        </Theme.Provider>
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
   });
 });
