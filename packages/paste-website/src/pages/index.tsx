90e4483bf5b92dbdac3fd25d456a7d9fe59ec861
Aayush Iyer <aiyer@twilio.com>
2020-05-29 14:26:49 -0700

    docs(website): content update for home page (#463)
    * docs(website): content update for home page
    
    * docs(website): homepage updates based on feedback
    
    * docs(website): added heading, stack, and card to homepage content
    
    * docs(website): fix PR checks
    
    * fix(website): PR checks resolved

--- packages/paste-website/src/pages/index.tsx
+++ packages/paste-website/src/pages/index.tsx
@@ -1,32 +1,21 @@
 import * as React from 'react';
 import {Helmet} from 'react-helmet';
-import {navigate} from 'gatsby';
 import styled from '@emotion/styled';
-import {themeGet} from '@styled-system/theme-get';
-import {Button} from '@twilio-paste/button';
 import {Box} from '@twilio-paste/box';
 import {Heading} from '@twilio-paste/heading';
 import {Paragraph} from '@twilio-paste/paragraph';
-import {Separator} from '@twilio-paste/separator';
+import {Anchor} from '@twilio-paste/anchor';
+import {Stack} from '@twilio-paste/stack';
+import {Card} from '@twilio-paste/card';
 import {SiteWrapper} from '../components/site-wrapper';
 import {Grid} from '../components/grid';
 import {P} from '../components/Typography';
-import {Brush} from '../components/icons/Brush';
-import {Terminal} from '../components/icons/Terminal';
-import {WindowLayout} from '../components/icons/WindowLayout';
 import {SiteLink} from '../components/SiteLink';
 import {SiteMetaDefaults} from '../constants';
 
 const LandingContainer = styled(Box)`
   margin-right: auto;
   margin-left: auto;
-`;
-
-const IconCircle = styled(Box)`
-  justify-content: center;
-  align-items: center;
-  width: ${themeGet('space.space160')};
-  height: ${themeGet('space.space160')};
 `;
 
 const IndexPage: React.FC<{}> = (): React.ReactElement => {
@@ -37,116 +26,51 @@ const IndexPage: React.FC<{}> = (): React.ReactElement => {
         <link rel="canonical" href="https://paste.twilio.design" />
         <meta name="description" content={SiteMetaDefaults.DESCRIPTION} />
       </Helmet>
-      <LandingContainer maxWidth="size60" marginTop="space200" marginBottom="space200">
+      <LandingContainer marginTop="space140">
         <Heading as="h1" variant="heading10">
           Paste
         </Heading>
-        <P variant="lead">
-          Paste is a design system used to build accessible, consistent, and high quality customer experiences at
-          Twilio. Paste is open source and contributions are welcome.
-        </P>
+        <Box maxWidth="size60">
+          <P variant="lead">
+            Paste is a design system used to build accessible, consistent, and high quality customer experiences at
+            Twilio. Paste is open source and contributions are welcome.
+          </P>
+        </Box>
         <Box marginTop="space80" marginBottom="space80">
-          <Box display="inline-block" marginRight="space40">
-            <Button
-              as="a"
-              href="/getting-started/engineering"
-              size="default"
-              variant="primary"
-              onClick={event => {
-                event.preventDefault();
-                navigate('/getting-started/engineering');
-              }}
-            >
-              Get Started
-            </Button>
-          </Box>
-          <Box display="inline-block" marginRight="space40">
-            <Button
-              as="a"
-              href="/components/"
-              size="default"
-              variant="secondary"
-              onClick={event => {
-                event.preventDefault();
-                navigate('/components/');
-              }}
-            >
-              View Components
-            </Button>
-          </Box>
-          <Button
-            as="a"
-            href="/roadmap/"
-            size="default"
-            variant="secondary"
-            onClick={event => {
-              event.preventDefault();
-              navigate('/roadmap/');
-            }}
-          >
-            Roadmap
-          </Button>
+          <Heading as="h2" variant="heading20">
+            Get Started
+          </Heading>
+          <Stack orientation="vertical" spacing="space30">
+            <SiteLink to="/getting-started/engineering">Guidelines for front-end engineers</SiteLink>
+            <SiteLink to="/roadmap">View our component roadmap</SiteLink>
+            <Anchor href="https://codesandbox.io/s/paste-starter-kit-rj7yy">
+              Check out our components in a Code Sandbox
+            </Anchor>
+          </Stack>
         </Box>
       </LandingContainer>
-      <Separator orientation="horizontal" />
-      <LandingContainer marginTop="space200" marginBottom="space200" maxWidth="size100">
+      <LandingContainer marginTop="space160" marginBottom="space200">
         <Grid columnWidth={220} gap={30}>
-          <Box>
-            <IconCircle
-              display="flex"
-              marginBottom="space50"
-              backgroundColor="colorBackground"
-              borderRadius="borderRadiusCircle"
-            >
-              <Brush size={24} />
-            </IconCircle>
+          <Card padding="space80">
             <Heading as="h2" variant="heading20">
-              Themes
+              <SiteLink to="/components">Components</SiteLink>
+            </Heading>
+            <Paragraph>Use Paste components in Sketch or React to compose your user interfaces.</Paragraph>
+          </Card>
+          <Card padding="space80">
+            <Heading as="h2" variant="heading20">
+              <SiteLink to="/content">Content</SiteLink>
             </Heading>
             <Paragraph>
-              Each component shares a common set of <SiteLink to="/tokens">design tokens</SiteLink> that allow us to
-              easily change their value based on a theme.
+              Read our content guidelines to write authentic ands inclusive content for Twilio&rsquo;s products.
             </Paragraph>
-          </Box>
-          <Box>
-            <IconCircle
-              display="flex"
-              marginBottom="space50"
-              backgroundColor="colorBackground"
-              borderRadius="borderRadiusCircle"
-            >
-              <WindowLayout size={24} />
-            </IconCircle>
+          </Card>
+          <Card padding="space80">
             <Heading as="h2" variant="heading20">
-              Usage Examples
+              <SiteLink to="/layout">Layout</SiteLink>
             </Heading>
-            <Paragraph>
-              Learn how to use Paste components and the underlying tokens through{' '}
-              <SiteLink to="/getting-started">usage examples and guidelines</SiteLink>.
-            </Paragraph>
-          </Box>
-          <Box>
-            <IconCircle
-              display="flex"
-              marginBottom="space50"
-              backgroundColor="colorBackground"
-              borderRadius="borderRadiusCircle"
-            >
-              <Terminal size={24} />
-            </IconCircle>
-            <Heading as="h2" variant="heading20">
-              Code Sandbox
-            </Heading>
-            <Paragraph>
-              We&rsquo;ve loaded a Code Sandbox with the latest components from Paste Core for you to get a feel for
-              working with Paste.
-            </Paragraph>
-            <Paragraph>
-              <Button as="a" href="https://codesandbox.io/s/paste-starter-kit-rj7yy" variant="secondary">
-                Code Sandbox
-              </Button>
-            </Paragraph>
-          </Box>
+            <Paragraph>Use layout components to space, align, and position your UI in a variety of ways.</Paragraph>
+          </Card>
         </Grid>
       </LandingContainer>
     </SiteWrapper>
