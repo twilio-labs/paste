70af843a228b6f358fa40b8fd1ee9f009a084e64
sisber <sisber@twilio.com>
2020-06-01 10:57:39 -0500

    feat(animation-library): use v9 and add useReducedMotion hook

--- packages/paste-libraries/animation/src/index.tsx
+++ packages/paste-libraries/animation/src/index.tsx
@@ -1,1 +1,34 @@
-export * from 'react-spring';
+import {
+  animated,
+  AnimatedProps,
+  useSpring,
+  UseSpringProps,
+  useTransition,
+  UseTransitionProps,
+  useTrail,
+  UseTrailProps,
+  useSprings,
+  UseSpringsProps,
+  useChain,
+  interpolate,
+} from '@react-spring/web';
+import {Globals} from '@react-spring/shared';
+import {useReducedMotion, isRenderingOnServer} from './useReducedMotion';
+
+export {
+  Globals,
+  animated,
+  AnimatedProps,
+  useSpring,
+  UseSpringProps,
+  useTransition,
+  UseTransitionProps,
+  useTrail,
+  UseTrailProps,
+  useSprings,
+  UseSpringsProps,
+  useChain,
+  interpolate,
+  useReducedMotion,
+  isRenderingOnServer,
+};
