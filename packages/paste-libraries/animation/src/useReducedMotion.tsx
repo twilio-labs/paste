65531c6008e5a8aa8e5ed79423237be583c9901f
sisber <sisber@twilio.com>
2020-06-01 10:57:39 -0500

    feat(animation-library): use v9 and add useReducedMotion hook

---
+++ packages/paste-libraries/animation/src/useReducedMotion.tsx
@@ -0,0 +1,51 @@
+// Credits:
+// https://github.com/infiniteluke/react-reduce-motion
+// https://joshwcomeau.com/react/prefers-reduced-motion/#the-hook
+import * as React from 'react';
+
+const REDUCED_MOTION_QUERY = '(prefers-reduced-motion: reduce)';
+
+// Due to a bug in how terser is compiling these functions,
+// we're getting into situations where SSR is checking for window.matchMedia.
+// Wrapping the second return in an else fixes how terser compiles the code.
+/* eslint-disable no-else-return */
+export const isRenderingOnServer = (() => {
+  if (typeof window == 'undefined' || !window.location || !window.location.href) {
+    return true;
+  } else {
+    // Disable animations during VRT
+    return Boolean(new URL(window.location.href).searchParams.get('eyes-storybook'));
+  }
+})();
+
+const getMediaQueryList = (): {matches: boolean; addListener: Function; removeListener: Function} => {
+  if (isRenderingOnServer) {
+    return {
+      matches: true, // When SSR, true === disable animations
+      addListener: () => {},
+      removeListener: () => {},
+    };
+  } else {
+    return window.matchMedia(REDUCED_MOTION_QUERY);
+  }
+};
+/* eslint-enable no-else-return */
+
+export function useReducedMotion(): boolean {
+  const [prefersReducedMotion, setPrefersReducedMotion] = React.useState(getMediaQueryList().matches);
+
+  React.useEffect((): (() => void) => {
+    const mediaQueryList = getMediaQueryList();
+
+    const handleChange = (): void => {
+      setPrefersReducedMotion(mediaQueryList.matches);
+    };
+
+    mediaQueryList.addListener(handleChange);
+    return () => {
+      mediaQueryList.removeListener(handleChange);
+    };
+  }, []);
+
+  return prefersReducedMotion;
+}
