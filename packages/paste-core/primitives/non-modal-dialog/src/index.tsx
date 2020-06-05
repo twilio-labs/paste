3252a5774e8c5a0696ec92341715f20c54d3b34c
richbachman <rb@richbachman.com>
2020-06-05 11:53:18 -0600

    feat(non-modal-dialog-primitive): add non-modal primitive package

---
+++ packages/paste-core/primitives/non-modal-dialog/src/index.tsx
@@ -0,0 +1,19 @@
+import {
+  usePopoverState as useNonModalDialogPrimitiveState,
+  Popover as NonModalDialogPrimitive,
+  PopoverProps as NonModalDialogPrimitiveProps,
+  PopoverDisclosure as NonModalDialogDisclosurePrimitive,
+  PopoverDisclosureProps as NonModalDialogDisclosurePrimitiveProps,
+  PopoverArrow as NonModalDialogArrowPrimitive,
+  PopoverArrowProps as NonModalDialogArrowPrimitiveProps,
+} from '@twilio-paste/reakit-library';
+
+export {
+  useNonModalDialogPrimitiveState,
+  NonModalDialogPrimitive,
+  NonModalDialogPrimitiveProps,
+  NonModalDialogDisclosurePrimitive,
+  NonModalDialogDisclosurePrimitiveProps,
+  NonModalDialogArrowPrimitive,
+  NonModalDialogArrowPrimitiveProps,
+};
