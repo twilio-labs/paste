89674a08a695a4b2ce030fc39ca6e04785342a85
sisber <sisber@twilio.com>
2020-06-06 13:42:53 -0500

    feat(core): adding Tabs component

--- packages/paste-core/core-bundle/src/index.tsx
+++ packages/paste-core/core-bundle/src/index.tsx
@@ -24,6 +24,7 @@ export * from '@twilio-paste/screen-reader-only';
 export * from '@twilio-paste/separator';
 export * from '@twilio-paste/spinner';
 export * from '@twilio-paste/stack';
+export * from '@twilio-paste/tabs';
 export * from '@twilio-paste/tabs-primitive';
 export * from '@twilio-paste/text';
 export * from '@twilio-paste/truncate';
