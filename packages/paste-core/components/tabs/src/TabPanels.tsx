06653c76889cba10c87db7ffd1e698d839f4397a
sisber <sisber@twilio.com>
2020-06-10 09:16:00 -0500

    fix(tabs): remove padding props from TabPanels

--- packages/paste-core/components/tabs/src/TabPanels.tsx
+++ packages/paste-core/components/tabs/src/TabPanels.tsx
@@ -1,39 +1,17 @@
 import * as React from 'react';
 import {Box, safelySpreadBoxProps} from '@twilio-paste/box';
-import {isPaddingTokenProp, PaddingProps} from '@twilio-paste/style-props';
 
-export interface TabPanelsProps extends PaddingProps {
+export interface TabPanelsProps {
   children: React.ReactNode;
 }
 
-const TabPanels = React.forwardRef<HTMLDivElement, TabPanelsProps>(
-  ({children, padding, paddingTop, paddingBottom, paddingRight, paddingLeft, ...props}, ref) => {
-    return (
-      <Box
-        {...safelySpreadBoxProps(props)}
-        width="100%"
-        padding={padding}
-        paddingTop={paddingTop}
-        paddingBottom={paddingBottom}
-        paddingRight={paddingRight}
-        paddingLeft={paddingLeft}
-        ref={ref}
-      >
-        {children}
-      </Box>
-    );
-  }
-);
-
-if (process.env.NODE_ENV === 'development') {
-  TabPanels.propTypes = {
-    padding: isPaddingTokenProp,
-    paddingTop: isPaddingTokenProp,
-    paddingRight: isPaddingTokenProp,
-    paddingBottom: isPaddingTokenProp,
-    paddingLeft: isPaddingTokenProp,
-  };
-}
+const TabPanels = React.forwardRef<HTMLDivElement, TabPanelsProps>(({children, ...props}, ref) => {
+  return (
+    <Box {...safelySpreadBoxProps(props)} width="100%" ref={ref}>
+      {children}
+    </Box>
+  );
+});
 
 TabPanels.displayName = 'TabPanels';
 export {TabPanels};
