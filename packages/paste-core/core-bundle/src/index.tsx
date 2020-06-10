8325ac5e19cc009b805c42529bb0d8a8f8d1d6b9
richbachman <rb@richbachman.com>
2020-06-10 17:02:34 -0600

    feat(core): add popover package to core

--- packages/paste-core/core-bundle/src/index.tsx
+++ packages/paste-core/core-bundle/src/index.tsx
@@ -20,6 +20,7 @@ export * from '@twilio-paste/modal';
 export * from '@twilio-paste/modal-dialog-primitive';
 export * from '@twilio-paste/non-modal-dialog-primitive';
 export * from '@twilio-paste/paragraph';
+export * from '@twilio-paste/popover';
 export * from '@twilio-paste/screen-reader-only';
 export * from '@twilio-paste/separator';
 export * from '@twilio-paste/spinner';
