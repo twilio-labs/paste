1ce6b0bc98978942c65a239c13cdc12d9ec1e328
sisber <sisber@twilio.com>
2020-06-11 07:36:30 -0500

    docs(website): adding tabs docs

---
+++ packages/paste-website/src/component-examples/TabsExamples.ts
@@ -0,0 +1,49 @@
+export const HorizontalTabsExample = `
+const HorizontalTabsExample = () => {
+  const selectedId = useUID();
+  return (
+    <Tabs selectedId={selectedId} baseId="horizontal-tabs-example">
+      <TabList aria-label="My tabs">
+        <Tab>Tab 1</Tab>
+        <Tab disabled>Tab 2</Tab>
+        <Tab id={selectedId}>Tab 3</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>Tab 1</TabPanel>
+        <TabPanel>Tab 2</TabPanel>
+        <TabPanel>Tab 3</TabPanel>
+      </TabPanels>
+    </Tabs>
+  );
+};
+
+
+render(
+  <HorizontalTabsExample />
+)
+`.trim();
+
+export const VerticalTabsExample = `
+const VerticalTabsExample = () => {
+  const selectedId = useUID();
+  return (
+    <Tabs orientation="vertical" selectedId={selectedId} baseId="vertical-tabs-example">
+      <TabList aria-label="My tabs">
+        <Tab id={selectedId}>Tab 1 is a long tab name because the server sent a long tab name</Tab>
+        <Tab>Tab 2</Tab>
+        <Tab>Tab 3</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>Tab 1</TabPanel>
+        <TabPanel>Tab 2</TabPanel>
+        <TabPanel>Tab 3</TabPanel>
+      </TabPanels>
+    </Tabs>
+  );
+};
+
+
+render(
+  <VerticalTabsExample />
+)
+`.trim();
