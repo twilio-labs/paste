49e4787f1efcd16f4c74a2263ddcc29e8015ee73
richbachman <rb@richbachman.com>
2020-06-18 16:30:35 -0600

    feat(tooltip-primitive): add tooltip primitive package

---
+++ packages/paste-core/primitives/tooltip/src/index.tsx
@@ -0,0 +1,12 @@
+export {
+  useTooltipState as useTooltipPrimitiveState,
+  TooltipState as TooltipPrimitiveState,
+  TooltipInitialState as TooltipPrimitiveInitialState,
+  TooltipStateReturn as TooltipPrimitiveStateReturn,
+  Tooltip as TooltipPrimitive,
+  TooltipProps as TooltipPrimitiveProps,
+  TooltipReference as TooltipPrimitiveReference,
+  TooltipReferenceProps as TooltipPrimitiveReferenceProps,
+  TooltipArrow as TooltipPrimitiveArrow,
+  TooltipArrowProps as TooltipPrimitiveArrowProps,
+} from '@twilio-paste/reakit-library';
