d3b2057b47fcae4eac2f1a51b8d614c7d46bcf65
Si Taggart <me@simontaggart.com>
2020-06-26 14:54:27 -0700

    feat(disclosure-primitive): export state props

--- packages/paste-core/primitives/disclosure/src/index.tsx
+++ packages/paste-core/primitives/disclosure/src/index.tsx
@@ -2,5 +2,10 @@
 export {
   useDisclosureState as useDisclosurePrimitiveState,
   Disclosure as DisclosurePrimitive,
+  DisclosureProps as DisclosurePrimitiveProps,
   DisclosureContent as DisclosurePrimitiveContent,
+  DisclosureContentProps as DisclosurePrimitiveContentProps,
+  DisclosureState as DisclosurePrimitiveState,
+  DisclosureInitialState as DisclosurePrimitiveInitialState,
+  DisclosureStateReturn as DisclosurePrimitveStateReturn,
 } from '@twilio-paste/reakit-library';
