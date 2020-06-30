5fb050b89dd37b7f66caa32304412c6819cb254e
Si Taggart <me@simontaggart.com>
2020-06-29 17:42:44 -0700

    feat(avatar): alpha component build

---
+++ packages/paste-core/components/avatar/src/index.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
+import {Text} from '@twilio-paste/text';
+import {Box, safelySpreadBoxProps} from '@twilio-paste/box';
+import {IconSize, isIconSizeTokenProp} from '@twilio-paste/style-props';
+import {getComputedTokenNames, getInitialsFromName} from './utils';
+
+export interface AvatarProps extends React.HTMLAttributes<'div'> {
+  name: string;
+  size: IconSize;
+  src?: string;
+}
+const Avatar: React.FC<AvatarProps> = ({name, children, size = 'sizeIcon70', src, ...props}) => {
+  const computedTokenNames = getComputedTokenNames(size);
+  if (src != null && name === undefined) {
+    console.error('Paste Avatar: You must provide a name if you are displaying an image');
+  }
+  return (
+    <Box
+      {...safelySpreadBoxProps(props)}
+      as="div"
+      backgroundColor="colorBackgroundPrimaryLight"
+      borderRadius="borderRadiusCircle"
+      overflow="hidden"
+      size={size}
+    >
+      {src != null ? (
+        <Box {...safelySpreadBoxProps(props)} as="img" alt={name} maxWidth="100%" src={src} size={size} title={name} />
+      ) : (
+        <Text
+          as="abbr"
+          display="block"
+          fontSize={computedTokenNames.fontSize}
+          fontWeight="fontWeightSemibold"
+          lineHeight={computedTokenNames.lineHeight}
+          textAlign="center"
+          textDecoration="none"
+          title={name}
+        >
+          {getInitialsFromName(name)}
+        </Text>
+      )}
+    </Box>
+  );
+};
+
+Avatar.displayName = 'Avatar';
+Avatar.propTypes = {
+  size: isIconSizeTokenProp,
+  src: PropTypes.string,
+  name: PropTypes.string.isRequired,
+};
+
+export {Avatar};
