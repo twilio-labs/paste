56223feb08f6e687a97a2d7013d51f266228a15c
richbachman <rb@richbachman.com>
2020-06-29 21:43:09 -0600

    fix(website): restructure feedback callout into menu

--- packages/paste-website/src/components/shortcodes/component-header/index.tsx
+++ packages/paste-website/src/components/shortcodes/component-header/index.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import {Anchor} from '@twilio-paste/anchor';
 import {Box} from '@twilio-paste/box';
-import {Text} from '@twilio-paste/text';
+import {ChevronDownIcon} from '@twilio-paste/icons/esm/ChevronDownIcon';
 import {Heading} from '@twilio-paste/heading';
+import {Text} from '@twilio-paste/text';
+import {Menu, MenuButton, MenuItem, useMenuState} from '@twilio-paste/menu';
 import {Breadcrumb, BreadcrumbItem} from '../../breadcrumb';
 import {SidebarCategoryRoutes} from '../../../constants';
 import {P, InlineCode} from '../../Typography';
@@ -34,9 +36,11 @@ const ComponentHeaderBasic: React.FC<{
       <BreadcrumbItem to="/">Home</BreadcrumbItem>
       <BreadcrumbItem to={categoryRoute}>{getCategoryNameFromRoute(categoryRoute)}</BreadcrumbItem>
     </Breadcrumb>
-    <Heading as="h1" variant="heading10">
-      {getHumanizedNameFromPackageName(name)}
-    </Heading>
+    <Box display="flex" alignItems="center" marginBottom="space70">
+      <Heading as="h1" variant="heading10" marginBottom="space0">
+        {getHumanizedNameFromPackageName(name)}
+      </Heading>
+    </Box>
   </>
 );
 
@@ -59,6 +63,13 @@ interface ComponentHeaderProps {
       };
     }
   ];
+  mdxData: {
+    fileAbsolutePath: string;
+    frontmatter: {
+      slug: string;
+      title: string;
+    };
+  };
 }
 
 const PackageValue: React.FC<{}> = ({children}) => {
@@ -88,11 +99,24 @@ const ComponentHeader: React.FC<ComponentHeaderProps> = ({
   storybookUrl,
   abstractUrl,
   data,
+  mdxData,
 }) => {
   if (data == null || data[0] == null || data[0].node == null) {
     return <ComponentHeaderBasic categoryRoute={categoryRoute} name={name} />;
   }
   const {description, status, name: packageName, version} = data[0].node;
+
+  const menu = useMenuState();
+  const handleClick = (): void => menu.hide();
+
+  const gitHubPagesPath = 'https://github.com/twilio-labs/paste/blob/master/packages/paste-website/src/pages';
+  const absolutePath = mdxData.fileAbsolutePath;
+  const filename = absolutePath.substring(absolutePath.lastIndexOf('/') + 1);
+  const gitHubURL = `${gitHubPagesPath + mdxData.frontmatter.slug}/${filename}`;
+  const gitHubIssueUrl = `https://github.com/twilio-labs/paste/issues/new?assignees=&labels=Type%3A+Bug&template=bug_report.md&title=${mdxData.frontmatter.title.replace(
+    /\s+/g,
+    '%20'
+  )}`;
 
   return (
     <>
@@ -121,12 +145,28 @@ const ComponentHeader: React.FC<ComponentHeaderProps> = ({
             {abstractUrl != null ? <Anchor href={abstractUrl}>Abstract</Anchor> : null}
           </PackageValue>
         </Box>
-        <Box marginBottom="space20">
+        <Box marginBottom="space60">
           <PackageLabel>Install</PackageLabel>
           <PackageValue>
             <PackageInstallSnippet>yarn add {packageName}</PackageInstallSnippet> &mdash; or &mdash;{' '}
             <PackageInstallSnippet>yarn add @twilio-paste/core</PackageInstallSnippet>
           </PackageValue>
+        </Box>
+        <Box marginBottom="space20">
+          <MenuButton {...menu} variant="link">
+            Feedback <ChevronDownIcon decorative />
+          </MenuButton>
+          <Menu {...menu} aria-label="Feedback">
+            <MenuItem {...menu} href={gitHubURL} onClick={handleClick}>
+              Edit this page on GitHub
+            </MenuItem>
+            <MenuItem {...menu} href="https://github.com/twilio-labs/paste/discussions/new" onClick={handleClick}>
+              Have a question? Submit a Github Discussion
+            </MenuItem>
+            <MenuItem {...menu} href={gitHubIssueUrl} onClick={handleClick}>
+              Find a bug? Submit a Github Issue
+            </MenuItem>
+          </Menu>
         </Box>
       </Box>
     </>
--- packages/paste-website/src/layouts/DefaultLayout.tsx
+++ packages/paste-website/src/layouts/DefaultLayout.tsx
@@ -1,12 +1,8 @@
 import * as React from 'react';
 import {Helmet} from 'react-helmet';
-import {Anchor} from '@twilio-paste/anchor';
-import {Box} from '@twilio-paste/box';
-import {Stack} from '@twilio-paste/stack';
 import {SiteWrapper} from '../components/site-wrapper';
 import {PasteMDXProvider} from '../components/paste-mdx-provider';
 import {SiteMetaDefaults} from '../constants';
-import {Callout, CalloutTitle} from '../components/callout';
 
 interface DefaultLayoutProps {
   children?: React.ReactElement;
@@ -16,29 +12,17 @@ interface DefaultLayoutProps {
   };
   pageContext: {
     frontmatter: {
-      title: string;
-      description: string;
+      title?: string;
+      description?: string;
     };
   };
 }
 
-const DefaultLayout: React.FC<DefaultLayoutProps> = ({children, data, pageContext, location}) => {
+const DefaultLayout: React.FC<DefaultLayoutProps> = ({children, pageContext, location}) => {
   const pageTitle = pageContext.frontmatter.title
     ? `${pageContext.frontmatter.title} - ${SiteMetaDefaults.TITLE}`
     : SiteMetaDefaults.TITLE;
   const pageDescription = pageContext.frontmatter.description || SiteMetaDefaults.DESCRIPTION;
-
-  const gitHubPagesPath = 'https://github.com/twilio-labs/paste/tree/master/packages/paste-website/src/pages';
-  // @ts-ignore
-  const absolutePath = data.mdx.fileAbsolutePath;
-  const filename = absolutePath.substring(absolutePath.lastIndexOf('/') + 1);
-  const gitHubURL = `${gitHubPagesPath + location.pathname}/${filename}`;
-
-  const gitHubIssueUrl = `https://github.com/twilio-labs/paste/issues/new?assignees=&labels=Type%3A+Bug&template=bug_report.md&title=${pageContext.frontmatter.title.replace(
-    /\s+/g,
-    '%20'
-  )}`;
-
   return (
     <SiteWrapper>
       <Helmet>
@@ -48,18 +32,6 @@ const DefaultLayout: React.FC<DefaultLayoutProps> = ({children, data, pageContex
       </Helmet>
 
       <PasteMDXProvider>{children}</PasteMDXProvider>
-      <Box marginTop="space90" maxWidth="size80">
-        <Callout>
-          <CalloutTitle as="h4">See a mistake or have some feedback?</CalloutTitle>
-          <Stack orientation="vertical" spacing="space40">
-            <Anchor href={gitHubURL}>Edit this page on GitHub</Anchor>
-            <Anchor href="https://github.com/twilio-labs/paste/discussions/new">
-              Have a question? Submit a Github Discussion
-            </Anchor>
-            <Anchor href={gitHubIssueUrl}>Find a bug? Submit a Github Issue</Anchor>
-          </Stack>
-        </Callout>
-      </Box>
     </SiteWrapper>
   );
 };
