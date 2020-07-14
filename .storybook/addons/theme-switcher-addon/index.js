c0cc91716675a2f35c7b5836b4a9103ce3db9a31
Shadi <TheSisb@users.noreply.github.com>
2020-07-14 10:14:29 -0500

    chore: add inter font (#572)

--- .storybook/addons/theme-switcher-addon/index.js
+++ .storybook/addons/theme-switcher-addon/index.js
@@ -1,33 +1,10 @@
 /* This file contains the code for the addon's decorator that wraps each story */
 import * as React from 'react';
-import {css, StylingGlobals} from '@twilio-paste/styling-library';
 import {Theme, ThemeVariants} from '@twilio-paste/theme';
 import {Box} from '@twilio-paste/box';
 import addonsAPI, {makeDecorator} from '@storybook/addons';
 import {Events, THEME_STORAGE_KEY} from './constants';
-
-const globalStyles = css`
-  @font-face {
-    font-family: Colfax;
-    font-style: normal;
-    font-weight: 300;
-    src: url(/fonts/colfax-light.woff2) format('woff2'), url(/fonts/colfax-light.woff) format('woff');
-  }
-
-  @font-face {
-    font-family: Colfax;
-    font-style: normal;
-    font-weight: 400;
-    src: url(/fonts/colfax-regular.woff2) format('woff2'), url(/fonts/colfax-regular.woff) format('woff');
-  }
-
-  @font-face {
-    font-family: Colfax;
-    font-style: normal;
-    font-weight: 600;
-    src: url(/fonts/colfax-medium.woff2) format('woff2'), url(/fonts/colfax-medium.woff) format('woff');
-  }
-`;
+import './fonts.css';
 
 class ThemeWrapper extends React.Component {
   state = {
@@ -53,7 +30,6 @@ class ThemeWrapper extends React.Component {
   render() {
     return (
       <Theme.Provider theme={this.state.theme}>
-        <StylingGlobals styles={globalStyles} />
         <Box padding="space40">{this.props.children}</Box>
       </Theme.Provider>
     );
