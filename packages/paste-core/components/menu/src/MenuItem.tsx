4429499bc8ee1d158c9bf8f320f61b6c7cca58f9
Si Taggart <me@simontaggart.com>
2020-08-06 16:02:17 -0700

    feat(menu): update styles for unified design language

---
+++ packages/paste-core/components/menu/src/MenuItem.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import {secureExternalLink} from '@twilio-paste/anchor';
+import {Box, safelySpreadBoxProps} from '@twilio-paste/box';
+import {MenuPrimitiveItem} from '@twilio-paste/menu-primitive';
+import {MenuItemProps} from './types';
+import {MenuItemVariants} from './constants';
+import {MenuGroupContext} from './MenuGroup';
+
+export const StyledMenuItem = React.forwardRef<HTMLDivElement | HTMLAnchorElement, MenuItemProps>((props, ref) => {
+  return (
+    <Box
+      {...(props.href && secureExternalLink(props.href))}
+      {...safelySpreadBoxProps(props)}
+      as="a"
+      color={props['aria-disabled'] ? 'colorTextWeaker' : 'colorText'}
+      display="block"
+      paddingY="space30"
+      paddingX={props.variant === MenuItemVariants.GROUP_ITEM ? 'space90' : 'space70'}
+      textDecoration={props.tabIndex === 0 ? 'underline' : 'none'}
+      _hover={{
+        cursor: 'pointer',
+      }}
+      _focus={{
+        color: 'colorTextLink',
+        outline: 'none',
+      }}
+      _disabled={{cursor: 'not-allowed'}}
+      ref={ref}
+    >
+      {props.children}
+    </Box>
+  );
+});
+
+const MenuItem = React.forwardRef<HTMLDivElement, MenuItemProps>(
+  ({as = StyledMenuItem, variant: _variant, ...props}, ref) => {
+    const variant = _variant || React.useContext(MenuGroupContext);
+    return <MenuPrimitiveItem {...props} variant={variant} as={as} ref={ref} />;
+  }
+);
+MenuItem.displayName = 'MenuItem';
+export {MenuItem};
