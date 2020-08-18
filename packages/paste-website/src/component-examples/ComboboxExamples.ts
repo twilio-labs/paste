84dd1a4aea0410f4e2de844538f60add6a3931b1
Si Taggart <me@simontaggart.com>
2020-08-17 19:07:57 -0700

    docs(combobox): document using the state hook

--- packages/paste-website/src/component-examples/ComboboxExamples.ts
+++ packages/paste-website/src/component-examples/ComboboxExamples.ts
@@ -356,3 +356,79 @@ render(
   <InverseErrorCombobox />
 )
 `.trim();
+
+export const stateHookCombobox = `
+const objectItems = [
+  {code: 'AD', label: 'Andorra', phone: '376'},
+  {code: 'AE', label: 'United Arab Emirates', phone: '971'},
+  {code: 'AF', label: 'Afghanistan', phone: '93'},
+  {code: 'AG', label: 'Antigua and Barbuda', phone: '1-268'},
+  {code: 'AI', label: 'Anguilla', phone: '1-264'},
+  {code: 'AL', label: 'Albania', phone: '355'},
+  {code: 'AM', label: 'Armenia', phone: '374'},
+  {code: 'AO', label: 'Angola', phone: '244'},
+  {code: 'AQ', label: 'Antarctica', phone: '672'},
+  {code: 'AR', label: 'Argentina', phone: '54'},
+  {code: 'AS', label: 'American Samoa', phone: '1-684'},
+  {code: 'AT', label: 'Austria', phone: '43'},
+];
+
+const ComboboxUsingStatehook = () => {
+  const [value, setValue] = React.useState('');
+  const [selectedItem, setSelectedItem] = React.useState({});
+  const [inputItems, setInputItems] = React.useState(objectItems);
+
+  const {reset, ...state} = useCombobox({
+    items: inputItems,
+    itemToString: item => (item ? item.label : null),
+    onSelectedItemChange: changes => {
+      setSelectedItem(changes.selectedItem);
+    },
+    onInputValueChange: ({inputValue}) => {
+      if (inputValue !== undefined) {
+        setInputItems(
+          _.filter(objectItems, item => item.label.toLowerCase().startsWith(inputValue.toLowerCase()))
+        );
+        setValue(inputValue);
+      }
+    },
+    inputValue: value,
+  });
+
+  return (
+    <>
+      <Combobox
+        state={state}
+        items={inputItems}
+        autocomplete
+        labelText="Choose a country:"
+        helpText="This is the help text"
+        optionTemplate={item => (
+          <div>
+            {item.code} | {item.label} | {item.phone}
+          </div>
+        )}
+        insertAfter={
+          <Button
+            variant="link"
+            size="reset"
+            onClick={() => {
+              reset();
+            }}
+          >
+            <CloseIcon decorative={false} title="Clear" />
+          </Button>
+        }
+      />
+      <Box paddingTop="space70">
+        Input value state: {JSON.stringify(value)}
+        <br />
+        Selected item state: {JSON.stringify(selectedItem)}
+      </Box>
+    </>
+  );
+}
+render(
+  <ComboboxUsingStatehook />
+)
+`.trim();
