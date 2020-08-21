c98b271bb18374fefa9d0b01481516f3ff0df9d4
thesisb <shadiisber@gmail.com>
2020-08-20 21:13:25 -0500

    chore: update applitools eyes

--- applitools.config.js
+++ applitools.config.js
@@ -1,5 +1,12 @@
+// https://github.com/applitools/eyes.sdk.javascript1/tree/master/packages/eyes-storybook#advanced-configuration
 module.exports = {
   serverUrl: 'https://twilioeyesapi.applitools.com',
   batchId: process.env.GITHUB_HEAD_SHA,
   exitcode: 0,
+  concurrency: 90,
+  browser: [
+    {width: 1024, height: 768, name: 'firefox'},
+    {width: 1024, height: 768, name: 'chrome'},
+    {width: 1024, height: 768, name: 'ie11'},
+  ],
 };
