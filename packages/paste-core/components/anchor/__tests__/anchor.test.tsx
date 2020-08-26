fb2623d8e7ccc188ac99f9ff812687a235b45dca
richbachman <rb@richbachman.com>
2020-08-26 17:10:53 -0600

    chore(anchor): refactor to use Text

--- packages/paste-core/components/anchor/__tests__/anchor.test.tsx
+++ packages/paste-core/components/anchor/__tests__/anchor.test.tsx
@@ -1,59 +1,48 @@
 import * as React from 'react';
-import renderer from 'react-test-renderer';
-import {render} from 'react-dom';
+import {render, screen} from '@testing-library/react';
 import {axe} from 'jest-axe';
-import {Theme} from '@twilio-paste/theme';
 import {Anchor} from '../src';
 
 describe('Anchor', () => {
-  it('it should render an anchor', (): void => {
-    const tree = renderer
-      .create(
-        <Theme.Provider theme="console">
-          <Anchor href="/">This is an anchor</Anchor>
-        </Theme.Provider>
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
+  it('it should render an anchor', () => {
+    const {asFragment} = render(<Anchor href="/">This is an anchor</Anchor>);
+    expect(asFragment()).toMatchSnapshot();
   });
-  it('it should render an external anchor', (): void => {
-    const tree = renderer
-      .create(
-        <Theme.Provider theme="console">
+
+  it('it should render an external anchor', () => {
+    render(
+      <Anchor href="https://twilio.com">
+        This is an anchor that links to Twilio.com with an external target and rel
+      </Anchor>
+    );
+    const renderedAnchor = screen.getByRole('link');
+    expect(renderedAnchor.getAttribute('target')).toEqual('_blank');
+    expect(renderedAnchor.getAttribute('rel')).toEqual('noreferrer noopener');
+  });
+
+  it('it should render an external anchor with overwritten target and rel', (): void => {
+    render(
+      <Anchor href="https://twilio.com" target="_self" rel="noopener">
+        This is an anchor that links to Twilio.com with the target and rel overwritten
+      </Anchor>
+    );
+    const renderedAnchor = screen.getByRole('link');
+    expect(renderedAnchor.getAttribute('target')).toEqual('_self');
+    expect(renderedAnchor.getAttribute('rel')).toEqual('noopener');
+  });
+
+  describe('Accessibility', () => {
+    it('Should have no accessibility violations', async () => {
+      const {container} = render(
+        <>
+          <Anchor href="/">This is an anchor</Anchor>
           <Anchor href="https://twilio.com">
             This is an anchor that links to Twilio.com with an external target and rel
           </Anchor>
-        </Theme.Provider>
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-  it('it should render an external anchor with overwritten target and rel', (): void => {
-    const tree = renderer
-      .create(
-        <Theme.Provider theme="console">
-          <Anchor href="https://twilio.com" target="_self" rel="noopener">
-            This is an anchor that links to Twilio.com with the target and rel overwritten
-          </Anchor>
-        </Theme.Provider>
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('should have no accessibility violations', async () => {
-    const container = document.createElement('div');
-    document.body.append(container);
-    render(
-      <Theme.Provider theme="console">
-        <Anchor href="/">This is an anchor</Anchor>
-        <Anchor href="https://twilio.com">
-          This is an anchor that links to Twilio.com with an external target and rel
-        </Anchor>
-      </Theme.Provider>,
-      container
-    );
-    const results = await axe(document.body);
-    expect(results).toHaveNoViolations();
+        </>
+      );
+      const results = await axe(container);
+      expect(results).toHaveNoViolations();
+    });
   });
 });
