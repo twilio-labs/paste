381269f8bde4e6a06d43d7e0c2f9a4203d88ef86
richbachman <rb@richbachman.com>
2020-08-26 17:10:53 -0600

    chore(anchor): refactor to use Text

--- packages/paste-core/components/anchor/src/index.tsx
+++ packages/paste-core/components/anchor/src/index.tsx
@@ -1,19 +1,16 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
-import {styled, css, compose, layout, space} from '@twilio-paste/styling-library';
-import {LayoutProps, SpaceProps} from '@twilio-paste/style-props';
+import {Text, safelySpreadTextProps} from '@twilio-paste/text';
 
 export type AnchorTargets = '_self' | '_blank' | '_parent' | '_top';
 export type AnchorTabIndexes = 0 | -1;
 
-export interface AnchorProps extends React.AnchorHTMLAttributes<HTMLAnchorElement>, LayoutProps, SpaceProps {
+export interface AnchorProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
   children: NonNullable<React.ReactNode>;
   href: string;
   rel?: string;
   tabIndex?: AnchorTabIndexes;
   target?: AnchorTargets;
-  id?: never;
-  className?: never;
 }
 
 const EXTERNAL_URL_REGEX = /^(https?:)[^\s]*$/;
@@ -29,35 +26,34 @@ export const secureExternalLink = (href: string): {} | undefined => {
   };
 };
 
-// eslint-disable-next-line emotion/syntax-preference
-const StyledAnchor = styled.a(
-  css({
-    color: 'colorTextLink',
-    textDecoration: 'underline',
-    outline: 'none',
-
-    '&:hover': {
-      color: 'colorTextLinkDarker',
-      textDecoration: 'none',
-    },
-
-    '&:focus, &:active': {
+const Anchor = React.forwardRef<HTMLAnchorElement, AnchorProps>((props, ref) => (
+  <Text
+    as="a"
+    color="colorTextLink"
+    textDecoration="underline"
+    outline="none"
+    ref={ref}
+    _active={{
       boxShadow: 'shadowFocus',
       color: 'colorTextLinkDarker',
       textDecoration: 'none',
-    },
-  }),
-  compose(
-    space,
-    layout
-  )
-);
-
-const Anchor = React.forwardRef<HTMLAnchorElement, AnchorProps>((props, ref) => (
-  <StyledAnchor {...secureExternalLink(props.href)} {...props} ref={ref}>
+    }}
+    _focus={{
+      boxShadow: 'shadowFocus',
+      color: 'colorTextLinkDarker',
+      textDecoration: 'none',
+    }}
+    _hover={{
+      color: 'colorTextLinkDarker',
+      textDecoration: 'none',
+    }}
+    {...secureExternalLink(props.href)}
+    {...safelySpreadTextProps(props)}
+  >
     {props.children}
-  </StyledAnchor>
+  </Text>
 ));
+
 Anchor.displayName = 'Anchor';
 
 if (process.env.NODE_ENV === 'development') {
