6ddb0105cb10841ddf4207504492c4dcd8fb65f8
Si Taggart <me@simontaggart.com>
2020-08-28 17:03:24 -0700

    feat(icons): add a json list export

--- packages/paste-icons/tools/actions/listIconsAction.js
+++ packages/paste-icons/tools/actions/listIconsAction.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const {readdirAsync, removeTsxExtension, maybeHandleError} = require('../utils');
 const {REACT_PATH, BLOCKLIST_FILES} = require('../constants');
-const {storybookListTemplate} = require('../templates/storybookListTemplate');
 const {rollupListTemplate} = require('../templates/rollupListTemplate');
+const {jsonTemplate} = require('../templates/jsonTemplate');
 const {writeToFile} = require('../../../../tools/utils/writeToFile');
 
 async function listIconsAction() {
@@ -14,9 +14,9 @@ async function listIconsAction() {
     maybeHandleError('Error occured while generating icon list!', error);
   }
 
-  // Write the icons list displayed on Storybook
-  writeToFile(path.join(__dirname, './../../stories/__IconList.tsx'), storybookListTemplate(destinationFiles), 'utf8', {
-    errorMessage: `Couldn't update '__IconList' storybook file!`,
+  // Write the icons list to JSON
+  writeToFile(path.join(__dirname, './../../json/icons.json'), jsonTemplate(destinationFiles), 'utf8', {
+    errorMessage: `Couldn't update raw JSON file!`,
   });
 
   // Write the cache file that rollup uses to generate bundles
