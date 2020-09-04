3074bc1cab569ed98128f8e8fb539ea31ab434bf
Si Taggart <me@simontaggart.com>
2020-09-04 14:35:11 -0700

    fix(website): landing page layout

--- packages/paste-website/src/pages/index.tsx
+++ packages/paste-website/src/pages/index.tsx
@@ -1,17 +1,11 @@
 import * as React from 'react';
-import { Helmet } from 'react-helmet';
-import { styled } from '@twilio-paste/styling-library';
-import { SiteWrapper } from '../components/site-wrapper';
-import { Box, Heading, Paragraph, Anchor, Stack, Card, Grid, Column, MediaObject, MediaFigure, MediaBody, UnorderedList, ListItem } from '@twilio-paste/core';
-import { P } from '../components/Typography';
-import { SiteLink } from '../components/SiteLink';
-import { SiteMetaDefaults } from '../constants';
-import { NewIcon } from '@twilio-paste/icons/esm/NewIcon';
-
-const LandingContainer = styled(Box)`
-  margin-right: auto;
-  margin-left: auto;
-`;
+import {Helmet} from 'react-helmet';
+import {Box, Heading, Paragraph, Anchor, Stack, Card, Grid, Column, UnorderedList, ListItem} from '@twilio-paste/core';
+import {NewIcon} from '@twilio-paste/icons/esm/NewIcon';
+import {SiteWrapper} from '../components/site-wrapper';
+import {P} from '../components/Typography';
+import {SiteLink} from '../components/SiteLink';
+import {SiteMetaDefaults} from '../constants';
 
 const IndexPage: React.FC<{}> = (): React.ReactElement => {
   return (
@@ -21,9 +15,9 @@ const IndexPage: React.FC<{}> = (): React.ReactElement => {
         <link rel="canonical" href="https://paste.twilio.design" />
         <meta name="description" content={SiteMetaDefaults.DESCRIPTION} />
       </Helmet>
-      <LandingContainer marginTop="space140">
-        <Grid>
-          <Column span={[12, 12, 4]}>
+      <Box maxWidth="size120">
+        <Grid gutter="space80" marginTop="space140">
+          <Column span={[12, 12, 6, 7, 8]}>
             <Heading as="h1" variant="heading10">
               Paste
             </Heading>
@@ -43,57 +37,68 @@ const IndexPage: React.FC<{}> = (): React.ReactElement => {
                 <SiteLink to="/roadmap">View our component roadmap</SiteLink>
                 <Anchor href="https://codesandbox.io/s/paste-starter-kit-rj7yy">
                   Check out our components in a Code Sandbox
-              </Anchor>
+                </Anchor>
               </Stack>
             </Box>
           </Column>
-          <Column span={[12, 12, 4]} offset={[0, 0, 1]}>
-            <Card>
-              <MediaObject as="div">
-                <MediaFigure as="div" spacing={["space0", "space0", "space60"]}>
-                  <NewIcon decorative={true} size="sizeIcon60" />
-                </MediaFigure>
-                <MediaBody as="div">
-                  <Heading as="h2" variant={"heading30"}>
-                    What's new in Paste?
-                 </Heading>
-                  <UnorderedList>
-                    <ListItem>Find icons easier than ever with our new <SiteLink to="/icons">icon list</SiteLink> page</ListItem>
-                    <ListItem>By popular demand, we've released a <SiteLink to="/components/button#destructive-secondary-button">secondary variant for destructive buttons</SiteLink></ListItem>
-                  </UnorderedList>
-                </MediaBody>
-              </MediaObject>
-            </Card>
-          </Column>
-        </Grid>
-      </LandingContainer>
-
-      <LandingContainer marginTop="space160" marginBottom="space200">
-        <Grid gutter="space30">
-          <Column span={[12, 12, 3]}><Card padding="space80">
-            <Heading as="h2" variant="heading20">
-              <SiteLink to="/components">Components</SiteLink>
-            </Heading>
-            <Paragraph>Use Paste components in Sketch or React to compose your user interfaces.</Paragraph>
-          </Card></Column>
-          <Column span={[12, 12, 3]}>
-            <Card padding="space80">
+          <Column span={[12, 12, 6, 5, 4]}>
+            <Card padding={['space60', 'space60', 'space60', 'space90']}>
               <Heading as="h2" variant="heading20">
-                <SiteLink to="/content">Content</SiteLink>
+                <Stack as="span" orientation="horizontal" spacing="space20">
+                  <NewIcon color="colorTextNew" decorative size="sizeIcon60" />
+                  What&rsquo;s new in Paste?
+                </Stack>
               </Heading>
-              <Paragraph>
-                Read our content guidelines to write authentic and inclusive content for Twilio&rsquo;s products.
-            </Paragraph>
+              <UnorderedList marginBottom="space0">
+                <ListItem>
+                  Find icons easier than ever with our new <SiteLink to="/icons">icon list</SiteLink> page
+                </ListItem>
+                <ListItem>
+                  By popular demand, we&rsquo;ve released a{' '}
+                  <SiteLink to="/components/button#destructive-secondary-button">
+                    secondary variant for destructive buttons
+                  </SiteLink>
+                </ListItem>
+              </UnorderedList>
             </Card>
           </Column>
-          <Column span={[12, 12, 3]}><Card padding="space80">
-            <Heading as="h2" variant="heading20">
-              <SiteLink to="/layout">Layout</SiteLink>
-            </Heading>
-            <Paragraph>Use layout components to space, align, and position your UI in a variety of ways.</Paragraph>
-          </Card></Column>
         </Grid>
-      </LandingContainer>
+
+        <Grid gutter="space30" marginTop="space160" marginBottom="space200">
+          <Column span={[12, 12, 4]}>
+            <Box marginBottom="space40">
+              <Card padding="space80">
+                <Heading as="h2" variant="heading30">
+                  <SiteLink to="/components">Components</SiteLink>
+                </Heading>
+                <Paragraph>Use Paste components in Sketch or React to compose your user interfaces.</Paragraph>
+              </Card>
+            </Box>
+          </Column>
+          <Column span={[12, 12, 4]}>
+            <Box marginBottom="space40">
+              <Card padding="space80">
+                <Heading as="h2" variant="heading20">
+                  <SiteLink to="/content">Content</SiteLink>
+                </Heading>
+                <Paragraph>
+                  Read our content guidelines to write authentic and inclusive content for Twilio&rsquo;s products.
+                </Paragraph>
+              </Card>
+            </Box>
+          </Column>
+          <Column span={[12, 12, 4]}>
+            <Box marginBottom="space40">
+              <Card padding="space80">
+                <Heading as="h2" variant="heading20">
+                  <SiteLink to="/layout">Layout</SiteLink>
+                </Heading>
+                <Paragraph>Use layout components to space, align, and position your UI in a variety of ways.</Paragraph>
+              </Card>
+            </Box>
+          </Column>
+        </Grid>
+      </Box>
     </SiteWrapper>
   );
 };
