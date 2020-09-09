ada45988573e418e7556504fb7557b6ad57dc4b3
Si Taggart <me@simontaggart.com>
2020-09-08 18:22:17 -0700

    feat(website): add illlustration to the homepage

--- packages/paste-website/types/index.d.ts
+++ packages/paste-website/types/index.d.ts
@@ -1,3 +1,8 @@
 declare module '@mdx-js/react';
 declare module 'gatsby-mdx';
 declare module 'prism-react-renderer';
+
+declare module '*.svg' {
+  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  export default content;
+}
