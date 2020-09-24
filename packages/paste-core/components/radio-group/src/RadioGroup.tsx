6e19dcebc7d0693bdc99daed772e6923048082e3
Si Taggart <me@simontaggart.com>
2020-09-24 10:29:15 -0700

    feat(radio-group): create the package

---
+++ packages/paste-core/components/radio-group/src/RadioGroup.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
+import {InlineControlGroup, InlineControlGroupProps} from '@twilio-paste/inline-control-group';
+import {RadioContext} from './RadioContext';
+
+export interface RadioGroupProps extends InlineControlGroupProps {
+  name: string;
+  onChange: (value: string) => void;
+}
+
+const RadioGroup: React.FC<RadioGroupProps> = ({
+  name,
+  value,
+  onChange,
+  disabled = false,
+  errorText,
+  children,
+  ...props
+}) => {
+  const onChangeHandler = React.useMemo(() => {
+    return (event: React.ChangeEvent<HTMLInputElement>): void => {
+      if (onChange != null) {
+        onChange(event.target.value);
+      }
+    };
+  }, [onChange]);
+
+  const contextValue = React.useMemo(() => {
+    return {
+      name,
+      value: value || '',
+      disabled,
+      hasError: errorText != null,
+      onChange: onChangeHandler,
+    };
+  }, [name, value, disabled, errorText, onChangeHandler]);
+
+  return (
+    <RadioContext.Provider value={contextValue}>
+      <InlineControlGroup {...props} disabled={disabled} errorText={errorText} name={name}>
+        {children}
+      </InlineControlGroup>
+    </RadioContext.Provider>
+  );
+};
+
+RadioGroup.displayName = 'RadioGroup';
+
+if (process.env.NODE_ENV === 'development') {
+  RadioGroup.propTypes = {
+    name: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+  };
+}
+
+export {RadioGroup};
