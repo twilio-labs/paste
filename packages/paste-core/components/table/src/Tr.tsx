9322c8cd20657e56f4958db1943499d708490288
richbachman <rb@richbachman.com>
2020-09-25 15:22:02 -0600

    feat(table): add component package

---
+++ packages/paste-core/components/table/src/Tr.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import {Box, safelySpreadBoxProps} from '@twilio-paste/box';
+import {TrProps, TrPropTypes} from './types';
+
+const Tr = React.forwardRef<HTMLTableRowElement, TrProps>(({verticalAlign = 'middle', ...props}, ref) => {
+  return (
+    <Box
+      {...safelySpreadBoxProps(props)}
+      ref={ref}
+      as="tr"
+      borderStyle="solid"
+      borderColor="colorBorderLighter"
+      borderWidth="borderWidth0"
+      borderBottomWidth="borderWidth10"
+      verticalAlign={verticalAlign}
+      _even={{backgroundColor: 'colorBackgroundRowStriped'}}
+    />
+  );
+});
+
+Tr.displayName = 'Tr';
+
+if (process.env.NODE_ENV === 'development') {
+  Tr.propTypes = TrPropTypes;
+}
+
+export {Tr};
