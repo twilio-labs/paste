502c31be61076adb3d3bac102e7dd6c2d150f83c
Kate Schwarz <kschwarz@twilio.com>
2020-10-07 23:02:33 -0400

    feat(icons): switch to compressed TwiML Bins icon

--- packages/paste-icons/src/ProductTwimlBinsIcon.tsx
+++ packages/paste-icons/src/ProductTwimlBinsIcon.tsx
@@ -5,14 +5,14 @@ import * as React from 'react';
 import {UID} from 'react-uid';
 import {IconWrapper, IconWrapperProps} from './helpers/IconWrapper';
 
-export interface ProductTwimlBinsIconProps extends IconWrapperProps {
+export interface ProductTwiMLBinsIconProps extends IconWrapperProps {
   title?: string;
   decorative: boolean;
 }
 
-const ProductTwimlBinsIcon: React.FC<ProductTwimlBinsIconProps> = ({as, display, size, color, title, decorative}) => {
+const ProductTwiMLBinsIcon: React.FC<ProductTwiMLBinsIconProps> = ({as, display, size, color, title, decorative}) => {
   if (!decorative && title == null) {
-    throw new Error('[ProductTwimlBinsIcon]: Missing a title for non-decorative icon.');
+    throw new Error('[ProductTwiMLBinsIcon]: Missing a title for non-decorative icon.');
   }
 
   return (
@@ -22,15 +22,16 @@ const ProductTwimlBinsIcon: React.FC<ProductTwimlBinsIconProps> = ({as, display,
           <svg
             role="img"
             aria-hidden={decorative}
-            height="100%"
             width="100%"
-            viewBox="0 0 1024 1024"
+            height="100%"
+            viewBox="0 0 20 20"
             aria-labelledby={titleId}
           >
             {title ? <title id={titleId}>{title}</title> : null}
             <path
               fill="currentColor"
-              d="M998.4 358.7H25.6C10.24 358.7 0 348.46 0 333.1v-256c0-15.36 10.24-25.6 25.6-25.6h972.8c15.36 0 25.6 10.24 25.6 25.6v256c0 15.36-10.24 25.6-25.6 25.6M51.2 307.5h921.6V102.7H51.2v204.8m901.12 665.6H76.8c-15.36 0-25.6-10.24-25.6-25.6V333.1c0-15.36 10.24-25.6 25.6-25.6h875.52c15.36 0 25.6 10.24 25.6 25.6v614.4c0 15.36-15.36 25.6-25.6 25.6M102.4 921.9h824.32V358.7H102.4v563.2m588.8-302.08H332.8c-15.36 0-25.6-10.24-25.6-25.6v-102.4c0-20.48 10.24-30.72 25.6-30.72h358.4c15.36 0 25.6 10.24 25.6 25.6v102.4c0 15.36-10.24 30.72-25.6 30.72m-332.8-51.2h307.2v-51.2H358.4v51.2"
+              fillRule="evenodd"
+              d="M17.5 3a.5.5 0 01.5.5v4a.5.5 0 01-.5.5H17v8.5a.5.5 0 01-.5.5h-13a.5.5 0 01-.5-.5V8h-.5a.5.5 0 01-.5-.5v-4a.5.5 0 01.5-.5h15zM16 8H4v7.999h12V8zm-3.5 1a.5.5 0 01.5.5v2a.5.5 0 01-.5.5h-5a.5.5 0 01-.5-.5v-2a.5.5 0 01.5-.5h5zm-.5 1H8v1h4v-1zm5-6H3v3h14V4z"
             />
           </svg>
         )}
@@ -39,5 +40,5 @@ const ProductTwimlBinsIcon: React.FC<ProductTwimlBinsIconProps> = ({as, display,
   );
 };
 
-ProductTwimlBinsIcon.displayName = 'ProductTwimlBinsIcon';
-export {ProductTwimlBinsIcon};
+ProductTwiMLBinsIcon.displayName = 'ProductTwiMLBinsIcon';
+export {ProductTwiMLBinsIcon};
