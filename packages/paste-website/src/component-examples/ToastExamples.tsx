da88f28f81d60977564d8a0b40780bd515cfe155
Si Taggart <me@simontaggart.com>
2020-10-20 09:45:38 -0700

    docs(toast): document the toaster
    Co-authored-by: Sarah <li.sarah.y@gmail.com>

---
+++ packages/paste-website/src/component-examples/ToastExamples.tsx
@@ -0,0 +1,85 @@
+import * as React from 'react';
+import {useUID} from 'react-uid';
+import {Toaster, useToaster, ToastVariants} from '@twilio-paste/toast';
+import {Button} from '@twilio-paste/button';
+import {Stack} from '@twilio-paste/stack';
+import {Input} from '@twilio-paste/input';
+import {Label} from '@twilio-paste/label';
+import {RadioGroup, Radio} from '@twilio-paste/radio-group';
+
+export const ToasterExample: React.FC = () => {
+  const toaster = useToaster();
+  const messageID = useUID();
+  const dismissAfterID = useUID();
+  const [messageText, setMessageText] = React.useState('Nice one, you just pushed a Toast to the Toaster!');
+  const [variantStyle, setVariantStyle] = React.useState<ToastVariants>('success');
+  const [timeout, setTimeout] = React.useState('');
+
+  const handleSubmit = (e: React.FormEvent): void => {
+    e.preventDefault();
+    toaster.push({
+      variant: variantStyle,
+      message: messageText,
+      ...(timeout !== '0' && timeout !== '' && {dismissAfter: parseInt(timeout, 10)}),
+    });
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Stack orientation="vertical" spacing="space80">
+          <div>
+            <Label htmlFor={messageID} required>
+              Message
+            </Label>
+            <Input
+              id={messageID}
+              value={messageText}
+              type="text"
+              onChange={e => setMessageText(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <Label htmlFor={dismissAfterID}>Dismiss after (milliseconds)</Label>
+            <Input
+              id={dismissAfterID}
+              value={timeout}
+              type="number"
+              onChange={e => setTimeout(e.target.value)}
+              placeholder="7000"
+            />
+          </div>
+          <div>
+            <RadioGroup
+              name="variant"
+              legend="Variant"
+              onChange={newVariant => {
+                setVariantStyle(newVariant as ToastVariants);
+              }}
+              value={variantStyle}
+              orientation="horizontal"
+              required
+            >
+              <Radio id={useUID()} required value="error">
+                Error
+              </Radio>
+              <Radio id={useUID()} required value="neutral">
+                Neutral
+              </Radio>
+              <Radio id={useUID()} required value="success">
+                Success
+              </Radio>
+              <Radio id={useUID()} required value="warning">
+                Warning
+              </Radio>
+            </RadioGroup>
+          </div>
+          <Button type="submit" variant="secondary">
+            Add toast
+          </Button>
+        </Stack>
+      </form>
+      <Toaster {...toaster} />
+    </div>
+  );
+};
