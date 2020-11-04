16fc2fb3371e73828175d86f381851ee342188d7
richbachman <rb@richbachman.com>
2020-11-04 15:22:59 -0700

    test: move visit to each test

--- cypress/integration/e2e/sidebar-navigation/index.spec.ts
+++ cypress/integration/e2e/sidebar-navigation/index.spec.ts
@@ -1,51 +1,55 @@
 describe('Sidebar navigation', () => {
-  beforeEach(() => {
-    cy.visit('/');
-  });
-
   it('opens the getting started discosure', () => {
+    cy.visit('/');
     cy.get('[data-cy=getting-started-button]')
       .click()
       .should('have.attr', 'aria-expanded', 'true');
   });
 
   it('opens the tokens discosure', () => {
+    cy.visit('/');
     cy.get('[data-cy=design-tokens-button]')
       .click()
       .should('have.attr', 'aria-expanded', 'true');
   });
 
   it('opens the components discosure', () => {
+    cy.visit('/');
     cy.get('[data-cy=components-button]')
       .click()
       .should('have.attr', 'aria-expanded', 'true');
   });
 
   it('opens the primitives discosure', () => {
+    cy.visit('/');
     cy.get('[data-cy=primitives-button]')
       .click()
       .should('have.attr', 'aria-expanded', 'true');
   });
 
   it('opens the layout discosure', () => {
+    cy.visit('/');
     cy.get('[data-cy=layout-button]')
       .click()
       .should('have.attr', 'aria-expanded', 'true');
   });
 
   it('opens the icons discosure', () => {
+    cy.visit('/');
     cy.get('[data-cy=icons-button]')
       .click()
       .should('have.attr', 'aria-expanded', 'true');
   });
 
   it('opens the content discosure', () => {
+    cy.visit('/');
     cy.get('[data-cy=content-button]')
       .click()
       .should('have.attr', 'aria-expanded', 'true');
   });
 
   it('opens the patterns discosure', () => {
+    cy.visit('/');
     cy.get('[data-cy=patterns-button]')
       .click()
       .should('have.attr', 'aria-expanded', 'true');
