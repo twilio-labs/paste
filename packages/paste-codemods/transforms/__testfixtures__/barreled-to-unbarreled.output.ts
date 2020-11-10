316fb6a44bd2cb98e22ee89cd497152095479eed
thesisb <shadiisber@gmail.com>
2020-11-10 17:34:06 -0500

    test(codemods): test transform and tooling

---
+++ packages/paste-codemods/transforms/__testfixtures__/barreled-to-unbarreled.output.ts
@@ -0,0 +1,7 @@
+import { TBodyPropTypes, Table } from '@twilio-paste/core/table';
+import { MediaObject } from '@twilio-paste/core/media-object';
+import { Label } from '@twilio-paste/core/label';
+import { Button, Button as Stuffin } from '@twilio-paste/core/button';
+import {Button as Stuff} from '@sendgrid/ui-components';
+import {Card} from '@twilio-paste/core/dist/card';
+import { Input } from '@twilio-paste/core/input';
