e4716b72d3b71326a098812068160a4fc6a2d192
richbachman <rb@richbachman.com>
2020-11-11 10:52:54 -0700

    feat(website): new footer and assets

--- packages/paste-website/src/components/site-wrapper-new/SiteFooter.tsx
+++ packages/paste-website/src/components/site-wrapper-new/SiteFooter.tsx
@@ -1,35 +1,131 @@
 import * as React from 'react';
-import {styled, themeGet} from '@twilio-paste/styling-library';
+import {useTheme} from '@twilio-paste/theme';
 import {Anchor} from '@twilio-paste/anchor';
 import {Box} from '@twilio-paste/box';
+import {Stack} from '@twilio-paste/stack';
 import {Text} from '@twilio-paste/text';
-import {Heading} from '@twilio-paste/heading';
-import {Paragraph} from '@twilio-paste/paragraph';
-import {TWILIO_BLUE} from '../../constants';
-import {TwilioIcon} from '../icons/TwilioIcon';
-
-const StyledFooter = styled.footer`
-  margin-top: 96px;
-  padding: ${themeGet('space.space150')};
-  background-color: ${themeGet('colors.colorGray10')};
-`;
-
-const StyledFooterLogo = styled(TwilioIcon)`
-  margin: 0 auto ${themeGet('space.space40')};
-`;
+import HeartDoodleIcon from '../icons/HeartDoodleIcon';
+import TwilioLogo from '../icons/TwilioLogo';
+import FooterBuildingBlocks from '../../assets/illustrations/FooterBuildingBlocks.svg';
 
 const SiteFooter: React.FC<{}> = () => {
+  const theme = useTheme();
   return (
-    <StyledFooter>
+    <Box backgroundColor="colorBackgroundBody" padding="space70">
       <Box
-        css={{
-          margin: '0 auto',
-          maxWidth: '640px',
+        backgroundColor="colorBackgroundPrimaryDarker"
+        borderRadius="borderRadius20"
+        padding={['space90', 'space180']}
+        position="relative"
+        _before={{
+          backgroundColor: 'colorBackgroundPrimaryDarker',
+          borderRadius: 'borderRadius20',
+          content: `""`,
+          display: 'block',
+          height: '50%',
+          left: '0',
+          position: 'absolute',
+          right: '0',
+          top: '-12%',
+          transform: 'skewY(-1.5deg)',
+          transformOrigin: '100% 0',
         }}
       >
-        TODO new footer
+        <Box
+          bottom={-6}
+          display={['none', 'none', 'none', 'block']}
+          right={theme.sizes.sizeSquare120}
+          position="absolute"
+        >
+          <FooterBuildingBlocks aria-hidden="true" role="img" />
+        </Box>
+        <Box position="relative">
+          <Box
+            alignItems="center"
+            display="flex"
+            flexDirection={['column', 'row']}
+            marginBottom="space80"
+            textAlign={['center', 'left']}
+          >
+            <HeartDoodleIcon
+              css={{height: theme.heights.sizeIcon70, width: theme.widths.sizeIcon70}}
+              color={theme.textColors.colorTextInverse}
+              decorative
+            />
+            <Text
+              as="h5"
+              color="colorTextInverse"
+              fontSize={['fontSize60', 'fontSize90']}
+              fontWeight="fontWeightBold"
+              lineHeight={['lineHeight60', 'lineHeight90']}
+              marginLeft={['space0', 'space40']}
+              marginTop={['space40', 'space0']}
+            >
+              Black lives matter.
+            </Text>
+          </Box>
+          <Box
+            display="flex"
+            flexDirection={['column', 'row']}
+            marginLeft={['space0', 'space120']}
+            textAlign={['center', 'left']}
+          >
+            <Box marginRight={['space0', 'space130']} marginBottom="space60">
+              <Stack as="nav" orientation="vertical" spacing="space60">
+                <Anchor href="/about" variant="inverse">
+                  About
+                </Anchor>
+                <Anchor href="/roadmap" variant="inverse">
+                  Our roadmap
+                </Anchor>
+                <Anchor href="/getting-started/design" variant="inverse">
+                  Design guidelines
+                </Anchor>
+                <Anchor href="/getting-started/engineering" variant="inverse">
+                  Engineering guidelines
+                </Anchor>
+              </Stack>
+            </Box>
+            <Box marginRight={['space0', 'space130']} marginBottom="space60">
+              <Stack as="nav" orientation="vertical" spacing="space60">
+                <Anchor href="https://github.com/twilio-labs/paste/discussions" variant="inverse" showExternal>
+                  Get support
+                </Anchor>
+                <Anchor href="https://github.com/twilio-labs/paste/issues/new/choose" variant="inverse" showExternal>
+                  Report a bug
+                </Anchor>
+                <Anchor href="/how-we-work" variant="inverse">
+                  How we work
+                </Anchor>
+              </Stack>
+            </Box>
+            <Box marginRight={['space0', 'space130']} marginBottom="space60">
+              <Stack as="nav" orientation="vertical" spacing="space60">
+                <Anchor href="#" variant="inverse" showExternal>
+                  Abstract
+                </Anchor>
+                <Anchor href="#" variant="inverse" showExternal>
+                  Figma
+                </Anchor>
+                <Anchor href="https://github.com/twilio-labs/paste" variant="inverse" showExternal>
+                  Github
+                </Anchor>
+                <Anchor href="https://twilio-labs.github.io/paste" variant="inverse" showExternal>
+                  Storybook
+                </Anchor>
+              </Stack>
+            </Box>
+          </Box>
+          <Box marginTop="space120" marginLeft={['space0', 'space120']} textAlign={['center', 'left']}>
+            <TwilioLogo
+              css={{height: theme.heights.sizeIcon40, width: theme.widths.size10}}
+              color={theme.textColors.colorTextInverse}
+              title="Twilio"
+            />
+          </Box>
+        </Box>
       </Box>
-    </StyledFooter>
+    </Box>
   );
 };
 
--- packages/paste-website/src/components/site-wrapper-new/SiteThemeProvider.tsx
+++ packages/paste-website/src/components/site-wrapper-new/SiteThemeProvider.tsx
@@ -5,7 +5,7 @@ import {useActiveSiteTheme} from '../../context/ActiveSiteThemeContext';
 const SiteThemeProvider: React.FC = ({children}) => {
   const {theme} = useActiveSiteTheme();
   return (
-    <Theme.Provider theme={theme} customBreakpoints={['768px', '1024px', '1280px']}>
+    <Theme.Provider theme={theme} customBreakpoints={['768px', '1024px', '1220px']}>
       {children}
     </Theme.Provider>
   );
