6713315c36ae1946016e0a1934ba73b7199efd9b
Rich Bachman <rb@richbachman.com>
2020-11-16 11:11:38 -0700

    feat(website): new footer and assets (#931)
    * feat(website): new footer and assets
    
    * chore(website): clean up slant
    
    * Apply suggestions from code review
    
    Co-authored-by: Sarah <li.sarah.y@gmail.com>
    
    * chore(website): fix breakpoints and links
    
    * chore(website): fix link focus and padding
    
    * chore(website): use unbarrelled imports
    
    * chore(website): break footer into separate files
    
    Co-authored-by: Sarah <li.sarah.y@gmail.com>

---
+++ packages/paste-website/src/components/site-wrapper-new/site-footer/SiteFooterTopAngle.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import {Box} from '@twilio-paste/core/box';
+
+const SiteFooterTopAngle: React.FC<{}> = () => {
+  return (
+    <Box borderRadius="borderRadius20" position="absolute" top={0} right={0} bottom={0} left={0}>
+      <Box
+        backgroundColor="colorBackgroundPrimaryDarker"
+        borderRadius="borderRadius20"
+        height="100%"
+        left={0}
+        position="relative"
+        top={0}
+        transform="skewY(-5deg)"
+        transformOrigin="100% 0"
+        width="100%"
+      />
+    </Box>
+  );
+};
+
+export {SiteFooterTopAngle};
