e4b537f298e9ea75eda7771db21ee87f5b77bb11
Si Taggart <me@simontaggart.com>
2020-11-18 10:18:36 -0800

    chore: applitools batch names

--- applitools.config.js
+++ applitools.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   serverUrl: 'https://twilioeyesapi.applitools.com',
   batchId: process.env.GITHUB_HEAD_SHA,
+  batchName: process.env.APPLITOOLS_BATCH_NAME,
   exitcode: 0,
 };
