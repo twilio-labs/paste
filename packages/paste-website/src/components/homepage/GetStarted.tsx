f0eedcc1415a7934633adc0b35780979eea79eba
thesisb <shadiisber@gmail.com>
2020-11-19 11:07:42 -0500

    chore: remove extra boilerplate out of homepage sections

--- packages/paste-website/src/components/homepage/GetStarted.tsx
+++ packages/paste-website/src/components/homepage/GetStarted.tsx
@@ -1,25 +1,21 @@
 import * as React from 'react';
-import {Box} from '@twilio-paste/box';
 import {Grid, Column} from '@twilio-paste/grid';
 import {GetStarterWhy} from './GetStartedWhy';
 import {GetStartedInclusive} from './GetStartedInclusive';
 import {GetStartedRunning} from './GetStartedRunning';
-import {SITE_CONTENT_MAX_WIDTH} from '../../constants';
 
 export const GetStarted: React.FC = () => {
   return (
-    <Box maxWidth={SITE_CONTENT_MAX_WIDTH} marginLeft="auto" marginRight="auto" paddingX="space70">
-      <Grid as="section" gutter="space40" vertical={[true, false, false]}>
-        <Column span={4}>
-          <GetStarterWhy />
-        </Column>
-        <Column span={4}>
-          <GetStartedInclusive />
-        </Column>
-        <Column span={4}>
-          <GetStartedRunning />
-        </Column>
-      </Grid>
-    </Box>
+    <Grid as="section" gutter="space40" vertical={[true, false, false]}>
+      <Column span={4}>
+        <GetStarterWhy />
+      </Column>
+      <Column span={4}>
+        <GetStartedInclusive />
+      </Column>
+      <Column span={4}>
+        <GetStartedRunning />
+      </Column>
+    </Grid>
   );
 };
--- packages/paste-website/src/components/popular-components-and-patterns/index.tsx
+++ packages/paste-website/src/components/popular-components-and-patterns/index.tsx
@@ -8,26 +8,16 @@ import {SITE_CONTENT_MAX_WIDTH} from '../../constants';
 
 const PopularComponentsAndPatterns: React.FC = () => {
   return (
-    <Box
-      backgroundColor="colorBackgroundBody"
-      borderRadius="borderRadius20"
-      margin="space70"
-      position="relative"
-      overflow="hidden"
-    >
-      <Box backgroundColor="colorBackground" padding={['space90', 'space180']}>
-        <Box maxWidth={SITE_CONTENT_MAX_WIDTH} marginLeft="auto" marginRight="auto">
-          <PopularSectionHeader />
-          <Grid gutter="space60">
-            <Column span={[12, 6]}>
-              <PopularComponents />
-            </Column>
-            <Column span={[12, 6]}>
-              <PopularPatterns />
-            </Column>
-          </Grid>
-        </Box>
-      </Box>
+    <Box backgroundColor="colorBackground" padding={['space90', 'space180']}>
+      <PopularSectionHeader />
+      <Grid gutter="space60">
+        <Column span={[12, 6]}>
+          <PopularComponents />
+        </Column>
+        <Column span={[12, 6]}>
+          <PopularPatterns />
+        </Column>
+      </Grid>
     </Box>
   );
 };
