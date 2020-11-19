36314bd67b3fe5cc8f2ce85f3e3be9800bebaa01
thesisb <shadiisber@gmail.com>
2020-11-19 11:08:38 -0500

    chore: use constant in scroll distance calc

--- packages/paste-website/src/components/site-wrapper/ScrollAnchorIntoView.tsx
+++ packages/paste-website/src/components/site-wrapper/ScrollAnchorIntoView.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import {getCurrentPathHash} from '../../utils/RouteUtils';
+import {SITE_MASTHEAD_HEIGHT} from '../../constants';
 
-const Y_OFFSET = -90;
+// Looks a bit better to have some extra space right above where we're scrolling
+const EXTRA_OFFSET = 15;
 
 function scrollIntoView(): void {
   const id = getCurrentPathHash();
@@ -11,7 +13,7 @@ function scrollIntoView(): void {
 
   const element = document.querySelector(`[id='${id.replace(/^#/, '')}']`);
   if (element != null) {
-    const top = element.getBoundingClientRect().top + window.pageYOffset + Y_OFFSET;
+    const top = element.getBoundingClientRect().top + window.pageYOffset - SITE_MASTHEAD_HEIGHT - EXTRA_OFFSET;
     window.scrollTo({top, behavior: 'smooth'});
   }
 }
