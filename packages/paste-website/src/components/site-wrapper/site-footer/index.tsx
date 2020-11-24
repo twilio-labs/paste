82da0961bb1cb8a0553fa42b83cdb6fbf972a280
richbachman <rb@richbachman.com>
2020-11-24 15:33:13 -0700

    chore(website): check pathname for footer slash

--- packages/paste-website/src/components/site-wrapper/site-footer/index.tsx
+++ packages/paste-website/src/components/site-wrapper/site-footer/index.tsx
@@ -3,12 +3,15 @@ import {Box} from '@twilio-paste/box';
 import {SiteFooterIllustration} from './SiteFooterIllustration';
 import {SiteFooterNav} from './SiteFooterNav';
 import {SiteFooterTopAngle} from './SiteFooterTopAngle';
+import {useNavigationContext} from '../../../context/NavigationContext';
 import {SITE_CONTENT_MAX_WIDTH} from '../../../constants';
 
 const SiteFooter: React.FC<{}> = () => {
+  const {pathname} = useNavigationContext();
+
   return (
     <Box margin="space70" marginTop="space200" paddingTop="space200" position="relative">
-      <SiteFooterTopAngle />
+      {pathname === '/' ? <SiteFooterTopAngle /> : null}
       <Box borderRadius="borderRadius20" backgroundColor="colorBackgroundPrimaryDarker">
         <Box
           maxWidth={SITE_CONTENT_MAX_WIDTH}
