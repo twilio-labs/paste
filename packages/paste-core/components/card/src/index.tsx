87eddf077bac4625488b4deda6a2e87d1245e750
thesisb <shadiisber@gmail.com>
2020-11-24 21:56:01 -0500

    fix(card): forwarding ref

--- packages/paste-core/components/card/src/index.tsx
+++ packages/paste-core/components/card/src/index.tsx
@@ -4,34 +4,29 @@ import {PaddingProps, isPaddingTokenProp} from '@twilio-paste/style-props';
 
 export interface CardProps extends React.HTMLAttributes<HTMLElement>, PaddingProps {}
 
-const Card: React.FunctionComponent<CardProps> = ({
-  children,
-  padding = 'space60',
-  paddingBottom,
-  paddingLeft,
-  paddingRight,
-  paddingTop,
-  ...props
-}) => {
-  return (
-    <Box
-      {...safelySpreadBoxProps(props)}
-      as="article"
-      borderWidth="borderWidth20"
-      borderColor="colorBorderLighter"
-      borderStyle="solid"
-      borderRadius="borderRadius20"
-      padding={padding}
-      paddingBottom={paddingBottom}
-      paddingLeft={paddingLeft}
-      paddingRight={paddingRight}
-      paddingTop={paddingTop}
-      backgroundColor="colorBackgroundBody"
-    >
-      {children}
-    </Box>
-  );
-};
+const Card = React.forwardRef<HTMLElement, CardProps>(
+  ({children, padding = 'space60', paddingBottom, paddingLeft, paddingRight, paddingTop, ...props}, ref) => {
+    return (
+      <Box
+        {...safelySpreadBoxProps(props)}
+        ref={ref}
+        as="article"
+        borderWidth="borderWidth20"
+        borderColor="colorBorderLighter"
+        borderStyle="solid"
+        borderRadius="borderRadius20"
+        padding={padding}
+        paddingBottom={paddingBottom}
+        paddingLeft={paddingLeft}
+        paddingRight={paddingRight}
+        paddingTop={paddingTop}
+        backgroundColor="colorBackgroundBody"
+      >
+        {children}
+      </Box>
+    );
+  }
+);
 
 if (process.env.NODE_ENV === 'development') {
   Card.propTypes = {
