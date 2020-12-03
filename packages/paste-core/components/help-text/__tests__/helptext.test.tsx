5ebefd4dd3ebbaaa856aeefca9dadd86e3b6060a
richbachman <rb@richbachman.com>
2020-12-03 16:04:24 -0700

    fix(help-text): stop icon from shrinking with long error text

--- packages/paste-core/components/help-text/__tests__/helptext.test.tsx
+++ packages/paste-core/components/help-text/__tests__/helptext.test.tsx
@@ -1,32 +1,6 @@
 import * as React from 'react';
-import * as renderer from 'react-test-renderer';
 import {shallow} from 'enzyme';
-import {Theme} from '@twilio-paste/theme';
 import {HelpText} from '../src';
-
-describe('HelpText render', () => {
-  it('should render', (): void => {
-    const tree = renderer
-      .create(
-        <Theme.Provider theme="console">
-          <HelpText>child</HelpText>
-        </Theme.Provider>
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('should render with an error icon', (): void => {
-    const tree = renderer
-      .create(
-        <Theme.Provider theme="console">
-          <HelpText variant="error">child</HelpText>
-        </Theme.Provider>
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-});
 
 describe('HelpText variant prop', () => {
   const container = shallow(<HelpText variant="error" />);
