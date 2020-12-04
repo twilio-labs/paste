b8f74d4a3edf1ca8ea4cd46fd15ede69a87ade41
richbachman <rb@richbachman.com>
2020-12-04 15:54:32 -0700

    fix(truncate): add forwardRef, use box
    BREAKING CHANGE: component is now using fowardRef

--- packages/paste-core/components/truncate/src/index.tsx
+++ packages/paste-core/components/truncate/src/index.tsx
@@ -1,12 +1,33 @@
-import {styled} from '@twilio-paste/styling-library';
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
+import {Box, BoxProps} from '@twilio-paste/box';
 
-const Truncate = styled.span`
-  display: inline-block;
-  max-width: 100%;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-`;
+export interface TruncateProps extends Pick<BoxProps, 'as'> {
+  children: NonNullable<React.ReactNode>;
+}
+
+const Truncate = React.forwardRef<HTMLSpanElement, TruncateProps>(({children, ...props}, ref) => {
+  return (
+    <Box
+      {...props}
+      as="span"
+      display="inline-block"
+      maxWidth="100%"
+      overflow="hidden"
+      ref={ref}
+      textOverflow="ellipsis"
+      whiteSpace="nowrap"
+    >
+      {children}
+    </Box>
+  );
+});
 Truncate.displayName = 'Truncate';
+
+if (process.env.NODE_ENV === 'development') {
+  Truncate.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+}
 
 export {Truncate};
