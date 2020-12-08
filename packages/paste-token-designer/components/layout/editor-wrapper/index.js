6dd4fd26f43283d511c9b0270e2f09ffc7b3956d
richbachman <rb@richbachman.com>
2020-12-08 16:17:04 -0700

    chore: adjust scrolling, work on preview content

--- packages/paste-token-designer/components/layout/editor-wrapper/index.js
+++ packages/paste-token-designer/components/layout/editor-wrapper/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head';
 import {Box} from '@twilio-paste/core/box';
+import {Button} from '@twilio-paste/core/button';
 import {TokenSelector} from './TokenSelector';
 import {TopBar} from '../../topbar';
 import PreviewPane from '../../../pages/playground/preview-pane';
+import {SITE_MASTHEAD_HEIGHT} from '../../../constants';
 
 const EditorWrapper = props => {
   return (
@@ -12,10 +14,17 @@ const EditorWrapper = props => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <TopBar />
-      <Box display="flex" height="100vh" overflow="hidden">
-        <Box maxWidth="size40" width="100%" overflow="scroll">
+      <Box display="flex" height={`calc(100vh - ${SITE_MASTHEAD_HEIGHT}px)`} overflow="hidden">
+        <Box maxWidth="size40" width="100%" display="flex" flexDirection="column">
           <TokenSelector />
-          <Box padding="space60">{props.children}</Box>
+          <Box padding="space60" flexGrow={1} overflow="scroll">
+            {props.children}
+          </Box>
+          <Box backgroundColor="colorBackground" padding="space60">
+            <Button variant="primary" onClick={() => alert('TODO')} fullWidth>
+              Export
+            </Button>
+          </Box>
         </Box>
         <PreviewPane />
       </Box>
