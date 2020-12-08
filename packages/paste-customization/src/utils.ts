784e589c94ed595e85d1a6af6bcc35c55a0ecbdc
Si Taggart <me@simontaggart.com>
2020-12-07 20:19:19 -0800

    feat(customization): add customization provider

---
+++ packages/paste-customization/src/utils.ts
@@ -0,0 +1,15 @@
+import {ThemeShape} from '@twilio-paste/theme';
+
+const merge = require('lodash.merge');
+
+interface CreateCustomTheme {
+  (baseTheme: Partial<ThemeShape>, overrides: Partial<ThemeShape>, breakpoints?: string[]): Partial<ThemeShape>;
+}
+export const createCustomTheme: CreateCustomTheme = (baseTheme, overrides, customBreakpoints) => {
+  const breakpoints = customBreakpoints || baseTheme.breakpoints;
+  const mergedTheme = merge({}, baseTheme, overrides);
+  return {
+    ...mergedTheme,
+    breakpoints,
+  };
+};
