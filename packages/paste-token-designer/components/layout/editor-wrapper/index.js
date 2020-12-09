48e59063333e9cfd945bba09419ae7ff33a15353
Shadi <TheSisb@users.noreply.github.com>
2020-12-09 17:47:15 -0500

    feat(token-designer): add export functionality (#1022)
    * chore(token-designer): export functionality
    
    * chore: add download button to modal footer
    
    Co-authored-by: richbachman <rb@richbachman.com>

--- packages/paste-token-designer/components/layout/editor-wrapper/index.js
+++ packages/paste-token-designer/components/layout/editor-wrapper/index.js
@@ -1,11 +1,47 @@
 import {Box} from '@twilio-paste/core/box';
 import {Button} from '@twilio-paste/core/button';
+import {useUID} from '@twilio-paste/uid-library';
+import {TextArea} from '@twilio-paste/core/textarea';
+import {generateThemeFromTokens} from '@twilio-paste/core/theme';
+import {Modal, ModalHeading, ModalHeader, ModalBody, ModalFooter, ModalFooterActions} from '@twilio-paste/core/modal';
 import {TokenSelector} from './TokenSelector';
+import {TokenContext} from '../../../context/TokenContext';
 import {TopBar} from '../../topbar';
 import {Preview} from '../../preview';
 import {SITE_MASTHEAD_HEIGHT} from '../../../constants';
 
 const EditorWrapper = props => {
+  const {tokens} = React.useContext(TokenContext);
+  const [isOpen, setIsOpen] = React.useState(false);
+  const handleOpen = () => setIsOpen(true);
+  const handleClose = () => setIsOpen(false);
+  const modalHeadingID = useUID();
+  const modalTextareaID = useUID();
+
+  const customTheme = React.useMemo(
+    () =>
+      generateThemeFromTokens(
+        tokens.backgroundColors,
+        tokens.borderColors,
+        tokens.borderWidths,
+        tokens.radii,
+        tokens.colors,
+        tokens.fonts,
+        tokens.fontSizes,
+        tokens.fontWeights,
+        tokens.lineHeights,
+        tokens.boxShadows,
+        tokens.sizings,
+        tokens.spacings,
+        tokens.textColors,
+        tokens.zIndices
+      ),
+    [tokens]
+  );
+
+  const themeJson = JSON.stringify(customTheme, null, '  ');
+  const themeDownload = 'data:application/json;charset=utf-8,' + encodeURIComponent(themeJson);
+
   return (
     <>
       <TopBar />
@@ -16,9 +52,28 @@ const EditorWrapper = props => {
             {props.children}
           </Box>
           <Box backgroundColor="colorBackground" padding="space60">
-            <Button variant="primary" onClick={() => alert('TODO')} fullWidth>
+            <Button variant="primary" onClick={handleOpen} fullWidth>
               Export
             </Button>
+            <Modal ariaLabelledby={modalHeadingID} isOpen={isOpen} onDismiss={handleClose} size="default">
+              <ModalHeader>
+                <ModalHeading as="h3" id={modalHeadingID}>
+                  Copy Theme JSON
+                </ModalHeading>
+              </ModalHeader>
+              <ModalBody>
+                <TextArea id={modalTextareaID} onChange={() => {}} readOnly>
+                  {themeJson}
+                </TextArea>
+              </ModalBody>
+              <ModalFooter>
+                <ModalFooterActions>
+                  <Button as="a" href={themeDownload} variant="primary" download="customTheme.json">
+                    Download
+                  </Button>
+                </ModalFooterActions>
+              </ModalFooter>
+            </Modal>
           </Box>
         </Box>
         <Preview />
