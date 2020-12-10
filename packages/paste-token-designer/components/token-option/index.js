15aa5d83a1bfd59dccaeb81fa0b38adb5338f26f
Shadi <TheSisb@users.noreply.github.com>
2020-12-10 17:48:02 -0500

    chore(token-designer): improve token editing controls (#1023)
    * chore: checkpoint
    
    * chore: several improvements
    
    * chore: change export modal heading text
    
    * chore: improve color input styles
    
    * chore: more progress
    
    * chore: nits
    
    * chore: fix logic bug

--- packages/paste-token-designer/components/token-option/index.js
+++ packages/paste-token-designer/components/token-option/index.js
@@ -1,45 +1,181 @@
-import {Box} from '@twilio-paste/core/box';
-import {useUIDSeed} from '@twilio-paste/uid-library';
-import {Input} from '@twilio-paste/core/input';
-import {Label} from '@twilio-paste/core/label';
+import {ColorTokenInput} from './ColorTokenInput';
+import {UnitTokenInput} from './UnitTokenInput';
+import {FontTokenInput} from './FontTokenInput';
+import {TextTokenInput} from './TextTokenInput';
 
 const collator = new Intl.Collator(undefined, {numeric: true, sensitivity: 'base'});
 
-const TokenItem = ({bucket, tokenName, tokenValue, onChange}) => {
-  const seed = useUIDSeed();
-
-  return (
-    <Box key={tokenName} marginBottom="space60">
-      <Label htmlFor={seed(tokenName)}>{tokenName}:</Label>
-      <Input
-        id={seed(tokenName)}
-        type="text"
-        value={tokenValue}
-        onChange={event => {
-          const {value} = event.target;
-          // TODO validate
-          if (value === '') return;
-          onChange(bucket, tokenName, event.target.value);
-        }}
-      />
-    </Box>
-  );
-};
-
 export const OptionsList = ({bucket, options, handleChange}) => {
-  const sortedNames = Object.keys(options).sort(collator.compare);
+  const sortedNames = React.useMemo(() => Object.keys(options).sort(collator.compare), [options]);
 
-  return (
-    <>
-      {sortedNames.map(tokenName => (
-        <TokenItem
-          bucket={bucket}
-          key={tokenName}
-          tokenName={tokenName}
-          tokenValue={options[tokenName]}
-          onChange={handleChange}
-        />
-      ))}
-    </>
-  );
+  switch (bucket) {
+    case 'backgroundColors':
+    case 'textColors':
+    case 'borderColors':
+      return (
+        <>
+          {sortedNames.map(tokenName => (
+            <ColorTokenInput
+              bucket={bucket}
+              key={tokenName}
+              tokenName={tokenName}
+              tokenValue={options[tokenName]}
+              onChange={handleChange}
+            />
+          ))}
+        </>
+      );
+    case 'fonts':
+      return (
+        <>
+          {sortedNames.map(tokenName => (
+            <FontTokenInput
+              bucket={bucket}
+              key={tokenName}
+              tokenName={tokenName}
+              tokenValue={options[tokenName]}
+              onChange={handleChange}
+            />
+          ))}
+        </>
+      );
+    case 'boxShadows':
+      return (
+        <>
+          {sortedNames.map(tokenName => (
+            <TextTokenInput
+              bucket={bucket}
+              key={tokenName}
+              tokenName={tokenName}
+              tokenValue={options[tokenName]}
+              onChange={handleChange}
+            />
+          ))}
+        </>
+      );
+    case 'radii':
+      return (
+        <>
+          {sortedNames.map(tokenName => {
+            if (tokenName === 'borderRadiusCircle') {
+              return (
+                <UnitTokenInput
+                  unit="%"
+                  bucket={bucket}
+                  key={tokenName}
+                  tokenName={tokenName}
+                  tokenValue={options[tokenName]}
+                  onChange={handleChange}
+                />
+              );
+            }
+            return (
+              <UnitTokenInput
+                unit="px"
+                bucket={bucket}
+                key={tokenName}
+                tokenName={tokenName}
+                tokenValue={options[tokenName]}
+                onChange={handleChange}
+              />
+            );
+          })}
+        </>
+      );
+    case 'borderWidths':
+      return (
+        <>
+          {sortedNames.map(tokenName => (
+            <UnitTokenInput
+              unit="px"
+              bucket={bucket}
+              key={tokenName}
+              tokenName={tokenName}
+              tokenValue={options[tokenName]}
+              onChange={handleChange}
+            />
+          ))}
+        </>
+      );
+    case 'fontSizes':
+    case 'lineHeights':
+    case 'spacings':
+    case 'sizings':
+      return (
+        <>
+          {sortedNames.map(tokenName => (
+            <UnitTokenInput
+              unit="rem"
+              bucket={bucket}
+              key={tokenName}
+              tokenName={tokenName}
+              tokenValue={options[tokenName]}
+              onChange={handleChange}
+            />
+          ))}
+        </>
+      );
+    case 'zIndices':
+      return (
+        <>
+          {sortedNames.map(tokenName => (
+            <UnitTokenInput
+              unit=""
+              bucket={bucket}
+              key={tokenName}
+              tokenName={tokenName}
+              tokenValue={options[tokenName]}
+              onChange={handleChange}
+            />
+          ))}
+        </>
+      );
+    case 'fontWeights':
+      return (
+        <>
+          <UnitTokenInput
+            unit=""
+            bucket={bucket}
+            key="fontWeightLight"
+            tokenName="fontWeightLight"
+            tokenValue={options['fontWeightLight']}
+            onChange={handleChange}
+          />
+          <UnitTokenInput
+            unit=""
+            bucket={bucket}
+            key="fontWeightNormal"
+            tokenName="fontWeightNormal"
+            tokenValue={options['fontWeightNormal']}
+            onChange={handleChange}
+          />
+          <UnitTokenInput
+            unit=""
+            bucket={bucket}
+            key="fontWeightMedium"
+            tokenName="fontWeightMedium"
+            tokenValue={options['fontWeightMedium']}
+            onChange={handleChange}
+          />
+          <UnitTokenInput
+            unit=""
+            bucket={bucket}
+            key="fontWeightSemibold"
+            tokenName="fontWeightSemibold"
+            tokenValue={options['fontWeightSemibold']}
+            onChange={handleChange}
+          />
+          <UnitTokenInput
+            unit=""
+            bucket={bucket}
+            key="fontWeightBold"
+            tokenName="fontWeightBold"
+            tokenValue={options['fontWeightBold']}
+            onChange={handleChange}
+          />
+        </>
+      );
+    default:
+      return <div>Invalid theme category</div>;
+  }
 };
