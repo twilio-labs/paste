acef2a54ad76639c8063d134c374792bd7cff181
Si Taggart <me@simontaggart.com>
2020-12-14 18:45:02 -0800

    feat(theme): load fonts.css file from theme provider

---
+++ packages/paste-theme/src/styles/base.ts
@@ -0,0 +1,10 @@
+import {css} from '@twilio-paste/styling-library';
+
+export const pasteBaseStyles = css({
+  color: 'colorText',
+  fontSize: 'fontSize30',
+  fontFamily: 'fontFamilyText',
+  lineHeight: 'lineHeight30',
+  fontWeight: 'fontWeightNormal',
+  fontVariantNumeric: 'tabular-nums',
+});
---
+++ packages/paste-theme/src/styles/global.ts
@@ -0,0 +1,28 @@
+import {css} from '@twilio-paste/styling-library';
+
+export const pasteGlobalStyles = css({
+  html: {
+    fontSize: '100%',
+  },
+  body: {
+    margin: 0,
+    fontSize: 'fontSize30',
+  },
+  '*, *::after, *::before': {
+    boxSizing: 'border-box',
+  },
+  '@media (prefers-reduced-motion: reduce)': {
+    '*': {
+      animationDuration: '0 !important',
+      animationIterationCount: '1 !important',
+      transitionDuration: '0 !important',
+      scrollBehavior: 'auto !important',
+    },
+  },
+  // To disable reach/dialog's style warning
+  // Issue: https://github.com/twilio-labs/paste/discussions/524
+  // Reach Docs Guidance: https://reach.tech/styling#skip-including-styles
+  ':root': {
+    ['--reach-dialog' as any]: '1',
+  },
+});
