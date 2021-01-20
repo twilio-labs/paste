0e498a48c11ce3e10d527159c52d39af6cb5dcea
TheSisb <shadiisber@gmail.com>
2021-01-20 10:27:13 -0500

    fix: cjs builds should point to cjs externals

--- tools/build/esbuild.js
+++ tools/build/esbuild.js
@@ -1,5 +1,25 @@
 const esbuild = require('esbuild');
 
+/**
+ * ESBuild plugin to fix CJS builds in Paste
+ * - Replaces icon imports from /esm/IconName in src to /cjs/IconName in dist.
+ * - Replaces design-tokens' theme src code from tokens.es6 to tokens.common in dist.
+ */
+const PasteCJSResolverPlugin = {
+  name: 'PasteCJSResolver',
+  setup(build) {
+    // Change all ESM icon imports to CJS
+    build.onResolve({filter: /@twilio-paste\/icons\/esm\//}, ({path}) => {
+      return {path: require.resolve(path).replace('/esm/', '/cjs/')};
+    });
+
+    // Change all .es6 design-token imports to .common
+    build.onResolve({filter: /tokens.es6$/}, ({path}) => {
+      return {path: require.resolve(path).replace('es6', 'common')};
+    });
+  },
+};
+
 function build(packageJson) {
   // Entry and Output file paths
   const entryPoints = [packageJson['main:dev']];
@@ -10,6 +30,7 @@ function build(packageJson) {
 
   // ESbuild config
   const config = {
+    color: true,
     entryPoints,
     mainFields: ['module', 'main'],
     bundle: true,
@@ -25,6 +46,8 @@ function build(packageJson) {
       ...config,
       format: 'cjs',
       outfile: outFileCJS,
+      // Needed to fix ES6 module import paths for CJS builds
+      plugins: [PasteCJSResolverPlugin],
     })
     .catch(() => process.exit(1));
 
