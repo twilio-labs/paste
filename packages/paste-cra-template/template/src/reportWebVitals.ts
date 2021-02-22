7fe65de744c2feec577927d481c2e0ec53d2a15d
Si Taggart <me@simontaggart.com>
2021-02-22 15:50:03 -0800

    fix(cra-template): eslint rules update

--- packages/paste-cra-template/template/src/reportWebVitals.ts
+++ packages/paste-cra-template/template/src/reportWebVitals.ts
@@ -2,9 +2,7 @@ import type {ReportHandler} from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    // eslint-disable-next-line promise/catch-or-return
     import('web-vitals').then(
-      // eslint-disable-next-line promise/always-return
       ({getCLS, getFID, getFCP, getLCP, getTTFB}) => {
         getCLS(onPerfEntry, true);
         getFID(onPerfEntry);
