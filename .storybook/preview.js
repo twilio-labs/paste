82706aa361c3b44ec3db32595536d96c1fa740bf
richbachman <rb@richbachman.com>
2021-03-12 09:25:54 -0700

    chore: add dark mode theme to storybook theme selector

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -8,13 +8,13 @@ export const globalTypes = {
   theme: {
     name: 'Paste Theme',
     description: 'Global theme for components',
-    defaultValue: 'default',
+    defaultValue: 'dark',
     toolbar: {
       // All available icons
       // https://github.com/storybookjs/storybook/blob/master/lib/components/src/icon/icons.tsx
       icon: 'paintbrush',
       // array of plain string values or MenuItem shape (see below)
-      items: ['sendgrid', 'console', 'default'],
+      items: ['sendgrid', 'console', 'default', 'dark'],
     },
   },
 };
@@ -24,7 +24,7 @@ export const decorators = [
     const theme = context.globals.theme;
     return (
       <Theme.Provider theme={theme}>
-        <Box padding="space40">
+        <Box padding="space40" backgroundColor="colorBackgroundBody">
           <Story />
         </Box>
       </Theme.Provider>
