80e7e9d994a062c09cdae74d50f7f93a9edac333
Shadi <TheSisb@users.noreply.github.com>
2021-03-16 13:09:24 -0400

    chore(website): add tooltip vs popover video, some other cleanup (#1279)
    * chore(website): improve popover and tooltip examples
    
    * chore(website): add popover vs tooltip video
    
    * chore(website): add mention about no trailing slash to breadcrumbs
    
    * chore(website): use AspectRatio component and update icon title

--- packages/paste-website/src/component-examples/PopoverExamples.ts
+++ packages/paste-website/src/component-examples/PopoverExamples.ts
@@ -4,9 +4,13 @@ const PopoverExample = () => {
     <PopoverContainer baseId="popover-example">
       <PopoverButton variant="primary">✊ Action</PopoverButton>
       <Popover aria-label="Popover">
+        <Heading as="h3" variant="heading30">Heads up!</Heading>
+        <Separator orientation="horizontal" verticalSpacing="space50" />
         <Text as="span">
           Black Lives Matter. We stand with the Black community
         </Text>
+        <Separator orientation="horizontal" verticalSpacing="space50" />
+        <Button onClick={() => {}} variat="pimary">Agree</Button>
       </Popover>
     </PopoverContainer>
   );
