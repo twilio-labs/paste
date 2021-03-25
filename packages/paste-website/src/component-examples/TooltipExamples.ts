6d3ef1de6aaf8423251e215b40aaeac457e6cc81
Shadi <TheSisb@users.noreply.github.com>
2021-03-25 12:54:42 -0400

    chore(website): fixes three documentation issues (#1303)
    * chore: remove maintainers private link
    
    * chore: misc fixes
    
    * chore: pr feedback

--- packages/paste-website/src/component-examples/TooltipExamples.ts
+++ packages/paste-website/src/component-examples/TooltipExamples.ts
@@ -23,3 +23,23 @@ render(
   <StateHookExample />
 )
 `.trim();
+
+export const NonPasteChildComponentExample = `
+const FancyButton = React.forwardRef((props, ref) => (
+  <button ref={ref} {...props} style={{backgroundColor: 'white', border: '1px solid #000', color: '#000', borderRadius: '5px', padding: '5px'}}>
+    Fancy button: {props.children}
+  </button>
+));
+
+const NonPasteChildComponent = () => {
+  return (
+    <Tooltip text="Forwarding refs correctly!">
+      <FancyButton>Fancy!</FancyButton>
+    </Tooltip>
+  );
+};
+
+render(
+  <NonPasteChildComponent />
+)
+`.trim();
