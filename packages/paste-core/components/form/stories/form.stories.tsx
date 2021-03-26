9df657df7e4994dee1b0dfa9071f8f50041adb0c
Si Taggart <me@simontaggart.com>
2021-03-26 13:45:51 -0700

    fix(inline-control-group): v. spacing of child items remove value prop
    - Adjust the vertical spacing on inline control group items, so that we don't leave space below last child item
    - Remove the unused value prop and it's prop types

--- packages/paste-core/components/form/stories/form.stories.tsx
+++ packages/paste-core/components/form/stories/form.stories.tsx
@@ -1,7 +1,150 @@
 import * as React from 'react';
-import {useUIDSeed} from '@twilio-paste/uid-library';
+import {useUID, useUIDSeed} from '@twilio-paste/uid-library';
+import {Text} from '@twilio-paste/text';
 import {Box} from '@twilio-paste/box';
-import {Checkbox, FormLabel, FormInput, Select, Option, Radio, FormTextArea} from '../src';
+import {Grid, Column} from '@twilio-paste/grid';
+import {Anchor} from '@twilio-paste/anchor';
+import {Checkbox, FormLabel, FormInput, Select, Option, Radio, FormTextArea, CheckboxGroup, RadioGroup} from '../src';
+import type {FormInputProps, SelectProps, FormTextAreaProps, CheckboxGroupProps, RadioGroupProps} from '../src';
+
+const InputField: React.FC<Pick<FormInputProps, 'disabled' | 'hasError'>> = ({disabled, hasError}) => {
+  const inputIDSeed = useUIDSeed();
+  return (
+    <>
+      <FormLabel htmlFor={inputIDSeed('input')} disabled={disabled}>
+        Text Input
+      </FormLabel>
+      <FormInput id={inputIDSeed('input')} type="text" value="" disabled={disabled} hasError={hasError} />
+    </>
+  );
+};
+
+const TextareaField: React.FC<Pick<FormTextAreaProps, 'disabled' | 'hasError'>> = ({disabled, hasError}) => {
+  const inputIDSeed = useUIDSeed();
+  return (
+    <>
+      <FormLabel htmlFor={inputIDSeed('textarea')} disabled={disabled}>
+        Textarea textarea
+      </FormLabel>
+      <FormTextArea id={inputIDSeed('textarea')} value="" disabled={disabled} hasError={hasError} />
+    </>
+  );
+};
+
+const SelectField: React.FC<Pick<SelectProps, 'disabled' | 'hasError'>> = ({disabled, hasError}) => {
+  const inputIDSeed = useUIDSeed();
+  return (
+    <>
+      <FormLabel htmlFor={inputIDSeed('select')} disabled={disabled}>
+        Select Input
+      </FormLabel>
+      <Select id={inputIDSeed('select')} value="" onChange={() => {}} disabled={disabled} hasError={hasError}>
+        <Option value="1">Option</Option>
+      </Select>
+    </>
+  );
+};
+
+const CheckboxGroupField: React.FC<Pick<CheckboxGroupProps, 'orientation' | 'errorText'>> = ({
+  orientation,
+  errorText,
+}) => {
+  const [checked1, setChecked1] = React.useState(true);
+  const [checked2, setChecked2] = React.useState(false);
+  const [checked3, setChecked3] = React.useState(true);
+  return (
+    <CheckboxGroup
+      name="bar"
+      legend={
+        <Text as="span" color="currentColor">
+          This is some help text with a <Anchor href="http://paste.twilio.com">link</Anchor>.
+        </Text>
+      }
+      helpText="Help text should go here."
+      orientation={orientation}
+      errorText={errorText}
+      required
+    >
+      <Checkbox
+        id={useUID()}
+        value="1"
+        checked={checked1}
+        onChange={(event) => {
+          setChecked1(event.currentTarget.checked);
+        }}
+        helpText={
+          <Text as="span" color="currentColor">
+            This is some help text with a <Anchor href="http://paste.twilio.com">link</Anchor>.
+          </Text>
+        }
+      >
+        First
+      </Checkbox>
+      <Checkbox
+        checked={checked2}
+        onChange={(event) => {
+          setChecked2(event.currentTarget.checked);
+        }}
+        id={useUID()}
+        value="2"
+        helpText="This is some help text."
+      >
+        Second
+      </Checkbox>
+      <Checkbox
+        checked={checked3}
+        onChange={(event) => {
+          setChecked3(event.currentTarget.checked);
+        }}
+        id={useUID()}
+        value="3"
+        helpText="This is some help text."
+      >
+        Third
+      </Checkbox>
+    </CheckboxGroup>
+  );
+};
+
+const RadioGroupField: React.FC<Pick<RadioGroupProps, 'orientation' | 'errorText'>> = ({orientation, errorText}) => {
+  const [value, setValue] = React.useState('2');
+  return (
+    <RadioGroup
+      name="bar"
+      value={value}
+      legend={
+        <Text as="span" color="currentColor">
+          This is some help text with a <Anchor href="http://paste.twilio.com">link</Anchor>.
+        </Text>
+      }
+      helpText="Help text should go here."
+      required
+      errorText={errorText}
+      orientation={orientation}
+      onChange={(newValue) => {
+        setValue(newValue);
+      }}
+    >
+      <Radio
+        id={useUID()}
+        value="1"
+        helpText={
+          <Text as="span" color="currentColor">
+            This is some legend text with a <Anchor href="http://paste.twilio.com">link</Anchor>.
+          </Text>
+        }
+      >
+        First
+      </Radio>
+      <Radio id={useUID()} value="2" helpText="This is some help text.">
+        Second
+      </Radio>
+      <Radio id={useUID()} value="3" helpText="This is some help text.">
+        Third
+      </Radio>
+    </RadioGroup>
+  );
+};
 
 // eslint-disable-next-line import/no-default-export
 export default {
@@ -13,14 +156,19 @@ export const All = (): React.ReactNode => {
   return (
     <>
       <Box marginBottom="space70">
-        <FormLabel htmlFor={inputIDSeed('input')}>Text Input</FormLabel>
-        <FormInput id={inputIDSeed('input')} type="text" value="" />
-        <FormLabel htmlFor={inputIDSeed('textarea')}>Textarea textarea</FormLabel>
-        <FormTextArea id={inputIDSeed('textarea')} value="" />
-        <FormLabel htmlFor={inputIDSeed('select')}>Select Input</FormLabel>
-        <Select id={inputIDSeed('select')} value="" onChange={() => {}}>
-          <Option value="1">Option</Option>
-        </Select>
+        <InputField />
+        <Grid>
+          <Column>
+            <CheckboxGroupField />
+          </Column>
+          <Column>
+            <RadioGroupField />
+          </Column>
+        </Grid>
+        <TextareaField />
+        <CheckboxGroupField orientation="horizontal" />
+        <RadioGroupField orientation="horizontal" />
+        <SelectField />
         <Checkbox id={inputIDSeed('chcekbox1')} value="1" name="foo">
           Label
         </Checkbox>
@@ -35,20 +183,9 @@ export const All = (): React.ReactNode => {
         </Radio>
       </Box>
       <Box marginBottom="space70">
-        <FormLabel disabled htmlFor={inputIDSeed('disabledinput')}>
-          Text Input
-        </FormLabel>
-        <FormInput disabled id={inputIDSeed('disabledinput')} type="text" value="" />
-        <FormLabel disabled htmlFor={inputIDSeed('disabledtextarea')}>
-          Textarea Input
-        </FormLabel>
-        <FormTextArea disabled id={inputIDSeed('disabledtextarea')} value="" />
-        <FormLabel disabled htmlFor={inputIDSeed('disabledselect')}>
-          Select Input
-        </FormLabel>
-        <Select disabled id={inputIDSeed('disabledselect')} value="" onChange={() => {}}>
-          <Option value="1">Option</Option>
-        </Select>
+        <InputField disabled />
+        <TextareaField disabled />
+        <SelectField disabled />
         <Checkbox disabled id={inputIDSeed('disabledcheckbox1')} value="1" name="foo">
           Label
         </Checkbox>
@@ -63,14 +200,19 @@ export const All = (): React.ReactNode => {
         </Radio>
       </Box>
       <Box marginBottom="space70">
-        <FormLabel htmlFor={inputIDSeed('errorinput')}>Text Input</FormLabel>
-        <FormInput hasError id={inputIDSeed('errorinput')} type="text" value="" />
-        <FormLabel htmlFor={inputIDSeed('errortextarea')}>Textarea Input</FormLabel>
-        <FormTextArea hasError id={inputIDSeed('errortextarea')} value="" />
-        <FormLabel htmlFor={inputIDSeed('errorselect')}>Select Input</FormLabel>
-        <Select hasError id={inputIDSeed('errorselect')} value="" onChange={() => {}}>
-          <Option value="1">Option</Option>
-        </Select>
+        <InputField hasError />
+        <Grid>
+          <Column>
+            <CheckboxGroupField errorText="this is an error" />
+          </Column>
+          <Column>
+            <RadioGroupField errorText="this is an error" />
+          </Column>
+        </Grid>
+        <TextareaField hasError />
+        <CheckboxGroupField orientation="horizontal" errorText="this is an error" />
+        <RadioGroupField orientation="horizontal" errorText="this is an error" />
+        <SelectField hasError />
         <Checkbox hasError id={inputIDSeed('errorcheck1')} value="1" name="foo">
           Label
         </Checkbox>
