d2850afe2b0dd1aa3ebaf91f142d2ae56c1c71ed
Sarah Li <sali@twilio.com>
2021-03-30 14:34:19 -0700

    feat: add Data, Show, Hide icons and fix viewbox errors

---
+++ packages/paste-icons/src/ShowIcon.tsx
@@ -0,0 +1,45 @@
+/**
+ * This file was automatically generated with @twilio-labs/svg-to-react
+ */
+import * as React from 'react';
+import {useUID} from '@twilio-paste/uid-library';
+import {IconWrapper} from './helpers/IconWrapper';
+import type {IconWrapperProps} from './helpers/IconWrapper';
+
+export interface ShowIconProps extends IconWrapperProps {
+  title?: string;
+  decorative: boolean;
+}
+
+const ShowIcon: React.FC<ShowIconProps> = ({as, display, size, color, title, decorative}) => {
+  const titleId = `ShowIcon-${useUID()}`;
+
+  if (!decorative && title == null) {
+    throw new Error('[ShowIcon]: Missing a title for non-decorative icon.');
+  }
+
+  return (
+    <IconWrapper as={as} display={display} size={size} color={color}>
+      <svg
+        role="img"
+        aria-hidden={decorative}
+        width="100%"
+        height="100%"
+        viewBox="0 0 20 20"
+        fill="none"
+        aria-labelledby={titleId}
+      >
+        {title ? <title id={titleId}>{title}</title> : null}
+        <path
+          fill="currentColor"
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M10.107 4c3.252 0 6.477 2.268 8.425 4.41a1.806 1.806 0 01.002 2.424c-1.949 2.145-5.174 4.414-8.427 4.414h-.234c-3.236 0-6.46-2.27-8.405-4.413a1.806 1.806 0 01-.002-2.423C3.436 6.244 6.716 3.963 10 4h.004L10.107 4zm-.016 10.123c2.903 0 5.824-2.08 7.609-4.044a.683.683 0 00-.002-.914c-1.783-1.961-4.705-4.04-7.589-4.04H9.889c-2.885 0-5.805 2.079-7.589 4.042a.683.683 0 00.002.914c1.802 1.985 4.763 4.091 7.687 4.041l.102.001zm-.092-7.874H10a3.373 3.373 0 013.373 3.373v.003A3.379 3.379 0 0110 12.998h-.002a3.375 3.375 0 010-6.75zm1.59 4.965c.422-.422.659-.994.66-1.59v-.002A2.249 2.249 0 0010 7.374h-.002a2.25 2.25 0 000 4.5c.597-.002 1.168-.239 1.59-.66z"
+        />
+      </svg>
+    </IconWrapper>
+  );
+};
+
+ShowIcon.displayName = 'ShowIcon';
+export {ShowIcon};
--- packages/paste-icons/src/WarningIcon.tsx
+++ packages/paste-icons/src/WarningIcon.tsx
@@ -20,12 +20,21 @@ const WarningIcon: React.FC<WarningIconProps> = ({as, display, size, color, titl
 
   return (
     <IconWrapper as={as} display={display} size={size} color={color}>
-      <svg role="img" aria-hidden={decorative} width="100%" height="100%" viewBox="0 0 20 20" aria-labelledby={titleId}>
+      <svg
+        role="img"
+        aria-hidden={decorative}
+        width="100%"
+        height="100%"
+        viewBox="0 0 20 20"
+        fill="none"
+        aria-labelledby={titleId}
+      >
         {title ? <title id={titleId}>{title}</title> : null}
         <path
           fill="currentColor"
           fillRule="evenodd"
-          d="M8.684 3.744c.582-.992 2.05-.992 2.632 0l6.482 11.048c.577.984-.152 2.208-1.316 2.208H3.518c-1.164 0-1.893-1.224-1.316-2.208zM10 13a1 1 0 100 2 1 1 0 000-2zm.01-5h-.02l-.112.007c-.549.067-.955.626-.866 1.237l.428 2.238.02.095c.074.247.29.423.543.423l.091-.008a.59.59 0 00.466-.51l.428-2.238.011-.121c.025-.602-.416-1.116-.989-1.123z"
+          clipRule="evenodd"
+          d="M17.798 14.791L11.316 3.744c-.582-.992-2.05-.992-2.632 0L2.202 14.79C1.625 15.776 2.354 17 3.518 17h12.964c1.164 0 1.893-1.224 1.316-2.209zM9.99 8h.02c.611.007 1.073.592.978 1.244l-.428 2.238c-.04.296-.278.518-.557.518h-.006c-.279 0-.516-.222-.557-.518l-.428-2.238C8.917 8.592 9.386 8 9.99 8zm.01 7a1 1 0 100-2 1 1 0 000 2z"
         />
       </svg>
     </IconWrapper>
