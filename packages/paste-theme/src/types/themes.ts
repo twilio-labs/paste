22de2c8bfef5be5b9f6e7dd2ce6bd020eb56d6cb
Si Taggart <me@simontaggart.com>
2021-04-28 18:14:19 -0700

    feat(theme): add the dark theme

--- packages/paste-theme/src/types/themes.ts
+++ packages/paste-theme/src/types/themes.ts
@@ -1,8 +1,10 @@
-import {DefaultTheme} from '../themes/default';
-import {SendGridTheme} from '../themes/sendgrid';
-import {ConsoleTheme} from '../themes/console';
+import type {DefaultTheme} from '../themes/default';
+import type {SendGridTheme} from '../themes/sendgrid';
+import type {ConsoleTheme} from '../themes/console';
+import type {DarkTheme} from '../themes/dark';
 
 export type SendGridThemeShape = typeof SendGridTheme;
 export type ConsoleThemeShape = typeof ConsoleTheme;
+export type DarkThemeShape = typeof DarkTheme;
 export type ThemeShape = typeof DefaultTheme;
 export type GenericThemeShape = keyof ThemeShape;
