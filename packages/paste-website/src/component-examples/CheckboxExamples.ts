c16a62e19d920ad6f6df2b4d50dd6649a792afb8
richbachman <rb@richbachman.com>
2021-05-13 12:36:45 -0600

    fix(website): update checkbox doc page with controlled examples

--- packages/paste-website/src/component-examples/CheckboxExamples.ts
+++ packages/paste-website/src/component-examples/CheckboxExamples.ts
@@ -1,14 +1,119 @@
+export const singleCheckboxExample = `
+const SingleCheckbox = () => {
+  const [checked, setChecked] = React.useState(true);
+
+  return (
+    <Checkbox
+      checked={checked}
+      id="blm"
+      value="blm"
+      name="blm"
+      onChange={(event) => {
+        setChecked(event.target.checked);
+      }}
+    >
+      Black lives matter
+    </Checkbox>
+  );
+};
+
+render(
+  <SingleCheckbox />
+)
+`.trim();
+
+export const helpTextCheckboxExample = `
+const CheckboxHelpText = () => {
+  const [checked, setChecked] = React.useState(true);
+
+  return (
+    <Checkbox
+      id="enabled"
+      value="enabled"
+      name="enabled"
+      helpText="Determines if certificate validation is performed on all Twilio originated requests"
+      checked={checked}
+      onChange={(event) => {
+        setChecked(event.target.checked);
+      }}
+    >
+      Enable SSL Certificate Validation
+    </Checkbox>
+  );
+};
+
+render(
+  <CheckboxHelpText />
+)
+`.trim();
+
+export const requiredCheckboxExample = `
+const RequiredCheckbox = () => {
+  const [checked, setChecked] = React.useState(true);
+
+  return (
+    <Checkbox
+      id="delete"
+      value="delete"
+      name="delete"
+      checked={checked}
+      onChange={(event) => {
+        setChecked(event.target.checked);
+      }}
+      required
+    >
+      Confirm this message should be deleted
+    </Checkbox>
+  );
+};
+
+render(
+  <RequiredCheckbox />
+)
+`.trim();
+
+export const checkboxDisclaimerExample = `
+const CheckboxDisclaimerText = () => {
+  const [checked, setChecked] = React.useState(true);
+
+  return (
+    <CheckboxDisclaimer
+      id="disclaimer"
+      value="disclaimer"
+      name="disclaimer"
+      checked={checked}
+      onChange={(event) => {
+        setChecked(event.target.checked);
+      }}
+    >
+      <Text as="span">
+        I declare the information provided above is accurate. I acknowledge that Twilio will process the information provided above for the purpose of identity verification, and will be sharing it with my local telecomm providers or authorities where required by local law. I understand that Twilio phone numbers may be taken out of service for inaccurate or false information.
+      </Text>
+    </CheckboxDisclaimer>
+  );
+};
+
+render(
+  <CheckboxDisclaimerText />
+)
+`.trim();
+
 export const defaultExample = `
 const ActiveDesktopClients = () => {
+  const [checked1, setChecked1] = React.useState(false);
+  const [checked2, setChecked2] = React.useState(false);
+  const handleChange1 = (event) => setChecked1(event.target.checked);
+  const handleChange2 = (event) => setChecked2(event.target.checked);
+
   return (
     <CheckboxGroup
       name="desktop"
       legend="Select the clients you would like to test."
     >
-      <Checkbox id="apple_mail" value="apple_mail">
+      <Checkbox id="apple_mail" value="apple_mail" checked={checked1 === true} onChange={handleChange1}>
         Apple Mail
       </Checkbox>
-      <Checkbox id="outlook" value="outlook">
+      <Checkbox id="outlook" value="outlook" checked={checked2 === true} onChange={handleChange2}>
         Outlook
       </Checkbox>
     </CheckboxGroup>
@@ -22,6 +127,11 @@ render(
 
 export const helpTextExample = `
 const AdPartners = () => {
+  const [checked1, setChecked1] = React.useState(false);
+  const [checked2, setChecked2] = React.useState(false);
+  const handleChange1 = (event) => setChecked1(event.target.checked);
+  const handleChange2 = (event) => setChecked2(event.target.checked);
+
   return (
     <CheckboxGroup
       name="ad_partners"
@@ -29,6 +139,8 @@ const AdPartners = () => {
       helpText="Select at least 1 ad partner to create a campaign."
     >
       <Checkbox
+        checked={checked1 === true}
+        onChange={handleChange1}
         id="facebook"
         value="facebook"
         helpText={
@@ -40,6 +152,8 @@ const AdPartners = () => {
         Facebook
       </Checkbox>
       <Checkbox
+        checked={checked2 === true}
+        onChange={handleChange2}
         id="instagram"
         value="instagram"
         helpText={
@@ -104,16 +218,33 @@ render(
 
 export const errorExample = `
 const APIPermissions = () => {
+  const [checked1, setChecked1] = React.useState(true);
+  const [checked2, setChecked2] = React.useState(false);
+
   return (
     <CheckboxGroup
       name="api"
       legend="API Key Permissions"
       errorText="Please select at least one option."
     >
-      <Checkbox id="full_access" value="full_access">
+      <Checkbox
+        id="full_access"
+        value="full_access"
+        checked={checked1}
+        onChange={(event) => {
+          setChecked1(event.currentTarget.checked);
+        }}
+      >
         Full Access
       </Checkbox>
-      <Checkbox id="restricted_access" value="restricted_access">
+      <Checkbox
+        id="restricted_access"
+        value="restricted_access"
+        checked={checked2}
+        onChange={(event) => {
+          setChecked2(event.currentTarget.checked);
+        }}
+      >
         Restricted Access
       </Checkbox>
     </CheckboxGroup>
@@ -127,6 +258,10 @@ render(
 
 export const requiredExample = `
 const EmailNotifications = () => {
+  const [checked1, setChecked1] = React.useState(true);
+  const [checked2, setChecked2] = React.useState(false);
+  const [checked3, setChecked3] = React.useState(false);
+
   return (
     <CheckboxGroup
       name="email"
@@ -134,13 +269,34 @@ const EmailNotifications = () => {
       helpText="We can remind yuou when one of your Buffer is looking a little empty."
       required
     >
-      <Checkbox id="empty_buffer" value="empty_buffer">
+      <Checkbox
+        id="empty_buffer"
+        value="empty_buffer"
+        checked={checked1}
+        onChange={(event) => {
+          setChecked1(event.currentTarget.checked);
+        }}
+      >
         Empty Buffer
       </Checkbox>
-      <Checkbox id="newsletter" value="newsletter">
+      <Checkbox
+        id="newsletter"
+        value="newsletter"
+        checked={checked2}
+        onChange={(event) => {
+          setChecked2(event.currentTarget.checked);
+        }}
+      >
         Newsletter
       </Checkbox>
-      <Checkbox id="update_failures" value="update_failures">
+      <Checkbox
+        id="update_failures"
+        value="update_failures"
+        checked={checked3}
+        onChange={(event) => {
+          setChecked3(event.currentTarget.checked);
+        }}
+      >
         Update Failures
       </Checkbox>
     </CheckboxGroup>
