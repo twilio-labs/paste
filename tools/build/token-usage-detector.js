11169d3583ead231815ab5a3bda90d96a4ef5763
TheSisb <shadiisber@gmail.com>
2021-06-01 13:57:28 -1000

    fix: rebase and fix algorithm for minimalPackageSet

--- tools/build/token-usage-detector.js
+++ tools/build/token-usage-detector.js
@@ -2,16 +2,17 @@ const path = require('path');
 const lodash = require('lodash');
 const {fileSearch} = require('search-in-file');
 const tokenJson = require('@twilio-paste/design-tokens/dist/tokens.json');
-const PastePackages = require('../.cache/packages.json');
 const {writeToFile} = require('../utils/writeToFile');
 
 const CACHE_FILE_PATH = path.join(__dirname, '../.cache/token-usage.json');
 const CORE_PACKAGES_PATH = path.join(__dirname, '../../packages/paste-core');
 const tokenNames = Object.keys(tokenJson).map(lodash.camelCase);
 
-// Finds a given full word in all of paste's core packages (minus core-bundle)
-// Shell CLI version
-// // grep -rnw --include=\*.{ts,tsx} --exclude-dir={dist,core-bundle,stories,__tests__} 'packages/paste-core/' -e 'colorBackgroundBody'
+/**
+ * Finds a given full word in all of paste's core packages (minus core-bundle)
+ * Shell CLI version:
+ * grep -rnw --include=\*.{ts,tsx} --exclude-dir={dist,core-bundle,stories,__tests__} 'packages/paste-core/' -e 'colorBackgroundBody'
+ */
 async function findFilesWithWord(word) {
   return fileSearch([CORE_PACKAGES_PATH], word, {
     recursive: true,
@@ -20,102 +21,129 @@ async function findFilesWithWord(word) {
   });
 }
 
-// Takes a list of file paths and converts them to a list of package names
-function convertPathsToPackageNames(fileList) {
-  // Using a set for its unique item property
-  const packageNames = new Set();
-
-  // For the list of files, get the package name
-  fileList.map(filePath => {
-    const packageObj = PastePackages.find(({location}) => filePath.includes(location));
-    packageNames.add(packageObj.name);
+/**
+ * Takes a list of file paths and converts them to a list of deduped package names
+ * @param {string[]} filePaths - Files found to have contained tokens
+ * @returns {string[]} packageNames - The package names of the files passed in, deduped.
+ */
+function convertFilePathsToPackageNames(filePaths) {
+  const packageNames = filePaths.map((filePath) => {
+    const componentPath = filePath.slice(filePath.indexOf('/paste-core/'));
+    const componentName = componentPath.split('/')[3];
+    return `@twilio-paste/${componentName}`;
   });
-
-  // Return array of package names
-  return [...packageNames];
+  return [...new Set(packageNames)];
 }
 
-(async () => {
-  const resultMap = {};
+/**
+ * Returns an object that maps token name keys to a list of package names where that token is used in paste-core.
+ * @returns {string[]} tokenPackageMap - {[tokenName]: packageNames[]}
+ */
+async function getTokenPackageMapping() {
+  const tokenPackageMap = {};
+
   // eslint-disable-next-line no-restricted-syntax
   for (const tokenName of tokenNames) {
     // eslint-disable-next-line no-await-in-loop
-    const fileList = await findFilesWithWord(tokenName);
-    resultMap[tokenName] = convertPathsToPackageNames(fileList);
+    const filesPathsThatContainToken = await findFilesWithWord(tokenName);
+    tokenPackageMap[tokenName] = convertFilePathsToPackageNames(filesPathsThatContainToken);
   }
 
-  // Get not-in-use tokens
-  const unusedTokenList = [];
-  Object.keys(resultMap).forEach(tokenName => {
-    if (resultMap[tokenName].length === 0) {
-      unusedTokenList.push(tokenName);
-    }
-  });
-  // console.log("These tokens aren't used: ", unusedTokenList);
+  return tokenPackageMap;
+}
 
-  // Get minimal components using all tokens
-  // Process:
-  /*
-   * First split up the resultMap into two lists, one with 1 value and another for >1 value
-   * loop through the one item list first adding it to the set
-   * loop through the multiple item value second and check if any are on the set
-   * if something is on the set, skip
-   * if none are on the set, add to _BEST GUESS MAP_
-   * shape: {
-   *   pkg: count,
-   *   pkg2: count2,
-   *   ...
-   * }
-   * Lastly use this best guess map to add the top used package to the set
-   * then regen the best guess map
-   * loop until best guess is empty
-   */
+/**
+ * Returns a list of our unused tokens from within paste-core.
+ * @param {Object} tokenPackageMap - A token to package mapping
+ * @returns {string[]} unusedTokenList - a list of all the unused token names in paste-core.
+ */
+function getUnusedTokenList(tokenPackageMap) {
+  const unusedTokenList = [];
+  for (const [key, value] of Object.entries(tokenPackageMap)) {
+    if (value.length === 0) {
+      unusedTokenList.push(key);
+    }
+  }
+  return unusedTokenList;
+}
+
+/**
+ * Get minimal components using all tokens
+ * Algorithm:
+ *
+ * 1- Split up the resultMap into two lists, one with 1 item in the array and another for >1 items
+ * 2- loop through the one item list, adding it to a set of package names.
+ * 3- loop through the multiple item list and check if any are on the set. Remember, this mapping is
+ *    from 1 tokenName to n packages, so we just need 1 of the packages to be listed.
+ *  3.1- if one packageName is already on the set, skip this token. It's covered.
+ *  3.2- if none of the packageNames are on the set already, add it to a _BEST GUESS MAP_
+ *    shape: {
+ *     pkg: count,
+ *     pkg2: count2,
+ *     ...
+ *   }
+ * 4- Lastly use this best guess map to add the top used package to the set
+ * 5- Repeat step 3 to 4 until best guess map is empty
+ */
+function getMinimalPackageList(tokenPackageMap) {
   const minimalUsedPackageSet = new Set();
 
-  const ONE_ITEM_RESULT_MAP = lodash.omitBy(resultMap, value => value.length === 0 || value.length > 1);
-  const MULTI_ITEM_RESULT_MAP = lodash.omitBy(resultMap, value => value.length < 2);
-  // console.log('one', ONE_ITEM_RESULT_MAP);
-  // console.log('multi', MULTI_ITEM_RESULT_MAP);
+  // Step 1
+  const ONE_ITEM_RESULT_MAP = lodash.omitBy(tokenPackageMap, (value) => value.length === 0 || value.length > 1);
+  const MULTI_ITEM_RESULT_MAP = lodash.omitBy(tokenPackageMap, (value) => value.length < 2);
 
-  Object.values(ONE_ITEM_RESULT_MAP).forEach(packageList => {
+  // Step 2
+  Object.values(ONE_ITEM_RESULT_MAP).forEach((packageList) => {
     minimalUsedPackageSet.add(packageList[0]);
   });
-  // console.log('pkglist at 1', minimalUsedPackageSet);
 
-  const bestGuessMapper = {};
-  Object.values(MULTI_ITEM_RESULT_MAP).forEach(packageList => {
-    let isAnyInSet = false;
-    packageList.forEach(packageName => {
-      if (minimalUsedPackageSet.has(packageName)) {
-        isAnyInSet = true;
+  let bestGuessMapper = {};
+  do {
+    bestGuessMapper = {};
+    // Step 3
+    Object.values(MULTI_ITEM_RESULT_MAP).forEach((packageList) => {
+      const isAnyInSet = packageList.some((packageName) => {
+        return minimalUsedPackageSet.has(packageName);
+      });
+      // If the token has a packageList with none in the set
+      // Add or increment the count in bestGuessMapper
+      // Step 3.1 and 3.2
+      if (!isAnyInSet) {
+        packageList.forEach((packageName) => {
+          if (!bestGuessMapper[packageName]) {
+            bestGuessMapper[packageName] = 1;
+          } else {
+            bestGuessMapper[packageName] += 1;
+          }
+        });
       }
     });
-    if (!isAnyInSet) {
-      packageList.forEach(packageName => {
-        if (!bestGuessMapper[packageName]) {
-          bestGuessMapper[packageName] = 1;
-        } else {
-          bestGuessMapper[packageName] += 1;
-        }
-      });
-    }
-  });
 
-  // TODO this edge case hasnt happened yet so i stopped coding here
-  // console.log('bestguess', bestGuessMapper);
-  if (Object.keys(bestGuessMapper).length > 0) {
-    console.log(
-      "[ERROR]: BestGuessMapper isn't empty anymore. Finish coding this function to find the real minimum package list!"
-    );
-  }
-  // console.log('minimal set of packages: ', [...usedPackageSet]);
+    // Step 4
+    if (!lodash.isEmpty(bestGuessMapper)) {
+      const sortedEntriesByVal = Object.entries(bestGuessMapper).sort(([, v1], [, v2]) => v2 - v1);
+      // first zero for first array entry which is sorted to the largest value
+      // second zero for the array index of the key (i.e.: ["@twilio-core/button", 5]), which is the package name
+      const mostUsedPackage = sortedEntriesByVal[0][0];
+      minimalUsedPackageSet.add(mostUsedPackage);
+    }
+    // Step 5
+  } while (!lodash.isEmpty(bestGuessMapper));
+
+  return [...minimalUsedPackageSet];
+}
+
+(async () => {
+  const tokenPackageMap = await getTokenPackageMapping();
+  const unusedTokenList = getUnusedTokenList(tokenPackageMap);
+  const minimalUsedPackageList = getMinimalPackageList(tokenPackageMap);
 
   writeToFile(
     CACHE_FILE_PATH,
     {
       unusedTokenList,
-      minimalUsedPackageSet: [...minimalUsedPackageSet],
-      tokenPackageMap: resultMap,
+      minimalUsedPackageList,
+      tokenPackageMap: tokenPackageMap,
     },
     {
       formatJson: true,
