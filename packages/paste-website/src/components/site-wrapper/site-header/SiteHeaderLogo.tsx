0116718b01e8cc9f431ce5fcc37d277c6ae400c7
Katie Porter <katie@janekporter.com>
2021-06-14 12:56:35 -0600

    feat(website): update to regular paste logo

--- packages/paste-website/src/components/site-wrapper/site-header/SiteHeaderLogo.tsx
+++ packages/paste-website/src/components/site-wrapper/site-header/SiteHeaderLogo.tsx
@@ -5,8 +5,8 @@ import {Text} from '@twilio-paste/text';
 import {Box} from '@twilio-paste/box';
 import {MediaObject, MediaBody, MediaFigure} from '@twilio-paste/media-object';
 import {styled} from '@twilio-paste/styling-library';
-// import {useTheme} from '@twilio-paste/theme';
-import {PasteIconPride} from '../../icons/PasteIconPride';
+import {useTheme} from '@twilio-paste/theme';
+import {PasteIconInverse} from '../../icons/PasteIconInverse';
 
 const StyledLink = styled(Link)`
   text-decoration: none;
@@ -25,7 +25,7 @@ interface SiteHeaderLogoProps {
 // Note: 'subtitle' isn't passed for the mobile view, so we use that fact
 // to render different sizes and spacing in mobile
 const SiteHeaderLogo: React.FC<SiteHeaderLogoProps> = ({title, subtitle}) => {
-  // const theme = useTheme();
+  const theme = useTheme();
   return (
     <Box
       display="flex"
@@ -39,8 +39,7 @@ const SiteHeaderLogo: React.FC<SiteHeaderLogoProps> = ({title, subtitle}) => {
     >
       <MediaObject verticalAlign="center">
         <MediaFigure spacing={subtitle ? 'space40' : 'space0'}>
-          {/* <PasteIconInverse color={theme.textColors.colorTextInverse} display="block" size={42} /> */}
-          <PasteIconPride display="block" size={42} />
+          <PasteIconInverse color={theme.textColors.colorTextInverse} display="block" size={42} />
         </MediaFigure>
         <MediaBody>
           <Text as="div" fontSize="fontSize40" lineHeight="lineHeight40" color="colorTextInverse">
