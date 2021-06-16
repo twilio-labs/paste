cd9fb82ea01370a2476c2e73ffd3dc1902452ea8
Si Taggart <me@simontaggart.com>
2021-06-15 18:21:35 -0700

    fix: prettier

--- packages/paste-website/src/components/page-components/customization/customization-provider/FontCallout.tsx
+++ packages/paste-website/src/components/page-components/customization/customization-provider/FontCallout.tsx
@@ -6,9 +6,9 @@ export const FontCallout: React.FC = () => {
     <Callout variant="warning">
       <CalloutTitle as="h2">Don&rsquo;t forget to load your custom fonts</CalloutTitle>
       <CalloutText>
-        Paste only handles loading the Inter font. If you override the font family to a custom font, be sure you are loading the
-        font files into the web page correctly. Depending on your application and font provider, the method by which you
-        do this will be specific to you.
+        Paste only handles loading the Inter font. If you override the font family to a custom font, be sure you are
+        loading the font files into the web page correctly. Depending on your application and font provider, the method
+        by which you do this will be specific to you.
       </CalloutText>
     </Callout>
   );
