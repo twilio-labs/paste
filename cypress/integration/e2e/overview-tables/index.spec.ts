f67d1f1b959030736f07b56dc6e8ff6b7f4e550d
richbachman <rb@richbachman.com>
2021-06-24 12:14:42 -0600

    chore(website): add comments to overview cypress tests

--- cypress/integration/e2e/overview-tables/index.spec.ts
+++ cypress/integration/e2e/overview-tables/index.spec.ts
@@ -1,7 +1,7 @@
 describe('Pattern overview table', () => {
   it('checks all links in table', () => {
     cy.visit('/patterns');
-
+    // Using :not for pages that don't exist yet. We need to make sure to keep this updated when new pages/packages are added.
     cy.get('[data-cy=overview-table] a:not([href*="/patterns/notifications-and-feedback"])').each((page) => {
       cy.request(page.prop('href'));
     });
@@ -11,7 +11,7 @@ describe('Pattern overview table', () => {
 describe('Components overview table', () => {
   it('checks all links in table', () => {
     cy.visit('/components');
-
+    // Using :not for pages that don't exist yet. We need to make sure to keep this updated when new pages/packages are added.
     cy.get(
       '[data-cy=overview-table] a:not([href*="/components/help-text"]):not([href*="/components/form-key"]):not([href*="/components/label"]):not([href*="/components/time-picker"])'
     ).each((page) => {
