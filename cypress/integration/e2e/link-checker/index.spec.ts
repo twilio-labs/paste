4b2acb99df23f66b5aca33c302e7ce3263b0b2a1
Nora Krantz <nkrantz@twilio.com>
2021-07-13 18:43:21 -0400

    chore(time-picker): remove time-picker from link checker ignore list

--- cypress/integration/e2e/link-checker/index.spec.ts
+++ cypress/integration/e2e/link-checker/index.spec.ts
@@ -14,7 +14,6 @@ const IGNORE_LIST = [
   // Left these in there because they're being called from the sidebar nav.
   // That will need to be refactored to pull from AirTable instead of packages.
   'primitives/sibling-box',
-  'components/time-picker',
   '/__/', // I don't know where this is being picked up
 ];
 
