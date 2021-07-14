750a38e05b9d91f82b14f49a160112f013629468
Simon Taggart <staggart@twilio.com>
2021-07-13 18:34:15 -0700

    fix(tooltip): prevent child props being overridden/swallowed by tooltip (#1655)
    * fix(tooltip): prevent child props being overridden/swallowed by tooltip
    
    * fix(tooltip): type issues in tests
    
    * test(tooltip): add visibility tests when using trigger event handlers
    
    * test(tooltip): fix visibility tests

--- packages/paste-core/components/tooltip/__test__/index.spec.tsx
+++ packages/paste-core/components/tooltip/__test__/index.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {render, screen} from '@testing-library/react';
+import {act, fireEvent, render, screen} from '@testing-library/react';
 import {Button} from '@twilio-paste/button';
 import {Theme} from '@twilio-paste/theme';
 // @ts-ignore typescript doesn't like js imports
@@ -39,7 +39,7 @@ describe('Tooltip', () => {
       if (ButtonOne === null || ButtonTwo === null || tooltip === null) {
         return;
       }
-      expect(tooltip.getAttribute('hidden')).toBeDefined();
+      expect(tooltip.getAttribute('hidden')).not.toBeNull();
 
       ButtonOne.click();
       tooltip = screen.queryByTestId('state-hook-tooltip');
@@ -53,7 +53,40 @@ describe('Tooltip', () => {
       if (tooltip === null) {
         return;
       }
-      expect(tooltip.getAttribute('hidden')).toBeDefined();
+      expect(tooltip.getAttribute('hidden')).not.toBeNull();
+    });
+  });
+
+  describe('Children', () => {
+    it('should not override child provided events such as onBlur or onFocus', async () => {
+      const focusHandlerMock: jest.Mock = jest.fn();
+      const clickHandlerMock: jest.Mock = jest.fn();
+      render(
+        <Theme.Provider theme="console">
+          <Tooltip text="Welcome to Paste!" data-testid="tooltip-children-example">
+            <Button variant="primary" onFocus={focusHandlerMock} onClick={clickHandlerMock}>
+              Open Tooltip
+            </Button>
+          </Tooltip>
+        </Theme.Provider>
+      );
+      const tooltip = screen.getByTestId('tooltip-children-example');
+
+      expect(tooltip.getAttribute('hidden')).not.toBeNull();
+
+      await act(async () => {
+        await screen.getByRole('button').focus();
+      });
+
+      expect(tooltip.getAttribute('hidden')).toBeNull();
+
+      await act(async () => {
+        // @ts-expect-error yes, I know activeElement MIGHT be null, but it's not, OK?
+        await fireEvent.click(document.activeElement);
+      });
+
+      expect(focusHandlerMock).toHaveBeenCalled();
+      expect(clickHandlerMock).toHaveBeenCalled();
     });
   });
 
