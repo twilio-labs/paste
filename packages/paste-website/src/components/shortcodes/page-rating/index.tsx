82e18b7e9c04b45e685a42800ccfbbc17e2922e8
TheSisb <shadiisber@gmail.com>
2021-07-15 12:35:07 -0500

    chore: checkpoint

--- packages/paste-website/src/components/shortcodes/page-rating/index.tsx
+++ packages/paste-website/src/components/shortcodes/page-rating/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import {Heading} from '@twilio-paste/heading';
 import {Text} from '@twilio-paste/text';
+import {Card} from '@twilio-paste/card';
+import {Paragraph} from '@twilio-paste/paragraph';
 import {Box} from '@twilio-paste/box';
 import {Anchor} from '@twilio-paste/anchor';
 import {Button} from '@twilio-paste/button';
@@ -28,65 +30,72 @@ interface PageRatingProps {
 }
 
 export const PageRating: React.FC<PageRatingProps> = ({data}) => {
-  const [pageRated, setPageRated] = React.useState<boolean>(SimpleStorage.get(window.location.pathname) != null);
+  const [pageRating, setPageRating] = React.useState<string>(SimpleStorage.get(window.location.pathname) || '');
   const absolutePath = data.fileAbsolutePath;
   const filename = absolutePath.slice(Math.max(0, absolutePath.lastIndexOf('/') + 1));
   const gitHubURL = `${gitHubPagesPath + data.frontmatter.slug}/${filename}`;
 
   const likePage = (): void => {
     SimpleStorage.set(window.location.pathname, 'like');
-    setPageRated(true);
+    setPageRating('like');
     trackCustomEvent({
-      category: 'Doc Page Rating',
-      action: 'click-rating',
-      label: 'Like',
+      category: 'page-rating',
+      label: window.location.pathname,
+      action: 'like-page',
     });
   };
   const dislikePage = (): void => {
     SimpleStorage.set(window.location.pathname, 'dislike');
-    setPageRated(true);
+    setPageRating('dislike');
     trackCustomEvent({
-      category: 'Doc Page Rating',
-      action: 'click-rating',
-      label: 'Dislike',
+      category: 'page-rating',
+      label: window.location.pathname,
+      action: 'dislike-page',
     });
   };
 
   return (
     <Box marginTop="space160">
-      <Heading as="h2" variant="heading20">
-        Share your feedback
-      </Heading>
-      <Text as="p" fontSize="fontSize30">
-        Let us know how we’re doing! Please rate this page:
-        <br />
-        {pageRated ? (
-          <Text as="span">
-            You’re the best! Thanks for helping us improve.{' '}
-            <span role="img" aria-label="raised hands emoji">
-              🙌
-            </span>
+      <Card>
+        <Heading as="h2" variant="heading20">
+          Share your feedback
+        </Heading>
+        <Text as="p" marginBottom="space50">
+          Let us know how we’re doing! Please rate this page:
+        </Text>
+        <Box as="div" marginBottom="space70">
+          {pageRating != '' ? (
+            <Text as="span">
+              You’re the best! Thanks for helping us improve.{' '}
+              <span role="img" aria-label="raised hands emoji">
+                🙌
+              </span>
+            </Text>
+          ) : (
+            <>
+              <Button variant="link" onClick={likePage}>
+                <ThumbsUpIcon size="sizeIcon50" decorative="false" title="Like this page" />
+                It's useful!
+              </Button>
+              <Box as="span" marginLeft="space60">
+                <Button variant="link" onClick={dislikePage}>
+                  <ThumbsDownIcon size="sizeIcon50" decorative="false" title="Dislike this page" />
+                  It has issues
+                </Button>
+              </Box>
+            </>
+          )}
+        </Box>
+        <Box marginTop="space40">
+          <Text as="p">
+            <strong>See a mistake?</strong> <Anchor href={gitHubURL}>Edit this page</Anchor>
           </Text>
-        ) : (
-          <>
-            <Button variant="link" size="icon" onClick={likePage}>
-              <ThumbsUpIcon decorative="false" title="Like this page" />
-            </Button>
-            <Button variant="link" size="icon" onClick={dislikePage}>
-              <ThumbsDownIcon decorative="false" title="Dislike this page" />
-            </Button>
-          </>
-        )}
-      </Text>
-      <Box marginTop="space40">
-        <Text as="p">
-          <strong>See a mistake?</strong> <Anchor href={gitHubURL}>Edit this page</Anchor>
-        </Text>
-        <Text as="p">
-          <strong>Have a question or suggestion?</strong>{' '}
-          <Anchor href="https://github.com/twilio-labs/paste/discussions">Create a Github Discussion</Anchor>
-        </Text>
-      </Box>
+          <Text as="p">
+            <strong>Have a question or suggestion?</strong>{' '}
+            <Anchor href="https://github.com/twilio-labs/paste/discussions">Create a Github Discussion</Anchor>
+          </Text>
+        </Box>
+      </Card>
     </Box>
   );
 };
