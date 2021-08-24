ca0bf6188ad7ac3375bec75d6461924b13edb391
Shadi <TheSisb@users.noreply.github.com>
2021-08-24 13:38:35 -0500

    feat(display-pill-group): add package (#1800)

---
+++ packages/paste-core/components/display-pill-group/stories/index.stories.tsx
@@ -0,0 +1,77 @@
+import * as React from 'react';
+import {CustomizationProvider} from '@twilio-paste/customization';
+import {CalendarIcon} from '@twilio-paste/icons/esm/CalendarIcon';
+import {DisplayPillGroup} from '../src/DisplayPillGroup';
+import {DisplayPill} from '../src/DisplayPill';
+
+export const Basic: React.FC = () => {
+  return (
+    <DisplayPillGroup data-testid="display-pill-group" aria-label="Your favorite sports:">
+      <DisplayPill
+        data-testid="display-pill-anchor"
+        onFocus={() => {
+          console.log('Focused Tennis!');
+        }}
+        onBlur={() => {
+          console.log('Blurred Tennis!');
+        }}
+        href="https://google.com"
+      >
+        <CalendarIcon decorative size="sizeIcon10" />
+        Tennis
+      </DisplayPill>
+      <DisplayPill data-testid="display-pill-standard">Football</DisplayPill>
+      <DisplayPill href="/">Baseball</DisplayPill>
+      <DisplayPill>Basketball</DisplayPill>
+      <DisplayPill>Soccer</DisplayPill>
+    </DisplayPillGroup>
+  );
+};
+
+export const CustomDisplayPillGroup: React.FC = () => {
+  return (
+    <CustomizationProvider
+      baseTheme="default"
+      theme={{
+        textColors: {colorTextLink: 'red'},
+        fonts: {fontFamilyText: 'arial'},
+      }}
+      elements={{
+        DISPLAY_PILL_GROUP: {
+          margin: 'space40',
+        },
+        DISPLAY_PILL: {
+          backgroundColor: 'colorBackgroundNew',
+          color: 'colorText',
+          padding: 'space40',
+        },
+      }}
+    >
+      <DisplayPillGroup data-testid="display-pill-group" aria-label="Your favorite sports:">
+        <DisplayPill
+          data-testid="display-pill-anchor"
+          onFocus={() => {
+            console.log('Focused Tennis!');
+          }}
+          onBlur={() => {
+            console.log('Blurred Tennis!');
+          }}
+          href="https://google.com"
+        >
+          <CalendarIcon decorative size="sizeIcon10" />
+          Tennis
+        </DisplayPill>
+        <DisplayPill data-testid="display-pill-standard">Football</DisplayPill>
+        <DisplayPill href="/">Baseball</DisplayPill>
+        <DisplayPill>Basketball</DisplayPill>
+        <DisplayPill>Soccer</DisplayPill>
+      </DisplayPillGroup>
+    </CustomizationProvider>
+  );
+};
+
+// eslint-disable-next-line import/no-default-export
+export default {
+  title: 'Components/Display Pill Group',
+  component: Basic,
+};
