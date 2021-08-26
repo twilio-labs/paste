a9e31cce0a68bdba0354d3f3224ea3dcb6823c9c
Andi Oneto <aoneto@twilio.com>
2021-08-26 15:24:14 -0700

    feat(table): update types with element

--- packages/paste-core/components/table/src/types.ts
+++ packages/paste-core/components/table/src/types.ts
@@ -1,6 +1,8 @@
 import * as PropTypes from 'prop-types';
-import {ValueOf} from '@twilio-paste/types';
-import {WidthOptions, isWidthTokenProp} from '@twilio-paste/style-props';
+import type {BoxProps} from '@twilio-paste/box';
+import type {ValueOf} from '@twilio-paste/types';
+import type {WidthOptions} from '@twilio-paste/style-props';
+import {isWidthTokenProp} from '@twilio-paste/style-props';
 import {TableAlignmentObject, TableLayoutObject, TableVariantObject, TableVerticalAlignmentObject} from './constants';
 
 export type TableAlignmentOptions = ValueOf<typeof TableAlignmentObject>;
@@ -14,6 +16,7 @@ export interface TableContextProps {
 
 export interface TableProps extends React.TableHTMLAttributes<HTMLTableElement> {
   children: NonNullable<React.ReactNode>;
+  element?: BoxProps['element'];
   striped?: boolean;
   tableLayout?: TableLayoutOptions;
   variant?: TableVariantOptions;
@@ -21,6 +24,7 @@ export interface TableProps extends React.TableHTMLAttributes<HTMLTableElement> 
 
 export const TablePropTypes = {
   children: PropTypes.node.isRequired,
+  element: PropTypes.string,
   striped: PropTypes.bool,
   tableLayout: PropTypes.oneOf(Object.values(TableLayoutObject)),
   variant: PropTypes.oneOf(Object.values(TableVariantObject)),
@@ -28,56 +32,68 @@ export const TablePropTypes = {
 
 export interface THeadProps extends React.TableHTMLAttributes<HTMLTableSectionElement> {
   children: NonNullable<React.ReactNode>;
+  element?: BoxProps['element'];
 }
 
 export const THeadPropTypes = {
   children: PropTypes.node.isRequired,
+  element: PropTypes.string,
 };
 
 export interface TBodyProps extends React.TableHTMLAttributes<HTMLTableSectionElement> {
   children: NonNullable<React.ReactNode>;
+  element?: BoxProps['element'];
 }
 
 export const TBodyPropTypes = {
   children: PropTypes.node.isRequired,
+  element: PropTypes.string,
 };
 
 export interface TrProps extends React.TableHTMLAttributes<HTMLTableRowElement> {
   children: NonNullable<React.ReactNode>;
+  element?: BoxProps['element'];
   verticalAlign?: TableVerticalAlignmentOptions;
 }
 
 export const TrPropTypes = {
   children: PropTypes.node.isRequired,
+  element: PropTypes.string,
   verticalAlign: PropTypes.oneOf(Object.values(TableVerticalAlignmentObject)),
 };
 
 export interface ThProps extends React.ThHTMLAttributes<HTMLTableHeaderCellElement> {
   children?: React.ReactNode;
+  element?: BoxProps['element'];
   textAlign?: TableAlignmentOptions;
   width?: WidthOptions;
 }
 
 export const ThPropTypes = {
   children: PropTypes.node,
+  element: PropTypes.string,
   textAlign: PropTypes.oneOf(Object.values(TableAlignmentObject)),
   width: isWidthTokenProp,
 };
 
 export interface TdProps extends React.TdHTMLAttributes<HTMLTableCellElement> {
   children?: React.ReactNode;
+  element?: BoxProps['element'];
   textAlign?: TableAlignmentOptions;
 }
 
 export const TdPropTypes = {
   children: PropTypes.node,
+  element: PropTypes.string,
   textAlign: PropTypes.oneOf(Object.values(TableAlignmentObject)),
 };
 
 export interface TFootProps extends React.TableHTMLAttributes<HTMLTableSectionElement> {
   children: NonNullable<React.ReactNode>;
+  element?: BoxProps['element'];
 }
 
 export const TFootPropTypes = {
   children: PropTypes.node.isRequired,
+  element: PropTypes.string,
 };
