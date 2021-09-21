a560c7b85b565f3f962c6cf38b1d5306fbf24fb6
TheSisb <shadiisber@gmail.com>
2021-09-21 14:31:29 -0500

    feat(pagination): add element props

--- packages/paste-core/components/pagination/src/PaginationArrow/PaginationForwardArrow.tsx
+++ packages/paste-core/components/pagination/src/PaginationArrow/PaginationForwardArrow.tsx
@@ -7,16 +7,26 @@ import type {PaginationArrowProps} from '../types';
 import {PaginationArrowPropTypes} from '../proptypes';
 
 const PaginationForwardArrow = React.forwardRef<HTMLButtonElement, PaginationArrowProps>(
-  ({as = 'button', disabled, isFocused, isHovered, label, visibleLabel, ...props}, ref) => {
+  (
+    {as = 'button', element = 'PAGINATION_ARROW', disabled, isFocused, isHovered, label, visibleLabel, ...props},
+    ref
+  ) => {
     return (
-      <PaginationArrowButton {...props} ref={ref} as={as} disabled={disabled} visibleLabel={visibleLabel}>
+      <PaginationArrowButton
+        {...props}
+        ref={ref}
+        element={`${element}_BUTTON`}
+        as={as}
+        disabled={disabled}
+        visibleLabel={visibleLabel}
+      >
         {visibleLabel ? (
-          <Text aria-hidden="true" as="span" color="inherit" marginRight="space30">
+          <Text aria-hidden="true" as="span" color="inherit" marginRight="space30" element={`${element}_TEXT`}>
             {visibleLabel}
           </Text>
         ) : null}
-        <PaginationArrowIconWrapper isFocused={isFocused} isHovered={isHovered}>
-          <ArrowForwardIcon decorative={false} display="block" title={label} />
+        <PaginationArrowIconWrapper isFocused={isFocused} isHovered={isHovered} element={`${element}_ICON_WRAPPER`}>
+          <ArrowForwardIcon decorative={false} display="block" title={label} element={`${element}_ICON`} />
         </PaginationArrowIconWrapper>
       </PaginationArrowButton>
     );
--- packages/paste-core/components/pagination/src/PaginationEllipsis.tsx
+++ packages/paste-core/components/pagination/src/PaginationEllipsis.tsx
@@ -3,23 +3,26 @@ import {Text, safelySpreadTextProps} from '@twilio-paste/text';
 import type {PaginationEllipsisProps} from './types';
 import {PaginationEllipsisPropTypes} from './proptypes';
 
-const PaginationEllipsis = React.forwardRef<HTMLSpanElement, PaginationEllipsisProps>(({label, ...props}, ref) => {
-  return (
-    <Text
-      {...safelySpreadTextProps(props)}
-      ref={ref}
-      aria-label={label}
-      as="span"
-      display="inline-block"
-      paddingBottom="space20"
-      paddingLeft="space30"
-      paddingRight="space30"
-      paddingTop="space20"
-    >
-      &hellip;
-    </Text>
-  );
-});
+const PaginationEllipsis = React.forwardRef<HTMLSpanElement, PaginationEllipsisProps>(
+  ({label, element = 'PAGINATION_ELLIPSIS', ...props}, ref) => {
+    return (
+      <Text
+        {...safelySpreadTextProps(props)}
+        ref={ref}
+        element={element}
+        aria-label={label}
+        as="span"
+        display="inline-block"
+        paddingBottom="space20"
+        paddingLeft="space30"
+        paddingRight="space30"
+        paddingTop="space20"
+      >
+        &hellip;
+      </Text>
+    );
+  }
+);
 
 PaginationEllipsis.displayName = 'PaginationEllipsis';
 
