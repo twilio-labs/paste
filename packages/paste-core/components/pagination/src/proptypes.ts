d7003b633068654591b68748ece3d0ac8a80c402
Shadi <TheSisb@users.noreply.github.com>
2021-09-22 16:52:13 -0500

    feat(pagination): add element customization (#1880)
    * feat(pagination): add element props
    
    * test(pagination): add customization stories
    
    * test(pagination): add unit tests for customization
    
    * chore(pagination): add changeset

--- packages/paste-core/components/pagination/src/proptypes.ts
+++ packages/paste-core/components/pagination/src/proptypes.ts
@@ -4,19 +4,23 @@ import type {AsTags, Variants} from './types';
 export const PaginationPropTypes = {
   label: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
+  element: PropTypes.string,
 };
 
 export const PaginationItemsPropTypes = {
   children: PropTypes.node.isRequired,
+  element: PropTypes.string,
 };
 
 export const PaginationNumbersPropTypes = {
   children: PropTypes.node.isRequired,
   pageLabel: PropTypes.string,
+  element: PropTypes.string,
 };
 
 export const PaginationLabelPropTypes = {
   children: PropTypes.node.isRequired,
+  element: PropTypes.string,
 };
 
 export const PaginationNumberPropTypes = {
@@ -25,12 +29,14 @@ export const PaginationNumberPropTypes = {
   isCurrent: PropTypes.bool,
   label: PropTypes.string.isRequired,
   href: PropTypes.string,
+  element: PropTypes.string,
 };
 
 export const PaginationArrowButtonPropTypes = {
   as: PropTypes.oneOf(['a', 'button'] as AsTags[]),
   disabled: PropTypes.bool,
   visibleLabel: PropTypes.string,
+  element: PropTypes.string,
 };
 
 export const PaginationArrowPropTypes = {
@@ -41,13 +47,16 @@ export const PaginationArrowPropTypes = {
   label: PropTypes.string.isRequired,
   variant: PropTypes.oneOf(['back', 'forward'] as Variants[]).isRequired,
   visibleLabel: PropTypes.string,
+  element: PropTypes.string,
 };
 
 export const PaginationArrowIconWrapperPropTypes = {
   children: PropTypes.node.isRequired,
   isHovered: PropTypes.bool,
+  element: PropTypes.string,
 };
 
 export const PaginationEllipsisPropTypes = {
   label: PropTypes.string.isRequired,
+  element: PropTypes.string,
 };
