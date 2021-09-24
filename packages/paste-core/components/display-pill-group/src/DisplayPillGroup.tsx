8e0037ab6c83e5b552758444fc9a5338437aeb00
Si Taggart <me@simontaggart.com>
2021-09-24 13:56:32 -0700

    fix(display-pill-group): improved visual design

--- packages/paste-core/components/display-pill-group/src/DisplayPillGroup.tsx
+++ packages/paste-core/components/display-pill-group/src/DisplayPillGroup.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react';
 import {Box, safelySpreadBoxProps} from '@twilio-paste/box';
 
-export interface DisplayPillGroupProps {
+export interface DisplayPillGroupProps extends React.OlHTMLAttributes<HTMLElement> {
   'aria-label': string;
   element?: string;
   children: React.ReactNode;
 }
 
+/**
+ * Use the DisplayPillGroup to statically represent a collection of data. DisplayPillGroups should not be used
+ * inside of forms, or for editing data.
+ * @example
+ * <DisplayPillGroup aria-label="Your favorite sports:">
+ *   ...
+ * </DisplayPillGroup>
+ * @see https://paste.twilio.design/components/display-pill-group
+ */
 export const DisplayPillGroup = React.forwardRef<HTMLUListElement, DisplayPillGroupProps>(
   ({element = 'DISPLAY_PILL_GROUP', ...props}, ref) => {
     return (
