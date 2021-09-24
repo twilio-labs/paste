1e9a9583c4957a401b9a808565395269d711e336
Si Taggart <me@simontaggart.com>
2021-09-24 13:57:19 -0700

    fix(form-pill-group): improved visual design

--- packages/paste-core/components/form-pill-group/src/FormPillGroup.tsx
+++ packages/paste-core/components/form-pill-group/src/FormPillGroup.tsx
@@ -3,7 +3,8 @@ import {Box, safelySpreadBoxProps} from '@twilio-paste/box';
 import {Composite} from '@twilio-paste/reakit-library';
 import type {CompositeProps} from '@twilio-paste/reakit-library';
 
-export interface FormPillGroupProps extends CompositeProps {
+export interface FormPillGroupProps
+  extends Omit<CompositeProps, 'unstable_virtual' | 'unstable_moves' | 'unstable_system' | 'wrapElement' | 'wrap'> {
   'aria-label': string;
   element?: string;
   children: React.ReactNode;
@@ -26,6 +27,16 @@ const FormPillGroupStyles = React.forwardRef<HTMLUListElement, FormPillGroupProp
   )
 );
 
+/**
+ * FormPillGroup should be used to wrap a collection of FormPills. It should be used in conjunction with
+ * useFormPillGroupState hook
+ *
+ * @example
+ * <FormPillGroup {...pillState} aria-label="Your favorite sports:">
+ *   ...
+ * </FormPillGroup>
+ * @see http://paste.twilio.design/components/form-pill-group
+ */
 export const FormPillGroup = React.forwardRef<HTMLUListElement, FormPillGroupProps>((props, ref) => {
   return (
     <Composite as={FormPillGroupStyles} {...props} ref={ref}>
