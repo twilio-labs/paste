ff9ea6000ca641418ba20b5b5d28376a3145ff9c
Si Taggart <me@simontaggart.com>
2021-09-24 13:56:32 -0700

    fix(display-pill-group): improved visual design

--- packages/paste-core/components/display-pill-group/src/DisplayPill.tsx
+++ packages/paste-core/components/display-pill-group/src/DisplayPill.tsx
@@ -3,13 +3,26 @@ import {Box, safelySpreadBoxProps} from '@twilio-paste/box';
 import type {BoxElementProps} from '@twilio-paste/box';
 import {secureExternalLink} from '@twilio-paste/anchor';
 
-export interface DisplayPillProps extends BoxElementProps {
-  href?: string;
-  element?: string;
-  children: React.ReactNode;
-}
+type DisplayPillProps = React.AnchorHTMLAttributes<HTMLAnchorElement> & Pick<BoxElementProps, 'element'>;
 
-export const DisplayPill = React.forwardRef<HTMLElement, DisplayPillProps>(
+/**
+ * Display represents a saved entity as part of a collection, usually as a result of multi selection.
+ * Display pills should not be used in forms or for editing
+ * @example
+ * <DisplayPill
+ *   onFocus={() => {
+ *     console.log('Focused Tennis!');
+ *   }}
+ *   onBlur={() => {
+ *     console.log('Blurred Tennis!');
+ *   }}
+ *   href="https://google.com"
+ * >
+ *   Tennis
+ * </DisplayPill>
+ * @see https://paste.twilio.design/components/display-pill-group
+ */
+export const DisplayPill = React.forwardRef<HTMLAnchorElement, DisplayPillProps>(
   ({element = 'DISPLAY_PILL', ...props}, ref) => {
     return (
       <Box as="li" listStyleType="none">
@@ -19,19 +32,27 @@ export const DisplayPill = React.forwardRef<HTMLElement, DisplayPillProps>(
           ref={ref}
           element={element}
           as={props.href ? 'a' : 'div'}
+          alignItems="center"
+          backgroundColor="colorBackgroundStrong"
+          borderRadius="borderRadius10"
+          color={props.href ? 'colorTextLinkStrong' : 'colorText'}
+          columnGap="space20"
           cursor={props.href ? 'pointer' : 'default'}
-          color={props.href ? 'colorTextLink' : 'colorText'}
-          backgroundColor="colorBackground"
-          borderWidth="borderWidth10"
-          borderColor="colorBorderWeak"
-          borderStyle="solid"
-          borderRadius="borderRadius20"
+          display="flex"
+          fontSize="fontSize20"
+          fontWeight="fontWeightSemibold"
+          lineHeight="lineHeight10"
           paddingX="space30"
           paddingY="space20"
-          height="30px"
-          display="flex"
-          columnGap="space20"
-          alignItems="center"
+          outline="none"
+          _hover={{
+            textDecoration: 'none',
+          }}
+          _focusVisible={{
+            backgroundColor: 'colorBackgroundPrimaryWeakest',
+            boxShadow: 'shadowFocus',
+            color: 'colorTextLinkStronger',
+          }}
         >
           {props.children}
         </Box>
