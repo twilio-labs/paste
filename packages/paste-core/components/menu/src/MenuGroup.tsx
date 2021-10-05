d1126c278bb14788208b998257a29806edf40ca8
Andi Oneto <aoneto@twilio.com>
2021-10-05 15:07:01 -0700

    feat(menu): add customization

--- packages/paste-core/components/menu/src/MenuGroup.tsx
+++ packages/paste-core/components/menu/src/MenuGroup.tsx
@@ -6,30 +6,39 @@ import type {MenuItemVariant, MenuGroupProps} from './types';
 
 export const MenuGroupContext = React.createContext<MenuItemVariant>(MenuItemVariants.DEFAULT);
 
-const MenuGroup = React.forwardRef<HTMLDivElement, MenuGroupProps>(({label, icon, children, ...props}, ref) => {
-  return (
-    <MenuGroupContext.Provider value={MenuItemVariants.GROUP_ITEM}>
-      <Box {...safelySpreadBoxProps(props)} role="presentation" aria-label={label} textDecoration="none" ref={ref}>
-        <Box display="flex" alignItems="center" paddingX="space70" paddingY="space30" cursor="default">
-          {React.isValidElement(icon) ? (
-            <Box flexShrink={0} size="sizeIcon30">
-              {React.cloneElement(icon, {color: 'colorTextIcon'})}
-            </Box>
-          ) : null}
-          <Text
-            as="span"
-            color="colorText"
-            role="presentation"
-            fontWeight="fontWeightBold"
-            paddingLeft={icon != null ? 'space20' : undefined}
-          >
-            {label}
-          </Text>
+const MenuGroup = React.forwardRef<HTMLDivElement, MenuGroupProps>(
+  ({label, icon, children, element = 'MENU_GROUP', ...props}, ref) => {
+    return (
+      <MenuGroupContext.Provider value={MenuItemVariants.GROUP_ITEM}>
+        <Box
+          {...safelySpreadBoxProps(props)}
+          element={element}
+          role="presentation"
+          aria-label={label}
+          textDecoration="none"
+          ref={ref}
+        >
+          <Box display="flex" alignItems="center" paddingX="space70" paddingY="space30" cursor="default">
+            {React.isValidElement(icon) ? (
+              <Box flexShrink={0} size="sizeIcon30">
+                {React.cloneElement(icon, {color: 'colorTextIcon'})}
+              </Box>
+            ) : null}
+            <Text
+              as="span"
+              color="colorText"
+              role="presentation"
+              fontWeight="fontWeightBold"
+              paddingLeft={icon != null ? 'space20' : undefined}
+            >
+              {label}
+            </Text>
+          </Box>
+          {children}
         </Box>
-        {children}
-      </Box>
-    </MenuGroupContext.Provider>
-  );
-});
+      </MenuGroupContext.Provider>
+    );
+  }
+);
 MenuGroup.displayName = 'MenuGroup';
 export {MenuGroup};
