8e481b426e483cb6cc7f29a9a3ef425e7b8744ae
Simon Taggart <staggart@twilio.com>
2021-10-06 21:25:04 -0700

    feat: Improved roadmap view and fixed TOC scroll tracking (#1876)

--- packages/paste-website/gatsby-node.js
+++ packages/paste-website/gatsby-node.js
@@ -32,3 +32,21 @@ exports.onCreateWebpackConfig = ({stage, loaders, actions}) => {
     });
   }
 };
+exports.createSchemaCustomization = ({actions, schema}) => {
+  actions.createTypes([
+    schema.buildObjectType({
+      name: 'Airtable',
+      interfaces: ['Node'],
+      fields: {
+        // For airtable roadmap create a custom boolean field to use as a filter. If the release date is greater than
+        // today, then it is a future release.
+        isFuture: {
+          type: 'Boolean!',
+          resolve: (source) => {
+            return new Date(source.data.Release_Date) > new Date();
+          },
+        },
+      },
+    }),
+  ]);
+};
