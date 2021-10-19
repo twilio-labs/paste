65b86b18d2218a429b9f50dd43bac0497883109c
Lee <leewhite128@gmail.com>
2021-10-19 10:53:59 -0700

    fix: update css to remove ScrollAnchorIntoView

--- packages/paste-website/src/components/site-wrapper/SiteBody.tsx
+++ packages/paste-website/src/components/site-wrapper/SiteBody.tsx
@@ -6,14 +6,23 @@ import {Sidebar} from './sidebar';
 import {SiteHeader} from './site-header';
 import {SiteFooter} from './site-footer';
 import {useWindowSize} from '../../hooks/useWindowSize';
-import {ScrollAnchorIntoView} from './ScrollAnchorIntoView';
+// import {ScrollAnchorIntoView} from './ScrollAnchorIntoView';
 import {PASTE_DOCS_CONTENT_AREA, SITE_BREAKPOINTS} from '../../constants';
 import {docSearchStyles} from '../../styles/docSearch';
+
+/* Wraps the entire doc site page */
+const StyledSiteWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
 
 /* Wraps the entire doc site page */
 const StyledSiteBody = styled.div`
   display: flex;
   min-width: 240px;
+  overflow: auto;
+  scroll-behavior: smooth;
 
   @supports (display: grid) {
     display: grid;
@@ -30,19 +39,19 @@ export const SiteBody: React.FC = ({children}) => {
   const themeObject = useTheme();
 
   return (
-    <>
+    <StyledSiteWrapper>
       <StylingGlobals styles={docSearchStyles({theme: themeObject})} />
       <SiteHeader />
       <StyledSiteBody>
         {breakpointIndex === undefined || breakpointIndex > 1 ? <Sidebar /> : null}
         <Box flex="1" minWidth="size0">
           <main id={PASTE_DOCS_CONTENT_AREA}>
-            <ScrollAnchorIntoView />
+            {/* <ScrollAnchorIntoView /> */}
             {children}
           </main>
           <SiteFooter />
         </Box>
       </StyledSiteBody>
-    </>
+    </StyledSiteWrapper>
   );
 };
