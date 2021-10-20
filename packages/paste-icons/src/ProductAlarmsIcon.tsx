a0499412d8919e8b5d0a3fc78c8cc38a99bc3bcc
Loreina Chew <lchew@twilio.com>
2021-10-20 15:05:14 -0400

    feat(icons): add alarms, text format, event streams and unsorted icons

---
+++ packages/paste-icons/src/ProductAlarmsIcon.tsx
@@ -0,0 +1,53 @@
+/**
+ * This file was automatically generated with @twilio-labs/svg-to-react
+ */
+import * as React from 'react';
+import {useUID} from '@twilio-paste/uid-library';
+import {IconWrapper} from './helpers/IconWrapper';
+import type {IconWrapperProps} from './helpers/IconWrapper';
+
+export interface ProductAlarmsIconProps extends IconWrapperProps {
+  title?: string;
+  decorative: boolean;
+}
+
+const ProductAlarmsIcon: React.FC<ProductAlarmsIconProps> = ({
+  as,
+  display,
+  element = 'ICON',
+  size,
+  color,
+  title,
+  decorative,
+}) => {
+  const titleId = `ProductAlarmsIcon-${useUID()}`;
+
+  if (!decorative && title == null) {
+    throw new Error('[ProductAlarmsIcon]: Missing a title for non-decorative icon.');
+  }
+
+  return (
+    <IconWrapper as={as} display={display} element={element} size={size} color={color}>
+      <svg
+        role="img"
+        aria-hidden={decorative}
+        width="100%"
+        height="100%"
+        fill="none"
+        viewBox="0 0 20 20"
+        aria-labelledby={titleId}
+      >
+        {title ? <title id={titleId}>{title}</title> : null}
+        <path
+          fill="currentColor"
+          fillRule="evenodd"
+          d="M10 3.08a4.376 4.376 0 00-3.111 1.303 4.48 4.48 0 00-1.291 3.151c0 2.794-.406 4.512-.837 5.55a5.742 5.742 0 01-.259.544h10.994a5.675 5.675 0 01-.256-.54c-.432-1.036-.838-2.754-.838-5.554a4.48 4.48 0 00-1.29-3.151A4.377 4.377 0 0010 3.08zm6.792 10.642a1.311 1.311 0 01-.15-.162c-.12-.148-.296-.415-.48-.856-.366-.882-.76-2.462-.76-5.17a5.48 5.48 0 00-1.58-3.855A5.377 5.377 0 0010 2.08c-1.434 0-2.81.576-3.822 1.6a5.48 5.48 0 00-1.58 3.854c0 2.702-.393 4.282-.76 5.166-.184.442-.361.71-.48.86a1.33 1.33 0 01-.152.163.5.5 0 00.294.905h13a.5.5 0 00.292-.906zm-8.42 1.874a.5.5 0 01.612.357c.072.275.235.52.464.695a1.32 1.32 0 001.604 0c.23-.175.392-.42.465-.695a.5.5 0 01.967.254c-.13.492-.42.926-.824 1.235a2.32 2.32 0 01-2.82 0 2.288 2.288 0 01-.824-1.235.5.5 0 01.357-.61z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </IconWrapper>
+  );
+};
+
+ProductAlarmsIcon.displayName = 'ProductAlarmsIcon';
+export {ProductAlarmsIcon};
---
+++ packages/paste-icons/src/TextFormatIcon.tsx
@@ -0,0 +1,57 @@
+/**
+ * This file was automatically generated with @twilio-labs/svg-to-react
+ */
+import * as React from 'react';
+import {useUID} from '@twilio-paste/uid-library';
+import {IconWrapper} from './helpers/IconWrapper';
+import type {IconWrapperProps} from './helpers/IconWrapper';
+
+export interface TextFormatIconProps extends IconWrapperProps {
+  title?: string;
+  decorative: boolean;
+}
+
+const TextFormatIcon: React.FC<TextFormatIconProps> = ({
+  as,
+  display,
+  element = 'ICON',
+  size,
+  color,
+  title,
+  decorative,
+}) => {
+  const titleId = `TextFormatIcon-${useUID()}`;
+
+  if (!decorative && title == null) {
+    throw new Error('[TextFormatIcon]: Missing a title for non-decorative icon.');
+  }
+
+  return (
+    <IconWrapper as={as} display={display} element={element} size={size} color={color}>
+      <svg
+        role="img"
+        aria-hidden={decorative}
+        width="100%"
+        height="100%"
+        fill="none"
+        viewBox="0 0 20 20"
+        aria-labelledby={titleId}
+      >
+        {title ? <title id={titleId}>{title}</title> : null}
+        <path
+          fill="currentColor"
+          fillRule="evenodd"
+          d="M7.522 11.625h4.956l1.193 2.938a.625.625 0 001.158-.47l-3.811-9.384a1.118 1.118 0 00-.393-.504 1.08 1.08 0 00-1.25 0c-.18.127-.314.304-.393.504l-3.811 9.384a.625.625 0 101.158.47l1.193-2.938zm.508-1.25L10 5.525l1.97 4.85H8.03z"
+          clipRule="evenodd"
+        />
+        <path
+          fill="currentColor"
+          d="M4 17c0-.345.28-.625.625-.625h10.75a.625.625 0 110 1.25H4.625A.625.625 0 014 17z"
+        />
+      </svg>
+    </IconWrapper>
+  );
+};
+
+TextFormatIcon.displayName = 'TextFormatIcon';
+export {TextFormatIcon};
