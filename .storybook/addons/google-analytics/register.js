5e9e9b4cb0e6c90a611e61847e2f1dc8de416216
Si Taggart <me@simontaggart.com>
2021-10-22 14:04:31 -0700

    fix: prevent GA from tracking chromatic builds

---
+++ .storybook/addons/google-analytics/register.js
@@ -0,0 +1,27 @@
+const {addons} = require('@storybook/addons');
+const {isChromatic} = require('chromatic');
+const {STORY_ERRORED, STORY_MISSING, STORY_RENDERED} = require('@storybook/core-events');
+const ReactGA = require('react-ga');
+
+addons.register('paste-google-analytics', (api) => {
+  if (!isChromatic()) {
+    ReactGA.initialize('G-1DZ9RS3QNT');
+
+    api.on(STORY_RENDERED, () => {
+      const {path} = api.getUrlState();
+      ReactGA.pageview(path);
+    });
+    api.on(STORY_ERRORED, ({description}) => {
+      ReactGA.exception({
+        description,
+        fatal: true,
+      });
+    });
+    api.on(STORY_MISSING, (id) => {
+      ReactGA.exception({
+        description: `attempted to render ${id}, but it is missing`,
+        fatal: false,
+      });
+    });
+  }
+});
