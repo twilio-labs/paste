d146d08a7a10b9bb185abf8d323fe5fe645bd7bc
Lee <leewhite128@gmail.com>
2021-10-28 15:45:40 -0700

    test: remove enzyme mount from tests

--- packages/paste-core/layout/aspect-ratio/__tests__/aspectRatio.spec.tsx
+++ packages/paste-core/layout/aspect-ratio/__tests__/aspectRatio.spec.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
-import {mount} from 'enzyme';
+import {render} from '@testing-library/react';
 import {AspectRatio} from '../src';
 
 describe('AspectRatio', () => {
   it('should render a 4:3 aspect ratio div', () => {
-    const wrapper = mount(
-      <AspectRatio ratio="4:3">
-        <p>This is the AspectRatio utility.</p>
+    const {getByTestId} = render(
+      <AspectRatio ratio="4:3" data-testid="foo">
+        <p data-testid="foo">This is the AspectRatio utility.</p>
       </AspectRatio>
     );
-    expect(wrapper.find('div').first().props().style!.paddingBottom).toBe('75%');
+
+    expect(getByTestId('foo')).toHaveStyleRule('padding', '75%');
   });
 });
