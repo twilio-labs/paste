8e1330bb192c1965990d28aa8559049c7e4f2605
Lee White <leewhite128@gmail.com>
2021-10-29 19:02:51 -0700

    docs(website): create quick start, manual installation pages (#1976)

--- packages/paste-website/src/components/site-wrapper/sidebar/SidebarNavigation.tsx
+++ packages/paste-website/src/components/site-wrapper/sidebar/SidebarNavigation.tsx
@@ -127,8 +127,13 @@ const SidebarNavigation: React.FC<SidebarNavigationProps> = () => {
           <DisclosurePrimitiveContent {...gettingStartedDisclosure} data-cy="getting-started-list">
             <SidebarNestedList>
               <SidebarNestedItem>
-                <SidebarAnchor nested to="/getting-started/engineering">
-                  Engineering Guidelines
+                <SidebarAnchor nested to="/getting-started/quickstart">
+                  Quick Start
+                </SidebarAnchor>
+              </SidebarNestedItem>
+              <SidebarNestedItem>
+                <SidebarAnchor nested to="/getting-started/manual-installation">
+                  Manual Installation
                 </SidebarAnchor>
               </SidebarNestedItem>
               <SidebarNestedItem>
