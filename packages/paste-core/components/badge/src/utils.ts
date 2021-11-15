c742f4626965853a79d181a775cc9203ce81b524
Lee <leewhite128@gmail.com>
2021-11-15 09:22:15 -0800

    fix: isFocusableElement

--- packages/paste-core/components/badge/src/utils.ts
+++ packages/paste-core/components/badge/src/utils.ts
@@ -1,7 +1,7 @@
 import {safelySpreadBoxProps} from '@twilio-paste/box';
 import type {BadgeProps} from './types';
 
-export const isFocusableElement = ({as}: Partial<BadgeProps>): boolean => as !== 'span';
+export const isFocusableElement = ({as}: Partial<BadgeProps>): boolean => as === 'button' || as === 'a';
 
 export const getBadgeSpanProps = (props: Partial<BadgeProps>): Partial<BadgeProps> => {
   if (isFocusableElement(props)) {
