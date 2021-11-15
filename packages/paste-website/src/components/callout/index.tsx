4fbd9f59ab1868bf94a9e4a93fb0e29139b7f18d
Jade Pennig <jpennig@twilio.com>
2021-11-15 12:23:34 -0600

    feat(callout-update): update callout component UI
    * Updates visual appearance
    * Cleans up variant naming

--- packages/paste-website/src/components/callout/index.tsx
+++ packages/paste-website/src/components/callout/index.tsx
@@ -1,63 +1,63 @@
 import * as React from 'react';
 import {Box} from '@twilio-paste/box';
 import {Text} from '@twilio-paste/text';
-import {Stack} from '@twilio-paste/stack';
 import type {asTags as HeadingAsTags} from '@twilio-paste/heading';
-import {Heading} from '@twilio-paste/heading';
-import type {BorderColor} from '@twilio-paste/style-props';
+import {NeutralIcon} from '@twilio-paste/icons/esm/NeutralIcon';
+import {WarningIcon} from '@twilio-paste/icons/esm/WarningIcon';
 
-type CalloutVariants = 'primary' | 'secondary' | 'warning';
-
-const borderColorPartial = (variant?: CalloutVariants): BorderColor => {
-  if (variant === 'secondary') {
-    return 'colorBorderPrimaryStrong';
-  }
-  if (variant === 'warning') {
-    return 'colorBorderWarning';
-  }
-  return 'colorBorderPrimaryWeak';
-};
+type CalloutVariants = 'info' | 'warning';
 
 interface CalloutTitleProps {
   as: HeadingAsTags;
 }
 
 const CalloutTitle: React.FC<CalloutTitleProps> = ({as = 'h3', children}) => (
-  <Heading as={as} variant="heading50">
+  <Text as={as} color="currentColor" marginBottom="space30">
     {children}
-  </Heading>
+  </Text>
 );
 
-const CalloutText: React.FC = ({children}) => <Text as="p">{children}</Text>;
+const CalloutText: React.FC = ({children}) => (
+  <Text as="p" color="currentColor" lineHeight="lineHeight40">
+    {children}
+  </Text>
+);
 
 interface CalloutProps {
   variant?: CalloutVariants;
 }
 
 const Callout: React.FC<CalloutProps> = ({variant, children}) => {
+  const backgroundColor = variant === 'warning' ? 'colorBackgroundWarningWeakest' : 'colorBackgroundNeutralWeakest';
+  const color = variant === 'warning' ? 'colorTextWarning' : 'colorTextNeutral';
+  const icon =
+    variant === 'warning' ? (
+      <WarningIcon decorative={false} title="Warning icon" />
+    ) : (
+      <NeutralIcon decorative={false} title="Information icon" />
+    );
+
   return (
     <Box
-      position="relative"
-      borderWidth="borderWidth20"
-      borderLeftWidth="borderWidth40"
-      borderStyle="solid"
-      borderColor={borderColorPartial(variant)}
+      display="flex"
       marginTop="space60"
       marginBottom="space60"
-      paddingTop="space40"
-      paddingRight="space60"
-      paddingBottom="space40"
-      paddingLeft="space60"
+      paddingTop="space70"
+      paddingRight="space70"
+      paddingBottom="space70"
+      paddingLeft="space70"
+      borderRadius="borderRadius20"
+      backgroundColor={backgroundColor}
+      color={color}
     >
-      <Stack orientation="vertical" spacing="space30">
-        {children}
-      </Stack>
+      <Box marginRight="space40">{icon}</Box>
+      <Box flex="1">{children}</Box>
     </Box>
   );
 };
 
 Callout.defaultProps = {
-  variant: 'primary',
+  variant: 'info',
 };
 
 export {Callout, CalloutTitle, CalloutText};
