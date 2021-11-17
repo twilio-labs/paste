c71d4d34a5e4db14a3bec9156401e8332b457d76
Lee <leewhite128@gmail.com>
2021-11-17 09:58:35 -0800

    fix: improve types, remove useTheme

--- packages/paste-website/src/components/shortcodes/package-status-legend/index.tsx
+++ packages/paste-website/src/components/shortcodes/package-status-legend/index.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import {Box} from '@twilio-paste/box';
 import {Badge} from '@twilio-paste/badge';
 import type {BadgeVariants} from '@twilio-paste/badge/src/types';
-import {useTheme} from '@twilio-paste/theme';
 
 import {NewIcon} from '@twilio-paste/icons/esm/NewIcon';
 import {ProcessDraftIcon} from '@twilio-paste/icons/esm/ProcessDraftIcon';
@@ -17,35 +16,40 @@ interface PackageStatusLegendProps {
   engineerCommitteeReview?: string;
 }
 
+interface StatusData {
+  badgeVariant: BadgeVariants;
+  popoverContent: string;
+}
+
 const PackageStatusBadge: React.FC<{status: string}> = ({status}) => {
-  let badgeVariant: BadgeVariants | undefined;
-  let popoverContent = '';
+  const statusMap: {[index: string]: StatusData} = {
+    Alpha: {
+      badgeVariant: 'new',
+      popoverContent: StatusDescriptions.ALPHA,
+    },
+    Beta: {
+      badgeVariant: 'info',
+      popoverContent: StatusDescriptions.BETA,
+    },
+  };
 
-  if (status === 'Alpha') {
-    badgeVariant = 'new';
-    popoverContent = StatusDescriptions.ALPHA;
+  const {badgeVariant, popoverContent} = statusMap[status];
+
+  if (badgeVariant) {
+    return (
+      <BadgePopoverContainer baseId="status">
+        <BadgePopoverButton variant={badgeVariant}>
+          <>
+            <NewIcon decorative size="sizeIcon10" />
+            {status}
+          </>
+        </BadgePopoverButton>
+        <BadgePopover aria-label={status}>{popoverContent}</BadgePopover>
+      </BadgePopoverContainer>
+    );
   }
 
-  if (status === 'Beta') {
-    badgeVariant = 'info';
-    popoverContent = StatusDescriptions.BETA;
-  }
-
-  return (
-    <>
-      {badgeVariant ? (
-        <BadgePopoverContainer baseId="status">
-          <BadgePopoverButton variant={badgeVariant}>
-            <>
-              <NewIcon decorative size="sizeIcon10" />
-              {status}
-            </>
-          </BadgePopoverButton>
-          <BadgePopover aria-label={status}>{popoverContent}</BadgePopover>
-        </BadgePopoverContainer>
-      ) : null}
-    </>
-  );
+  return null;
 };
 
 const PackageStatusLegend: React.FC<PackageStatusLegendProps> = ({
@@ -54,31 +58,29 @@ const PackageStatusLegend: React.FC<PackageStatusLegendProps> = ({
   designCommitteeReview,
   engineerCommitteeReview,
 }) => {
-  const theme = useTheme();
-
   const shouldShowStatus = packageStatus || !figmaStatus || !designCommitteeReview || !engineerCommitteeReview;
 
-  return (
-    <>
-      {shouldShowStatus && (
-        <Box display="flex" alignItems="center" flexWrap="wrap" flexGrow={1} css={{gap: theme.space.space40}}>
-          {packageStatus && <PackageStatusBadge status={packageStatus} />}
-          {!figmaStatus && (
-            <Badge as="span" variant="default">
-              <ProcessDraftIcon decorative size="sizeIcon10" />
-              <Box>Design assets pending</Box>
-            </Badge>
-          )}
-          {!designCommitteeReview || !engineerCommitteeReview ? (
-            <Badge as="span" variant="default">
-              <ProcessDraftIcon decorative size="sizeIcon10" />
-              <Box>Peer review pending</Box>
-            </Badge>
-          ) : null}
-        </Box>
-      )}
-    </>
-  );
+  if (shouldShowStatus) {
+    return (
+      <Box display="flex" alignItems="center" flexWrap="wrap" flexGrow={1} columnGap="space40">
+        {packageStatus && <PackageStatusBadge status={packageStatus} />}
+        {!figmaStatus && (
+          <Badge as="span" variant="default">
+            <ProcessDraftIcon decorative size="sizeIcon10" />
+            <Box>Design assets pending</Box>
+          </Badge>
+        )}
+        {!designCommitteeReview || !engineerCommitteeReview ? (
+          <Badge as="span" variant="default">
+            <ProcessDraftIcon decorative size="sizeIcon10" />
+            <Box>Peer review pending</Box>
+          </Badge>
+        ) : null}
+      </Box>
+    );
+  }
+
+  return null;
 };
 
 export {PackageStatusLegend};
