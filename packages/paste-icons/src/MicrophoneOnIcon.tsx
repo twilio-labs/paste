0d5756e6c4d31d2e4045af5ef74619fdd96551c1
kylegilbreath <kylegilbreath@gmail.com>
2021-11-18 16:21:39 -0700

    chore(icons): added new media icons and segment product icon

---
+++ packages/paste-icons/src/MicrophoneOnIcon.tsx
@@ -0,0 +1,57 @@
+/**
+ * This file was automatically generated with @twilio-labs/svg-to-react
+ */
+import * as React from 'react';
+import {useUID} from '@twilio-paste/uid-library';
+import {IconWrapper} from './helpers/IconWrapper';
+import type {IconWrapperProps} from './helpers/IconWrapper';
+
+export interface MicrophoneOnIconProps extends IconWrapperProps {
+  title?: string;
+  decorative: boolean;
+}
+
+const MicrophoneOnIcon: React.FC<MicrophoneOnIconProps> = ({
+  as,
+  display,
+  element = 'ICON',
+  size,
+  color,
+  title,
+  decorative,
+}) => {
+  const titleId = `MicrophoneOnIcon-${useUID()}`;
+
+  if (!decorative && title == null) {
+    throw new Error('[MicrophoneOnIcon]: Missing a title for non-decorative icon.');
+  }
+
+  return (
+    <IconWrapper as={as} display={display} element={element} size={size} color={color}>
+      <svg
+        role="img"
+        aria-hidden={decorative}
+        width="100%"
+        height="100%"
+        fill="none"
+        viewBox="0 0 20 20"
+        aria-labelledby={titleId}
+      >
+        {title ? <title id={titleId}>{title}</title> : null}
+        <path
+          fill="currentColor"
+          fillRule="evenodd"
+          d="M10 3.071A2.357 2.357 0 007.643 5.43v4a2.357 2.357 0 104.714 0v-4A2.357 2.357 0 0010 3.07zm-2.374-.016a3.357 3.357 0 015.731 2.374v4a3.357 3.357 0 01-6.714 0v-4c0-.89.354-1.745.983-2.374z"
+          clipRule="evenodd"
+        />
+        <path
+          fill="currentColor"
+          d="M12.732 15.217a5.63 5.63 0 01-1.947.422v.004H10.5v1.785a.5.5 0 11-1 0v-1.785h-.071v-.004A5.63 5.63 0 014 10a.5.5 0 011 0 4.631 4.631 0 004.62 4.642h.974A4.632 4.632 0 0015.214 10a.5.5 0 111-.002 5.632 5.632 0 01-3.482 5.218z"
+        />
+      </svg>
+    </IconWrapper>
+  );
+};
+
+MicrophoneOnIcon.displayName = 'MicrophoneOnIcon';
+export {MicrophoneOnIcon};
---
+++ packages/paste-icons/src/StopScreenShareIcon.tsx
@@ -0,0 +1,57 @@
+/**
+ * This file was automatically generated with @twilio-labs/svg-to-react
+ */
+import * as React from 'react';
+import {useUID} from '@twilio-paste/uid-library';
+import {IconWrapper} from './helpers/IconWrapper';
+import type {IconWrapperProps} from './helpers/IconWrapper';
+
+export interface StopScreenShareIconProps extends IconWrapperProps {
+  title?: string;
+  decorative: boolean;
+}
+
+const StopScreenShareIcon: React.FC<StopScreenShareIconProps> = ({
+  as,
+  display,
+  element = 'ICON',
+  size,
+  color,
+  title,
+  decorative,
+}) => {
+  const titleId = `StopScreenShareIcon-${useUID()}`;
+
+  if (!decorative && title == null) {
+    throw new Error('[StopScreenShareIcon]: Missing a title for non-decorative icon.');
+  }
+
+  return (
+    <IconWrapper as={as} display={display} element={element} size={size} color={color}>
+      <svg
+        role="img"
+        aria-hidden={decorative}
+        width="100%"
+        height="100%"
+        fill="none"
+        viewBox="0 0 20 20"
+        aria-labelledby={titleId}
+      >
+        {title ? <title id={titleId}>{title}</title> : null}
+        <path
+          fill="currentColor"
+          d="M12.14 7.148a.504.504 0 11.712.713L10.713 10l2.14 2.14a.504.504 0 11-.714.712L10 10.713l-2.14 2.14a.504.504 0 01-.712-.714L9.287 10l-2.14-2.14a.504.504 0 11.714-.712L10 9.287l2.14-2.14z"
+        />
+        <path
+          fill="currentColor"
+          fillRule="evenodd"
+          d="M1 5a2 2 0 012-2h14a2 2 0 012 2v10a2 2 0 01-2 2H3a2 2 0 01-2-2V5zm2-1h14a1 1 0 011 1v10a1 1 0 01-1 1H3a1 1 0 01-1-1V5a1 1 0 011-1z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </IconWrapper>
+  );
+};
+
+StopScreenShareIcon.displayName = 'StopScreenShareIcon';
+export {StopScreenShareIcon};
