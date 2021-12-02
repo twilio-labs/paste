a0b4b476836bcb48168fd406aaaa2f1e348b99f7
Lee <leewhite128@gmail.com>
2021-12-01 17:05:36 -0800

    feat: remove website specific badgePopover

--- packages/paste-website/src/components/badge-popover/BadgePopoverContext.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from 'react';
-import type {NonModalDialogPrimitiveState} from '@twilio-paste/non-modal-dialog-primitive';
-
-export const BadgePopoverContext = React.createContext<Partial<NonModalDialogPrimitiveState>>({});
