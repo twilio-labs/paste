ace93308535344fe0e5d29d5a360167b11c957b1
Lee <leewhite128@gmail.com>
2021-12-01 16:38:02 -0800

    feat: add types for popoverbutton

---
+++ packages/paste-core/components/popover/src/types.ts
@@ -0,0 +1,15 @@
+import type {BadgeProps} from '@twilio-paste/badge';
+import type {ButtonProps} from '@twilio-paste/button';
+import type {BoxProps} from '@twilio-paste/box';
+
+export type ButtonBadgeProps = Omit<ButtonProps, 'variant' | 'children'> & Pick<BadgeProps, 'children' | 'variant'>;
+
+type PopoverButtonBaseProps = Pick<BoxProps, 'element'> & {
+  id?: string;
+  toggle?: () => void;
+};
+
+type PopoverButtonAsButtonProps = ButtonProps & {as?: 'button'};
+type PopoverButtonAsBadgeProps = Omit<BadgeProps, 'as'> & {as: 'badge'};
+
+export type PopoverButtonProps = (PopoverButtonAsBadgeProps | PopoverButtonAsButtonProps) & PopoverButtonBaseProps;
