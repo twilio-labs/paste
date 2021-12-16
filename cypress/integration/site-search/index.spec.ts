017e3b829191055414f5e05498701c8ed5009b95
Andi Oneto <aoneto@twilio.com>
2021-12-15 16:17:44 -0800

    chore(website): simplify search tests until upgrade

--- cypress/integration/site-search/index.spec.ts
+++ cypress/integration/site-search/index.spec.ts
@@ -1,86 +1,40 @@
-const NO_RESULT = 'no_result';
-
-const addQueryMetaData = ({query, result}) => {
-  const [results] = result.results;
-
-  return {results: [{...results, query, params: `query=${query}&hitsPerPage=5`}]};
-};
-const extractRequestParams = (parsedBody) => Cypress._.get(parsedBody, 'requests[0].params');
-const extractHits = ({response: {body}}) => Cypress._.get(JSON.parse(body), 'results[0].hits', []);
-
-const testSearch = (searchPhrase) => {
-  const searchPhraseCopy = searchPhrase.slice();
-
-  searchPhraseCopy.split('').forEach((letter) => {
-    cy.get('@inputText').then((current) => {
-      cy.wrap(current + letter).as('inputText');
-    });
-
-    cy.get<string>('@inputText').then((inputText) => {
-      cy.get('[data-cy="paste-docs-search-input"]').should('be.visible').click().should('have.focus').type(letter);
-
-      cy.wait('@searchRequest').then((xhr) => {
-        cy.get<string>('@searchString').then((searchParamString) => {
-          cy.verifyExpectedQuery(searchParamString, {query: inputText});
-
-          const hits = extractHits(xhr);
-
-          cy.get('[id*="algolia-autocomplete-listbox"]')
-            .should('be.visible')
-            .find('[role="option"]')
-            .should('have.length', hits.length);
-        });
-      });
-    });
-  });
-};
-
 describe('Docs website search', () => {
-  beforeEach(() => {
+  let searchTerm = '';
+  before(() => {
     // Start an XHR server with the built-in cypress utilities.
     cy.server();
-
-    cy.route('/**/**/page-data.json').as('pageDataResponse');
+    cy.route('**/**/page-data.json').as('pageDataResponse');
+    cy.route('**/**/app-data.json').as('appDataResponse');
     cy.visit('/');
     // Wait for the JS assets to load since they are necessary for the search functionality.
-    cy.wait('@pageDataResponse');
+    cy.wait(['@pageDataResponse', '@appDataResponse']);
+  });
 
-    cy.fixture('./search.json').then((data) => {
-      // Note: When we upgrade to >= 6.0.0 we can use .intercept
-      // "Cypress.Chainable.route2" is the early preview of their new(er) intercept and stubbing utility
-      // It is advantageous over the legacy Cypress.Chainable.route here because it allows us to gracefully intercept a response and modify the body dynamically
+  beforeEach(() => {
+    cy.server();
+    cy.route({url: 'https://bh4d9od16a-dsn.algolia.net/1/indexes/*/**', method: 'POST'}).as('searchRequest');
+  });
 
-      // Note: Unfortunately our current version of Cypress does not allow us to fully stub responses using these utilities, so the XHR is actaully being sent to the search service
-      // In the future we can configure this so that it does not send a request the service.
-      cy.route2({pathname: '/1/indexes/*/queries', method: 'POST'}, (req) => {
-        const {body: jsonBody, reply} = req;
+  beforeEach(() => {
+    cy.get('[data-cy="paste-docs-search-input"]').as('searchInputEl');
+  });
 
-        // For now, this is a bit dense, but in future version of Cypress this body parsing has been improved as well. For now, there are utilities above to assist.
-        const searchString = extractRequestParams(JSON.parse(jsonBody)).as('searchString');
+  'dsys'.split('').forEach(async (letter) => {
+    await it(`should handle search new letter "${letter}"`, () => {
+      cy.get('@searchInputEl').should('be.visible').click().should('have.focus').type(letter);
 
-        const parsedParams = new URLSearchParams(searchString);
+      searchTerm += letter;
 
-        // Use the empty hits response fixture if there is not a parameter "query" in POST body
-        const queryString = parsedParams.get('query') != null ? parsedParams.get('query') : NO_RESULT;
+      console.log({searchTerm, letter});
 
-        // Use the empty hits response fixture if the query key is not in the fixture (e.g. no matches on the search)
-        const final = addQueryMetaData({query: queryString, result: data[queryString] || data[NO_RESULT]});
+      cy.get('@searchInputEl').should('have.attr', 'value', searchTerm);
 
-        reply((res) => {
-          res.send(final);
-        });
-      }).as('searchRequest');
+      cy.wait('@searchRequest');
+
+      cy.get('[id*="algolia-autocomplete-listbox"]')
+        .should('be.visible')
+        .find('[role="option"]')
+        .should('have.length.above', 0);
     });
-
-    // Create alias to store the input text for our tests
-    cy.wrap('').as('inputText');
-  });
-
-  it('should handle search (basic)', () => {
-    testSearch('hello');
-  });
-
-  it('should handle search (for no results)', () => {
-    testSearch('?!$%^');
   });
 });
