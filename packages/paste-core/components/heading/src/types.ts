ab0b98bc78e81731a443234a4f8e32d4f4f6b0a6
Glorili Alejandro <galejandro@twilio.com>
2021-12-16 17:51:26 -0800

    feat(heading): add display prop
    chore(heading): give display a default value

--- packages/paste-core/components/heading/src/types.ts
+++ packages/paste-core/components/heading/src/types.ts
@@ -3,7 +3,7 @@ import type {TextProps} from '@twilio-paste/text';
 export type AsTags = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'div' | 'label' | 'span' | 'header';
 export type HeadingVariants = 'heading10' | 'heading20' | 'heading30' | 'heading40' | 'heading50' | 'heading60';
 
-export interface HeadingProps extends React.HTMLAttributes<HTMLHeadingElement>, Pick<TextProps, 'element'> {
+export interface HeadingProps extends React.HTMLAttributes<HTMLHeadingElement>, Pick<TextProps, 'element' | 'display'> {
   as: AsTags;
   className?: never;
   id?: string;
