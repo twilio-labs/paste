80995f9f25aaa33853107b2bfdc9ea1b1045897d
Andi Oneto <aoneto@twilio.com>
2021-12-17 12:51:30 -0800

    chore(website): start to set up applitools eyes

--- cypress/integration/e2e/components/overview-page.spec.ts
+++ cypress/integration/e2e/components/overview-page.spec.ts
@@ -1,3 +1,8 @@
+export const standardViewport = {
+  width: 1600,
+  height: 1800,
+};
+
 describe('Overview page', () => {
   beforeEach(() => {
     cy.visit('/components');
@@ -7,5 +12,23 @@ describe('Overview page', () => {
     cy.pageHeaderShouldBeVisible('Components');
 
     cy.overviewTableRendersCorrectly();
+  });
+
+  it('test eyes', () => {
+    cy.openTwilioEyes({
+      batchName: 'Testing testing',
+      testName: 'Test eyes',
+      browser: standardViewport,
+    });
+
+    cy.visit('/components');
+
+    cy.checkTwilioEyes({
+      tag: 'testing testing testing',
+    });
+
+    cy.wait(500);
+
+    cy.closeTwilioEyes();
   });
 });
