38100280fe87650894ca6d7077a9e2224974d4ca
Lee <leewhite128@gmail.com>
2022-01-12 21:29:03 -0800

    feat: add storybook examples for vrt, incl storybook-addon-gatsby

--- packages/paste-website/stories/SidebarNavigation.stories.tsx
+++ packages/paste-website/stories/SidebarNavigation.stories.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {Box} from '@twilio-paste/box';
 
 import {SidebarAnchor} from '../src/components/site-wrapper/sidebar/SidebarAnchor';
 import {SidebarSeparator} from '../src/components/site-wrapper/sidebar/SidebarSeparator';
@@ -6,15 +7,83 @@ import {SidebarDisclosure} from '../src/components/site-wrapper/sidebar/sidebar-
 import {SidebarDisclosureButton} from '../src/components/site-wrapper/sidebar/sidebar-disclosure/SidebarDisclosureButton';
 import {SidebarDisclosureContent} from '../src/components/site-wrapper/sidebar/sidebar-disclosure/SidebarDisclosureContent';
 
-// export const BasicSidebarAnchor = (): React.ReactNode => (
-//   <SidebarAnchor to='https://www.google.com'>Sample</SidebarAnchor>
-// );
+export const ComplexSidebar = (): React.ReactNode => (
+  <Box as="ul" listStyleType="none">
+    <SidebarAnchor level={0} to="https://www.google.com">
+      Level 0 anchor
+    </SidebarAnchor>
+    <SidebarAnchor level={0} to="https://www.google.com">
+      Level 0 anchor
+    </SidebarAnchor>
+    <SidebarDisclosure categoryRoute="/introduction" visible>
+      <SidebarDisclosureButton level={0}>Level 0 disclosure</SidebarDisclosureButton>
+      <SidebarDisclosureContent>
+        <SidebarAnchor level={1} to="https://www.google.com">
+          Level 1 anchor
+        </SidebarAnchor>
+        <SidebarDisclosure categoryRoute="/introduction/for-designers" visible>
+          <SidebarDisclosureButton level={1}>Level 1 disclosure</SidebarDisclosureButton>
+          <SidebarDisclosureContent>
+            <SidebarAnchor level={2} to="https://www.google.com">
+              Level 2 anchor
+            </SidebarAnchor>
+            <SidebarAnchor level={2} to="https://www.google.com">
+              Level 2 anchor
+            </SidebarAnchor>
+          </SidebarDisclosureContent>
+        </SidebarDisclosure>
+      </SidebarDisclosureContent>
+    </SidebarDisclosure>
+  </Box>
+);
 
-// export const NestedSidebarAnchor = (): React.ReactNode => (
-//   <SidebarAnchor nested to='https://www.google.com'>Nested Sample</SidebarAnchor>
-// );
+export const SidebarAnchorLevels = (): React.ReactNode => (
+  <Box as="ul" listStyleType="none">
+    <SidebarAnchor level={0} to="https://www.google.com">
+      Level 0 anchor
+    </SidebarAnchor>
+    <SidebarAnchor level={1} to="https://www.google.com">
+      Level 1 anchor
+    </SidebarAnchor>
+    <SidebarAnchor level={2} to="https://www.google.com">
+      Level 2 anchor
+    </SidebarAnchor>
+  </Box>
+);
 
-export const SeparatorExample = (): React.ReactNode => <SidebarSeparator />;
+export const BasicSidebarDisclosure = (): React.ReactNode => (
+  <Box as="ul" listStyleType="none">
+    <SidebarDisclosure categoryRoute="/introduction" visible>
+      <SidebarDisclosureButton level={0}>Level 0 disclosure</SidebarDisclosureButton>
+      <SidebarDisclosureContent>
+        <SidebarAnchor level={1} to="https://www.google.com">
+          Level 1 anchor
+        </SidebarAnchor>
+        <SidebarAnchor level={1} to="https://www.google.com">
+          Level 1 anchor
+        </SidebarAnchor>
+      </SidebarDisclosureContent>
+    </SidebarDisclosure>
+  </Box>
+);
+
+export const NestedSidebarDisclosure = (): React.ReactNode => (
+  <Box as="ul" listStyleType="none">
+    <SidebarDisclosure categoryRoute="/introduction" visible>
+      <SidebarDisclosureButton level={1}>Level 1 disclosure</SidebarDisclosureButton>
+      <SidebarDisclosureContent>
+        <SidebarAnchor level={2} to="https://www.google.com">
+          Level 2 anchor
+        </SidebarAnchor>
+        <SidebarAnchor level={2} to="https://www.google.com">
+          Level 2 anchor
+        </SidebarAnchor>
+      </SidebarDisclosureContent>
+    </SidebarDisclosure>
+  </Box>
+);
+
+export const BasicSeparator = (): React.ReactNode => <SidebarSeparator />;
 
 export default {
   title: 'Website/SidebarNavigation',
