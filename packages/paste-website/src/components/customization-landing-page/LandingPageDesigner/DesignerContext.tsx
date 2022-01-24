e02424fc06b623cbd3dfe477df9cd727e6b24298
TheSisb <shadiisber@gmail.com>
2022-01-24 12:01:53 -0600

    fix(design-tokens): check data-visualization tokens w/ backgroundBody

--- packages/paste-website/src/components/customization-landing-page/LandingPageDesigner/DesignerContext.tsx
+++ packages/paste-website/src/components/customization-landing-page/LandingPageDesigner/DesignerContext.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import * as Tokens from '@twilio-paste/core/design-tokens';
+import * as Tokens from '@twilio-paste/design-tokens';
 import type {GenericTokensShape} from '@twilio-paste/design-tokens/types/GenericTokensShape';
 
 export type DesignerContextProps = {
