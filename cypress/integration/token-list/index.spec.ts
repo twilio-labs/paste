6a47d75b3e63df7d11a6dd8fad9677d3a6c52d88
Andi Oneto <aoneto@twilio.com>
2022-01-27 15:23:18 -0800

    test(meow): meow

--- cypress/integration/token-list/index.spec.ts
+++ cypress/integration/token-list/index.spec.ts
@@ -4,6 +4,45 @@ const AVAILABLE_BACKGROUND_COLOR = 'rgb(20, 176, 83)';
 
 const NEW_CONTENT = 'hello test test test test';
 
+const changeContent = () => {
+  cy.get('p')
+    .first()
+    .then(($el) => {
+      $el.append(NEW_CONTENT);
+      console.log($el.text());
+
+      cy.wrap($el);
+    })
+    .contains(NEW_CONTENT)
+    .end();
+};
+
+const changeElSize = () => {
+  cy.get('[data-cy="tokens-table-container"]')
+    .then(($el) => {
+      $el.css('width', '88px');
+      cy.wrap($el);
+    })
+    .should('have.css', 'width')
+    .then((width) => {
+      const numericalWidth = Number(((width as unknown) as string)?.replace('px', ''));
+      expect(Math.round(numericalWidth)).to.equal(88);
+    })
+    .end();
+};
+
+const changeBackgroundColor = () => {
+  cy.get('p')
+    .first()
+    .then(($el) => {
+      $el.css('background-color', AVAILABLE_BACKGROUND_COLOR);
+
+      cy.wrap($el);
+    })
+    .should('have.css', 'background-color', AVAILABLE_BACKGROUND_COLOR)
+    .end();
+};
+
 describe('Token list filter', function () {
   // ℹ️ We are able to reference `this.title` because we have bound this describe block to the Cypress context.
   const testSuiteName = this.title;
@@ -15,10 +54,12 @@ describe('Token list filter', function () {
     cy.visit('/tokens');
   });
 
-  it.skip('searches for a background color', () => {
-    cy.get('input[name="tokens-filter"]').type('background').should('have.value', 'background');
-    cy.get('#background-colors').should('exist');
-    cy.get('#border-colors').should('not.exist');
+  beforeEach(() => {
+    // changeContent();
+
+    // changeBackgroundColor();
+
+    changeElSize();
   });
 
   it('test actual', () => {
@@ -26,8 +67,11 @@ describe('Token list filter', function () {
     if (eyesAreEnabled()) {
       cy.log('[Applitools]: Eyes are enabled, proceed with eyes check.');
       const openParams: Partial<Eyes.Open.Options> = {
-        ...DEFAULT_OPEN_PARAMS,
-        batchId,
+        // Match level: "Strict", "Layout"
+        // // Will not consider: XXXX"Exact", XXXX"Content"
+        matchLevel: 'Layout', // !!!!!!!
+        branchName: 'test/website-vrt',
+        baselineBranch: 'test/website-vrt',
         batchName: `${batchId}-vrt-website`,
         testName: testSuiteName,
       };
@@ -36,8 +80,10 @@ describe('Token list filter', function () {
 
       cy.eyesOpen(openParams);
 
+      // https://help.applitools.com/hc/en-us/articles/4408368066957-useDom-and-enablePatterns-Flags
       const checkParams: Partial<Eyes.Check.Options> = {
-        ...DEFAULT_CHECK_PARAMS,
+        fully: true,
+        ignoreDisplacements: true, // !!!!!!!
         tag: `${testSuiteName}-actual`,
       };
 
@@ -50,151 +96,4 @@ describe('Token list filter', function () {
       cy.log('[Applitools]: Eyes not enabled, skipping eyes check');
     }
   });
-
-  it.skip('test content change', () => {
-    cy.get('p')
-      .first()
-      .then(($el) => {
-        $el.append(NEW_CONTENT);
-        console.log($el.text());
-
-        cy.wrap($el);
-      })
-      .contains(NEW_CONTENT);
-
-    if (eyesAreEnabled()) {
-      cy.log('[Applitools]: Eyes are enabled, proceed with eyes check.');
-      const openParams: Partial<Eyes.Open.Options> = {
-        ...DEFAULT_OPEN_PARAMS,
-        batchId,
-        batchName: `${batchId}-vrt-website`,
-        testName: testSuiteName,
-      };
-
-      cy.log(`starting eyes with these params: ${openParams}`);
-
-      cy.eyesOpen(openParams);
-
-      const checkParams: Partial<Eyes.Check.Options> = {
-        ...DEFAULT_CHECK_PARAMS,
-        tag: `${testSuiteName}-actual`,
-      };
-
-      cy.log(`[Applitools]: Checking window with these params: ${checkParams}`);
-
-      cy.eyesCheckWindow(checkParams);
-
-      cy.eyesClose();
-    } else {
-      cy.log('[Applitools]: Eyes not enabled, skipping eyes check');
-    }
-  });
-
-  // it('test background color change', () => {
-  //   cy.get('p')
-  //     .first()
-  //     .then(($el) => {
-  //       $el.css('background-color', AVAILABLE_BACKGROUND_COLOR);
-
-  //       cy.wrap($el);
-  //     })
-  //     .should('have.css', 'background-color', AVAILABLE_BACKGROUND_COLOR);
-
-  //   if (eyesAreEnabled()) {
-  //     cy.log('[Applitools]: Eyes are enabled, proceed with eyes check.');
-  //     const openParams: Partial<Eyes.Open.Options> = {
-  //       ...DEFAULT_OPEN_PARAMS,
-  //       batchId,
-  //       batchName: `${batchId}-vrt-website`,
-  //       testName: testSuiteName,
-  //     };
-
-  //     cy.log(`starting eyes with these params: ${openParams}`);
-
-  //     cy.eyesOpen(openParams);
-
-  //     const checkParams: Partial<Eyes.Check.Options> = {
-  //       ...DEFAULT_CHECK_PARAMS,
-  //       tag: `${testSuiteName}-actual`,
-  //     };
-
-  //     cy.log(`[Applitools]: Checking window with these params: ${checkParams}`);
-
-  //     cy.eyesCheckWindow(checkParams);
-
-  //     cy.eyesClose();
-  //   } else {
-  //     cy.log('[Applitools]: Eyes not enabled, skipping eyes check');
-  //   }
-  // });
-
-  // it('test layout change', () => {
-  //   cy.get('[data-cy="tokens-table-container"]')
-  //     .then(($el) => {
-  //       $el.css('width', '88px');
-  //       cy.wrap($el);
-  //     })
-  //     .should('have.css', 'width')
-  //     .then((width) => {
-  //       const numericalWidth = Number(((width as unknown) as string)?.replace('px', ''));
-  //       expect(Math.round(numericalWidth)).to.equal(88);
-  //     });
-
-  //   if (eyesAreEnabled()) {
-  //     cy.log('[Applitools]: Eyes are enabled, proceed with eyes check.');
-  //     const openParams: Partial<Eyes.Open.Options> = {
-  //       ...DEFAULT_OPEN_PARAMS,
-  //       batchId,
-  //       batchName: `${batchId}-actual`,
-  //       testName: testSuiteName,
-  //     };
-
-  //     cy.log(`starting eyes with these params: ${openParams}`);
-
-  //     cy.eyesOpen(openParams);
-
-  //     const checkParams: Partial<Eyes.Check.Options> = {
-  //       ...DEFAULT_CHECK_PARAMS,
-  //     };
-
-  //     cy.log(`[Applitools]: Checking window with these params: ${checkParams}`);
-
-  //     cy.eyesCheckWindow(checkParams);
-
-  //     cy.eyesClose();
-  //   } else {
-  //     cy.log('[Applitools]: Eyes not enabled, skipping eyes check');
-  //   }
-  // });
-
-  // describe.skip('Visual regression tests', () => {
-  //   it('Basic VRT', () => {
-  //     cy.log('[Applitools]: checking if eyes are enabled');
-  //     if (eyesAreEnabled()) {
-  //       cy.log('[Applitools]: Eyes are enabled, proceed with eyes check.');
-  //       const openParams: Partial<Eyes.Open.Options> = {
-  //         ...DEFAULT_OPEN_PARAMS,
-  //         testName: testSuiteName,
-  //       };
-
-  //       cy.log(`starting eyes with these params: ${openParams}`);
-
-  //       cy.eyesOpen(openParams);
-
-  //       const checkParams: Partial<Eyes.Check.Options> = {
-  //         ...DEFAULT_CHECK_PARAMS,
-  //         tag: 'local testing configuration',
-  //       };
-
-  //       cy.log(`[Applitools]: Checking window with these params: ${checkParams}`);
-
-  //       cy.eyesCheckWindow(checkParams);
-
-  //       cy.eyesClose();
-  //     } else {
-  //       cy.log('[Applitools]: Eyes not enabled, skipping eyes check');
-  //     }
-  //     expect(true).to.equal(true);
-  //   });
-  // });
 });
--- cypress/support/utils/applitools.ts
+++ cypress/support/utils/applitools.ts
@@ -3,12 +3,12 @@ enum EnvironmentVariables {
 }
 
 export const DEFAULT_OPEN_PARAMS: Partial<Eyes.Open.Options> = {
-  matchLevel: 'Strict',
+  matchLevel: 'Layout',
   branchName: 'test/website-vrt',
   baselineBranch: 'test/website-vrt',
 };
 // https://help.applitools.com/hc/en-us/articles/4408368066957-useDom-and-enablePatterns-Flags
-export const DEFAULT_CHECK_PARAMS: Partial<Eyes.Check.Options> = {fully: true, useDom: true, enablePatterns: true};
+export const DEFAULT_CHECK_PARAMS: Partial<Eyes.Check.Options> = {fully: true};
 
 export const eyesAreEnabled = (): boolean => {
   const eyesEnabled = Cypress.env(EnvironmentVariables.UseCypressEyes);
