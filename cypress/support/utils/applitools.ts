be5c0911812683395396c56009a15698d777e1b4
Jade Pennig <jade@pennig.name>
2022-02-24 21:57:28 -0600

    chore(applitools): ensure Applitools does full-page capture

--- cypress/support/utils/applitools.ts
+++ cypress/support/utils/applitools.ts
@@ -3,16 +3,16 @@ enum EnvironmentVariables {
 }
 
 export const DEFAULT_OPEN_PARAMS: Partial<Eyes.Open.Options> = {
-  // ℹ️ Instructs matching algorithm to verify layout only.
-  // Read more: https://help.applitools.com/hc/en-us/articles/360007188591-Match-Levels
-  matchLevel: 'Layout',
+  batchName: 'Paste',
 };
 
 export const DEFAULT_CHECK_PARAMS: Partial<Eyes.Check.Options> = {
-  // ℹ️ Distinguish between mismatches that are caused by content that has moved on the page,
-  // as opposed to content that is new, missing, or has changed.
-  // Read more: https://applitools.com/docs/api/eyes-sdk/classes-gen/class_configuration/method-configuration-setignoredisplacements-imagesappkit-objectivec.html
-  ignoreDisplacements: true,
+  target: 'region',
+  selector: {
+    type: 'css',
+    selector: '#styled-site-body',
+  },
+  fully: true,
 };
 
 /** eyesAreEnabled: Extract environment variable for eyes usage
@@ -32,3 +32,16 @@ export const getLocalRunConfig = () => ({
   baselineBranch: 'test/website-vrt',
   batchName: `${Date.now()}-vrt-website`,
 });
+
+/**
+ * Applitools has a problem w/ capturing with smooth scroll behavior.
+ * This will unset smooth scrolling so Applitools can do a proper capture.
+ */
+export const prepareForEyes = () => {
+  cy.document().then((document) => {
+    const styledSiteBody = document.getElementById('styled-site-body');
+    if (styledSiteBody) {
+      styledSiteBody.style.scrollBehavior = 'auto';
+    }
+  });
+};
