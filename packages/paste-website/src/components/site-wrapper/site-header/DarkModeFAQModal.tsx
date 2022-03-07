0c4e73928b496fa54c3a15a37bb5d6af1c903b3a
Katie Porter <katie@janekporter.com>
2022-03-07 15:23:20 -0700

    fix(website): add spaces before Anchors and change heading size

--- packages/paste-website/src/components/site-wrapper/site-header/DarkModeFAQModal.tsx
+++ packages/paste-website/src/components/site-wrapper/site-header/DarkModeFAQModal.tsx
@@ -9,7 +9,7 @@ export const DarkModeFAQModal: React.FC = () => {
     <>
       <Paragraph>
         We’re excited to release Dark theme in Paste! We’ve done this work in
-        partnership with
+        partnership with{" "}
         <Anchor href="https://www.twilio.com/flex" showExternal>
           Flex
         </Anchor>
@@ -24,7 +24,7 @@ export const DarkModeFAQModal: React.FC = () => {
         </Anchor>
         .
       </Paragraph>
-      <Heading variant="heading20" as="h2">
+      <Heading variant="heading30" as="h2">
         Why did we make a Dark theme?
       </Heading>
       <Paragraph>
@@ -37,7 +37,7 @@ export const DarkModeFAQModal: React.FC = () => {
         And with Paste’s Dark theme, it’s simple to enable dark mode in your
         products if your UIs are built completely with Paste.
       </Paragraph>
-      <Heading variant="heading20" as="h2">
+      <Heading variant="heading30" as="h2">
         But wait, what’s the difference between “dark mode” and the “Dark theme”
         in Paste?
       </Heading>
@@ -48,7 +48,7 @@ export const DarkModeFAQModal: React.FC = () => {
         what Paste theme is used for the UI. Paste’s Dark theme allows you, our
         consumers, to enable light/dark mode in your products with ease.
       </Paragraph>
-      <Heading variant="heading20" as="h2">
+      <Heading variant="heading30" as="h2">
         When can I start using Dark theme?
       </Heading>
       <Paragraph>
@@ -62,7 +62,7 @@ export const DarkModeFAQModal: React.FC = () => {
         dropdown from another library, when you switch into dark mode, that one
         component will remain light and look very much out of place.
       </Paragraph>
-      <Heading variant="heading20" as="h2">
+      <Heading variant="heading30" as="h2">
         How can I start using Dark theme?
       </Heading>
       <Paragraph>
@@ -72,11 +72,11 @@ export const DarkModeFAQModal: React.FC = () => {
         </SiteLink>
         . Simply change it from “default” to “dark”.
       </Paragraph>
-      <Heading variant="heading20" as="h2">
+      <Heading variant="heading30" as="h2">
         How can I start using Dark theme in Figma?
       </Heading>
       <Paragraph>
-        We have a
+        We have a{" "}
         <Anchor
           href="https://www.figma.com/file/TZMBaCxXkX8u5aVRVYboid/Paste-Dark-Theme"
           showExternal
@@ -89,7 +89,7 @@ export const DarkModeFAQModal: React.FC = () => {
       <Paragraph>
         We are planning to provide documentation around the best way to use the
         Dark theme library in your work very soon, but in the meantime, feel
-        free to test out Figma’s
+        free to test out Figma’s{" "}
         <Anchor
           href="https://help.figma.com/hc/en-us/articles/4404856784663-Swap-style-and-component-libraries"
           showExternal
@@ -103,14 +103,14 @@ export const DarkModeFAQModal: React.FC = () => {
         Figma. The library swap won’t work for raw hex codes, because Figma
         won’t know what to swap them to.
       </Paragraph>
-      <Heading variant="heading20" as="h2">
+      <Heading variant="heading30" as="h2">
         Where can I see the Dark theme in action?
       </Heading>
       <Paragraph>
         Currently, you can see Dark theme in the Paste documentation site, Flex
         UI 2.0, and the new Billing interface, Billio.
       </Paragraph>
-      <Heading variant="heading20" as="h2">
+      <Heading variant="heading30" as="h2">
         When do we get to see Dark theme in Twilio Console?
       </Heading>
       <Paragraph>
@@ -118,18 +118,18 @@ export const DarkModeFAQModal: React.FC = () => {
         then will we be able to unlock dark mode in Console via Paste’s Dark
         theme. Get migrating, Twilions!
       </Paragraph>
-      <Heading variant="heading20" as="h2">
+      <Heading variant="heading30" as="h2">
         Does it pass our accessibility standards?
       </Heading>
       <Paragraph>
         Dark theme now lives up to our inclusive design benchmarks and is fully
         accessible.
       </Paragraph>
-      <Heading variant="heading20" as="h2">
+      <Heading variant="heading30" as="h2">
         Where do I go with feedback?
       </Heading>
       <Paragraph>
-        You can leave thoughts, questions, and feedback in our
+        You can leave thoughts, questions, and feedback in our{" "}
         <Anchor
           href="https://help.figma.com/hc/en-us/articles/4404856784663-Swap-style-and-component-libraries"
           showExternal
