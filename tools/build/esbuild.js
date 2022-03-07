2b84492d7516fa6e0bed0338e2ecceea3824dbec
Si Taggart <me@simontaggart.com>
2022-03-06 19:21:45 -0800

    fix: do you work now?

--- tools/build/esbuild.js
+++ tools/build/esbuild.js
@@ -1,6 +1,89 @@
 const esbuild = require('esbuild');
-const {EsmExternalsPlugin} = require('./build-plugins/EsmExternals');
-const {PasteCJSResolverPlugin} = require('./build-plugins/PasteCJSResolver');
+
+// https://github.com/sindresorhus/escape-string-regexp but it's esm and our node scripts
+// can't handle importing them, so copy pasta'd here as it doesn't seem like a big deal.
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function escapeStringRegexp(string) {
+  if (typeof string !== 'string') {
+    throw new TypeError('Expected a string');
+  }
+
+  // Escape characters with special meaning either inside or outside character sets.
+  // Use a simple backslash escape when it’s always valid, and a `\xnn` escape when the simpler form would be disallowed by Unicode patterns’ stricter grammar.
+  // eslint-disable-next-line unicorn/better-regex
+  return string.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&').replace(/-/g, '\\x2d');
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function makeFilter(externals) {
+  return new RegExp(
+    // eslint-disable-next-line prefer-template
+    '^(' + externals.map(escapeStringRegexp).join('|') + ')(\\/.*)?$' // TODO support for query strings?
+  );
+}
+
+const NAME = 'esm-externals';
+const NAMESPACE = NAME;
+
+// this is a modified version of https://github.com/remorses/esbuild-plugins/tree/master/esm-externals
+// this plugin worked, but was a little over the top in that it tried to convert existing import
+// statements too, not just require calls. That would lead to correct import statements like
+// import JSON from '@twilio-paste/package/jsonfile.json'
+// to be converted incorrectly to
+// import * as JSON from '@twilio-paste/pacakge/jsonfile.json'
+// which would then blow up webpack 5. By targeting only require statements, we solve this
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function EsmExternalsPlugin({externals}) {
+  return {
+    name: NAME,
+    // eslint-disable-next-line @typescript-eslint/no-shadow
+    setup(build) {
+      const filter = makeFilter(externals);
+      build.onResolve({filter: /.*/, namespace: NAMESPACE}, (args) => {
+        return {
+          path: args.path,
+          external: true,
+        };
+      });
+      build.onResolve({filter}, (args) => {
+        if (args.kind !== 'import-statement') {
+          return {
+            path: args.path,
+            namespace: NAMESPACE,
+          };
+        }
+      });
+      build.onLoad({filter: /.*/, namespace: NAMESPACE}, (args) => {
+        return {
+          contents: `export * as default from ${JSON.stringify(args.path)}; export * from ${JSON.stringify(
+            args.path
+          )};`,
+        };
+      });
+    },
+  };
+}
+
+/**
+ * ESBuild plugin to fix CJS builds in Paste
+ * - Replaces icon imports from /esm/IconName in src to /cjs/IconName in dist.
+ * - Replaces design-tokens' theme src code from tokens.es6 to tokens.common in dist.
+ */
+const PasteCJSResolverPlugin = {
+  name: 'PasteCJSResolver',
+  // eslint-disable-next-line @typescript-eslint/no-shadow
+  setup(build) {
+    // Change all ESM icon imports to CJS
+    build.onResolve({filter: /@twilio-paste\/icons\/esm\//}, ({path}) => {
+      return {path: path.replace('/esm/', '/cjs/'), external: true};
+    });
+
+    // Change all .es6 design-token imports to .common
+    build.onResolve({filter: /\/tokens.es6$/}, ({path}) => {
+      return {path: path.replace('.es6', '.common'), external: true};
+    });
+  },
+};
 
 /**
  * ESBuild handles externals literally so that `@twilio-paste/design-tokens` won't
@@ -51,7 +134,7 @@ async function build(packageJson) {
     bundle: true,
     // Sets the target environment so the code is changed into a format that
     // works  with node12 and the listed browsers
-    target: ['chrome97', 'firefox96', 'safari14', 'edge96', 'node16.14.0'],
+    target: ['chrome66', 'firefox58', 'safari11', 'edge79', 'node12.19.0'],
     define: {
       'process.env.NODE_ENV': `"${process.env.NODE_ENV}"`,
     },
