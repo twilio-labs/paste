f1a18316511778d694a7e21abb16109474aaff9f
Andrea <andreareneeo@gmail.com>
2022-04-08 16:08:11 -0700

    chore(website): add localization ux guidelines page

--- packages/paste-website/src/constants.ts
+++ packages/paste-website/src/constants.ts
@@ -46,6 +46,7 @@ export const SidebarCategoryRoutes = {
   ACCESSIBILITY: '/accessibility',
   FOUNDATIONS: '/foundations',
   CONTENT: '/foundations/content',
+  FOUNDATIONS_LOCALIZATION: '/foundations/localization', // @TODO would like to clean this up it is more manageable.
   PATTERNS: '/patterns',
   EXPERIENCES: '/experiences',
   COMPONENTS: '/components',
