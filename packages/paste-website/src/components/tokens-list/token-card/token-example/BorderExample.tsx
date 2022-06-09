3ccc0b7b1b10e8469d88b222a3e81255890e03d5
Jade Pennig <jade@pennig.name>
2022-06-09 10:28:33 -0500

    feat(token-card): building out token card for all token types (wip checkin)

---
+++ packages/paste-website/src/components/tokens-list/token-card/token-example/BorderExample.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import {Box} from '@twilio-paste/box';
+import type {BoxProps} from '@twilio-paste/box';
+
+type BorderExampleProps = Pick<BoxProps, 'borderWidth'>;
+
+export const BorderExample: React.FC<BorderExampleProps> = ({borderWidth}) => {
+  return (
+    <Box
+      borderRadius="borderRadius20"
+      borderTopStyle="solid"
+      borderTopColor="colorBorder"
+      borderTopWidth={borderWidth}
+      width="100%"
+    />
+  );
+};
---
+++ packages/paste-website/stories/TokenCard.stories.tsx
@@ -0,0 +1,137 @@
+import * as React from 'react';
+import {useTheme} from '@twilio-paste/theme';
+import camelCase from 'lodash/camelCase';
+import {TokenCard} from '../src/components/tokens-list/token-card';
+import Tokens from '@twilio-paste/design-tokens/dist/tokens.gatsby';
+import type {ComponentStory, ComponentMeta} from '@storybook/react';
+
+export default {
+  title: 'Website/Token Card',
+  component: TokenCard,
+} as ComponentMeta<typeof TokenCard>;
+
+interface Token {
+  name: string;
+}
+
+const Template: ComponentStory<typeof TokenCard> = ({name, category}) => {
+  const theme = useTheme();
+  const categoryTokens = Tokens[category];
+  console.log(Tokens);
+
+  const {comment} = categoryTokens.find((token: Token) => {
+    return token.name === name;
+  });
+  let themeCategory: string;
+
+  console.log(categoryTokens);
+
+  // todo: what is the mapping between pure tokens and theme tokens? category names are diff in some cases.
+  switch (category) {
+    case 'box-shadows':
+      themeCategory = 'shadows';
+      break;
+    default:
+      themeCategory = camelCase(category);
+      break;
+  }
+
+  return (
+    <TokenCard
+      category={category}
+      name={camelCase(name)}
+      value={theme[themeCategory][camelCase(name)]}
+      comment={comment}
+    />
+  );
+};
+
+export const BackgroundColorToken = Template.bind({});
+BackgroundColorToken.args = {
+  category: 'background-colors',
+  name: 'color-background-available',
+};
+
+export const BackgroundColorTokenWithBorder = Template.bind({});
+BackgroundColorTokenWithBorder.args = {
+  category: 'background-colors',
+  name: 'color-background-body',
+};
+
+export const BackgroundColorTokenInverse = Template.bind({});
+BackgroundColorTokenInverse.args = {
+  category: 'background-colors',
+  name: 'color-background-inverse',
+};
+
+export const BorderColorToken = Template.bind({});
+BorderColorToken.args = {
+  category: 'border-colors',
+  name: 'color-border',
+};
+
+export const BorderColorInverseToken = Template.bind({});
+BorderColorInverseToken.args = {
+  category: 'border-colors',
+  name: 'color-border-inverse',
+};
+
+export const BorderWidthToken = Template.bind({});
+BorderWidthToken.args = {
+  category: 'border-widths',
+  name: 'border-width-20',
+};
+
+export const BorderRadiusToken = Template.bind({});
+BorderRadiusToken.args = {
+  category: 'radii',
+  name: 'border-radius-circle',
+};
+
+export const BoxShadowToken = Template.bind({});
+BoxShadowToken.args = {
+  category: 'box-shadows',
+  name: 'shadow',
+};
+
+export const FontFamilyToken = Template.bind({});
+FontFamilyToken.args = {
+  category: 'fonts',
+  name: 'font-family-code',
+};
+
+export const FontSizeToken = Template.bind({});
+FontSizeToken.args = {
+  category: 'font-sizes',
+  name: 'font-size-110',
+};
+
+export const FontWeightToken = Template.bind({});
+FontWeightToken.args = {
+  category: 'font-weights',
+  name: 'font-weight-bold',
+};
+
+export const LineHeightTokenLarge = Template.bind({});
+LineHeightTokenLarge.args = {
+  category: 'line-heights',
+  name: 'line-height-110',
+};
+
+export const LineHeightTokenSmall = Template.bind({});
+LineHeightTokenSmall.args = {
+  category: 'line-heights',
+  name: 'line-height-10',
+};
+
+export const TextColorToken = Template.bind({});
+TextColorToken.args = {
+  category: 'text-colors',
+  name: 'color-text-brand-highlight',
+};
+
+export const ZIndexToken = Template.bind({});
+ZIndexToken.args = {
+  category: 'z-indices',
+  name: 'z-index-90',
+};
