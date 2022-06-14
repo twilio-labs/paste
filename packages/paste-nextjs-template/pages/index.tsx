82f325abaaa77f861d1bdd9e27965b4cd64b120d
TheSisb <shadiisber@gmail.com>
2022-06-14 09:47:33 -0500

    chore(nextjs-template): revert removal of helpful links

--- packages/paste-nextjs-template/pages/index.tsx
+++ packages/paste-nextjs-template/pages/index.tsx
@@ -1,7 +1,10 @@
 import Head from 'next/head';
+import {Anchor} from '@twilio-paste/core/anchor';
 import {Heading} from '@twilio-paste/core/heading';
 import {Box} from '@twilio-paste/core/box';
 import {Paragraph} from '@twilio-paste/core/paragraph';
+import {ListItem, UnorderedList} from '@twilio-paste/core/list';
+import {Separator} from '@twilio-paste/core/separator';
 import type {NextPage} from 'next';
 
 const Home: NextPage = () => {
@@ -20,6 +23,40 @@ const Home: NextPage = () => {
         Everything you need to get started using Paste in a Production app. Start by editing{' '}
         <code>pages/index.tsx</code>
       </Paragraph>
+      <Separator orientation="horizontal" verticalSpacing="space120" />
+
+      <Heading as="h2" variant="heading20">
+        Useful links
+      </Heading>
+
+      <UnorderedList>
+        <ListItem>
+          <Heading as="h3" variant="heading30">
+            <Anchor href="https://paste.twilio.design" showExternal>
+              Paste Documentation
+            </Anchor>
+          </Heading>
+          <Paragraph>
+            Start here. Find in-depth information about using the Paste Design System to build your Next app.
+          </Paragraph>
+        </ListItem>
+        <ListItem>
+          <Heading as="h3" variant="heading30">
+            <Anchor href="https://nextjs.org/docs" showExternal>
+              NextJS Documentation
+            </Anchor>
+          </Heading>
+          <Paragraph>Find in-depth information about Next.js features and API.</Paragraph>
+        </ListItem>
+        <ListItem>
+          <Heading as="h3" variant="heading30">
+            <Anchor href="https://vercel.com/import?filter=next.js" showExternal>
+              Deploy
+            </Anchor>
+          </Heading>
+          <Paragraph>Instantly deploy your Next.js site to a public URL with Vercel.</Paragraph>
+        </ListItem>
+      </UnorderedList>
     </Box>
   );
 };
