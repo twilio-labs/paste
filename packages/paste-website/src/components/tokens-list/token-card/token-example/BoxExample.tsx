40a61eb85bacfdec7ccd46c3c2fa34190b467653
Jade Pennig <jade@pennig.name>
2022-06-15 12:35:03 -0500

    feat(token-card): add sizing examples

--- packages/paste-website/src/components/tokens-list/token-card/token-example/BoxExample.tsx
+++ packages/paste-website/src/components/tokens-list/token-card/token-example/BoxExample.tsx
@@ -2,13 +2,14 @@ import * as React from 'react';
 import {Box} from '@twilio-paste/box';
 import type {BoxProps} from '@twilio-paste/box';
 
-type BoxExampleProps = Pick<BoxProps, 'backgroundColor' | 'borderColor' | 'boxShadow' | 'borderRadius'>;
+type BoxExampleProps = Pick<BoxProps, 'backgroundColor' | 'borderColor' | 'boxShadow' | 'borderRadius' | 'size'>;
 
 export const BoxExample: React.FC<BoxExampleProps> = ({
   backgroundColor,
   borderColor,
   boxShadow,
   borderRadius = 'borderRadius20',
+  size = 'sizeSquare110',
 }) => {
   const appliedBorderColor = borderColor
     ? borderColor
@@ -25,7 +26,7 @@ export const BoxExample: React.FC<BoxExampleProps> = ({
       borderColor={appliedBorderColor}
       borderWidth={appliedBorderColor ? 'borderWidth20' : null}
       borderStyle={appliedBorderColor ? 'solid' : null}
-      size="sizeSquare110"
+      size={size}
       borderRadius={borderRadius}
       boxShadow={boxShadow}
     />
