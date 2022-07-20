5199871e62f56459d368176e9bbd828b9038243d
Nora Krantz <75342690+nkrantz@users.noreply.github.com>
2022-07-20 16:23:59 -0400

    test: handlePropValidation (#2545)

--- packages/paste-core/layout/aspect-ratio/__tests__/aspectRatio.spec.tsx
+++ packages/paste-core/layout/aspect-ratio/__tests__/aspectRatio.spec.tsx
@@ -15,3 +15,44 @@ describe('AspectRatio', () => {
     expect(getByTestId('wrapper').firstChild).toHaveAttribute('style', 'padding-bottom: 75%;');
   });
 });
+
+describe('handlePropValidation function', () => {
+  it('should throw an error if no ratio prop was passed', () => {
+    // hide console errors from terminal when throwing expected errors
+    const spy = jest.spyOn(console, 'error');
+    spy.mockImplementation(() => {});
+    expect(() =>
+      render(
+        <AspectRatio ratio="">
+          <p>This is the AspectRatio utility without a passed ratio.</p>
+        </AspectRatio>
+      )
+    ).toThrow(`[Paste: AspectRatio] Missing 'ratio' prop.`);
+  });
+
+  it('should throw an error if passed ratio prop is invalid', () => {
+    // hide console errors from terminal when throwing expected errors
+    const spy = jest.spyOn(console, 'error');
+    spy.mockImplementation(() => {});
+    expect(() =>
+      render(
+        <AspectRatio ratio="10:a">
+          <p>This is the AspectRatio utility without a valid ratio.</p>
+        </AspectRatio>
+      )
+    ).toThrow(`[Paste: AspectRatio] 'ratio' is invalid. Use a colon separated number pattern (4:3).`);
+  });
+
+  it('should throw no errors if passed ratio prop is valid', () => {
+    // hide console errors from terminal when throwing expected errors
+    const spy = jest.spyOn(console, 'error');
+    spy.mockImplementation(() => {});
+    expect(() =>
+      render(
+        <AspectRatio ratio="5:8">
+          <p>This is the AspectRatio utility with a valid ratio.</p>
+        </AspectRatio>
+      )
+    ).not.toThrow();
+  });
+});
