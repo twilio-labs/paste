7224e8f2b5a185a0b6fe72c4170886dc934c62ce
gkassym <gkassym@twilio.com>
2022-07-25 10:35:12 +0300

    fix(tsconfig): using own config

--- apps/vs-code-intellisense/src/test/suite/index.ts
+++ apps/vs-code-intellisense/src/test/suite/index.ts
@@ -12,17 +12,17 @@ export function run(): Promise<void> {
   const testsRoot = path.resolve(__dirname, '..');
 
   return new Promise((c, e) => {
-    glob('**/**.test.js', {cwd: testsRoot}, (err, files) => {
+    glob('**/**.test.js', {cwd: testsRoot}, (err: any, files: string[]) => {
       if (err) {
         return e(err);
       }
 
       // Add files to the test suite
-      files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
+      files.forEach((f: string) => mocha.addFile(path.resolve(testsRoot, f)));
 
       try {
         // Run the mocha test
-        mocha.run((failures) => {
+        mocha.run((failures: number) => {
           if (failures > 0) {
             e(new Error(`${failures} tests failed.`));
           } else {
