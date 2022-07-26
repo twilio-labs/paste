46d14f058f7b1301ac20fdaa4b2220e05b90a4d1
TheSisb <shadiisber@gmail.com>
2022-07-26 09:49:52 -0500

    test(website): fix token list search tests

--- packages/paste-website/src/__tests__/tokensList.test.tsx
+++ packages/paste-website/src/__tests__/tokensList.test.tsx
@@ -1,78 +1,104 @@
 import {filterTokenList} from '../components/tokens-list/helpers';
 
+const DefaultThemeTokens = {
+  'border-widths': [
+    {
+      category: 'border-widths',
+      altValue: null,
+      comment: 'Border width reset',
+      name: 'border-width-0',
+      type: 'size',
+      value: '0',
+    },
+    {
+      category: 'border-widths',
+      altValue: null,
+      comment: 'Constant border width token for border width 10',
+      name: 'border-width-10',
+      type: 'size',
+      value: '1px',
+    },
+  ],
+  'background-colors': [
+    {
+      category: 'background-color',
+      name: 'color-background',
+      value: 'rgb(244, 244, 246)',
+      comment: 'Background color used for containers.',
+      type: 'color',
+      altValue: '#F4F4F6',
+    },
+  ],
+  'border-colors': [
+    {
+      altValue: '#8891AA',
+      comment: 'Default border color',
+      name: 'color-border',
+      type: 'color',
+      uicontrol_contrast_pairing: ['color-background-body'],
+      value: 'rgb(136, 145, 170)',
+      category: 'border-colors',
+    },
+  ],
+};
+
 describe('filterTokenList', () => {
-  let filterString = '';
-  const defaultTokens = [
-    {
-      categoryName: 'background-colors',
-      tokens: [
+  it('correctly filters tokens by name, value, and altValue', () => {
+    const backgroundFilter = filterTokenList('background', DefaultThemeTokens);
+    expect(backgroundFilter).toEqual({
+      'background-colors': [
         {
+          altValue: '#F4F4F6',
           category: 'background-color',
+          comment: 'Background color used for containers.',
           name: 'color-background',
+          type: 'color',
           value: 'rgb(244, 244, 246)',
-          comment: '',
-          type: '',
-          deprecated: false,
         },
       ],
-    },
-    {
-      categoryName: 'border-colors',
-      tokens: [
+    });
+
+    const byValue = filterTokenList('rgb(244, 244, 246)', DefaultThemeTokens);
+    expect(byValue).toEqual({
+      'background-colors': [
         {
-          category: 'border-color',
-          name: 'color-border',
-          value: 'rgb(136, 145, 170)',
-          comment: '',
-          type: '',
-          deprecated: false,
-        },
-      ],
-    },
-  ];
-  const darkTokens = [
-    {
-      categoryName: 'background-colors',
-      tokens: [
-        {
+          altValue: '#F4F4F6',
           category: 'background-color',
+          comment: 'Background color used for containers.',
           name: 'color-background',
-          value: 'rgb(18, 28, 45)',
-          comment: '',
-          type: '',
-          deprecated: false,
+          type: 'color',
+          value: 'rgb(244, 244, 246)',
         },
       ],
-    },
-    {
-      categoryName: 'border-colors',
-      tokens: [
+    });
+
+    const byValue2 = filterTokenList('1px', DefaultThemeTokens);
+    expect(byValue2).toEqual({
+      'border-widths': [
         {
-          category: 'border-color',
-          name: 'color-border',
-          value: 'rgb(96, 107, 133)',
-          comment: '',
-          type: '',
-          deprecated: false,
+          category: 'border-widths',
+          altValue: null,
+          comment: 'Constant border width token for border width 10',
+          name: 'border-width-10',
+          type: 'size',
+          value: '1px',
         },
       ],
-    },
-  ];
-  const props = {
-    defaultTokens: [{node: {tokens: defaultTokens}}],
-    darkTokens: [{node: {tokens: darkTokens}}],
-  };
-  let theme = 'default';
+    });
 
-  it('should correctly filter tokens based on theme', () => {
-    const filteredTokens = filterTokenList(filterString, props, theme);
-    expect(filteredTokens).toEqual(defaultTokens);
-  });
-
-  it('should correctly filter tokens based on theme and filter string', () => {
-    filterString = 'background';
-    theme = 'dark';
-    const filteredTokens = filterTokenList(filterString, props, theme);
-    expect(filteredTokens).toEqual([darkTokens[0]]);
+    const byAltValue = filterTokenList('#8891AA', DefaultThemeTokens);
+    expect(byAltValue).toEqual({
+      'border-colors': [
+        {
+          altValue: '#8891AA',
+          comment: 'Default border color',
+          name: 'color-border',
+          type: 'color',
+          uicontrol_contrast_pairing: ['color-background-body'],
+          value: 'rgb(136, 145, 170)',
+          category: 'border-colors',
+        },
+      ],
+    });
   });
 });
