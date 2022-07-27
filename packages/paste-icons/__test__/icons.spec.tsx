e8bd2e64c36371d2ba9ffebb68b27eba2fc76264
Nora Krantz <nkrantz@twilio.com>
2022-07-27 15:24:24 -0400

    chore: improve test names

--- packages/paste-icons/__test__/icons.spec.tsx
+++ packages/paste-icons/__test__/icons.spec.tsx
@@ -64,18 +64,20 @@ describe('Icons', () => {
 });
 
 describe('icons util functions', () => {
-  const testIconList = ['icon1', 'icon2', 'icon3'];
-  const fileName = 'iAmAFile.tsx';
+  let testIconList: string[];
+  beforeEach(() => {
+    testIconList = ['icon1', 'icon2', 'icon3'];
+  });
 
   describe('tools/templates/buildListTemplate util', () => {
-    it('returns the expected output', () => {
-      const expectedOutput = `module.exports = [\"src/icon1.tsx\",\"src/icon2.tsx\",\"src/icon3.tsx\"];`;
-      expect(buildListTemplate(testIconList)).toEqual(expectedOutput);
+    it('accepts an array of icon names and returns a formatted string for esbuild', () => {
+      const buildList = `module.exports = [\"src/icon1.tsx\",\"src/icon2.tsx\",\"src/icon3.tsx\"];`;
+      expect(buildListTemplate(testIconList)).toEqual(buildList);
     });
   });
 
   describe('tools/templates/jsonTemplate util', () => {
-    it('returns the expected output', () => {
+    it('accepts an array of icon names and returns json', () => {
       const expectedOutput =
         '[{"name":"icon1","svg":"/svg/icon1.svg","cjs":"/cjs/icon1.js","esm":"/esm/icon1.js"},{"name":"icon2","svg":"/svg/icon2.svg","cjs":"/cjs/icon2.js","esm":"/esm/icon2.js"},{"name":"icon3","svg":"/svg/icon3.svg","cjs":"/cjs/icon3.js","esm":"/esm/icon3.js"}]';
       expect(jsonTemplate(testIconList)).toEqual(expectedOutput);
@@ -85,59 +87,61 @@ describe('icons util functions', () => {
   describe('tools/templates/reactIconTemplate util', () => {
     const testReactIconTemplateArgs = {componentName: 'testIcon', svg: 'testIconSVG'};
     const name = testReactIconTemplateArgs.componentName;
-    const output = reactIconTemplate(testReactIconTemplateArgs);
-    it('returns the expected output', () => {
-      expect(typeof output).toBe('string');
-      expect(output.includes(`export interface ${name}Props extends IconWrapperProps`)).toBeTruthy();
-      expect(output.includes(`${name}.displayName = '${name}'`)).toBeTruthy();
+    const TestReactIcon = reactIconTemplate(testReactIconTemplateArgs);
+    it('accepts an object and creates a React component template literal', () => {
+      expect(typeof TestReactIcon).toBe('string');
+      expect(TestReactIcon.includes(`export interface ${name}Props extends IconWrapperProps`)).toBeTruthy();
+      expect(TestReactIcon.includes(`${name}.displayName = '${name}'`)).toBeTruthy();
     });
   });
 
   describe('tools/actions/convertAllAction util', () => {
-    it('filters built files', () => {
+    it('filterBuiltFiles accepts an array of icon files and filters non tsx files', () => {
       expect(filterBuiltFiles(['icon1.tsx', 'icon2', 'icon3.tsx'])).toEqual(['icon1.tsx', 'icon3.tsx']);
     });
   });
 
   describe('tools/actions/convertNewAction util', () => {
-    it('normalizes source files', () => {
+    it('normalizeSourceFiles accepts an array of icon files and filters blocked directories', () => {
       expect(normalizeSourceFiles(['icon1.tsx', 'helpers', 'types', 'icon2.tsx'])).toEqual(['icon1', 'icon2']);
     });
   });
 
   describe('tools/actions/listIconsAction util', () => {
-    it('gets the icon names', async () => {
+    it('gets all the icon names of non blocked icons', async () => {
       const iconNames = await getIconNames();
       expect(iconNames).toMatchSnapshot();
     });
   });
 
   describe('tools/utils.ts utils', () => {
-    it('getOutputComponentName util returns the expected output', () => {
+    it('getOutputComponentName util takes an icon svg and strips extension and suffix and converts to startcase', () => {
       expect(getOutputComponentName('brandLogoIcon.svg')).toEqual('BrandLogoIcon');
     });
 
-    it('pascalCaseWordSplitter util returns the expected output', () => {
+    it('pascalCaseWordSplitter util takes a string and splits at lowercase-uppercase instances', () => {
       expect(pascalCaseWordSplitter('hElLo iM nORA')).toEqual('h El Lo i M n ORA');
     });
-    it('cleanFileName util returns the expected output', () => {
-      expect(cleanFileName(fileName)).toEqual('IAmAFileTsx');
+    it('cleanFileName util takes a file name string and converts to startcase and removes spaces', () => {
+      expect(cleanFileName('iAmAFile.tsx')).toEqual('IAmAFileTsx');
     });
-    it('normalizeFileName util returns the expected output', () => {
-      expect(normalizeFileName(`${fileName}--.svg`)).toEqual('iamafile');
+    it('normalizeFileName util takes a file name and removes dashes and extensions and suffix and converts to lowercase', () => {
+      expect(normalizeFileName('iAmA File.tsx--.svg')).toEqual('iamafile');
     });
 
-    it('removeTsxExtension util returns the expected output', () => {
+    it('removeTsxExtension util takes an icon file and removes the tsx extension', () => {
       expect(removeTsxExtension('icons.tsx')).toEqual('icons');
     });
-    it('getInputPath util returns the expected output', () => {
-      expect(getInputPath(fileName).includes(`paste/packages/paste-icons/svg/${fileName}`)).toBeTruthy();
+    it('getInputPath util takes a file name and returns the complete path', () => {
+      expect(getInputPath('iAmAFile.tsx').includes('paste/packages/paste-icons/svg/iAmAFile.tsx')).toBeTruthy();
     });
-    it('getReactOutputPath util returns the expected output', () => {
-      expect(getReactOutputPath(fileName).includes(`paste/packages/paste-icons/src/IAmAFileTsxIcon.tsx`)).toBeTruthy();
+    it('getReactOutputPath util takes a file name and returns the complete path for the React component', () => {
+      expect(
+        getReactOutputPath('iAmAFile.tsx').includes(`paste/packages/paste-icons/src/IAmAFileTsxIcon.tsx`)
+      ).toBeTruthy();
     });
-    it('getBuildFileName util returns the expected output', () => {
-      expect(getBuildFileName(removeTsxExtension(fileName))).toEqual(`src/${fileName}`);
+    it('getBuildFileName util takes a file and adds src/ and .tsx', () => {
+      expect(getBuildFileName('iAmAFile')).toEqual('src/iAmAFile.tsx');
     });
 
     it('maybeHandleError util throws if error is present', () => {
