bb352019bd55762d3b877eaef9c81b5a9350d049
shleewhite <leewhite128@gmail.com>
2022-08-17 19:50:53 -0700

    feat: add callout component

---
+++ packages/paste-core/components/callout/src/CalloutText.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
+import {Text, safelySpreadTextProps} from '@twilio-paste/text';
+import type {BoxProps} from '@twilio-paste/box';
+
+export interface CalloutTextProps extends Partial<Omit<HTMLParagraphElement, 'children'>> {
+  children?: React.ReactNode;
+  element?: BoxProps['element'];
+}
+
+export const CalloutText = React.forwardRef<HTMLParagraphElement, CalloutTextProps>(
+  ({element = 'CALLOUT_TEXT', children, ...props}, ref) => (
+    <Text
+      {...safelySpreadTextProps(props)}
+      as="p"
+      ref={ref}
+      element={element}
+      color="currentColor"
+      lineHeight="lineHeight40"
+    >
+      {children}
+    </Text>
+  )
+);
+
+CalloutText.displayName = 'CalloutText';
+CalloutText.propTypes = {
+  children: PropTypes.node,
+  element: PropTypes.string,
+};
