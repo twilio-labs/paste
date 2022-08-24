05ae02d3461e0f996017002c3a78db85a33cc8f6
jb-twilio <109178184+jb-twilio@users.noreply.github.com>
2022-08-24 17:23:36 -0400

    feat(website): add ScrollToTopLink UI to tokens page (#2635)

---
+++ packages/paste-website/src/components/tokens-list/ScrollToTopLink.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {Box} from '@twilio-paste/box';
+import {Button} from '@twilio-paste/button';
+import {ArrowUpIcon} from '@twilio-paste/icons/esm/ArrowUpIcon';
+
+export const ScrollToTopLink: React.FC = () => (
+  <Box as="div" position="absolute" top="110vh" bottom="0" right="0">
+    <Box as="span" position="sticky" top="85vh">
+      <Button as="a" href="#paste-docs-content-area" variant="secondary">
+        Scroll to top <ArrowUpIcon decorative />
+      </Button>
+    </Box>
+  </Box>
+);
