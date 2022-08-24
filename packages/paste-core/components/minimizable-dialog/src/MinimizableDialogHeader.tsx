b8c3d33c9b55b6967b075159f8fe490f7cba2d38
Lee White <leewhite128@gmail.com>
2022-08-24 12:22:07 -0700

    feat: create MinimizableDialog component (#2623)
    * feat: add MinimizableDialog component
    
    * chore: add new page to VRT
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ packages/paste-core/components/minimizable-dialog/src/MinimizableDialogHeader.tsx
@@ -0,0 +1,92 @@
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
+import {Box, safelySpreadBoxProps} from '@twilio-paste/box';
+import {Button} from '@twilio-paste/button';
+import {ChevronUpIcon} from '@twilio-paste/icons/esm/ChevronUpIcon';
+import {CloseIcon} from '@twilio-paste/icons/esm/CloseIcon';
+import {ScreenReaderOnly} from '@twilio-paste/screen-reader-only';
+import type {BoxProps} from '@twilio-paste/box';
+import {MinusIcon} from '@twilio-paste/icons/esm/MinusIcon';
+
+import {MinimizableDialogContext} from './MinimizableDialogContext';
+
+export interface MinimizableDialogHeaderProps extends Partial<Omit<HTMLDivElement, 'children'>> {
+  children?: React.ReactNode;
+  i18nDismissLabel?: string;
+  i18nMinimizeLabel?: string;
+  element?: BoxProps['element'];
+}
+
+const MinimizableDialogHeader = React.forwardRef<HTMLDivElement, MinimizableDialogHeaderProps>(
+  (
+    {
+      children,
+      element = 'MINIMIZABLE_DIALOG_HEADER',
+      i18nDismissLabel = 'close',
+      i18nMinimizeLabel = 'minimize',
+      ...props
+    },
+    ref
+  ) => {
+    // @ts-ignore
+    // Property 'hide' does not exist on type 'Partial<PopoverState>'
+    // But reakit docs suggest using it
+    // https://reakit.io/docs/popover/#initial-focus
+    const {hide, minimized, minimize, expand} = React.useContext(MinimizableDialogContext);
+
+    return (
+      <Box
+        {...safelySpreadBoxProps(props)}
+        ref={ref}
+        display="grid"
+        gridTemplateColumns="1fr auto auto"
+        columnGap="space30"
+        backgroundColor="colorBackgroundInverse"
+        paddingX="space70"
+        paddingY="space40"
+        color="colorTextInverse"
+        overflow="hidden"
+        borderTopLeftRadius="borderRadius20"
+        borderTopRightRadius="borderRadius20"
+        element={element}
+      >
+        <Box
+          as="h2"
+          element={`${element}_HEADING`}
+          fontWeight="fontWeightBold"
+          fontSize="fontSize30"
+          lineHeight="lineHeight30"
+          margin="space0"
+        >
+          {children}
+        </Box>
+        <Button
+          element={`${element}_MINIMIZE_BUTTON`}
+          variant="reset"
+          size="reset"
+          onClick={minimized ? expand : minimize}
+          aria-pressed={minimized === undefined ? false : minimized}
+        >
+          {minimized ? (
+            <ChevronUpIcon element={`${element}_MINIMIZE_ICON`} decorative size="sizeIcon20" />
+          ) : (
+            <MinusIcon element={`${element}_MINIMIZE_ICON`} decorative size="sizeIcon20" />
+          )}
+          <ScreenReaderOnly>{i18nMinimizeLabel}</ScreenReaderOnly>
+        </Button>
+        <Button element={`${element}_CLOSE_BUTTON`} variant="reset" size="reset" onClick={hide}>
+          <CloseIcon element={`${element}_CLOSE_ICON`} decorative size="sizeIcon20" />
+          <ScreenReaderOnly>{i18nDismissLabel}</ScreenReaderOnly>
+        </Button>
+      </Box>
+    );
+  }
+);
+
+MinimizableDialogHeader.displayName = 'MinimizableDialogHeader';
+
+MinimizableDialogHeader.propTypes = {
+  children: PropTypes.node,
+};
+
+export {MinimizableDialogHeader};
