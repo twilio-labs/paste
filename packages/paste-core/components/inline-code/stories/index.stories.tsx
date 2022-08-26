5f1b56c63136842c136e85f8a0a8589b5f9cddb3
Glorili Alejandro <galejandro@twilio.com>
2022-08-26 12:46:58 -0700

    chore(website): add docs page

--- packages/paste-core/components/inline-code/stories/index.stories.tsx
+++ packages/paste-core/components/inline-code/stories/index.stories.tsx
@@ -11,16 +11,24 @@ export default {
   component: InlineCode,
 };
 
-// make sure it wraps
 export const Default: Story = () => {
   return <InlineCode>0000001111111122234444444</InlineCode>;
+};
+
+export const ReallyLongContent: Story = () => {
+  return (
+    <InlineCode>
+      000 00 0111111 1122 2344444 44 000 00 0111111 1122 2344444 44 000 00 0111111 1122 2344444 44 000 00 0111111 1122
+      2344444 44
+    </InlineCode>
+  );
 };
 
 export const InlineWithText: Story = () => {
   return (
     <Text as="span">
       If the composite has a single row or column (one-dimensional), the <InlineCode>orientation</InlineCode> value
-      determines which arrow keys can be used to move focus:
+      determines which arrow keys can be used to move focus.
     </Text>
   );
 };
