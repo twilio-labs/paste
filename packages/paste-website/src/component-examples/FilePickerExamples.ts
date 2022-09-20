aae407cc35fde484d674c2a6be34c225fc7337ce
Nora Krantz <nkrantz@twilio.com>
2022-09-20 14:09:13 -0400

    docs: file picker

---
+++ packages/paste-website/src/component-examples/FilePickerExamples.ts
@@ -0,0 +1,75 @@
+export const defaultFilePicker = `
+const FilePickerExample = () => {
+  const id = useUID();
+  const helpText = useUID();
+  return (
+    <>
+    <Label htmlFor={id}>Profile picture</Label>
+    <FilePicker id={id} accept="image/*" aria-describedby={helpText}>
+      <FilePickerButton variant="secondary">Choose a file</FilePickerButton>
+    </FilePicker>
+    <HelpText id={helpText}>Upload an image</HelpText>
+  </>
+  );
+}
+
+render(
+  <FilePickerExample />
+)
+`.trim();
+
+export const disabledFilePicker = `
+const FilePickerExample = () => {
+  const id = useUID();
+  return (
+    <>
+    <Label htmlFor={id}>Receipt to expense</Label>
+    <FilePicker id={id} accept=".pdf" disabled>
+      <FilePickerButton variant="secondary">Choose a file</FilePickerButton>
+    </FilePicker>
+  </>
+  );
+}
+
+render(
+  <FilePickerExample />
+)
+`.trim();
+
+export const requiredFilePicker = `
+const FilePickerExample = () => {
+  const id = useUID();
+  const helpText = useUID();
+  return (
+    <>
+    <Label htmlFor={id} required>Proof of employment</Label>
+    <FilePicker id={id} aria-describedby={helpText} required>
+      <FilePickerButton variant="secondary">Choose a file</FilePickerButton>
+    </FilePicker>
+    <HelpText id={helpText}>Acceptable formats include W2, I9, or latest pay stub.</HelpText>
+  </>
+  );
+}
+
+render(
+  <FilePickerExample />
+)
+`.trim();
+
+export const i18nFilePicker = `
+const FilePickerExample = () => {
+  const id = useUID();
+  return (
+    <>
+    <Label htmlFor={id}>Foto de perfil</Label>
+    <FilePicker id={id} accept="image/*" i18nNoSelectionText="Ningún archivo seleccionado">
+      <FilePickerButton variant="secondary">Seleccione un archivo</FilePickerButton>
+    </FilePicker>
+  </>
+  );
+}
+
+render(
+  <FilePickerExample />
+)
+`.trim();
