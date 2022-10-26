38a72ad0c64ad0e11316cf410505dcec640d0c16
Shadi <TheSisb@users.noreply.github.com>
2022-10-26 18:43:15 -0500

    feat(switch): update API to match Checkbox package (#2753)
    * chore: checkpoint progress
    
    * fix(switch): remove indeterminate, selectAll options, and add changeset
    
    * test(switch): update tests
    
    * fix: customization element handling and add error styling
    
    * docs: update switch docs
    
    * chore: add missing pkg
    
    * fix: smol error on example
    
    * fix: switch checked state
    
    * chore: pr fixes
    
    * fix: switch knob icon color
    
    * fix: pr comments
    
    * docs: update upgrade guide
    
    * fix: knob color and add to percy

---
+++ packages/paste-core/components/switch/src/SwitchGroup.tsx
@@ -0,0 +1,79 @@
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
+import {InlineControlGroup} from '@twilio-paste/inline-control-group';
+import type {InlineControlGroupProps} from '@twilio-paste/inline-control-group';
+import {SwitchContext} from './SwitchContext';
+
+export interface SwitchGroupProps extends InlineControlGroupProps {
+  name: string;
+  onChange?: (checked: boolean) => void;
+}
+
+const SwitchGroup = React.forwardRef<HTMLFieldSetElement, SwitchGroupProps>(
+  (
+    {
+      children,
+      element = 'SWITCH_GROUP',
+      disabled = false,
+      errorText,
+      name,
+      onChange,
+      orientation = 'vertical',
+      i18nRequiredLabel = '(required)',
+      ...props
+    },
+    ref
+  ) => {
+    const onChangeHandler = React.useCallback(
+      (event: React.ChangeEvent<HTMLInputElement>): void => {
+        if (onChange != null) {
+          onChange(event.target.checked);
+        }
+      },
+      [onChange]
+    );
+
+    const contextValue = React.useMemo(() => {
+      return {
+        disabled,
+        name,
+        onChange: onChangeHandler,
+        hasError: errorText ? true : undefined,
+      };
+    }, [disabled, name, onChangeHandler]);
+
+    return (
+      <SwitchContext.Provider value={contextValue}>
+        <InlineControlGroup
+          {...props}
+          element={element}
+          disabled={disabled}
+          errorText={errorText}
+          name={name}
+          orientation={orientation}
+          ref={ref}
+          i18nRequiredLabel={i18nRequiredLabel}
+        >
+          {children}
+        </InlineControlGroup>
+      </SwitchContext.Provider>
+    );
+  }
+);
+
+SwitchGroup.displayName = 'SwitchGroup';
+
+if (process.env.NODE_ENV === 'development') {
+  SwitchGroup.propTypes = {
+    name: PropTypes.string.isRequired,
+    element: PropTypes.string,
+    onChange: PropTypes.func,
+    disabled: PropTypes.bool,
+    errorText: PropTypes.string,
+    helpText: PropTypes.string,
+    orientation: PropTypes.oneOf(['vertical', 'horizontal']),
+    i18nRequiredLabel: PropTypes.string,
+  };
+}
+
+export {SwitchGroup};
--- packages/paste-website/src/component-examples/SwitchExamples.ts
+++ packages/paste-website/src/component-examples/SwitchExamples.ts
@@ -1,12 +1,10 @@
 export const basicSwitch = `
-const SwitchExample = () => {
-  const id = useUID()
-  return (
-    <SwitchContainer id={id} label="Enable notifications">
-      <Switch />
-    </SwitchContainer>
-  )
-}
+const SwitchExample = () => (
+  <Switch name="settings" value="notifications">
+    Notifications
+  </Switch>
+);
+
 render(
   <SwitchExample />
 )
@@ -14,13 +12,13 @@ render(
 
 export const helpTextSwitch = `
 const SwitchExample = () => {
-  const id = useUID()
   return (
-    <SwitchContainer id={id} label="Enroll in Privacy Program" helpText={<Anchor href="#" showExternal>Read more about the program</Anchor>}>
-      <Switch />
-    </SwitchContainer>
-  )
+    <Switch value="privacy-program-enrollment" helpText={<Anchor href="#" showExternal>Read more about the program</Anchor>}>
+      Enroll in Privacy Program
+    </Switch>
+  );
 }
+
 render(
   <SwitchExample />
 )
@@ -28,27 +26,11 @@ render(
 
 export const requiredSwitch = `
 const SwitchExample = () => {
-  const id = useUID()
   return (
-    <SwitchContainer id={id} label="Opt into newsletter updates" required>
-      <Switch />
-    </SwitchContainer>
-  )
+    <Switch required value="newsletter-updates">Opt into newsletter updates</Switch>
+  );
 }
-render(
-  <SwitchExample />
-)
-`.trim();
 
-export const onSwitch = `
-const SwitchExample = () => {
-  const id = useUID()
-  return (
-    <SwitchContainer id={id} label="Enable SMS notifications">
-      <Switch on />
-    </SwitchContainer>
-  )
-}
 render(
   <SwitchExample />
 )
@@ -56,13 +38,29 @@ render(
 
 export const disabledSwitch = `
 const SwitchExample = () => {
-  const id = useUID()
   return (
-    <SwitchContainer id={id} label="Enable email notifications" disabled>
-      <Switch />
-    </SwitchContainer>
-  )
+    <Switch disabled helpText="Your account must have a verified email for this feature.">
+      Enable email notifications
+    </Switch>
+  );
 }
+
+render(
+  <SwitchExample />
+)
+`.trim();
+
+export const onSwitch = `
+const SwitchExample = () => {
+  const [checked, setChecked] = React.useState(true);
+
+  return (
+    <Switch checked={checked} onChange={() => setChecked(!checked)}>
+      Enable SMS notifications
+    </Switch>
+  );
+}
+
 render(
   <SwitchExample />
 )
@@ -70,11 +68,10 @@ render(
 
 export const disabledOnSwitch = `
 const SwitchExample = () => {
-  const id = useUID()
   return (
-    <SwitchContainer id={id} label="Enroll in paperless notifications" disabled>
-      <Switch on />
-    </SwitchContainer>
+    <Switch checked disabled helpText="Your account must have a verified email for this feature.">
+      Enroll in paperless notifications
+    </Switch>
   )
 }
 render(
@@ -84,13 +81,14 @@ render(
 
 export const uncontrolledSwitch = `
 const SwitchExample = () => {
-  const id = useUID()
+  const switchRef = React.useRef(null);
   return (
-    <SwitchContainer id={id} label="I am an uncontrolled Switch">
-      <Switch />
-    </SwitchContainer>
+    <Switch ref={switchRef} name="settings" value="notifications">
+      Notifications
+    </Switch>
   )
 }
+
 render(
   <SwitchExample />
 )
@@ -98,14 +96,107 @@ render(
 
 export const controlledSwitch = `
 const SwitchExample = () => {
-  const id = useUID()
-  const [on, setOn] = React.useState(false)
+  const [on, setOn] = React.useState(false);
+
   return (
-    <SwitchContainer id={id} label="I am a controlled Switch">
-      <Switch on={on} onClick={(evt) => setOn(!on)} />
-    </SwitchContainer>
+    <Switch checked={on} onChange={() => setOn(!on)}>
+      Notifications
+    </Switch>
   )
 }
+render(
+  <SwitchExample />
+)
+`.trim();
+
+export const switchGroup = `
+const SwitchExample = () => {
+  const [checked1, setChecked1] = React.useState(true);
+  const [checked2, setChecked2] = React.useState(false);
+  const [checked3, setChecked3] = React.useState(true);
+
+  return (
+    <SwitchGroup
+      name="email-notifications"
+      legend={
+        <Text as="span" color="currentColor">
+          Adjust your email notification settings
+        </Text>
+      }
+      helpText={<Text as="span">To learn more about configurable settings, visit our <Anchor href="#">help center</Anchor>.</Text>}
+    >
+      <Switch
+        value="marketing-updates"
+        checked={checked1}
+        onChange={() => {
+          setChecked1(!checked);
+        }}
+        helpText={
+          <Text as="span" color="currentColor">
+            Periodic sales and promotions from our partners.
+          </Text>
+        }
+      >
+        Marketing emails
+      </Switch>
+      <Switch
+        value="product-updates"
+        checked={checked2}
+        onChange={() => {
+          setChecked2(!checked2);
+        }}
+        helpText="Get the latest news and updates about our products."
+      >
+        Product updates
+      </Switch>
+      <Switch
+        value="account-updates"
+        checked={checked3}
+        onChange={() => {
+          setChecked3(!checked3);
+        }}
+        helpText="Get the latest news and updates about your account."
+      >
+        Account updates
+      </Switch>
+    </SwitchGroup>
+  );
+}
+
+render(
+  <SwitchExample />
+)
+`.trim();
+
+export const switchGroupInternationalized = `
+const SwitchExample = () => {
+  return (
+    <SwitchGroup
+      orientation="horizontal"
+      name="days"
+      legend="Jours préférés"
+      i18nRequiredLabel="(requis)"
+      required
+    >
+      <Switch value="monday">
+        Lundi
+      </Switch>
+      <Switch value="tuesday">
+        Mardi
+      </Switch>
+      <Switch value="wednesday">
+        Mercredi
+      </Switch>
+      <Switch value="thursday">
+        Jeudi
+      </Switch>
+      <Switch value="friday">
+        Vendredi
+      </Switch>
+    </SwitchGroup>
+  )
+}
+
 render(
   <SwitchExample />
 )
