96b6d4ebf57b439fddd0eab970caf024d0cd7c08
Robert Niznik <rniznik@twilio.com>
2022-12-08 10:36:17 -0500

    chore: update Storybook to v6.5

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,14 +9,15 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-a11y',
-    '@storybook/addon-actions',
-    '@storybook/addon-viewport',
     '@storybook/addon-interactions',
     'storybook-addon-gatsby',
     './addons/google-analytics/register',
   ],
+  framework: '@storybook/react',
   features: {
     interactionsDebugger: true,
+    reactOptions: {legacyRootApi: true},
+    storyStoreV7: true,
   },
   staticDirs: ['./static'],
   typescript: {
@@ -79,6 +80,9 @@ module.exports = {
 
     // FIX: Tell Storybook to look at dev files if available
     config.resolve.mainFields = ['main:dev', 'browser', 'module', 'main'];
+
+    // https://github.com/prismicio-community/storybook-addon-gatsby/issues/7
+    config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/];
 
     return config;
   },
