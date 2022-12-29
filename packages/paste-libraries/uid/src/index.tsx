7a4b4fe8c67b7db834c338bb68573350a6627745
Pablo Espinosa <espipj@gmail.com>
2022-12-29 17:50:47 +0000

    chore: added useUIDSeed custom using React useId method
    (cherry picked from commit d39cdc9daea7af36c066c847b96dd231308336b9)

--- packages/paste-libraries/uid/src/index.tsx
+++ packages/paste-libraries/uid/src/index.tsx
@@ -1,1 +1,16 @@
-export {UIDFork, useUID, useUIDSeed, uid} from 'react-uid';
+import * as React from 'react';
+import {useUID as useUIDLegacy, useUIDSeed as useUIDSeedLegacy, UIDFork, uid} from 'react-uid';
+
+// eslint-disable-next-line no-useless-concat -- Workaround for https://github.com/webpack/webpack/issues/14814
+const maybeReactUseId: undefined | (() => string) = (React as any)['useId' + ''];
+
+const useUID = maybeReactUseId !== undefined ? maybeReactUseId : useUIDLegacy;
+
+const useUIDSeed =
+  maybeReactUseId !== undefined
+    ? () => {
+        return (seed: string) => `${maybeReactUseId()}-${seed}`;
+      }
+    : useUIDSeedLegacy;
+
+export {useUIDSeed, UIDFork, uid, useUID};
