369e730b34c33235ad0b6f50a556c5923521aa51
Pablo Espinosa <espipj@gmail.com>
2023-01-03 18:04:56 +0000

    [AIONAPFMWK-174]: refactor react-uid & React 18 useId (#2903)
    * chore: added useUIDSeed custom using React useId method
    
    (cherry picked from commit d39cdc9daea7af36c066c847b96dd231308336b9)

--- packages/paste-libraries/uid/src/index.tsx
+++ packages/paste-libraries/uid/src/index.tsx
@@ -1,1 +1,20 @@
-export {UIDFork, useUID, useUIDSeed, uid} from 'react-uid';
+import React from 'react';
+import {useUID as useUIDLegacy, useUIDSeed as useUIDSeedLegacy, UIDFork, uid} from 'react-uid';
+
+/**
+ * We found a workaround here but for webpack Workaround for https://github.com/webpack/webpack/issues/14814
+ * The line below is an adaptation to make it work in ESBuild (bundler we're using)
+ */
+const useId = 'useId';
+const maybeReactUseId: undefined | (() => string) = React[useId];
+
+const useUID = maybeReactUseId !== undefined ? maybeReactUseId : useUIDLegacy;
+
+const useUIDSeed =
+  maybeReactUseId !== undefined
+    ? () => {
+        return (seed: string) => `${maybeReactUseId()}-${seed}`;
+      }
+    : useUIDSeedLegacy;
+
+export {useUIDSeed, UIDFork, uid, useUID};
