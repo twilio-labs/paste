b6766e6fc607806490cd2305fa5c2390e548f65a
Robert Niznik <rniznik@twilio.com>
2023-01-06 08:34:47 -0500

    feat: add story for groups

--- packages/paste-core/primitives/listbox/stories/index.stories.tsx
+++ packages/paste-core/primitives/listbox/stories/index.stories.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import {Stack} from '@twilio-paste/stack';
 
-import {useListboxPrimitiveState, ListboxPrimitive, ListboxPrimitiveItem} from '../src';
+import {useListboxPrimitiveState, ListboxPrimitive, ListboxPrimitiveGroup, ListboxPrimitiveItem} from '../src';
 
 // eslint-disable-next-line import/no-default-export
 export default {
   title: 'Primitives/Listbox',
   component: ListboxPrimitive,
-  subcomponents: {ListboxPrimitiveItem},
+  subcomponents: {ListboxPrimitiveItem, ListboxPrimitiveGroup},
 };
 
 const ITEMS = ['Item 1', 'Item 2', 'Item 3'];
@@ -54,6 +54,41 @@ export const HorizontalListbox = (): React.ReactNode => {
           {item}
         </ListboxPrimitiveItem>
       ))}
+    </ListboxPrimitive>
+  );
+};
+
+export const GroupedOptions = (): React.ReactNode => {
+  const [selected, setSelected] = React.useState<string>();
+  const listbox = useListboxPrimitiveState();
+  const renderItem = (item: string, index: number): React.ReactNode => (
+    <ListboxPrimitiveItem
+      key={item}
+      data-testid={`item-${index}`}
+      {...listbox}
+      selected={selected === item}
+      onSelect={() => {
+        setSelected(item);
+      }}
+    >
+      {item}
+    </ListboxPrimitiveItem>
+  );
+
+  return (
+    <ListboxPrimitive {...listbox} aria-label="Grouped">
+      <ListboxPrimitiveGroup aria-labelledby="group-1">
+        <Stack orientation="vertical" spacing="space0">
+          <span id="group-1">Even</span>
+          {ITEMS.filter((item, index) => (index + 1) % 2 === 0).map(renderItem)}
+        </Stack>
+      </ListboxPrimitiveGroup>
+      <ListboxPrimitiveGroup aria-labelledby="group-2">
+        <Stack orientation="vertical" spacing="space0">
+          <span id="group-2">Odd</span>
+          {ITEMS.filter((item, index) => (index + 1) % 2 === 1).map(renderItem)}
+        </Stack>
+      </ListboxPrimitiveGroup>
     </ListboxPrimitive>
   );
 };
