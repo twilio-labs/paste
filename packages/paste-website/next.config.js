9eb10048dbb9f9631a18ac6a468662960913fd80
Robert Niznik <rniznik@twilio.com>
2023-01-11 09:23:30 -0500

    chore: support mdx

--- packages/paste-website/next.config.js
+++ packages/paste-website/next.config.js
@@ -1,4 +1,17 @@
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-require-imports, import/no-extraneous-dependencies
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    providerImportSource: '@mdx-js/react',
+  },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  reactStrictMode: true,
   swcMinify: true,
   experimental: {
     newNextLinkBehavior: true,
@@ -6,3 +19,5 @@ module.exports = {
     legacyBrowsers: false,
   },
 };
+
+module.exports = withMDX(nextConfig);
