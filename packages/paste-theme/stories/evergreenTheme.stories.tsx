00a1062faf77e5b68478aa91c45ee1cb5006ce67
JB <109178184+jb-twilio@users.noreply.github.com>
2023-01-12 18:42:34 -0500

    chore: add Evergreen theme stories

---
+++ packages/paste-theme/stories/evergreenTheme.stories.tsx
@@ -0,0 +1,80 @@
+import * as React from 'react';
+import {Box} from '@twilio-paste/box';
+import {Button} from '@twilio-paste/button';
+import type {ButtonProps} from '@twilio-paste/button';
+import {Callout} from '@twilio-paste/callout';
+import type {CalloutProps} from '@twilio-paste/callout';
+import {Input} from '@twilio-paste/input';
+import {Paragraph} from '@twilio-paste/paragraph';
+import {Heading} from '@twilio-paste/heading';
+import {Select, Option} from '@twilio-paste/select';
+import {Stack} from '@twilio-paste/stack';
+import {ButtonGroup} from '@twilio-paste/button-group';
+import {TextArea} from '@twilio-paste/textarea';
+
+import {ThemeProvider} from '../src/themeProvider';
+
+// eslint-disable-next-line import/no-default-export
+export default {
+  title: 'Theme/Themes',
+  component: ThemeProvider,
+};
+
+export const Evergreen = (): React.ReactNode => (
+  <ThemeProvider theme="evergreen">
+    <Stack orientation="vertical" spacing="space200">
+      <Box>
+        <Heading as="h2" variant="heading10">
+          Evergreen Theme
+        </Heading>
+        <Heading as="h3" variant="heading20">
+          Paragraph and Inputs
+        </Heading>
+        <Paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Nisi porta lorem mollis aliquam ut porttitor leo. Hendrerit gravida rutrum quisque non. A arcu
+          cursus vitae congue mauris rhoncus aenean vel elit. Tortor dignissim convallis aenean et tortor at risus.
+          Vestibulum lorem sed risus ultricies. Tempor nec feugiat nisl pretium fusce id. Morbi tempus iaculis urna id
+          volutpat lacus laoreet non curabitur. In ante metus dictum at. Sit amet risus nullam eget felis eget nunc
+          lobortis.
+        </Paragraph>
+        <Stack orientation="vertical" spacing="space50">
+          <Input aria-label="Search" placeholder="Search options..." type="text" />
+          <Select aria-label="Options">
+            <Option value="option1">Option 1</Option>
+          </Select>
+          <TextArea aria-label="Feedback" value="Lorem ipsum dolor sit amet, consectetur adipiscing elit" />
+        </Stack>
+      </Box>
+      <Box>
+        <Heading as="h3" variant="heading20">
+          Button variants
+        </Heading>
+        <ButtonGroup>
+          {'primary primary_icon secondary secondary_icon destructive destructive_icon destructive_link destructive_secondary link inverse_link inverse reset'
+            .split(' ')
+            .filter((variant) => !variant.includes('icon'))
+            .map((variant) => (
+              <Button key={variant} variant={variant as ButtonProps['variant']} onClick={() => {}}>
+                {variant} variant
+              </Button>
+            ))}
+        </ButtonGroup>
+      </Box>
+      <Box>
+        <Heading as="h3" variant="heading20">
+          Callout variants
+        </Heading>
+        <Stack orientation="horizontal" spacing="space40">
+          {'neutral warning error success new'.split(' ').map((variant) => {
+            return (
+              <Callout key={variant} variant={variant as CalloutProps['variant']}>
+                {variant} variant
+              </Callout>
+            );
+          })}
+        </Stack>
+      </Box>
+    </Stack>
+  </ThemeProvider>
+);
