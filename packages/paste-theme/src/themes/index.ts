7fb936bd53109a0f0e75d0ad7570a4415abe00e1
Robert Niznik <rniznik@twilio.com>
2023-01-13 11:19:30 -0500

    feat(theme): add twilio and twilio-dark themes

--- packages/paste-theme/src/themes/index.ts
+++ packages/paste-theme/src/themes/index.ts
@@ -1,7 +1,9 @@
 import {DefaultTheme} from './default';
 import {SendGridTheme} from './sendgrid';
 import {DarkTheme} from './dark';
+import {TwilioTheme} from './twilio';
+import {TwilioDarkTheme} from './twilio-dark';
 
 const ConsoleTheme = DefaultTheme;
 
-export {DefaultTheme, ConsoleTheme, SendGridTheme, DarkTheme};
+export {DefaultTheme, ConsoleTheme, SendGridTheme, DarkTheme, TwilioTheme, TwilioDarkTheme};
