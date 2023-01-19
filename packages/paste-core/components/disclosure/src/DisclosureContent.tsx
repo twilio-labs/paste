36d03c51690499cfbc2294797d7165d578440e91
shleewhite <leewhite128@gmail.com>
2023-01-18 17:40:10 -0800

    feat: update disclosure styles

--- packages/paste-core/components/disclosure/src/DisclosureContent.tsx
+++ packages/paste-core/components/disclosure/src/DisclosureContent.tsx
@@ -1,17 +1,43 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import {useSpring, animated} from '@twilio-paste/animation-library';
 import {Box, safelySpreadBoxProps} from '@twilio-paste/box';
 import {DisclosurePrimitiveContent} from '@twilio-paste/disclosure-primitive';
+import type {BoxProps, BoxStyleProps} from '@twilio-paste/box';
+import type {DisclosurePrimitiveContentProps} from '@twilio-paste/disclosure-primitive';
 
 import {DisclosureContext} from './DisclosureContext';
-import type {DisclosureContentProps} from './types';
-import {DisclosureContentPropTypes} from './PropTypes';
 
 export const AnimatedDisclosureContent = animated(Box);
 
+const baseContainedStyles: BoxStyleProps = {
+  borderWidth: 'borderWidth10',
+  borderStyle: 'solid',
+  borderBottomLeftRadius: 'borderRadius20',
+  borderBottomRightRadius: 'borderRadius20',
+  borderTop: 'none',
+  transition: 'border-color 100ms ease-out',
+};
+
+const containedStyles: BoxStyleProps = {
+  ...baseContainedStyles,
+  borderColor: 'colorBorderWeaker',
+};
+
+const containedHoverStyles: BoxStyleProps = {
+  ...baseContainedStyles,
+  borderColor: 'colorBorderWeak',
+};
+
+export interface DisclosureContentProps
+  extends Omit<DisclosurePrimitiveContentProps, keyof BoxStyleProps>,
+    Pick<BoxProps, 'element'> {
+  children: NonNullable<React.ReactNode>;
+}
+
 const DisclosureContent = React.forwardRef<HTMLDivElement, DisclosureContentProps>(
   ({children, element = 'DISCLOSURE_CONTENT', visible, ...props}, ref) => {
-    const {disclosure} = React.useContext(DisclosureContext);
+    const {disclosure, variant, isHeadingHovered, isDisabled} = React.useContext(DisclosureContext);
     const {opacity} = useSpring({
       opacity: disclosure.visible ? 1 : 0,
       onRest: disclosure.stopAnimation,
@@ -22,6 +48,14 @@ const DisclosureContent = React.forwardRef<HTMLDivElement, DisclosureContentProp
         duration: 100,
       },
     });
+
+    let variantStyles: BoxStyleProps = {};
+    if (variant === 'contained') {
+      variantStyles = containedStyles;
+      if (isHeadingHovered && !isDisabled) {
+        variantStyles = containedHoverStyles;
+      }
+    }
 
     return (
       <DisclosurePrimitiveContent
@@ -35,6 +69,7 @@ const DisclosureContent = React.forwardRef<HTMLDivElement, DisclosureContentProp
         style={{
           opacity,
         }}
+        {...variantStyles}
       >
         {children}
       </DisclosurePrimitiveContent>
@@ -43,9 +78,9 @@ const DisclosureContent = React.forwardRef<HTMLDivElement, DisclosureContentProp
 );
 
 DisclosureContent.displayName = 'DisclosureContent';
-
-if (process.env.NODE_ENV === 'development') {
-  DisclosureContent.propTypes = DisclosureContentPropTypes;
-}
+DisclosureContent.propTypes = {
+  children: PropTypes.node.isRequired,
+  element: PropTypes.string,
+};
 
 export {DisclosureContent};
