47c485b754193fd7661a8189ebfd2eb7527802f2
gloriliale <77117846+gloriliale@users.noreply.github.com>
2023-02-06 16:16:07 -0800

    chore(input-box): fix disabled preffix/suffix (#3018)

--- packages/paste-core/components/select/stories/select.stories.tsx
+++ packages/paste-core/components/select/stories/select.stories.tsx
@@ -333,7 +333,7 @@ export const SelectDisabledInsertBeforeAndAfter = (): React.ReactNode => {
         onFocus={action('handleFocus')}
         onBlur={action('handleBlur')}
         insertBefore={
-          <Text color="colorTextWeaker" as="span" fontWeight="fontWeightSemibold">
+          <Text color="colorTextWeak" as="span" fontWeight="fontWeightSemibold">
             $10.99
           </Text>
         }
@@ -415,7 +415,7 @@ export const SelectDisabedInsertBeforeAndAfterInverse = (): React.ReactNode => {
         onFocus={action('handleFocus')}
         onBlur={action('handleBlur')}
         insertBefore={
-          <Text as="span" color="colorTextInverseWeaker" lineHeight="lineHeight20" fontWeight="fontWeightSemibold">
+          <Text as="span" color="colorTextInverseWeak" lineHeight="lineHeight20" fontWeight="fontWeightSemibold">
             $10.99
           </Text>
         }
