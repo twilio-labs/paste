521938b62d1e29d382ed2ec93593b48d49ae3af4
shleewhite <leewhite128@gmail.com>
2023-02-22 14:55:09 -0800

    chore: update display heading styles

--- packages/paste-core/components/display-heading/src/index.tsx
+++ packages/paste-core/components/display-heading/src/index.tsx
@@ -57,6 +57,8 @@ const DisplayHeading = React.forwardRef<HTMLHeadingElement, DisplayHeadingProps>
         element={element}
         id={id}
         color="colorText"
+        letterSpacing="0.04em"
+        fontFamily="fontFamilyDisplay"
         variant={variant}
         ref={ref}
       >
