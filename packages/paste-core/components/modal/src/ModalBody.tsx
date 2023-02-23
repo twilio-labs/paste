05075e4c76495fbc9dda3a0ad0311b61032639a5
Lee White <leewhite128@gmail.com>
2023-02-23 13:34:38 -0800

    feat: update Modal and Alert Dialog styles (#3042)

--- packages/paste-core/components/modal/src/ModalBody.tsx
+++ packages/paste-core/components/modal/src/ModalBody.tsx
@@ -3,16 +3,23 @@ import PropTypes from 'prop-types';
 import {Box, safelySpreadBoxProps} from '@twilio-paste/box';
 import type {BoxElementProps} from '@twilio-paste/box';
 
-import {modalBodyStyles} from './styles';
-
 export interface ModalBodyProps extends React.HTMLAttributes<HTMLDivElement> {
   children: NonNullable<React.ReactNode>;
   element?: BoxElementProps['element'];
 }
+
 const ModalBody = React.forwardRef<HTMLDivElement, ModalBodyProps>(
   ({children, element = 'MODAL_BODY', ...props}, ref) => {
     return (
-      <Box {...safelySpreadBoxProps(props)} {...modalBodyStyles} as="div" element={element} ref={ref}>
+      <Box
+        {...safelySpreadBoxProps(props)}
+        overflowY="auto"
+        padding="space90"
+        paddingTop="space0"
+        as="div"
+        element={element}
+        ref={ref}
+      >
         {children}
       </Box>
     );
