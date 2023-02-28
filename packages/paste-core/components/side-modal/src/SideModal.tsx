c9c7ab4965d3a53f589feb4f1da20340102792b4
TheSisb <shadiisber@gmail.com>
2023-02-28 13:00:32 -0600

    chore: checkpoint

--- packages/paste-core/components/side-modal/src/SideModal.tsx
+++ packages/paste-core/components/side-modal/src/SideModal.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import type {BoxProps} from '@twilio-paste/box';
+import {useTransition, animated} from '@twilio-paste/animation-library';
 import {Box, safelySpreadBoxProps} from '@twilio-paste/box';
 import {StyledBase} from '@twilio-paste/theme';
 import {NonModalDialogPrimitive} from '@twilio-paste/non-modal-dialog-primitive';
@@ -11,19 +12,35 @@ const StyledSideModal = React.forwardRef<HTMLDivElement, BoxProps>(({style, ...p
   <Box
     {...safelySpreadBoxProps(props)}
     ref={ref}
-    style={style}
+    style={{...style, position: 'fixed'}}
     boxShadow="shadow"
     width="size80"
     zIndex="zIndex80"
-    transform="none!important"
-    inset="0rem 0rem 0rem auto!important"
+    top="0 !important"
+    left="auto !important"
+    right="0 !important"
+    bottom="0 !important"
     _focus={{
       outline: 'none',
     }}
   />
 ));
-
 StyledSideModal.displayName = 'StyledSideModal';
+
+const AnimatedStyledSideModal = animated(StyledSideModal);
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const getAnimationStates = (): any => ({
+  from: {opacity: 0, transform: `translateX(100%)`},
+  enter: {opacity: 1, transform: `translateX(0%)`},
+  leave: {opacity: 0, transform: `translateX(100%)`},
+  // https://www.react-spring.io/docs/hooks/api
+  config: {
+    mass: 0.5,
+    tension: 370,
+    friction: 26,
+  },
+});
 
 export interface SideModalProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
@@ -34,30 +51,38 @@ export interface SideModalProps extends React.HTMLAttributes<HTMLDivElement> {
 export const SideModal = React.forwardRef<HTMLDivElement, SideModalProps>(
   ({children, element = 'SIDE_MODAL', ...props}, ref) => {
     const dialog = React.useContext(SideModalContext);
+    const transitions = useTransition(dialog.visible, getAnimationStates());
 
     return (
-      <NonModalDialogPrimitive
-        {...dialog}
-        {...safelySpreadBoxProps(props)}
-        as={StyledSideModal}
-        element={`${element}_CONTAINER`}
-        ref={ref}
-        preventBodyScroll={false}
-        hideOnClickOutside={false}
-      >
-        {/* import Paste Theme Based Styles due to portal positioning. */}
-        <StyledBase>
-          <Box
-            element={element}
-            display="grid"
-            gridTemplateRows="auto 1fr auto"
-            height="100vh"
-            backgroundColor="colorBackgroundBody"
-          >
-            {children}
-          </Box>
-        </StyledBase>
-      </NonModalDialogPrimitive>
+      <div>
+        {transitions(
+          (styles, item) =>
+            item && (
+              <NonModalDialogPrimitive
+                {...dialog}
+                {...safelySpreadBoxProps(props)}
+                as={AnimatedStyledSideModal}
+                element={`${element}_CONTAINER`}
+                ref={ref}
+                preventBodyScroll={false}
+                hideOnClickOutside={false}
+                style={styles}
+              >
+                <StyledBase>
+                  <Box
+                    element={element}
+                    display="grid"
+                    gridTemplateRows="auto 1fr auto"
+                    height="100vh"
+                    backgroundColor="colorBackgroundBody"
+                  >
+                    {children}
+                  </Box>
+                </StyledBase>
+              </NonModalDialogPrimitive>
+            )
+        )}
+      </div>
     );
   }
 );
--- packages/paste-core/components/side-modal/src/SideModalContainer.tsx
+++ packages/paste-core/components/side-modal/src/SideModalContainer.tsx
@@ -1,21 +1,18 @@
 import * as React from 'react';
 import type {
-  NonModalDialogPrimitiveStateReturn,
+  NonModalDialogPrimitiveStateReturn as SideModalStateReturn,
   NonModalDialogPrimitivePopoverInitialState,
 } from '@twilio-paste/non-modal-dialog-primitive';
 import {useNonModalDialogPrimitiveState} from '@twilio-paste/non-modal-dialog-primitive';
 
 import {SideModalContext} from './SideModalContext';
 
-export interface SideModalStateReturn extends NonModalDialogPrimitiveStateReturn {
-  [key: string]: any;
-}
-export interface SideModalContainerProps extends NonModalDialogPrimitivePopoverInitialState {
+interface SideModalContainerProps extends NonModalDialogPrimitivePopoverInitialState {
   children: NonNullable<React.ReactNode>;
   state?: SideModalStateReturn;
 }
 
-const BaseSideModalContainer: React.FC<React.PropsWithChildren<SideModalContainerProps>> = ({
+const BaseSideModalContainer: React.FC<SideModalContainerProps> = ({
   gutter,
   children,
   placement,
@@ -36,8 +33,7 @@ const BaseSideModalContainer: React.FC<React.PropsWithChildren<SideModalContaine
 BaseSideModalContainer.displayName = 'BaseSideModalContainer';
 
 const SideModalContainer = React.memo(BaseSideModalContainer);
-
 SideModalContainer.displayName = 'SideModalContainer';
-export {SideModalContainer};
 
-export {useNonModalDialogPrimitiveState as useSideModalState} from '@twilio-paste/non-modal-dialog-primitive';
+export type {SideModalStateReturn, SideModalContainerProps};
+export {SideModalContainer};
