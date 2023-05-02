f09de07177346516c2b53f625bdf41c21e1ae770
TheSisb <shadiisber@gmail.com>
2023-05-02 11:28:09 -0500

    fix(sidebar): overlay content wrapper margin when compact

--- packages/paste-core/components/sidebar/src/SidebarOverlayContentWrapper.tsx
+++ packages/paste-core/components/sidebar/src/SidebarOverlayContentWrapper.tsx
@@ -20,7 +20,7 @@ export const SidebarOverlayContentWrapper = React.forwardRef<HTMLDivElement, Sid
     if (!isMobile && variant === 'compact') {
       return (
         <Box display="flex" justifyContent="flex-start">
-          <Box role="presentation" width="sizeSidebarCompact" height="100%" minHeight="1px" />
+          <Box role="presentation" width="sizeSidebarCompact" height="100%" minHeight="1px" flexShrink={0} />
           <Box {...safelySpreadBoxProps(props)} element={element} as="div" ref={ref} width="100%">
             {children}
           </Box>
