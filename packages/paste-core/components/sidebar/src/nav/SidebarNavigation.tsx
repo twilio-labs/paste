fe201ac873b0785a14f51c2474c55b4928854abc
TheSisb <shadiisber@gmail.com>
2023-05-10 16:19:52 -0500

    fix(sidebar): `SidebarNavigation` fixes and animation

--- packages/paste-core/components/sidebar/src/nav/SidebarNavigation.tsx
+++ packages/paste-core/components/sidebar/src/nav/SidebarNavigation.tsx
@@ -1,56 +1,53 @@
 import * as React from 'react';
-import {Box, safelySpreadBoxProps} from '@twilio-paste/box';
+import {Box} from '@twilio-paste/box';
 import type {BoxProps} from '@twilio-paste/box';
 import {styled, css} from '@twilio-paste/styling-library';
-import {Disclosure, DisclosureHeading, DisclosureContent} from '@twilio-paste/disclosure';
+import type {ThemeShape} from '@twilio-paste/theme';
 
-const SidebarNavigationWrapper = styled(Box)(({theme}) => {
-  const colorA = theme.backgroundColors.colorBackgroundInverse;
-  const colorB = theme.backgroundColors.colorBackgroundInverseStrongest;
+import {SidebarContext} from '../SidebarContext';
+
+const SidebarNavigationWrapper = styled.div(({theme}: {theme: ThemeShape}) => {
+  const {colorBackgroundInverse, colorBackgroundInverseStronger} = theme.backgroundColors;
+
   return css({
-    maxHeight: '100%',
-    overflow: 'auto',
     overflowScrolling: 'touch',
-    flexGrow: 1,
     background: `linear-gradient(
-      ${colorA} 30%,
+      ${colorBackgroundInverse} 30%,
       rgba(255, 255, 255, 0)
     ) center top,
     linear-gradient(
       rgba(255, 255, 255, 0),
-      ${colorA} 70%
+      ${colorBackgroundInverse} 70%
     ) center bottom,
     radial-gradient(
       farthest-side at 50% 0,
-      rgba(0, 0, 0, 0.9),
+      rgba(0, 0, 0, 0.6),
       rgba(0, 0, 0, 0)
     ) center top,
     radial-gradient(
       farthest-side at 50% 100%,
-      rgba(0, 0, 0, 0.9),
+      rgba(0, 0, 0, 0.6),
       rgba(0, 0, 0, 0)
     ) center bottom`,
-
     backgroundRepeat: 'no-repeat',
     backgroundSize: '100% 40px, 100% 40px, 100% 14px, 100% 14px',
     backgroundAttachment: `local, local, scroll, scroll`,
-    padding: 'space50',
-
+    // Scrollbar
     '::-webkit-scrollbar': {
       width: '10px',
     },
     // Track
     '::-webkit-scrollbar-track': {
-      background: 'colorBackgroundInverseStronger',
+      background: colorBackgroundInverse,
     },
     // Handle
     '::-webkit-scrollbar-thumb': {
-      background: '#394762',
+      background: colorBackgroundInverseStronger,
       borderRadius: '10px',
     },
     // Handle on hover
     '::-webkit-scrollbar-thumb:hover': {
-      background: '#555',
+      background: colorBackgroundInverseStronger,
     },
   });
 });
@@ -62,10 +59,22 @@ export interface SidebarNavigationProps extends React.HTMLAttributes<HTMLDivElem
 
 export const SidebarNavigation = React.forwardRef<HTMLDivElement, SidebarNavigationProps>(
   ({element = 'SIDEBAR_NAVIGATION', children}, ref) => {
+    const {collapsed} = React.useContext(SidebarContext);
     return (
-      <SidebarNavigationWrapper element={element} ref={ref}>
+      <Box
+        as={SidebarNavigationWrapper as any}
+        element={element}
+        ref={ref}
+        opacity={collapsed ? 0 : 1}
+        transition="opacity 150ms ease"
+        maxHeight="100%"
+        overflowY="auto"
+        overflowX="hidden"
+        padding="space50"
+        flexGrow={1}
+      >
         {children}
-      </SidebarNavigationWrapper>
+      </Box>
     );
   }
 );
