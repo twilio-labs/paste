6000aa23bc1a02a6035c312a5fa3a6ea3b664162
“nora <nkrantz@twilio.com>
2023-06-28 18:07:59 -0400

    chore: make list items hover style similar to menu

--- packages/paste-core/components/user-dialog/src/UserDialogSeparator.tsx
+++ packages/paste-core/components/user-dialog/src/UserDialogSeparator.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import {Separator, type SeparatorProps} from '@twilio-paste/separator';
+import {Box} from '@twilio-paste/box';
 
 export const UserDialogSeparator = React.forwardRef<
   HTMLHRElement,
   Omit<SeparatorProps, 'orientation' | 'horizontalSpacing' | 'verticalSpacing'>
 >(({element = 'USER_DIALOG_SEPARATOR', ...props}, ref) => {
-  return <Separator {...props} element={element} ref={ref} orientation="horizontal" />;
+  return (
+    <Box paddingY="space30">
+      <Separator {...props} element={element} ref={ref} orientation="horizontal" />
+    </Box>
+  );
 });
 UserDialogSeparator.displayName = 'UserDialogSeparator';
 
