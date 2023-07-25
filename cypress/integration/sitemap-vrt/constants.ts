8229f71237130307ffcacc0ee9b289f21b111719
“nora <nkrantz@twilio.com>
2023-07-25 15:29:00 -0400

    chore: add confirmations page to cypress constants list

--- cypress/integration/sitemap-vrt/constants.ts
+++ cypress/integration/sitemap-vrt/constants.ts
@@ -117,6 +117,7 @@ export const SITEMAP = [
   '/introduction/for-engineers/quickstart/',
   '/patterns/button-vs-anchor/',
   '/introduction/working-with-us/',
+  '/patterns/confirmations/',
   '/patterns/delete/',
   '/patterns/data-export/',
   '/patterns/filter-group/',
