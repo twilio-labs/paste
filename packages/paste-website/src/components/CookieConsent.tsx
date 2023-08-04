bab79fd8705eacc0821ae355454e2c5dd3bab372
Si Taggart <me@simontaggart.com>
2023-08-04 11:24:35 -0700

    fix: responsive layout

--- packages/paste-website/src/components/CookieConsent.tsx
+++ packages/paste-website/src/components/CookieConsent.tsx
@@ -14,13 +14,21 @@ export const CookieConsent: React.FC<{onAccept: () => void; onReject: () => void
       borderTopWidth="borderWidth10"
       borderTopColor="colorBorderWeaker"
       paddingY="space60"
+      paddingX="space40"
       display="flex"
       justifyContent="space-around"
     >
       <Box display="flex" alignItems="center" columnGap="space40" maxWidth="1200px">
         To enhance our site, we utilize tools that establish cookies. The data gathered by these tools is anonymized. If
         you opt to decline the use of cookies on this site, no analytics service will be initiated.
-        <Box flexShrink={0} flexGrow={1} columnGap="space40" display="flex">
+        <Box
+          flexShrink={0}
+          flexGrow={1}
+          columnGap="space40"
+          rowGap="space40"
+          display="flex"
+          flexDirection={['column', 'column', 'row']}
+        >
           <Button variant="primary" size="small" onClick={onAccept}>
             Accept cookies
           </Button>
