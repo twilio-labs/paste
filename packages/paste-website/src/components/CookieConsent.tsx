1dcdd4cf224fea2ac36152158c7cee6d086f8660
Si Taggart <me@simontaggart.com>
2023-08-04 10:52:59 -0700

    chore: fix styling and event firing when ga not loaded

--- packages/paste-website/src/components/CookieConsent.tsx
+++ packages/paste-website/src/components/CookieConsent.tsx
@@ -1,23 +1,34 @@
 import * as React from 'react';
 import {Box} from '@twilio-paste/box';
-import {Alert} from '@twilio-paste/alert';
 import {Button} from '@twilio-paste/button';
 
 export const CookieConsent: React.FC<{onAccept: () => void; onReject: () => void}> = ({onAccept, onReject}) => {
   return (
-    <Box zIndex="zIndex90" position="fixed" bottom="0" width="100%">
-      <Alert variant="neutral">
-        <Box display="flex" alignItems="center" columnGap="space40">
-          To help us improve this site we use tools that set cookies. Data collected by those tools is annonimized. If
-          you choose to reject the use of cookies on this site, no analytics service will be initialized.
-          <Button variant="secondary" size="small" onClick={onAccept}>
+    <Box
+      zIndex="zIndex90"
+      position="fixed"
+      bottom="0"
+      width="100%"
+      backgroundColor="colorBackgroundBody"
+      borderTopStyle="solid"
+      borderTopWidth="borderWidth10"
+      borderTopColor="colorBorderWeaker"
+      paddingY="space60"
+      display="flex"
+      justifyContent="space-around"
+    >
+      <Box display="flex" alignItems="center" columnGap="space40" maxWidth="1200px">
+        To enhance our site, we utilize tools that establish cookies. The data gathered by these tools is anonymized. If
+        you opt to decline the use of cookies on this site, no analytics service will be initiated.
+        <Box flexShrink={0} flexGrow={1} columnGap="space40" display="flex">
+          <Button variant="primary" size="small" onClick={onAccept}>
             Accept cookies
           </Button>
-          <Button variant="destructive_link" size="small" onClick={onReject}>
+          <Button variant="secondary" size="small" onClick={onReject}>
             Reject cookies
           </Button>
         </Box>
-      </Alert>
+      </Box>
     </Box>
   );
 };
