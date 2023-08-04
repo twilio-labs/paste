4edd4f6c7efd812d44c2061c8560ccdd53ea54a1
Simon Taggart <me@simontaggart.com>
2023-08-04 15:57:24 -0700

    feat: updated DD RUM settings (#3379)
    * feat: updated DD RUM settings
    
    * feat: cookie consent banner
    
    * feat: cookie consent banner
    
    * chore: fix styling and event firing when ga not loaded
    
    * chore: comment DD rum config
    
    * fix: responsive layout
    
    * fix: initial render of banner
    
    * chore: copy

---
+++ packages/paste-website/src/components/CookieConsent.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import {Box} from '@twilio-paste/box';
+import {Button} from '@twilio-paste/button';
+
+export const CookieConsent: React.FC<{onAccept: () => void; onReject: () => void}> = ({onAccept, onReject}) => {
+  return (
+    <Box
+      zIndex="zIndex90"
+      position="fixed"
+      bottom="0"
+      width="100%"
+      backgroundColor="colorBackgroundBody"
+      borderTopStyle="solid"
+      borderTopWidth="borderWidth10"
+      borderTopColor="colorBorderWeaker"
+      paddingY="space60"
+      paddingX="space40"
+      display="flex"
+      justifyContent="space-around"
+    >
+      <Box display="flex" alignItems="center" columnGap="space40" maxWidth="1200px">
+        To help us improve this site, we use tools that set cookies. The data gathered by these tools is anonymized. If
+        you reject the use of cookies, no analytics service will be initiated.
+        <Box
+          flexShrink={0}
+          flexGrow={1}
+          columnGap="space40"
+          rowGap="space40"
+          display="flex"
+          flexDirection={['column', 'column', 'row']}
+        >
+          <Button variant="primary" size="small" onClick={onAccept}>
+            Accept cookies
+          </Button>
+          <Button variant="secondary" size="small" onClick={onReject}>
+            Reject cookies
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
