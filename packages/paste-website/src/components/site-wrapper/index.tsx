689a7865eaafa2d6de7f408a05d7f2e429f53577
TheSisb <shadiisber@gmail.com>
2023-08-28 09:42:44 -0500

    fix: website lint

--- packages/paste-website/src/components/site-wrapper/index.tsx
+++ packages/paste-website/src/components/site-wrapper/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+
 import {SiteBody} from './SiteBody';
 import {NavigationContext} from '../../context/NavigationContext';
 import type {NavigationQuery} from '../../context/NavigationContext';
--- packages/paste-website/src/context/DarkModeContext.tsx
+++ packages/paste-website/src/context/DarkModeContext.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+
 import type {UseDarkModeReturn} from '../hooks/useDarkMode';
 
 type DarkModeContextValue = {
