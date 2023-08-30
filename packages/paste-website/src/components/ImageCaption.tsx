cd1fb9617c1620e4291a8b6bf14f5cd426e7927d
Sarah <sali@twilio.com>
2023-08-30 11:45:01 -0700

    feat(website): Use DetailText component in ImageCaption

--- packages/paste-website/src/components/ImageCaption.tsx
+++ packages/paste-website/src/components/ImageCaption.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
 import {Box} from '@twilio-paste/box';
-import {Text} from '@twilio-paste/text';
+import {DetailText} from '@twilio-paste/detail-text';
 
 export const ImageCaption: React.FC<React.PropsWithChildren> = ({children}) => (
-  <Box as="p" marginTop="space40" marginBottom="space110">
-    <Text as="span" color="colorTextWeak" fontSize="fontSize20" lineHeight="lineHeight20">
-      <Text as="span" fontWeight="fontWeightBold" color="colorTextWeak" fontSize="fontSize20" lineHeight="lineHeight20">
-        Image caption:
-      </Text>{' '}
+  <Box marginBottom="space110">
+    <DetailText>
       {children}
-    </Text>
+    </DetailText>
   </Box>
 );
