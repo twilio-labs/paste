6feadaf049aaae77696445403e0af0c8994efb2a
Sarah <sali@twilio.com>
2023-09-05 10:49:22 -0700

    chore(website): Use DetailText component in ImageCaption (#3460)
    * feat(website): Use DetailText component in ImageCaption
    * feat: remove top margin from ImageCaption

--- packages/paste-website/src/components/ImageCaption.tsx
+++ packages/paste-website/src/components/ImageCaption.tsx
@@ -1,14 +1,9 @@
 import * as React from 'react';
 import {Box} from '@twilio-paste/box';
-import {Text} from '@twilio-paste/text';
+import {DetailText} from '@twilio-paste/detail-text';
 
 export const ImageCaption: React.FC<React.PropsWithChildren> = ({children}) => (
-  <Box as="p" marginTop="space40" marginBottom="space110">
-    <Text as="span" color="colorTextWeak" fontSize="fontSize20" lineHeight="lineHeight20">
-      <Text as="span" fontWeight="fontWeightBold" color="colorTextWeak" fontSize="fontSize20" lineHeight="lineHeight20">
-        Image caption:
-      </Text>{' '}
-      {children}
-    </Text>
+  <Box marginBottom="space110">
+    <DetailText marginTop="space0">{children}</DetailText>
   </Box>
 );
