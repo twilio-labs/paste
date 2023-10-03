822d30b2070a20ca1361ff5e552c11e61411a5cd
Si Taggart <me@simontaggart.com>
2023-10-03 10:40:10 -0700

    chore: missing typedocs

--- tools/build/generate-type-docs/check-uncommited-type.sh
+++ tools/build/generate-type-docs/check-uncommited-type.sh
@@ -5,7 +5,7 @@ MODIFIED_FILES=$(git status --porcelain | grep "^ M.*type-docs.json$" | awk '{pr
 
 # If there are any modified files, exit with a non-zero status code
 if [ -n "$MODIFIED_FILES" ]; then
-  echo "Error: The following type-docs.json files have been modified but not committed:"
+  echo "Error: The following type-docs.json files have been modified but not committed, run 'yarn build:typedocs' to update:"
   echo "$MODIFIED_FILES"
   exit 1
 fi
