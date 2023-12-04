f5792cc57224b7c2c632fc762dfe594137446aee
Si Taggart <me@simontaggart.com>
2023-12-04 12:00:56 -0800

    chore: set deployment context for vercel deployments

--- packages/paste-theme-designer/next.config.js
+++ packages/paste-theme-designer/next.config.js
@@ -2,9 +2,9 @@ module.exports = {
   env: {
     DATADOG_APPLICATION_ID: process.env.DATADOG_APPLICATION_ID,
     DATADOG_CLIENT_TOKEN: process.env.DATADOG_CLIENT_TOKEN,
-    // Netlify environment context
-    ENVIRONMENT_CONTEXT: process.env.CONTEXT,
-    // Netlify deploy url
-    DEPLOY_URL: process.env.DEPLOY_URL,
+    // Netlify or Vercel (whilst we migrate) environment context
+    ENVIRONMENT_CONTEXT: process.env.CONTEXT || process.env.VERCEL_ENV,
+    // Netlify or Vercel (whilst we migrate) deploy url
+    DEPLOY_URL: process.env.DEPLOY_URL || process.env.VERCEL_URL,
   },
 };
