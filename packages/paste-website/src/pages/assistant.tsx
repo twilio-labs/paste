f419fa3bb34340c8681cd8f9d6e738067f474bfc
Si Taggart <me@simontaggart.com>
2024-01-17 17:07:53 -0700

    chore: cannot use server side props

--- packages/paste-website/src/pages/assistant.tsx
+++ packages/paste-website/src/pages/assistant.tsx
@@ -1,19 +1,15 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import type { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import type { NextPage } from "next";
 import Head from "next/head";
 
 import { Assistant as AssistantPage } from "../components/assistant/Assistant";
+import { useLocationOrigin } from "../utils/RouteUtils";
 
 const queryClient = new QueryClient();
 
-export const getServerSideProps = (async (context) => {
-  return {
-    props: { host: context.req.headers.host || null },
-  };
-}) satisfies GetServerSideProps<{ host: string | null }>;
-
-const Assistant = ({ host }: InferGetServerSidePropsType<typeof getServerSideProps>): React.ReactNode => {
+const Assistant: NextPage = () => {
+  const origin = useLocationOrigin();
   return (
     <QueryClientProvider client={queryClient}>
       <Head>
@@ -24,7 +20,7 @@ const Assistant = ({ host }: InferGetServerSidePropsType<typeof getServerSidePro
         />
         <meta
           property="og:image"
-          content={`//${host}/api/simple-og-image?title=Paste%20Assistant&description=A%20GPt-4%20powered%20assistant%20to%20help%20you%20with%20understanding%20how%20to%20use%20Paste%20to%20design%20and%20build%20your%20software%20applications.%20It%20can%20assist%20you%20with%20code,%20design,%20or%20content%20guidelines%20and%20troubleshooting.`}
+          content={`${origin}/api/simple-og-image?title=Paste%20Assistant&description=A%20GPt-4%20powered%20assistant%20to%20help%20you%20with%20understanding%20how%20to%20use%20Paste%20to%20design%20and%20build%20your%20software%20applications.%20It%20can%20assist%20you%20with%20code,%20design,%20or%20content%20guidelines%20and%20troubleshooting.`}
         />
       </Head>
       <AssistantPage />
