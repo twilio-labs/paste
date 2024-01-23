e8d6d9f8372da4bfc56f9fd2a1a24c628d1302a4
“nora <nkrantz@twilio.com>
2024-01-23 14:38:15 -0300

    chore: add pageHeaderSeparator to docs and megan updates

--- packages/paste-website/src/component-examples/PageHeaderExamples.ts
+++ packages/paste-website/src/component-examples/PageHeaderExamples.ts
@@ -49,6 +49,9 @@ const PageHeaderExample = () => {
         <PageHeaderHeading>Getting started with Segment</PageHeaderHeading>
         <PageHeaderMeta><Anchor href="#">Download the help guide PDF</Anchor></PageHeaderMeta>
       </PageHeaderDetails>
+      <PageHeaderSeparator>
+        <Separator orientation="horizontal" />
+      </PageHeaderSeparator>
     </PageHeader>
   );
 };
@@ -144,6 +147,9 @@ const PageHeaderExample = () => {
         </PageHeaderActions>
         <PageHeaderParagraph>Use numbers you own as caller ID or the "To" number for outbound calls and messages. Phone numbers you buy from Twilio or port to Twilio can always be used as caller IDs.</PageHeaderParagraph>
       </PageHeaderDetails>
+      <PageHeaderSeparator>
+        <Separator orientation="horizontal" />
+      </PageHeaderSeparator>
     </PageHeader>
   );
 };
@@ -205,6 +211,9 @@ const PageHeaderExample = () => {
         <PageHeaderHeading>Connect Azure SQL data warehouse</PageHeaderHeading>
         <PageHeaderParagraph>Follow the steps below to start setting up Azure SQL data warehouse.</PageHeaderParagraph>
       </PageHeaderDetails>
+      <PageHeaderSeparator>
+        <Separator orientation="horizontal" />
+      </PageHeaderSeparator>
     </PageHeader>
   );
 };
