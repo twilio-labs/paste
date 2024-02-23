baabbe7864c09e65e9d358fc033392b2aa2e67b8
TheSisb <shadiisber@gmail.com>
2024-02-23 16:56:23 -0600

    chore: update figma link

--- packages/paste-website/src/components/homepage/DesignEfficiency.tsx
+++ packages/paste-website/src/components/homepage/DesignEfficiency.tsx
@@ -48,10 +48,7 @@ const DesignEfficiency: React.FC = (): React.ReactElement => {
             efficient design and engineering collaboration.
           </Text>
           <Box fontWeight="fontWeightSemibold" fontSize="fontSize40" lineHeight="lineHeight40">
-            <BouncyAnchor
-              text="Find us on Figma Community"
-              href="https://www.figma.com/community/file/1207476064127503112/twilio-paste-components"
-            />
+            <BouncyAnchor text="Find us on Figma Community" href="https://www.figma.com/@twilio" />
           </Box>
           <Box
             marginTop="space170"
