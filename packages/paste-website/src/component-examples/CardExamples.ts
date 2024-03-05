35c40054d0746860ba95d2741d6e69b53b4c0859
“nora <nkrantz@twilio.com>
2024-03-05 09:47:21 -0700

    chore: finish card docs content

---
+++ packages/paste-website/src/component-examples/CardExamples.ts
@@ -0,0 +1,219 @@
+export const DefaultExample = `const CardExample = () => {
+  return (
+    <Card>
+      <Heading as="h2" variant="heading20">
+        Parable of the Talents
+      </Heading>
+      <Paragraph>
+        Choose your leaders with wisdom and forethought. To be led by a coward is to be controlled by all that the
+        coward fears. To be led by a fool is to be led by the opportunists who control the fool. To be led by a thief is
+        to offer up your most precious treasures to be stolen. To be led by a liar is to ask to be lied to. To be led by
+        a tyrant is to sell yourself and those you love into slavery.
+      </Paragraph>
+      <Paragraph marginBottom="space0">
+        —
+        <Anchor href="https://www.goodreads.com/book/show/60932.Parable_of_the_Talents">Octavia Butler</Anchor>
+      </Paragraph>
+    </Card>
+  );
+};
+
+render(<CardExample />);`.trim();
+
+export const AdjustingPaddingExample = `const CardExample = () => {
+  return (
+    <Box maxWidth="size40">
+      <Card padding="space0">
+        <Box padding="space160" display="flex" justifyContent="center">
+          <AlertsAndNotifications />
+        </Box>
+        <Separator orientation="horizontal" verticalSpacing="space0" />
+        <Box paddingY="space60" paddingX="space90">
+          <Heading as="h2" variant="heading40">
+            Alerts and notifications
+          </Heading>
+          <Paragraph>
+            Inform, engage, and drive customers to take action. 
+            Deliver reliable multichannel notifications through Twilio.
+          </Paragraph>
+          <Button variant="secondary" size="small">
+            Try notifications
+          </Button>
+        </Box>
+      </Card>
+    </Box>
+  );
+};
+
+render(<CardExample />);`.trim();
+
+export const TitleBodyButtonExample = `const CardExample = () => {
+  return (
+    <Stack orientation="vertical" spacing="space40">
+      <Card padding="space120">
+        <Heading as="h2" variant="heading20">The Transgender District</Heading>
+        <Paragraph>
+          The mission of the Transgender District is to create an urban environment that fosters the rich history, culture, legacy, and empowerment of transgender
+          people and its deep roots in the southeastern Tenderloin neighborhood. The transgender district aims to stabilize and economically empower the transgender
+          community through ownership of homes, businesses, historic and cultural sites, and safe community spaces.
+        </Paragraph>
+        <Button variant="primary" as="a" href="https://www.transgenderdistrictsf.com/">Support The Transgender District</Button>
+      </Card>
+      <Card padding="space120">
+        <Heading as="h2" variant="heading20">Inside Out</Heading>
+        <Paragraph>
+          Inside Out empowers, educates, and advocates for LGBTQ+ of youth from the Pikes Peak Region in Southern Colorado. Inside Out does this by creating safe spaces,
+          support systems and teaching life skills to all youth in the community and working to make the community safer and more accepting of gender and sexual orientation
+          diversity.
+        </Paragraph>
+        <Button variant="primary" as="a" href="https://insideoutys.org/">Support Inside Out</Button>
+      </Card>
+      <Card padding="space120">
+        <Heading as="h2" variant="heading20">The Audre Lorde Project</Heading>
+        <Paragraph>
+          The Audre Lorde Project is a Lesbian, Gay, Bisexual, Two Spirit, Trans and Gender Non Conforming People of Color center for community organizing, focusing on
+          the New York City area. Through mobilization, education and capacity-building, they work for community wellness and progressive social and economic justice. Committed
+          to struggling across differences, they seek to responsibly reflect, represent and serve their various communities.
+        </Paragraph>
+        <Button variant="primary" as="a" href="https://alp.org/">Support The Audre Lorde Project</Button>
+      </Card>
+    </Stack>
+  );
+};
+
+render(<CardExample />);`.trim();
+
+export const MarketingOneExample = `const CardExample = () => {
+  return (
+    <Box maxWidth="size40">
+      <Card padding="space0">
+        <Box padding="space160" display="flex" justifyContent="center">
+          <AlertsAndNotifications />
+        </Box>
+        <Separator orientation="horizontal" verticalSpacing="space0" />
+        <Box paddingY="space60" paddingX="space90">
+          <Heading as="h2" variant="heading40">
+            Alerts and notifications
+          </Heading>
+          <Paragraph>
+            Inform, engage, and drive customers to take action. 
+            Deliver reliable multichannel notifications through Twilio.
+          </Paragraph>
+          <Button variant="secondary" size="small">
+            Try notifications
+          </Button>
+        </Box>
+      </Card>
+    </Box>
+  );
+};
+
+render(<CardExample />);`.trim();
+
+export const MarketingTwoExample = `const CardExample = () => {
+  return (
+    <Box maxWidth="size40">
+      <Card padding="space0">
+        <Box padding="space160" display="flex" justifyContent="center">
+          <AlertsAndNotifications />
+        </Box>
+        <Separator orientation="horizontal" verticalSpacing="space0" />
+        <Box paddingY="space60" paddingX="space90">
+          <Heading as="h2" variant="heading40">
+            Alerts and notifications
+          </Heading>
+          <Paragraph>
+            Inform, engage, and drive customers to take action. 
+            Deliver reliable multichannel notifications through Twilio.
+          </Paragraph>
+          <Button variant="secondary" size="small">
+            Try notifications
+          </Button>
+        </Box>
+      </Card>
+    </Box>
+  );
+};
+
+render(<CardExample />);`.trim();
+
+export const MarketingThreeExample = `const CardExample = () => {
+  return (
+    <Box maxWidth="size40">
+      <Card padding="space0">
+        <Box padding="space160" display="flex" justifyContent="center">
+          <AlertsAndNotifications />
+        </Box>
+        <Separator orientation="horizontal" verticalSpacing="space0" />
+        <Box paddingY="space60" paddingX="space90">
+          <Heading as="h2" variant="heading40">
+            Alerts and notifications
+          </Heading>
+          <Paragraph>
+            Inform, engage, and drive customers to take action. 
+            Deliver reliable multichannel notifications through Twilio.
+          </Paragraph>
+          <Button variant="secondary" size="small">
+            Try notifications
+          </Button>
+        </Box>
+      </Card>
+    </Box>
+  );
+};
+
+render(<CardExample />);`.trim();
+
+export const MarketingFourExample = `const CardExample = () => {
+  return (
+    <Box maxWidth="size40">
+      <Card padding="space0">
+        <Box padding="space160" display="flex" justifyContent="center">
+          <AlertsAndNotifications />
+        </Box>
+        <Separator orientation="horizontal" verticalSpacing="space0" />
+        <Box paddingY="space60" paddingX="space90">
+          <Heading as="h2" variant="heading40">
+            Alerts and notifications
+          </Heading>
+          <Paragraph>
+            Inform, engage, and drive customers to take action. 
+            Deliver reliable multichannel notifications through Twilio.
+          </Paragraph>
+          <Button variant="secondary" size="small">
+            Try notifications
+          </Button>
+        </Box>
+      </Card>
+    </Box>
+  );
+};
+
+render(<CardExample />);`.trim();
+
+export const PricingExample = `const CardExample = () => {
+  return (
+    <Box maxWidth="size40">
+      <Card padding="space0">
+        <Box padding="space160" display="flex" justifyContent="center">
+          <AlertsAndNotifications />
+        </Box>
+        <Separator orientation="horizontal" verticalSpacing="space0" />
+        <Box paddingY="space60" paddingX="space90">
+          <Heading as="h2" variant="heading40">
+            Alerts and notifications
+          </Heading>
+          <Paragraph>
+            Inform, engage, and drive customers to take action. 
+            Deliver reliable multichannel notifications through Twilio.
+          </Paragraph>
+          <Button variant="secondary" size="small">
+            Try notifications
+          </Button>
+        </Box>
+      </Card>
+    </Box>
+  );
+};
+
+render(<CardExample />);`.trim();
