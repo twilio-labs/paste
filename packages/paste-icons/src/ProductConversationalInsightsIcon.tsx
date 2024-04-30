b39683bec4c3d1a019e4c67ed4f9b1dbef904d53
“nora <nkrantz@twilio.com>
2024-04-30 15:45:53 -0400

    chore: format files for biome

--- packages/paste-icons/src/ProductConversationalInsightsIcon.tsx
+++ packages/paste-icons/src/ProductConversationalInsightsIcon.tsx
@@ -1,8 +1,8 @@
+import { useUID } from "@twilio-paste/uid-library";
 /**
  * This file was automatically generated with @twilio-labs/svg-to-react
  */
 import * as React from "react";
-import { useUID } from "@twilio-paste/uid-library";
 
 import { IconWrapper } from "./helpers/IconWrapper";
 import type { IconWrapperProps } from "./helpers/IconWrapper";
