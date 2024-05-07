c1458bf3b5c02a71798b3815e3a6c9ce26c8fa74
“nora <nkrantz@twilio.com>
2024-05-07 18:47:26 -0400

    chore: use useTransitions instead of useSpring

--- packages/paste-core/components/side-panel/src/SidePanel.tsx
+++ packages/paste-core/components/side-panel/src/SidePanel.tsx
@@ -1,7 +1,6 @@
-import { animated, useSpring } from "@twilio-paste/animation-library";
+import { animated, useTransition } from "@twilio-paste/animation-library";
 import { Box, safelySpreadBoxProps } from "@twilio-paste/box";
 import type { BoxProps } from "@twilio-paste/box";
-import { useTheme } from "@twilio-paste/theme";
 import type { HTMLPasteProps } from "@twilio-paste/types";
 import { useWindowSize } from "@twilio-paste/utils";
 import * as React from "react";
@@ -12,15 +11,16 @@ const StyledSidePanel = React.forwardRef<HTMLDivElement, BoxProps>((props, ref) 
     role="dialog"
     display="flex"
     flexDirection="column"
-    width={["100%", "100%", "size40"]}
+    width={["100%", "size40", "size40"]}
     ref={ref}
     aria-label={props.label}
     borderStyle="solid"
-    borderRadius="borderRadius70"
+    borderRadius={["borderRadius0", "borderRadius70", "borderRadius70"]}
     borderWidth="borderWidth10"
     borderColor="colorBorderWeaker"
     backgroundColor="colorBackgroundBody"
-    margin="space40" // make right margin show
+    margin={["space0", "space40", "space40"]}
+    marginTop="space40"
     zIndex="zIndex50"
     position="fixed"
     top={0}
@@ -38,21 +38,39 @@ const config = {
   friction: 20,
 };
 
-const getHiddenSpringConfig = (collapsed: boolean, sidePanelWidth: string): any => ({
-  opacity: collapsed ? 0 : 1,
-  width: sidePanelWidth,
-  transform: collapsed ? "translateX(100%)" : "translateX(0%)",
-  config,
-  left: "unset",
-});
+/*
+ * const getHiddenSpringConfig = (collapsed: boolean, sidePanelWidth: string): any => ({
+ *   opacity: collapsed ? 0 : 1,
+ *   width: sidePanelWidth,
+ *   transform: collapsed ? "translateX(100%)" : "translateX(0%)",
+ *   config,
+ *   left: "unset",
+ * });
+ */
 
-const getMobileSpringConfig = (collapsed: boolean): any => ({
-  opacity: collapsed ? 0 : 1,
-  width: "100%",
-  transform: collapsed ? "translateX(100%)" : "translateX(0%)",
+/*
+ * const getMobileSpringConfig = (collapsed: boolean): any => ({
+ *   opacity: collapsed ? 0 : 1,
+ *   width: "100%",
+ *   transform: collapsed ? "translateY(100%)" : "translateY(0%)",
+ *   config,
+ *   left: 0,
+ * });
+ */
+
+const transitionStyles = {
+  from: { opacity: 0, transform: "translateX(100%)" },
+  enter: { opacity: 1, transform: "translateX(0%)" },
+  leave: { opacity: 0, transform: "translateX(100%)" },
   config,
-  left: 0,
-});
+};
+
+const mobileTransitionStyles = {
+  from: { opacity: 0, transform: "translateY(100%)" },
+  enter: { opacity: 1, transform: "translateY(0%)" },
+  leave: { opacity: 0, transform: "translateY(100%)" },
+  config,
+};
 
 export interface SidePanelProps extends HTMLPasteProps<"div"> {
   children?: React.ReactNode;
@@ -82,22 +100,26 @@ export interface SidePanelProps extends HTMLPasteProps<"div"> {
 
 const SidePanel = React.forwardRef<HTMLDivElement, SidePanelProps>(
   ({ element = "SIDE_PANEL", collapsed = false, children, ...props }, ref) => {
-    // use useTransition instead of useAnimation to un-render the side panel content when collapsed
-
     // handle initial focus
 
     const { breakpointIndex } = useWindowSize();
-    const theme = useTheme();
-    const springConfig =
-      breakpointIndex === (0 || 1)
-        ? getMobileSpringConfig(collapsed)
-        : getHiddenSpringConfig(collapsed, theme.sizes.size40);
-    const styles = useSpring(springConfig);
+
+    const transitions =
+      breakpointIndex === 0
+        ? useTransition(!collapsed, mobileTransitionStyles)
+        : useTransition(!collapsed, transitionStyles);
 
     return (
-      <AnimatedStyledSidePanel {...safelySpreadBoxProps(props)} ref={ref} element={element} style={styles}>
-        {children}
-      </AnimatedStyledSidePanel>
+      <>
+        {transitions(
+          (styles, item) =>
+            item && (
+              <AnimatedStyledSidePanel {...safelySpreadBoxProps(props)} ref={ref} element={element} style={styles}>
+                {children}
+              </AnimatedStyledSidePanel>
+            ),
+        )}
+      </>
     );
   },
 );
