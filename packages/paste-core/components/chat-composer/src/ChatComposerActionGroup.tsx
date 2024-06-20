3ddda4567fd99e4ffed2962f269e0e2e83bd92cd
Kristian Antrobus <kantrobus@twilio.com>
2024-06-20 12:31:54 -0500

    feat(composer): wip base elements

---
+++ packages/paste-core/components/chat-composer/src/ChatComposerActionGroup.tsx
@@ -0,0 +1,24 @@
+import type { BoxProps } from "@twilio-paste/box";
+import { Box, safelySpreadBoxProps } from "@twilio-paste/box";
+import * as React from "react";
+
+export interface ChatComposerActionGroupProps {
+  children?: React.ReactNode;
+  /**
+   * Overrides the default element name to apply unique styles with the Customization Provider
+   * @default 'CHAT_COMPOSER_ACTION_GROUP'
+   * @type {BoxProps['element']}
+   * @memberof PageHeaderProps
+   */
+  element?: BoxProps["element"];
+}
+
+export const ChatComposerActionGroup = React.forwardRef<HTMLDivElement, ChatComposerActionGroupProps>(
+  ({ element = "CHAT_COMPOSER_ACTION_GROUP", children, ...props }, ref) => (
+    <Box {...safelySpreadBoxProps(props)} element={element} ref={ref} gridArea="actions">
+      {children}
+    </Box>
+  ),
+);
+
+ChatComposerActionGroup.displayName = "ChatComposerActionGroup";
