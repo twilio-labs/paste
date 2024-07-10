0679fe2c7c0b4756a114c07a99a03c6def243a9c
Kristian Antrobus <kantrobus@twilio.com>
2024-07-10 18:52:57 -0500

    chore(docs/chat-composer): lint fix

--- packages/paste-core/components/ai-chat-log/stories/composer.stories.tsx
+++ packages/paste-core/components/ai-chat-log/stories/composer.stories.tsx
@@ -130,7 +130,7 @@ export const AIChatLogComposer = (): React.ReactNode => {
       content: (
         <AIChatMessage variant="user">
           <AIChatMessageAuthor aria-label="you said at 2:36pm">Gibby Radki</AIChatMessageAuthor>
-          <AIChatMessageBody>Hi, I'm getting errors codes when sending an SMS.</AIChatMessageBody>
+          <AIChatMessageBody>Hi, I am getting errors codes when sending an SMS.</AIChatMessageBody>
         </AIChatMessage>
       ),
     },
