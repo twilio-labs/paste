103813a807994ece75d0e3900a9e4dc8da31680c
Kristian Antrobus <kantrobus@twilio.com>
2024-07-24 15:37:06 -0500

    feat(corner-ornament): refactor of masking paths in element

--- packages/paste-core/components/corner-ornament/stories/index.stories.tsx
+++ packages/paste-core/components/corner-ornament/stories/index.stories.tsx
@@ -1,6 +1,7 @@
 import { Avatar } from "@twilio-paste/avatar";
 import { Box } from "@twilio-paste/box";
 import {Badge} from "@twilio-paste/badge";
+import { Stack } from "@twilio-paste/stack";
 import * as React from "react";
 
 import { CornerOrnament, CornerOrnamentContainer, CornerOrnamentElement } from "../src";
@@ -13,7 +14,7 @@ export default {
 
 export const Default = (): React.ReactNode => {
   return (
-    <Box>
+    <Stack orientation="vertical" spacing="space30">
       <CornerOrnamentContainer cornerOrnamentType="dot" size="sizeIcon80">
         <CornerOrnamentElement>
           <Avatar size="sizeIcon80" src="https://avatars.githubusercontent.com/u/55083528?v=4" name="GitHub avatar" />
@@ -31,6 +32,6 @@ export const Default = (): React.ReactNode => {
           <Badge variant="notification_counter" size="small">9+</Badge>
         </CornerOrnament>
       </CornerOrnamentContainer>
-    </Box>
+    </Stack>
   );
 };
