b9298ceb9cddbf8f0c6aba078b89fc2ba5a3e0e3
Kristian Antrobus <kantrobus@twilio.com>
2024-07-25 08:22:17 -0500

    feat(corner-ornament): updated stories

--- packages/paste-core/components/corner-ornament/stories/index.stories.tsx
+++ packages/paste-core/components/corner-ornament/stories/index.stories.tsx
@@ -2,6 +2,8 @@ import { Avatar } from "@twilio-paste/avatar";
 import { Badge } from "@twilio-paste/badge";
 import { Badge } from "@twilio-paste/badge";
 import { Box } from "@twilio-paste/box";
+import { GitIcon } from "@twilio-paste/icons/esm/GitIcon";
+import { ScreenReaderOnly } from "@twilio-paste/screen-reader-only";
 import { Stack } from "@twilio-paste/stack";
 import * as React from "react";
 
@@ -21,6 +23,7 @@ export const Default = (): React.ReactNode => {
           <Avatar size="sizeIcon80" src="https://avatars.githubusercontent.com/u/55083528?v=4" name="GitHub avatar" />
         </CornerOrnamentElement>
         <CornerOrnament>
+          <ScreenReaderOnly>ornament has notification</ScreenReaderOnly>
           <Box backgroundColor="colorBackgroundRequired" height="6px" width="6px" borderRadius="borderRadiusCircle" />
         </CornerOrnament>
       </CornerOrnamentContainer>
@@ -30,7 +33,20 @@ export const Default = (): React.ReactNode => {
           <Avatar size="sizeIcon80" src="https://avatars.githubusercontent.com/u/55083528?v=4" name="GitHub avatar" />
         </CornerOrnamentElement>
         <CornerOrnament>
-          <Badge variant="notification_counter" size="small">
+          <ScreenReaderOnly>notification value</ScreenReaderOnly>
+          <Badge as="span" variant="notification_counter" size="small">
+            9+
+          </Badge>
+        </CornerOrnament>
+      </CornerOrnamentContainer>
+
+      <CornerOrnamentContainer cornerOrnamentType="badge" size="sizeIcon80">
+        <CornerOrnamentElement>
+          <GitIcon size="sizeIcon80" title="GitHub avatar" />
+        </CornerOrnamentElement>
+        <CornerOrnament>
+          <ScreenReaderOnly>notification value</ScreenReaderOnly>
+          <Badge as="span" variant="notification_counter" size="small">
             9+
           </Badge>
         </CornerOrnament>
