3a9a3d25aaf1c1174b48354e1477500b243eac93
Kristian Antrobus <kantrobus@twilio.com>
2024-07-25 13:51:34 -0500

    feat(corner-ornament): stories ad size mappings

--- packages/paste-core/components/corner-ornament/src/CornerOrnament.tsx
+++ packages/paste-core/components/corner-ornament/src/CornerOrnament.tsx
@@ -17,39 +17,43 @@ export const CornerOrnament = React.forwardRef<HTMLDivElement, CornerOrnamentPro
     const Positions: OrnamentSpacingMapping = {
       badge: {
         bottom_end: {
+          sizeIcon70: { top: "space50", left: "space50" },
           sizeIcon80: { top: "space70", left: "space70" },
-          sizeIcon50: { top: "space80", left: "space80" },
         },
         top_end: {
+          sizeIcon70: { left: "space50" },
           sizeIcon80: { left: "space70" },
-          sizeIcon50: { left: "space70" },
         },
       },
       icon: {
         bottom_end: {
-          sizeIcon80: { top: "24px", left: "24px" },
-          sizeIcon50: { top: "space80", left: "space80" },
+          sizeIcon70: { top: "space50", left: "space50" },
+          sizeIcon80: { top: "space70", left: "space70" },
         },
         top_end: {
-          sizeIcon80: { top: "space80", left: "space80" },
-          sizeIcon50: { top: "space80", left: "space80" },
+          sizeIcon70: { left: "space50" },
+          sizeIcon80: { left: "space70" },
         },
       },
       dot: {
         bottom_end: {
-          sizeIcon80: { top: "space80", left: "space80" },
-          sizeIcon50: { top: "space80", left: "space80" },
+          sizeIcon30: { top: "space30", left: "space30" },
+          sizeIcon40: { top: "space40", left: "space40" },
+          sizeIcon50: { top: "space50", left: "space50" },
+          sizeIcon70: { top: "space50", left: "space50" },
         },
         top_end: {
-          sizeIcon80: { top: "space80", left: "space80" },
-          sizeIcon50: { top: "space80", left: "space80" },
+          sizeIcon30: { left: "space30" },
+          sizeIcon40: { left: "space40" },
+          sizeIcon50: { left: "space50" },
+          sizeIcon70: { left: "space50" },
         },
       },
     };
 
     if (!Positions[cornerOrnamentType][position][size]) {
       throw new Error(
-        "[Paste: CornerOrnament] the size and/or position you have chosen is not currently supported. Please raise a new disucssion to get this supported at https://github.com/twilio-labs/paste/discussions.",
+        "[Paste: CornerOrnament] the size/position/type combination you have chosen is not currently supported. Please refer to our guildinges in our docs or raise a new disucssion to get this supported at https://github.com/twilio-labs/paste/discussions.",
       );
     }
 
--- packages/paste-core/components/corner-ornament/stories/index.stories.tsx
+++ packages/paste-core/components/corner-ornament/stories/index.stories.tsx
@@ -1,10 +1,12 @@
 import { Avatar } from "@twilio-paste/avatar";
 import { Badge } from "@twilio-paste/badge";
-import { Badge } from "@twilio-paste/badge";
 import { Box } from "@twilio-paste/box";
+import { CustomizationProvider } from "@twilio-paste/customization";
+import { Heading } from "@twilio-paste/heading";
 import { GitIcon } from "@twilio-paste/icons/esm/GitIcon";
 import { ScreenReaderOnly } from "@twilio-paste/screen-reader-only";
 import { Stack } from "@twilio-paste/stack";
+import { useTheme } from "@twilio-paste/theme";
 import * as React from "react";
 
 import { CornerOrnament, CornerOrnamentContainer, CornerOrnamentElement } from "../src";
@@ -17,17 +19,16 @@ export default {
 
 export const Default = (): React.ReactNode => {
   return (
-    <Stack orientation="vertical" spacing="space30">
-      <CornerOrnamentContainer cornerOrnamentType="dot" size="sizeIcon80">
+    <Stack orientation="horizontal" spacing="space60">
+      <CornerOrnamentContainer cornerOrnamentType="dot" size="sizeIcon70">
         <CornerOrnamentElement>
-          <Avatar size="sizeIcon80" src="https://avatars.githubusercontent.com/u/55083528?v=4" name="GitHub avatar" />
+          <Avatar size="sizeIcon70" src="https://avatars.githubusercontent.com/u/55083528?v=4" name="GitHub avatar" />
         </CornerOrnamentElement>
         <CornerOrnament>
           <ScreenReaderOnly>ornament has notification</ScreenReaderOnly>
           <Box backgroundColor="colorBackgroundRequired" height="6px" width="6px" borderRadius="borderRadiusCircle" />
         </CornerOrnament>
       </CornerOrnamentContainer>
-
       <CornerOrnamentContainer cornerOrnamentType="badge" size="sizeIcon80">
         <CornerOrnamentElement>
           <Avatar size="sizeIcon80" src="https://avatars.githubusercontent.com/u/55083528?v=4" name="GitHub avatar" />
@@ -39,7 +40,6 @@ export const Default = (): React.ReactNode => {
           </Badge>
         </CornerOrnament>
       </CornerOrnamentContainer>
-
       <CornerOrnamentContainer cornerOrnamentType="badge" size="sizeIcon80">
         <CornerOrnamentElement>
           <GitIcon size="sizeIcon80" title="GitHub avatar" />
@@ -53,4 +53,132 @@ export const Default = (): React.ReactNode => {
       </CornerOrnamentContainer>
     </Stack>
   );
+};
+
+export const CornerOrnamentPositions = (): React.ReactNode => {
+  return (
+    <Stack orientation="horizontal" spacing="space60">
+      <CornerOrnamentContainer cornerOrnamentType="dot" size="sizeIcon70">
+        <CornerOrnamentElement>
+          <Avatar size="sizeIcon70" src="https://avatars.githubusercontent.com/u/55083528?v=4" name="GitHub avatar" />
+        </CornerOrnamentElement>
+        <CornerOrnament>
+          <ScreenReaderOnly>ornament has notification</ScreenReaderOnly>
+          <Box backgroundColor="colorBackgroundRequired" height="6px" width="6px" borderRadius="borderRadiusCircle" />
+        </CornerOrnament>
+      </CornerOrnamentContainer>
+      <CornerOrnamentContainer cornerOrnamentType="badge" size="sizeIcon80">
+        <CornerOrnamentElement>
+          <Avatar size="sizeIcon80" src="https://avatars.githubusercontent.com/u/55083528?v=4" name="GitHub avatar" />
+        </CornerOrnamentElement>
+        <CornerOrnament>
+          <ScreenReaderOnly>notification value</ScreenReaderOnly>
+          <Badge as="span" variant="notification_counter" size="small">
+            9+
+          </Badge>
+        </CornerOrnament>
+      </CornerOrnamentContainer>
+      <CornerOrnamentContainer cornerOrnamentType="badge" size="sizeIcon80">
+        <CornerOrnamentElement>
+          <GitIcon size="sizeIcon80" title="GitHub avatar" />
+        </CornerOrnamentElement>
+        <CornerOrnament>
+          <ScreenReaderOnly>notification value</ScreenReaderOnly>
+          <Badge as="span" variant="notification_counter" size="small">
+            9+
+          </Badge>
+        </CornerOrnament>
+      </CornerOrnamentContainer>
+    </Stack>
+  );
+};
+
+CornerOrnamentPositions.storyName = "Corner Ornament Positions";
+
+export const CornerOrnamentSizes = (): React.ReactNode => {
+  return (
+    <Stack orientation="vertical" spacing="space60">
+      <Heading as="h3" variant="heading30">
+        Avatar:
+      </Heading>
+      <Stack orientation="horizontal" spacing="space60">
+        <CornerOrnamentContainer cornerOrnamentType="dot" size="sizeIcon70">
+          <CornerOrnamentElement>
+            <Avatar size="sizeIcon70" src="https://avatars.githubusercontent.com/u/55083528?v=4" name="GitHub avatar" />
+          </CornerOrnamentElement>
+          <CornerOrnament>
+            <ScreenReaderOnly>ornament has notification</ScreenReaderOnly>
+            <Box backgroundColor="colorBackgroundRequired" size={"size10"} borderRadius="borderRadiusCircle" />
+          </CornerOrnament>
+        </CornerOrnamentContainer>
+      </Stack>
+      <CornerOrnamentContainer cornerOrnamentType="badge" size="sizeIcon80">
+        <CornerOrnamentElement>
+          <Avatar size="sizeIcon80" src="https://avatars.githubusercontent.com/u/55083528?v=4" name="GitHub avatar" />
+        </CornerOrnamentElement>
+        <CornerOrnament>
+          <ScreenReaderOnly>notification value</ScreenReaderOnly>
+          <Badge as="span" variant="notification_counter" size="small">
+            9+
+          </Badge>
+        </CornerOrnament>
+      </CornerOrnamentContainer>
+      <CornerOrnamentContainer cornerOrnamentType="badge" size="sizeIcon70">
+        <CornerOrnamentElement>
+          <GitIcon size="sizeIcon80" title="GitHub avatar" />
+        </CornerOrnamentElement>
+        <CornerOrnament>
+          <ScreenReaderOnly>notification value</ScreenReaderOnly>
+          <Badge as="span" variant="notification_counter" size="small">
+            9+
+          </Badge>
+        </CornerOrnament>
+      </CornerOrnamentContainer>
+    </Stack>
+  );
+};
+
+CornerOrnamentSizes.storyName = "Corner Ornament Sizes";
+
+export const CornerOrnamentCustomization = (): React.ReactNode => {
+  const currentTheme = useTheme();
+
+  return (
+    <Stack orientation="horizontal" spacing="space60">
+      <CustomizationProvider
+        theme={currentTheme}
+        elements={{
+          CORNER_ORNAMENT: {
+            color: "colorTextSuccess",
+          },
+          CORNER_ORNAMENT_ELEMENT: {
+            borderStyle: "solid",
+            borderWidth: "borderWidth10",
+            borderColor: "colorBorderPrimaryWeak",
+          },
+          CORNER_ORNAMENT_CONTAINER: {
+            backgroundColor: "colorBackgroundPrimaryStronger",
+            padding: "space70",
+          },
+        }}
+      >
+        <CornerOrnamentContainer cornerOrnamentType="icon" size="sizeIcon80">
+          <CornerOrnamentElement>
+            <Avatar size="sizeIcon80" src="https://avatars.githubusercontent.com/u/55083528?v=4" name="GitHub avatar" />
+          </CornerOrnamentElement>
+          <CornerOrnament>
+            <GitIcon size="sizeIcon40" decorative />
+          </CornerOrnament>
+        </CornerOrnamentContainer>
+      </CustomizationProvider>
+    </Stack>
+  );
+};
+
+CornerOrnamentCustomization.storyName = "Corner Ornament Customization";
+CornerOrnamentCustomization.parameters = {
+  a11y: {
+    // no need to a11y check customization
+    disable: true,
+  },
 };
