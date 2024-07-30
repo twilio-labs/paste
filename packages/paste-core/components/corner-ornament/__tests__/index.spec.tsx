ac25554ae79d52edf9f96e3812805d7c569f11a6
Kristian Antrobus <kantrobus@twilio.com>
2024-07-30 13:49:30 -0500

    feat(corner-ornament): pr comments

--- packages/paste-core/components/corner-ornament/__tests__/index.spec.tsx
+++ packages/paste-core/components/corner-ornament/__tests__/index.spec.tsx
@@ -5,50 +5,27 @@ import { LogoTwilioIcon } from "@twilio-paste/icons/esm/LogoTwilioIcon";
 import { IconSizeOptions } from "@twilio-paste/style-props";
 import * as React from "react";
 
-import {
-  CornerOrnament,
-  CornerOrnamentContainer,
-  CornerOrnamentElement,
-  CornerOrnamentPosition,
-  CornerOrnamentType,
-} from "../src";
+import { CornerOrnament, CornerOrnamentBase, CornerOrnamentContainer } from "../src";
+import { CornerOrnamentType } from "../src/types";
 
 const ExampleCornerOrnament: React.FC<{
   size?: IconSizeOptions;
-  position?: CornerOrnamentPosition;
   type?: CornerOrnamentType;
-}> = ({ size, type }) => (
+  element?: string;
+}> = ({ size, type, element }) => (
   <CornerOrnamentContainer
     data-testid="cornerOrnamentContainer"
     cornerOrnamentType={type || "dot"}
     size={size || "sizeIcon50"}
+    element={element ? `${element}_CORNER_ORNAMENT_CONTAINER` : undefined}
   >
-    <CornerOrnamentElement data-testid="cornerOrnamentElement">
+    <CornerOrnamentBase
+      data-testid="cornerOrnamentBase"
+      element={element ? `${element}_CORNER_ORNAMENT_BASE` : undefined}
+    >
       <Avatar data-testid="ornament-element" size="sizeIcon50" src="./avatars/avatar8.png" name="GitHub avatar" />
-    </CornerOrnamentElement>
-    <CornerOrnament data-testid="cornerOrnament">
-      <Box data-testid="ornament">
-        <LogoTwilioIcon decorative size="sizeIcon40" />
-      </Box>
-    </CornerOrnament>
-  </CornerOrnamentContainer>
-);
-
-const CustomizedCornerOrnament: React.FC<{
-  size?: IconSizeOptions;
-  position?: CornerOrnamentPosition;
-  type?: CornerOrnamentType;
-}> = ({ size, type }) => (
-  <CornerOrnamentContainer
-    data-testid="cornerOrnamentContainer"
-    cornerOrnamentType={type || "dot"}
-    size={size || "sizeIcon50"}
-    element="CUSTOMIZED_CORNER_ORNAMENT_CONTAINER"
-  >
-    <CornerOrnamentElement data-testid="cornerOrnamentElement" element="CUSTOMIZED_CORNER_ORNAMENT_ELEMENT">
-      <Avatar data-testid="ornament-element" size="sizeIcon50" src="./avatars/avatar8.png" name="GitHub avatar" />
-    </CornerOrnamentElement>
-    <CornerOrnament data-testid="cornerOrnament" element="CUSTOMIZED_CORNER_ORNAMENT">
+    </CornerOrnamentBase>
+    <CornerOrnament data-testid="cornerOrnament" element={element ? `${element}_CORNER_ORNAMENT` : undefined}>
       <Box data-testid="ornament">
         <LogoTwilioIcon decorative size="sizeIcon40" />
       </Box>
@@ -58,7 +35,36 @@ const CustomizedCornerOrnament: React.FC<{
 
 describe("CornerOrnament", () => {
   it("should render", () => {
-    const { getByText } = render(<CornerOrnament>test</CornerOrnament>);
-    expect(getByText("test")).toBeDefined();
+    const { getByTestId } = render(<ExampleCornerOrnament />);
+    expect(getByTestId("ornament-element")).toBeDefined();
+    expect(getByTestId("ornament")).toBeDefined();
+  });
+
+  it("should throw errors for unsupported size and type combinations", () => {
+    expect(() => {
+      render(<ExampleCornerOrnament size="sizeIcon30" type="avatar" />);
+    }).toThrow();
+  });
+
+  describe("Customization", () => {
+    it("should set element data attribute", () => {
+      const { getByTestId } = render(<ExampleCornerOrnament />);
+      expect(getByTestId("cornerOrnamentContainer").getAttribute("data-paste-element")).toEqual(
+        "CORNER_ORNAMENT_CONTAINER",
+      );
+      expect(getByTestId("cornerOrnamentBase").getAttribute("data-paste-element")).toEqual("CORNER_ORNAMENT_BASE");
+      expect(getByTestId("cornerOrnament").getAttribute("data-paste-element")).toEqual("CORNER_ORNAMENT");
+    });
+
+    it("should set custom element data attribute", () => {
+      const { getByTestId } = render(<ExampleCornerOrnament element="CUSTOMIZED" />);
+      expect(getByTestId("cornerOrnamentContainer").getAttribute("data-paste-element")).toEqual(
+        "CUSTOMIZED_CORNER_ORNAMENT_CONTAINER",
+      );
+      expect(getByTestId("cornerOrnamentBase").getAttribute("data-paste-element")).toEqual(
+        "CUSTOMIZED_CORNER_ORNAMENT_BASE",
+      );
+      expect(getByTestId("cornerOrnament").getAttribute("data-paste-element")).toEqual("CUSTOMIZED_CORNER_ORNAMENT");
+    });
   });
 });
