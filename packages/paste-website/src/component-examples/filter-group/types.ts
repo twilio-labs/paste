9944e62016c395d79d20f82f61633c86d5a4aa68
Shoaib Ahmed <shahmed@twilio.com>
2024-07-31 17:02:02 +0530

    feat(filter): add search filter example

--- packages/paste-website/src/component-examples/filter-group/types.ts
+++ packages/paste-website/src/component-examples/filter-group/types.ts
@@ -66,5 +66,6 @@ export interface SampleDataGridProps {
 export type RoomTypes = "Group" | "WebRTC Go" | "Peer to Peer";
 export type ParticipantsType = { min: string; max: string };
 export type DateRangeType = { startDate: string; startTime: string; endDate: string; endTime: string };
+export type SearchFilterType = { search: string };
 
-export type selectedFilterProps = RoomTypes | ParticipantsType | DateRangeType;
+export type selectedFilterProps = RoomTypes | ParticipantsType | DateRangeType | SearchFilterType;
