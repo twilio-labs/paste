d3b7432d48a2f6ce3bcc746fe004928b101aa090
Shoaib Ahmed <shahmed@twilio.com>
2024-07-31 17:51:23 +0530

    feat(filter): update DefaultFilterGroup to accept filterList

--- packages/paste-website/stories/FilterGroup.stories.tsx
+++ packages/paste-website/stories/FilterGroup.stories.tsx
@@ -3,13 +3,20 @@ import * as React from "react";
 import { CustomDateFilterGroup } from "../src/component-examples/filter-group/components/CustomDateFilterGroup";
 import { DefaultFilterGroup } from "../src/component-examples/filter-group/components/DefaultFilterGroup";
 import { STATIC_TABLE_DATA } from "../src/component-examples/filter-group/constants";
+import type { FilterListType } from "../src/component-examples/filter-group/types";
 
 export default {
   title: "Website/FilterGroupExamples",
 };
 
-export const DefaultFilterGroupExample = (): JSX.Element => <DefaultFilterGroup data={STATIC_TABLE_DATA} />;
+export const DefaultFilterGroupExample = (): JSX.Element => {
+  const filterList: FilterListType = ["room-type", "participants", "date-time"];
+  return <DefaultFilterGroup data={STATIC_TABLE_DATA} filterList={filterList} />;
+};
 
-export const SearchFilterGroupExample = (): JSX.Element => <DefaultFilterGroup data={STATIC_TABLE_DATA} withSearch />;
+export const SearchFilterGroupExample = (): JSX.Element => {
+  const filterList: FilterListType = ["room-type", "participants", "date-time"];
+  return <DefaultFilterGroup data={STATIC_TABLE_DATA} withSearch filterList={filterList} />;
+};
 
 export const CustomDateFilterGroupExample = (): JSX.Element => <CustomDateFilterGroup data={STATIC_TABLE_DATA} />;
