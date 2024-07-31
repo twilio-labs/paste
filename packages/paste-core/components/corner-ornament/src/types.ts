0170271e282b226cc0ec96e874398a0c144955d4
Kristian Antrobus <kantrobus@twilio.com>
2024-07-31 14:10:03 -0500

    feat(corner-ornament): corner ornament rename

--- packages/paste-core/components/corner-ornament/src/types.ts
+++ packages/paste-core/components/corner-ornament/src/types.ts
@@ -40,14 +40,14 @@ export interface CornerOrnamentContainerProps extends HTMLPasteProps<"div"> {
    */
   position?: CornerOrnamentPosition;
   /**
-   * The size of the CornerOrnamentElement used to determine spacing
+   * The size of the CornerOrnamentBase used to determine spacing
    * @default null
    * @type {IconSizeOptions}
    * @memberof CornerOrnamentContainerProps
    */
   size: IconSizeOptions;
   /**
-   * The type of the corner ornament used. This determines that correct mask to apply to CornerOrnamentElement and without it the cutout will be incorrect.
+   * The type of the corner ornament used. This determines that correct mask to apply to CornerOrnamentBase and without it the cutout will be incorrect.
    * @default null
    * @type {CornerOrnamentType}
    * @memberof CornerOrnamentContainerProps
@@ -68,14 +68,14 @@ export interface CornerOrnamentProps extends HTMLPasteProps<"div"> {
 
 export interface CornerOrnamentContextInterface {
   /**
-   * The size of the CornerOrnamentElement used to determine spacing
+   * The size of the CornerOrnamentBase used to determine spacing
    * @default null
    * @type {IconSizeOptions}
    * @memberof CornerOrnamentContextInterface
    */
   size: IconSizeOptions;
   /**
-   * The type of the corner ornament used. This determines that correct mask to apply to CornerOrnamentElement and without it the cutout will be incorrect.
+   * The type of the corner ornament used. This determines that correct mask to apply to CornerOrnamentBase and without it the cutout will be incorrect.
    * @default null
    * @type {CornerOrnamentType}
    * @memberof CornerOrnamentContextInterface
