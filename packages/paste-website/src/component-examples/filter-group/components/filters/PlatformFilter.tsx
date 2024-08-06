2385f926b5b59857c9ac756a22ecf2a0d014ac34
Shoaib Ahmed <shahmed@twilio.com>
2024-08-06 19:23:45 +0530

    feat(filter): add department and platform filter

---
+++ packages/paste-website/src/component-examples/filter-group/components/filters/PlatformFilter.tsx
@@ -0,0 +1,65 @@
+import { Box } from "@twilio-paste/box";
+import { Checkbox, CheckboxGroup } from "@twilio-paste/checkbox";
+import type { Item } from "@twilio-paste/combobox/dist/types";
+import type { usePopoverState } from "@twilio-paste/popover";
+import React from "react";
+
+import { FilterAction } from "../FilterAction";
+
+const platformList = ["Zoom", "Meets", "Microsoft Teams", "Slack", "Others"];
+
+export const PlatformFilter: React.FC = ({
+  onApply,
+  popover,
+}: {
+  onApply?: (type: string, value: Item[]) => void;
+  popover?: ReturnType<typeof usePopoverState>;
+}) => {
+  const [values, setValues] = React.useState<string[]>([]);
+  return (
+    <Box>
+      <CheckboxGroup name="platform-list" legend="Platform" helpText="Select one or more options">
+        {platformList.map((item) => {
+          return (
+            <Checkbox
+              key={item}
+              id={item}
+              value={item}
+              checked={values.includes(item)}
+              onChange={(e) => {
+                if (e.target.checked) {
+                  const updatedList = [...values, item];
+                  setValues(updatedList);
+                  return;
+                }
+
+                const updatedList = values.filter((value) => value !== item);
+                setValues(updatedList);
+              }}
+            >
+              {item}
+            </Checkbox>
+          );
+        })}
+      </CheckboxGroup>
+
+      <FilterAction
+        onApply={() => {
+          if (onApply && popover) {
+            if (values.length === 0) {
+              popover.hide();
+              return;
+            }
+
+            onApply("platform", values);
+            popover.hide();
+          }
+        }}
+        clearCondition={values.length > 0}
+        onClear={() => {
+          setValues([]);
+        }}
+      />
+    </Box>
+  );
+};
