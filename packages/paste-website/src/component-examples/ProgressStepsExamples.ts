5b4ee3437eb89e7bb6a2ce181af46ed7c1967395
Shoaib Ahmed <shahmed@twilio.com>
2024-09-12 12:54:17 +0530

    feat(progress-steps): update docs site with additonal content example

--- packages/paste-website/src/component-examples/ProgressStepsExamples.ts
+++ packages/paste-website/src/component-examples/ProgressStepsExamples.ts
@@ -81,3 +81,66 @@ export const i18nExample = `const ProgressStepsExample = () => {
 };
 
 render(<ProgressStepsExample />);`.trim();
+
+export const additionalContentExample = `const ProgressStepsExample = () => {
+  return (
+    <ProgressSteps orientation="vertical">
+      <ProgressStepComplete as="div">
+        Data warehouse connected
+        <ProgressStepContent>
+          <Box paddingX="space10" paddingY="space30">
+            <Box display="flex" columnGap="space50" paddingY="space40">
+              <Avatar size="sizeIcon90" name="Twilio Paste" variant="entity" icon={BusinessIcon} />
+              <Box>
+                <Heading variant="heading50">Snowflake</Heading>
+                <Box display="flex" flexDirection="column">
+                  <Paragraph>Account: accountname</Paragraph>
+                  <Paragraph>Database: snowflakedatabasename</Paragraph>
+                  <Paragraph>Warehouse: snowflakewarehousename</Paragraph>
+                  <Paragraph>User: bsmith</Paragraph>
+                  <Paragraph marginBottom="space0">Password: *****</Paragraph>
+                </Box>
+                <Box marginTop="space80">
+                  <ButtonGroup>
+                    <Button variant="secondary">Edit</Button>
+                    <Button variant="destructive_secondary">Remove</Button>
+                  </ButtonGroup>
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+        </ProgressStepContent>
+      </ProgressStepComplete>
+      <ProgressStepCurrent as="div">
+        Create a model
+        <ProgressStepContent>
+          <Box paddingX="space10" paddingY="space30">
+            <Paragraph marginBottom="space0">
+              Models are SQL queries that define sets of data to sync using Reverse ETL.
+            </Paragraph>
+            <Box marginTop="space50">
+              <Button variant="primary">Define model</Button>
+            </Box>
+          </Box>
+        </ProgressStepContent>
+      </ProgressStepCurrent>
+      <ProgressStepIncomplete as="div">
+        Create mapping
+        <ProgressStepContent>
+          <Box paddingX="space10" paddingY="space30">
+            <Paragraph marginBottom="space0">
+              Mappings determine how data extracted from your warehouse is mapped to fields in Flex
+            </Paragraph>
+            <Box marginTop="space50">
+              <Button variant="primary" disabled>
+                Continue mapping
+              </Button>
+            </Box>
+          </Box>
+        </ProgressStepContent>
+      </ProgressStepIncomplete>
+    </ProgressSteps>
+  );
+};
+
+render(<ProgressStepsExample />);`.trim();
