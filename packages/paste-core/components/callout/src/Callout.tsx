727b0be9f90884c64db5d2e3a74df61a358d6513
Kristian Antrobus <kantrobus@twilio.com>
2024-10-25 09:53:44 -0500

    chore(callout): updated stylings to match Figma

--- packages/paste-core/components/callout/src/Callout.tsx
+++ packages/paste-core/components/callout/src/Callout.tsx
@@ -129,9 +129,7 @@ export const Callout = React.forwardRef<HTMLDivElement, CalloutProps>(
         ref={ref}
         element={element}
         display="flex"
-        flexDirection="column"
         marginY={marginY}
-        rowGap="space50"
         paddingTop="space70"
         paddingLeft="space70"
         paddingRight="space70"
@@ -139,23 +137,23 @@ export const Callout = React.forwardRef<HTMLDivElement, CalloutProps>(
         borderLeftStyle="solid"
         borderLeftWidth="borderWidth20"
         variant={variant}
+        alignItems="start"
+        columnGap="space50"
         {...variantStyles[variant]}
       >
-        <Box display="flex" columnGap="space60" alignItems="start">
-          <Box element={`${element}_ICON`}>
-            {IconComponent}
-            <ScreenReaderOnly>{iconLabel}</ScreenReaderOnly>
-          </Box>
-          <Box display="flex" flexDirection="column" rowGap="space30" flex="1">
-            {children}
-          </Box>
-          {onDismiss && typeof onDismiss === "function" && (
-            <Button onClick={onDismiss} variant="secondary_icon" size="reset" element={`${element}_DISMISS_BUTTON`}>
-              <CloseIcon element={`${element}_DISMISS_ICON`} decorative size="sizeIcon20" />
-              <ScreenReaderOnly>{i18nDismissLabel}</ScreenReaderOnly>
-            </Button>
-          )}
+        <Box element={`${element}_ICON`}>
+          {IconComponent}
+          <ScreenReaderOnly>{iconLabel}</ScreenReaderOnly>
         </Box>
+        <Box display="flex" flexDirection="column" rowGap="space30" flex="1">
+          {children}
+        </Box>
+        {onDismiss && typeof onDismiss === "function" && (
+          <Button onClick={onDismiss} variant="secondary_icon" size="reset" element={`${element}_DISMISS_BUTTON`}>
+            <CloseIcon element={`${element}_DISMISS_ICON`} decorative size="sizeIcon20" />
+            <ScreenReaderOnly>{i18nDismissLabel}</ScreenReaderOnly>
+          </Button>
+        )}
       </Box>
     );
   },
