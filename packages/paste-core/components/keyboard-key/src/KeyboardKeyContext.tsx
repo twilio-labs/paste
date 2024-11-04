f0cb7ec45d5a2ce142508974c41f6fc741d9bea9
Kristian Antrobus <kantrobus@twilio.com>
2024-11-04 14:07:41 -0600

    chore(keyboard-key): typedocs

--- packages/paste-core/components/keyboard-key/src/KeyboardKeyContext.tsx
+++ packages/paste-core/components/keyboard-key/src/KeyboardKeyContext.tsx
@@ -3,9 +3,33 @@ import * as React from "react";
 import { KeyboardKeyVariants } from "./KeyboardKeyGroup";
 
 export interface KeyboardCombinationState {
+  /**
+   * The aray of currently active keys
+   * @default []
+   * @type { string[] }
+   * @memberof KeyboardCombinationState
+   */
   activeKeys?: string[];
+  /**
+   * Whether the keys should show the disbled state
+   * @default null
+   * @type { boolean }
+   * @memberof KeyboardCombinationState
+   */
   disabled?: boolean;
+  /**
+   * Whether the keys should show pressed state when active keys contains the keyText mapped to KeyboardKey
+   * @default null
+   * @type { boolean }
+   * @memberof KeyboardCombinationState
+   */
   enablePressStyles?: boolean;
+  /**
+   * The variant of the keys to display
+   * @default 'default'
+   * @type { KeyboardKeyVariants }
+   * @memberof KeyboardCombinationState
+   */
   variant?: KeyboardKeyVariants;
 }
 
