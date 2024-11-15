3cb4843aded94f44af36a663e8ade1f2f270e581
“nora <nkrantz@twilio.com>
2024-11-15 10:09:29 -0500

    chore: customization and biome

--- packages/paste-core/components/example-text/stories/index.stories.tsx
+++ packages/paste-core/components/example-text/stories/index.stories.tsx
@@ -1,5 +1,7 @@
 import { Box } from "@twilio-paste/box";
+import { CustomizationProvider } from "@twilio-paste/customization";
 import { Paragraph } from "@twilio-paste/paragraph";
+import { useTheme } from "@twilio-paste/theme";
 import * as React from "react";
 
 import { ExampleText } from "../src";
@@ -25,4 +27,29 @@ export const Inline = (): React.ReactNode => {
       </Paragraph>
     </Box>
   );
+};
+
+export const Customized = (): React.ReactNode => {
+  const theme = useTheme();
+  return (
+    <CustomizationProvider
+      theme={theme}
+      elements={{
+        CUSTOMIZED_EXAMPLE_TEXT_WRAPPER: {
+          padding: "space30",
+          backgroundColor: "colorBackgroundBrand10",
+          borderRadius: "borderRadius30",
+        },
+        CUSTOMIZED_EXAMPLE_TEXT: { fontFamily: "fontFamilyCode" },
+      }}
+    >
+      <ExampleText element="CUSTOMIZED_EXAMPLE_TEXT">HTTPBin echoes data in your HTTP request back to you</ExampleText>
+    </CustomizationProvider>
+  );
+};
+Customized.parameters = {
+  a11y: {
+    // no need to a11y check customization
+    disable: true,
+  },
 };
