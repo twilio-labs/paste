a720e5e0c33350f92618eab3ac84a3aa998a2001
krisantrobus <55083528+krisantrobus@users.noreply.github.com>
2024-11-19 13:53:54 -0600

    feat(tooltip): add keyboard shortcuts to tooltip (#4161)
    * feat(keyboard-key): component init
    
    * chore(tools): plopfile syntax fix
    
    * chore(tools): plopfile syntax fix
    
    * chore(tools): plopfile syntax fix
    
    * feat(keyboard-key):  styled w/ hook unit tests
    
    * feat(keyboard-key):  added variants and styles
    
    * chore(plop): update tsx dependency
    
    * chore(keyboard-key): internal exports in core
    
    * chore(keyboard-key): typedocs & build
    
    * feat(keyboard-key): change to infline-flex style
    
    * chore(keyboard-key): added stroy
    
    * chore(keyboard-key): linting
    
    * fix(keyboard-key): command logic
    
    * feat(design-tokens): added new box shadows to support keyboard-keys
    
    * chore(ci-cd): added chagesets
    
    * fix(keyboard-key): boxShadow stylings
    
    * fix(keyboard-key): remove null component wrapper
    
    * fix(keyboard-key): aria-hidden
    
    * chore(keyboard-key): refactor
    
    * chore(keyboard-key): code cleanup
    
    * chore(keyboard-key): code cleanup
    
    * chore(keyboard-key): typedocs
    
    * chore(keyboard-key): fix tests
    
    * fix(keyboard-key): aria and diableBrowserShortcuts
    
    * fix(keyboard-key): props fix
    
    * chore(keyboard-key): playgorund storybook
    
    * chore(keyboard-key): formatting fix
    
    * chore(keyboard-key): stories update
    
    * chore(keyboard-key): formatting fix
    
    * chore(keyboard-key): typo
    
    * Update .changeset/sweet-mugs-admire.md
    
    Co-authored-by: Nora Krantz <75342690+nkrantz@users.noreply.github.com>
    
    * Update .changeset/shaggy-sheep-confess.md
    
    Co-authored-by: Nora Krantz <75342690+nkrantz@users.noreply.github.com>
    
    * chore(keyboard-key): address PR comments
    
    * feat(keyboard-tooltip): wip
    
    * chore(tooltip): update types
    
    * chore(tooltip): update types
    
    * chore(tooltip): better var naming
    
    * chore(tooltip): fail safe check
    
    * chore(tooltip): linting
    
    * chore(tooltip): linting
    
    * chore(tooltip): yarn lock
    
    * chore(tooltip): changeset
    
    * chore(tooltip): fix spelling
    
    * chore(tooltip): uppdate type name
    
    * feat(tooltip): add anonymous keybaord action to tooltip
    
    * Update packages/paste-core/components/tooltip/src/Tooltip.tsx
    
    Co-authored-by: Sarah <sali@twilio.com>
    
    * fix(tooltip): update keyboardkey styles
    
    * chore(tooltip): formatting
    
    * Update packages/paste-core/components/tooltip/src/Tooltip.tsx
    
    Co-authored-by: Nora Krantz <75342690+nkrantz@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Nora Krantz <75342690+nkrantz@users.noreply.github.com>
    Co-authored-by: Sarah <sali@twilio.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/paste-core/components/tooltip/src/index.tsx
+++ packages/paste-core/components/tooltip/src/index.tsx
@@ -3,6 +3,8 @@ import type { TooltipStateReturn } from "./Tooltip";
 export { Tooltip, useTooltipState } from "./Tooltip";
 export type {
   TooltipProps,
+  TooltipVariantProps,
+  KeyboardKeyTooltipProps,
   TooltipStateReturn,
   UseTooltipInitialStateProps,
 } from "./Tooltip";
