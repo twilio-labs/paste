c720773c948fac5a991927679177797317aeaae6
Shoaib Ahmed <shahmed@twilio.com>
2024-11-22 18:29:20 +0530

    feat(blockquote): add custom story

--- packages/paste-core/components/blockquote/stories/index.stories.tsx
+++ packages/paste-core/components/blockquote/stories/index.stories.tsx
@@ -2,6 +2,8 @@ import type { StoryFn } from "@storybook/react";
 import { Box } from "@twilio-paste/box";
 import * as React from "react";
 
+import { CustomizationProvider } from "@twilio-paste/customization";
+import { useTheme } from "@twilio-paste/theme";
 import { Blockquote, BlockquoteContent, BlockquoteSource } from "../src";
 
 // eslint-disable-next-line import/no-default-export
@@ -45,3 +47,62 @@ export const WithoutSource: StoryFn = () => (
     </Blockquote>
   </Box>
 );
+
+export const CustomizationBlockquote: StoryFn = (_args, { parameters: { isTestEnvironment } }) => {
+  const currentTheme = useTheme();
+  return (
+    <CustomizationProvider
+      disableAnimations={isTestEnvironment}
+      theme={currentTheme}
+      elements={{
+        BLOCKQUOTE: {
+          backgroundColor: "colorBackgroundBody",
+          borderColor: "colorBorderPrimary",
+          borderRadius: "borderRadius20",
+          borderStyle: "solid",
+          borderWidth: "borderWidth20",
+          color: "colorTextSuccess",
+          padding: "space60",
+        },
+        BLOCKQUOTE_CONTENT: {
+          color: "colorTextWarning",
+          fontFamily: "fontFamilyText",
+          fontSize: "fontSize30",
+          fontWeight: "fontWeightSemibold",
+          lineHeight: "lineHeight40",
+        },
+        BLOCKQUOTE_SOURCE: {
+          color: "colorTextError",
+          fontFamily: "fontFamilyText",
+          fontSize: "fontSize20",
+          fontWeight: "fontWeightSemibold",
+          lineHeight: "lineHeight20",
+        },
+        BLOCKQUOTE_ICON: {
+          color: "colorTextWarning",
+        },
+        BLOCKQUOTE_SOURCE_AUTHOR: {
+          color: "colorTextWarning",
+        },
+      }}
+    >
+      <Box maxWidth="600px">
+        <Blockquote url="#">
+          <BlockquoteContent>
+            With AI-driven products, the design process is no longer just about aesthetics. It’s about designing for the
+            human experience as a whole.
+          </BlockquoteContent>
+          <BlockquoteSource author="Google" source="People + AI Guidebook" />
+        </Blockquote>
+      </Box>
+    </CustomizationProvider>
+  );
+};
+
+CustomizationBlockquote.storyName = "Customized";
+CustomizationBlockquote.parameters = {
+  a11y: {
+    // no need to a11y check customization
+    disable: true,
+  },
+};
