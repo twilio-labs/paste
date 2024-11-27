42a4dcbf4f5f217fb77d06d0aa7f02e07717cf6a
krisantrobus <55083528+krisantrobus@users.noreply.github.com>
2024-11-27 09:43:30 -0600

    docs(keyboard-key): add documentation (#4164)
    * feat(keyboard-key): component init
    
    * chore(tools): plopfile syntax fix
    
    * chore(tools): plopfile syntax fix
    
    * chore(tools): plopfile syntax fix
    
    * feat(keyboard-key):  styled w/ hook unit tests
    
    * feat(keyboard-key):  added variants and styles
    
    * chore(plop): update tsx dependency
    
    * chore(keyboard-key): internal exports in core
    
    * chore(keyboard-key): typedocs & build
    
    * chore(docs): use KeyboardKey in nav
    
    * docs(keyboard-key): init content
    
    * feat(keyboard-key): change to infline-flex style
    
    * docs(keybaord-key): added examples
    
    * chore(keyboard-key): added stroy
    
    * chore(keyboard-key): linting
    
    * fix(keyboard-key): command logic
    
    * feat(design-tokens): added new box shadows to support keyboard-keys
    
    * chore(ci-cd): added chagesets
    
    * fix(keyboard-key): boxShadow stylings
    
    * fix(keyboard-key): remove null component wrapper
    
    * fix(keyboard-key): aria-hidden
    
    * chore(keyboard-key): refactor
    
    * chore(keyboard-key): code cleanup
    
    * chore(keyboard-key): code cleanup
    
    * chore(keyboard-key): typedocs
    
    * chore(keyboard-key): fix tests
    
    * fix(keyboard-key): aria and diableBrowserShortcuts
    
    * fix(keyboard-key): props fix
    
    * chore(keyboard-key): playgorund storybook
    
    * chore(keyboard-key): formatting fix
    
    * chore(keyboard-key): stories update
    
    * chore(keyboard-key): formatting fix
    
    * chore(keyboard-key): typo
    
    * Update .changeset/sweet-mugs-admire.md
    
    Co-authored-by: Nora Krantz <75342690+nkrantz@users.noreply.github.com>
    
    * Update .changeset/shaggy-sheep-confess.md
    
    Co-authored-by: Nora Krantz <75342690+nkrantz@users.noreply.github.com>
    
    * chore(keyboard-key): address PR comments
    
    * feat(keyboard-tooltip): wip
    
    * chore(tooltip): update types
    
    * chore(tooltip): update types
    
    * chore(tooltip): better var naming
    
    * chore(tooltip): fail safe check
    
    * chore(tooltip): linting
    
    * chore(tooltip): linting
    
    * chore(tooltip): yarn lock
    
    * chore(tooltip): changeset
    
    * chore(tooltip): fix spelling
    
    * chore(tooltip): uppdate type name
    
    * docs(keyboard-key): wip
    
    * feat(tooltip): add anonymous keybaord action to tooltip
    
    * fix(keyboard-key): ignore case sensitivty to overcome shift combo bug
    
    * fix(keyboard-key): ignore case sensitivty to overcome shift combo bug
    
    * Update packages/paste-core/components/tooltip/src/Tooltip.tsx
    
    Co-authored-by: Sarah <sali@twilio.com>
    
    * fix(tooltip): update keyboardkey styles
    
    * chore(tooltip): formatting
    
    * docs(keyboard-key): hook examples
    
    * docs(keyboard-key): fix composition notes
    
    * docs(keyboard-key): fix docs search
    
    * docs(keyboard-key): change example trigger
    
    * docs(keyboard-key): fix build issue
    
    * docs(keyboard-key): yanlock
    
    * docs(keyboard-key): import fix
    
    * docs(keyboard-key): lint and format
    
    * docs(keyboard-key): changeset
    
    * Apply suggestions from code review
    
    Co-authored-by: Nora Krantz <75342690+nkrantz@users.noreply.github.com>
    
    * docs(keyboard-key): address PR comments
    
    * Apply suggestions from code review
    
    Co-authored-by: Nora Krantz <75342690+nkrantz@users.noreply.github.com>
    
    * docs(keyboard-key): address PR comments
    
    * docs(keyboard-key): remopve unused import
    
    * docs(keyboard-key): address PR comments
    
    * docs(keyboard-key): address PR comments
    
    * docs(keyboard-key): address PR comments
    
    * docs(keyboard-key): pressed stlying example
    
    ---------
    
    Co-authored-by: Nora Krantz <75342690+nkrantz@users.noreply.github.com>
    Co-authored-by: Sarah <sali@twilio.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ packages/paste-website/src/component-examples/KeyboardKeyExamples.ts
@@ -0,0 +1,182 @@
+export const basicExample = `<KeyboardKeyGroup>
+  <KeyboardKey>Control</KeyboardKey>
+  <KeyboardKey>B</KeyboardKey>
+</KeyboardKeyGroup>
+`.trim();
+
+export const defaultExample = `<>
+    Press{" "}<KeyboardKeyGroup>
+        <KeyboardKey>Control</KeyboardKey>
+        <KeyboardKey>S</KeyboardKey>
+    </KeyboardKeyGroup>{" "}to save.
+</>`.trim();
+
+export const disabledExample = `const DisabledExample = () => {
+  const menu = useMenuState();
+  return (
+    <Box minHeight="230px">
+        <Paragraph>Open the Menu for the disabled Keyboard Key in context:</Paragraph>
+        <MenuButton {...menu} variant="secondary">
+            Edit <ChevronDownIcon decorative />
+        </MenuButton>
+        <Menu {...menu} aria-label="Actions">
+            <MenuItem {...menu} aria-keyshortcuts="command+x">
+                <Box display="flex" justifyContent="space-between">
+                    Cut
+                    <KeyboardKeyGroup>
+                        <KeyboardKey>Cmd</KeyboardKey>
+                        <KeyboardKey>X</KeyboardKey>
+                    </KeyboardKeyGroup>
+                </Box>
+            </MenuItem>
+            <MenuItem {...menu} aria-keyshortcuts="command+v">
+                <Box display="flex" justifyContent="space-between">
+                    Paste
+                    <KeyboardKeyGroup>
+                        <KeyboardKey>Cmd</KeyboardKey>
+                        <KeyboardKey>V</KeyboardKey>
+                    </KeyboardKeyGroup>
+                </Box>
+            </MenuItem>
+            <MenuItem disabled aria-disabled {...menu}  aria-keyshortcuts="command+s">
+                <Box display="flex" justifyContent="space-between">
+                    Save
+                    <KeyboardKeyGroup disabled>
+                        <KeyboardKey>Cmd</KeyboardKey>
+                        <KeyboardKey>S</KeyboardKey>
+                    </KeyboardKeyGroup>
+                </Box>
+            </MenuItem>
+        </Menu>
+    </Box>
+  );
+};
+
+render(
+    <DisabledExample />
+)`.trim();
+
+export const pressedExample = `const PressedExample = () => {
+    const keyCombinationState = useKeyCombination({
+        keys: ["Shift", "s"],
+        onCombinationPress: ()=> {},
+        enablePressStyles: true,
+    });
+
+    return (
+        <>
+            <Paragraph>Press the “Shift” or “S” key to reveal the pressed states below:</Paragraph>
+            <KeyboardKeyGroup {...keyCombinationState}>
+                <KeyboardKey keyEvent="Shift">Shift</KeyboardKey>
+                <KeyboardKey keyEvent="s">S</KeyboardKey>
+            </KeyboardKeyGroup>
+        </>
+    )
+}
+
+render(
+    <PressedExample />
+)`.trim();
+
+export const tooltipExample = `<Stack orientation="horizontal" spacing="space60">
+    <Tooltip
+        actionHeader="Search shortcut"
+        keyCombinationsActions={[
+            { name: "Mac", eventKeyCombination: ["Command", "K"] },
+            { name: "Windows", eventKeyCombination: ["Control", "K"] },
+        ]}
+    >
+        <Button aria-keyshortcuts="command+k" variant="primary">
+            <SearchIcon title="search" />
+        </Button>
+    </Tooltip>
+    <Tooltip
+        actionHeader="Search shortcut"
+        keyCombinationsActions={[
+            { eventKeyCombination: ["Command", "K"] },
+        ]}
+    >
+        <Button aria-keyshortcuts="command+k" variant="primary">
+            <SearchIcon title="search" />
+        </Button>
+    </Tooltip>
+</Stack>`.trim();
+
+export const useKeyCombinationExample = `const HookExample = () => {
+    const [combinationTriggeredText, setCombinationTriggeredText] = React.useState("");
+    useKeyCombination({
+        keys: ["Shift", "q"],
+        onCombinationPress: () => {
+            setCombinationTriggeredText("Shift + Q pressed");
+        },
+        enablePressStyles: true,
+    });
+
+    return (
+        <>
+            <Paragraph>Press the <KeyboardKeyGroup><KeyboardKey>Shift</KeyboardKey><KeyboardKey>Q</KeyboardKey></KeyboardKeyGroup> key to reveal the pressed states below:</Paragraph>
+            <Paragraph>Combination triggered: {combinationTriggeredText}</Paragraph>
+        </>
+    )
+}
+
+render(
+    <HookExample />
+)`.trim();
+
+export const useKeyCombinationsExample = `const HookExample = () => {
+  const [combinationTriggeredText, setCombinationTriggeredText] = React.useState("");
+
+  useKeyCombinations({
+    combinations: [
+      {
+        keys: ["Control", "b"],
+        onCombinationPress: () => {
+          setCombinationTriggeredText("Control + B pressed");
+        },
+        disabled: false,
+      },
+      {
+        keys: ["Control", "k"],
+        onCombinationPress: () => {
+          setCombinationTriggeredText("Control + K pressed");
+        },
+        disabled: false,
+      },
+      {
+        keys: ["Control", "y"],
+        onCombinationPress: () => {
+          setCombinationTriggeredText("Control + Y pressed");
+        },
+        disabled: false,
+      },
+    ],
+  });
+
+  return (
+    <>
+        <Paragraph marginBottom="space0">Use the following combinations to test. You can also set the disabled state in the code block below.</Paragraph>
+        <Box marginY="space30">
+            <Stack orientation="vertical" spacing="space20">
+                <KeyboardKeyGroup>
+                    <KeyboardKey>Control</KeyboardKey>
+                    <KeyboardKey>B</KeyboardKey>
+                </KeyboardKeyGroup>
+                <KeyboardKeyGroup>
+                    <KeyboardKey>Control</KeyboardKey>
+                    <KeyboardKey>K</KeyboardKey>
+                </KeyboardKeyGroup>
+                <KeyboardKeyGroup>
+                    <KeyboardKey>Control</KeyboardKey>
+                    <KeyboardKey>Y</KeyboardKey>
+                </KeyboardKeyGroup>
+            </Stack>
+        </Box>
+        <Paragraph>Combination triggered: {combinationTriggeredText}</Paragraph>
+    </>
+  );
+};
+
+render(
+    <HookExample />
+)`.trim();
