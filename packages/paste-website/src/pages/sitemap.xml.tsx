8e13c3529e256c65a9654944611c7c7e1ed98d80
Kristian Antrobus <kantrobus@twilio.com>
2024-12-17 12:15:33 -0600

    chore(docs): add root to sitemap

--- packages/paste-website/src/pages/sitemap.xml.tsx
+++ packages/paste-website/src/pages/sitemap.xml.tsx
@@ -25,6 +25,11 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>${BASE_URL}</loc>
+        <changefreq>daily</changefreq>
+        <priority>0.7</priority>
+      </url>
       ${urlPaths
         .map((url) => {
           return `
