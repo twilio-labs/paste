9e58d87fb361d3b3406c42d1f03c8cedf066a1f6
Kristian Antrobus <kantrobus@twilio.com>
2024-12-19 08:18:22 -0600

    chore(docs): debug

--- packages/paste-website/src/pages/sitemap.xml.tsx
+++ packages/paste-website/src/pages/sitemap.xml.tsx
@@ -5,12 +5,37 @@ const Sitemap = (): React.ReactElement | null => {
   return null;
 };
 
+export const revalidate = "force-cache";
+
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   const BASE_URL = "https://paste.twilio.design";
+
+  // Get a list of all pages currently in the site, must be mdx and not tsx which they all currently are
+  const uncompiledPaths = await globby(["**/*.mdx"]);
+
+  const cachedpaths = await globby(["**/*"], { cwd: __dirname });
+
+  // eslint-disable-next-line no-console
+  console.log(cachedpaths);
+  // eslint-disable-next-line no-console
+  console.log(process.cwd(), __dirname);
+
+  const urlPaths = uncompiledPaths.map((path) => {
+    // Remove `src/pages/`
+    let modifiedPath = path.replace(/^src\/pages\//, "");
+    // Remove `.mdx`
+    modifiedPath = modifiedPath.replace(/\.mdx$/, "");
+    // Remove `/index` if it's at the end of the path
+    modifiedPath = modifiedPath.replace(/\/index$/, "");
+    return `${BASE_URL}/${path}`;
+  });
 
   const paths = await globby(["**/*.js", "!sitemap.xml.js", "!404.js", "!_*.js"], {
     cwd: __dirname,
   });
+
+  console.log("cached pages:", paths);
+
   const staticPaths = paths.map((staticPagePath) => {
     const path = staticPagePath.replace(".js", "");
     const route = path === "index" ? "" : `${path}/`;
@@ -20,7 +45,12 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${staticPaths
+      <url>
+        <loc>${BASE_URL}</loc>
+        <changefreq>daily</changefreq>
+        <priority>0.7</priority>
+      </url>
+      ${urlPaths
         .map((url) => {
           return `
             <url>
