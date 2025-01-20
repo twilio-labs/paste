41b5d789a568ccccf88ddfb4c2e18a19f73933a9
Kristian Antrobus <kantrobus@twilio.com>
2025-01-20 11:22:33 -0600

    fix(ci): update acitons version

--- apps/backend/supabase/migrations/20230928013336_initial_schema.sql
+++ apps/backend/supabase/migrations/20230928013336_initial_schema.sql
@@ -1,4 +1,4 @@
-create extension if not exists "vector" with schema "public" version '0.5.0';
+create extension if not exists "vector" with schema "public" version '0.7.0';
 
 create sequence "public"."page_id_seq";
 
