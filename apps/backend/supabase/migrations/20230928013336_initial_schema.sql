9791387ddc9cb0e1e30ac49619305176c6310c88
Kristian Antrobus <kantrobus@twilio.com>
2025-01-20 14:42:47 -0600

    fix(ci): debug actions

--- apps/backend/supabase/migrations/20230928013336_initial_schema.sql
+++ apps/backend/supabase/migrations/20230928013336_initial_schema.sql
@@ -1,4 +1,4 @@
-create extension if not exists "vector" with schema "public" version '0.8.0';
+create extension if not exists "vector" with schema "public";
 
 create sequence "public"."page_id_seq";
 
