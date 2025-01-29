8f156b52b5119ff47f1b70182cd34573ee0f3877
PixeledCode <ashoaib540918@gmail.com>
2025-01-29 16:28:06 +0530

    feat(minimizable-dialog): add elevation changes

--- packages/paste-core/components/minimizable-dialog/src/StyledMinimizableDialog.tsx
+++ packages/paste-core/components/minimizable-dialog/src/StyledMinimizableDialog.tsx
@@ -2,21 +2,27 @@ import { Box } from "@twilio-paste/box";
 import type { BoxProps } from "@twilio-paste/box";
 import * as React from "react";
 
+import { MinimizableDialogContext } from "./MinimizableDialogContext";
+
 export const StyledMinimizableDialog: React.FC<React.PropsWithChildren<Pick<BoxProps, "element">>> = ({
   element,
   children,
-}) => (
-  <Box
-    element={element}
-    borderRadius="borderRadius20"
-    display="flex"
-    flexDirection="column"
-    backgroundColor="colorBackgroundBody"
-    width="size40"
-    boxShadow="shadow"
-  >
-    {children}
-  </Box>
-);
+}) => {
+  const { minimized } = React.useContext(MinimizableDialogContext);
+
+  return (
+    <Box
+      element={element}
+      borderRadius="borderRadius20"
+      display="flex"
+      flexDirection="column"
+      backgroundColor="colorBackgroundWeaker"
+      width="size40"
+      boxShadow="shadowElevation20"
+    >
+      {children}
+    </Box>
+  );
+};
 
 StyledMinimizableDialog.displayName = "StyledMinimizableDialog";
