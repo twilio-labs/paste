bfbab1881fa14dbe577bd72d649f459b50b3a494
PixeledCode <ashoaib540918@gmail.com>
2025-04-10 13:32:39 +0530

    feat(button): update border radius based on size

--- packages/paste-core/components/button/src/InverseLinkButton.tsx
+++ packages/paste-core/components/button/src/InverseLinkButton.tsx
@@ -25,6 +25,11 @@ const disabledStyles: BoxStyleProps = merge(BaseStyles.disabled, {
   textAlign: "left",
 });
 
+const linkSizeStyles = (size: keyof typeof SizeStyles): BoxStyleProps =>
+  merge(SizeStyles[size], {
+    borderRadius: "borderRadius20",
+  });
+
 const ButtonStyleMapping = {
   default: defaultStyles,
   loading: loadingStyles,
@@ -40,7 +45,7 @@ const InverseLinkButton = React.forwardRef<HTMLButtonElement, DirectButtonProps>
         width={fullWidth ? "100%" : "auto"}
         {...safelySpreadBoxProps({ as, ...props })}
         {...ButtonStyleMapping[buttonState]}
-        {...SizeStyles[size]}
+        {...linkSizeStyles(size)}
       />
     );
   },
