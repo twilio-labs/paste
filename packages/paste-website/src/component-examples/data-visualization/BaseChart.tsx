427a1bde9d278fe837a742b058cf8e6060ee9b1b
Kristian Antrobus <kantrobus@twilio.com>
2025-04-14 10:53:26 -0500

    feat(data-viz): wip

---
+++ packages/paste-website/src/component-examples/data-visualization/BaseChart.tsx
@@ -0,0 +1,47 @@
+import { Box } from "@twilio-paste/box";
+import { ChartContext } from "@twilio-paste/chart-provider";
+import * as Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import HighchartsAccessibilityModule from "highcharts/modules/accessibility";
+import { applyPasteHighchartsModules, usePasteHighchartsTheme } from "@twilio-paste/data-visualization-library";
+import * as React from "react";
+
+
+const Chart: React.FC = () => {
+  applyPasteHighchartsModules(Highcharts, HighchartsAccessibilityModule);
+  const chartRef = React.useRef<HTMLElement | null>(null);
+  const { options, setChart, setChartRef } = React.useContext(ChartContext);
+  const [chartOptions, setChartOptions] = React.useState<Highcharts.Options>(
+    usePasteHighchartsTheme(options),
+  );
+
+  React.useLayoutEffect(() => {
+    setChartOptions(Highcharts.merge(chartOptions, options));
+  }, [options]);
+
+  React.useEffect(() => {
+    if (chartRef.current) {
+      setChartRef(chartRef.current);
+    }
+  }, [chartRef.current]);
+
+  const callback = (chart: Highcharts.Chart) => {
+    if (chart?.series?.length > 0) {
+      setChart(chart);
+    }
+  };
+
+  return (
+    <Box gridArea="base-chart" ref={chartRef} position="relative">
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={chartOptions}
+        constructorType={chartOptions.chart?.map ? "mapChart" : undefined}
+        updateArgs={[true, true, false]}
+        callback={callback}
+      />
+    </Box>
+  );
+};
+
+export const ExamplesDataVizBaseChart = React.memo(Chart);
---
+++ packages/paste-website/src/component-examples/data-visualization/ChartProviderExamples.ts
@@ -0,0 +1,57 @@
+export const SimpleChartProviderExample = `
+const ChartProviderExample = () => {
+    const lineSeriesData = [
+        {
+            name: "Installation",
+            data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
+        },
+        {
+            name: "Manufacturing",
+            data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434],
+        },
+    ]
+
+    return (
+        <ChartProvider highchartsOptions={{ chart: { type: "line" }, series: lineSeriesData }}>
+            <BaseChart />
+        </ChartProvider>
+    );
+};
+
+render(<ChartProviderExample />);
+`.trim();
+
+export const CustomChartProviderExample = `
+const ChartProviderExample = () => {
+    const lineSeriesData = [
+        {
+            name: "Installation",
+            data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
+            type: "line",
+        },
+        {
+            name: "Manufacturing",
+            data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434],
+            type: "line",
+        },
+        {
+            name: "Sales & Distribution",
+            data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387],
+            type: "column",
+        },
+        {
+            name: "Project Development",
+            data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227],
+            type: "column",
+        },
+    ]
+
+    return (
+        <ChartProvider highchartsOptions={series: lineSeriesData}>
+            <BaseChart />
+        </ChartProvider>
+    );
+};
+
+render(<ChartProviderExample />);
+`.trim();
