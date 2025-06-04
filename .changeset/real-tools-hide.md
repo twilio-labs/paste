219e25c1bfa7001ee9d19a3a3c74b6de4d680149
Shoaib Ahmed <ashoaib540918@gmail.com>
2025-06-04 07:24:54 -0700

    Update ai chat (#4331)
    * feat(ai-chat): align user chat to right
    
    * feat(ai-chat): update varaint user style
    
    * feat(ai-chat): update full screen styles
    
    * feat(ai-chat): update author component styles
    
    * feat(ai-chat): add timestamp prop
    
    * feat(ai-chat): add ai chat event
    
    * feat(ai-chat): add agent variant
    
    * feat(ai-chat): update yarn
    
    * feat(ai-chat): update api for fullscreen size
    
    * feat(ai-chat): update typedocs
    
    * feat(ai-chat): add changeset
    
    * feat(ai-chat): change order of disclosure opening
    
    * feat(ai-chat): resolve github comments
    
    * feat(ai-chat): update padding
    
    * feat(ai-chat): update examples
    
    * feat(ai-chat): update withSeparator comment
    
    * feat(ai-chat): update typedocs
    
    * feat(ai-chat): update typedocs
    
    * feat(ai-chat): update AIChatMessageBody
    
    * feat(ai-chat): fix eslint issue
    
    * feat(ai-chat): update docs
    
    * feat(ai-chat): update changeset
    
    * feat(ai-chat): fix space
    
    ---------
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ .changeset/real-tools-hide.md
@@ -0,0 +1,7 @@
+---
+"@twilio-paste/ai-chat-log": minor
+"@twilio-paste/core": minor
+"@twilio-paste/codemods": minor
+---
+
+[AI Chat Log] Update styles of AI Chat Log, add new props, add event component. Deprecates `size` prop from `AIChatMessageBody` and move it to `AIChatLog`
--- packages/paste-core/components/ai-chat-log/src/AIChatMessageAuthor.tsx
+++ packages/paste-core/components/ai-chat-log/src/AIChatMessageAuthor.tsx
@@ -6,6 +6,7 @@ import type { GenericIconProps } from "@twilio-paste/icons/esm/types";
 import type { HTMLPasteProps } from "@twilio-paste/types";
 import * as React from "react";
 
+import { AILogContext } from "./AILogContext";
 import { AIMessageContext } from "./AIMessageContext";
 
 export interface AIChatMessageAuthorProps extends HTMLPasteProps<"div"> {
@@ -57,6 +58,8 @@ export interface AIChatMessageAuthorProps extends HTMLPasteProps<"div"> {
 export const AIChatMessageAuthor = React.forwardRef<HTMLDivElement, AIChatMessageAuthorProps>(
   ({ children, element = "AI_CHAT_MESSAGE_AUTHOR", avatarName, avatarIcon, avatarSrc, ...props }, ref) => {
     const { variant } = React.useContext(AIMessageContext);
+    const { size } = React.useContext(AILogContext);
+    const isFullScreen = size === "fullScreen";
 
     return (
       <Box
@@ -67,19 +70,21 @@ export const AIChatMessageAuthor = React.forwardRef<HTMLDivElement, AIChatMessag
         display="flex"
         alignItems="center"
         columnGap="space40"
-        fontWeight="fontWeightMedium"
+        fontWeight="fontWeightSemibold"
+        fontSize={isFullScreen ? "fontSize30" : "fontSize20"}
+        color="colorTextWeak"
       >
         {variant === "bot" ? (
           <Avatar
             name={children}
-            size="sizeIcon50"
+            size="sizeIcon30"
             color="decorative20"
             icon={ArtificialIntelligenceIcon}
             element={`${element}_BOT_AVATAR`}
           />
         ) : (
           <Avatar
-            size="sizeIcon50"
+            size="sizeIcon30"
             color="decorative30"
             element={`${element}_USER_AVATAR`}
             name={avatarName || children}
